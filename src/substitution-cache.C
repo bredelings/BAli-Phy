f528369f023ae088cea732b1ac7b7cde1a8130ca
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2004-10-01 21:01:08 +0000

    Oops, forgot to add the files for 'Conditional_Likelihood'

---
+++ src/substitution-cache.C
@@ -0,0 +1,37 @@
+#include "substitution-cache.H"
+
+using std::vector;
+
+void Conditional_Likelihoods::invalidate_all() {
+}
+
+void Conditional_Likelihoods::invalidate_branch(const Tree& T,int b) {
+  vector<const_branchview> branch_list = branches_after(T,b);
+  for(int i=0;i<branch_list.size();i++)
+    up_to_date[i] = false;
+}
+
+void Conditional_Likelihoods::invalidate_node(const Tree& T,int n) {
+  vector<const_branchview> branch_list = branches_from_node(T,n);
+  for(int i=0;i<branch_list.size();i++)
+    up_to_date[i] = false;
+}
+
+void Conditional_Likelihoods::set_length(int l2) {
+  int delta = l2 - size();
+  if (delta <= 0) return;
+
+  reserve(l2);
+  for(int i=0;i<delta;i++)
+    push_back(Matrix(2*n_branches+1,asize));
+}
+
+
+Conditional_Likelihoods::Conditional_Likelihoods(const Tree& T, int r, int s) 
+  :root(r),
+   asize(s),
+   n_branches(T.n_branches()),
+   up_to_date(2*T.n_branches(),false)
+{
+  
+}
