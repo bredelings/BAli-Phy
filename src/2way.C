f00c438e6a724d2e7bc5215d993dea3bfab4b66a
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2004-12-04 17:51:01 +0000

    Turn the letters_OK() check on even for the NDEBUG builds.

--- src/2way.C
+++ src/2way.C
@@ -100,9 +100,9 @@ alignment construct(const alignment& old, const vector<int>& path, const valarra
     assert(A.seqlength(i) == old.seqlength(i));
 
   //  std::cerr<<"new = "<<A<<endl;  
-  assert(letters_OK(old));
+  letters_OK(old);
   assert(valid(A));
-  assert(letters_OK(A));
+  letters_OK(A);
 
   return A;
 }
