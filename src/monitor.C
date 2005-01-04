e35f706357a897c3831804ee110cd4d465e1f8b1
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-01-04 04:14:21 +0000

    [NEW] Print out '*' in front of fixed parameters.

--- src/monitor.C
+++ src/monitor.C
@@ -37,8 +37,12 @@ void show_frequencies(std::ostream& o,const substitution::MultiModel& MModel) {
 }
 
 void show_parameters(std::ostream& o,const Model& M) {
-  for(int i=0;i<M.parameters().size();i++)
-    o<<"    "<<M.parameter_name(i)<<" = "<<M.parameters()[i];
+  for(int i=0;i<M.parameters().size();i++) {
+    o<<"    ";
+    if (M.fixed[i]) 
+      o<<"*";
+    o<<M.parameter_name(i)<<" = "<<M.parameters()[i];
+  }
   o<<"\n";
 }
 
