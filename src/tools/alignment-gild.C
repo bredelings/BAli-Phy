36a806325c1759d32047577180a6fdf2f8a2bf19
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-01-07 00:31:32 +0000

    [CLEANUP] Clean up alignment-gild.C           Remove old, unused code that put un-alignment events on a star tree.

--- src/tools/alignment-gild.C
+++ src/tools/alignment-gild.C
@@ -35,8 +35,9 @@ inline double log_fact(int n) {
   return total;
 }
 
-// v[i][j] represents the column of the feature j in alignment i.
-// so if v[i][j] == v[i][k] then j and k are paired in alignment i.
+// Compute the probability that residues (i,j) are aligned
+//   - v[i][j] represents the column of the feature j in alignment i.
+//   - so if v[i][j] == v[i][k] then j and k are paired in alignment i.
 Matrix counts_to_probability(const Tree& T,const vector< vector<int> >& v) {
   assert(v.size() > 0);
   assert(v[0].size() > 0);
@@ -45,8 +46,8 @@ Matrix counts_to_probability(const Tree& T,const vector< vector<int> >& v) {
 
 
   const double pseudocount = 1;
-  const double edge_prior = 0.25/T.n_branches();
-  const double prior = 0.25;
+  const double edge_prior = 0.5/(T.n_branches()/2);
+  const double prior = 0.5;
 
   // initialize the matrix, and add a pseudocount for a conjugate prior
   Matrix Pr_align_pair(N,N);
@@ -70,144 +71,13 @@ Matrix counts_to_probability(const Tree& T,const vector< vector<int> >& v) {
     for(int j=0;j<N;j++)
       Pr_align_pair(i,j) /= (v.size() + pseudocount*(prior + edge_prior*T.edges_distance(i,j)));
 
+  // Check symmetry
   for(int i=0;i<N;i++)
     for(int j=0;j<N;j++)
-      if (std::abs(Pr_align_pair(i,j) -Pr_align_pair(j,i)) > 1.0e-9) {
-	std::cerr<<"("<<i<<","<<j<<") = "<<Pr_align_pair(i,j)<<std::endl;
-	std::cerr<<"("<<j<<","<<i<<") = "<<Pr_align_pair(j,i)<<std::endl;
-	std::abort();
-      }
+      assert(std::abs(Pr_align_pair(i,j) -Pr_align_pair(j,i)) < 1.0e-9);
 
   return Pr_align_pair;
 }
-
-
-/// Function to optimize for the star tree algorithm
-class alignment_probability: public function {
-  int leaves;
-  int leafbranches;
-  vector< vector<int> > labels;
-
-  vector< vector<int> > branch_to_group;
-
-  vector< vector< vector<int> > > groups;
-  double alignment_probability::Pr(const optimize::Vector&,int,int) const;
-
-  double alignment_probability::Pr(const optimize::Vector&,int) const;
-
-public:
-  double operator()(const optimize::Vector& v) const;
-
-  alignment_probability(const vector< vector<int> >& v1):labels(v1)
-  { 
-    leaves = labels[0].size();
-    leafbranches = leaves;
-    if (leaves==2)
-      leafbranches=1;
-
-    for(int L=0;L<labels.size();L++) {
-      const vector<int>& label = labels[L];
-
-      // map each branch to a group
-      branch_to_group.push_back(vector<int>(leaves));
-
-      // map each group to a set of branches
-      groups.push_back(vector< vector<int> >());
-
-      // compute the mappings
-      for(int i=0;i<label.size();i++) {
-
-	// find which group we're in
-	int group = -1;
-	for(int j=0;j<i;j++) {
-	  if (label[j] == label[i]) {
-	    group = branch_to_group[L][j];
-	    break;
-	  }
-	}
-
-	// if we are the first letter w/ this column, start a new group
-	if (group == -1) {
-	  group = groups[L].size();
-	  groups[L].push_back(vector<int>());
-	}      
-	
-	branch_to_group[L][i] = group;
-	groups[L][group].push_back(i);
-
-      }
-    }
-  }
-
-};
-
-double alignment_probability::Pr(const optimize::Vector& v,int L,int mask) const
-{
-  const vector<int>& label = labels[L];
-  double total=0;
-
-  // calculate the probability of branches being connected to the center
-  int nconnected=0;
-  for(int i=0;i<label.size();i++) {
-    if (mask & (1<<i)) {
-      total += -v[i];
-      nconnected++;
-    }
-    else {
-      total += log(1.0-exp(-v[i]));
-      if (groups[L][branch_to_group[L][i]].size() > 1)
-	return log_0;
-    }
-  }
-  
-  double sun = v[label.size()];
-  int o = groups[L].size();
-  int m = nconnected;
-  if (o==0)
-    return total;
-
-  // calculate the probability of there being groups.size() groups
-  // given than nconnected leaves are connected to the center;
-
-  // poisson probability of splitting (o-1) times into o groups
-  total += (o-1)*log(sun)-sun-log_fact(o-1);
-  total += log_fact(o) - log_fact(m) + log_fact(m-o);
-  for(int i=0;i<groups[L].size();i++)
-    total -= log_fact(groups[L][i].size()-1);
-
-  return total;
-}
-
-
-double alignment_probability::Pr(const optimize::Vector& v,int L) const {
-  const int max = 1<<labels[L].size();
-  assert(max != 0);
-
-  // sum of probability of each possibility for each branch
-  double total = log_0;
-  for(int i=0;i<max;i++)
-    total = logsum(total,Pr(v,L,i));
-
-  return total;
-}
-
-
-double alignment_probability::operator()(const optimize::Vector& v) const {
-  assert(v.size() == leafbranches+1);
-
-  // probability need to be positive :P
-  for(int i=0;i<v.size();i++)
-    if (v[i]<0)
-      return log_0;
-
-  // product of probability of each label
-  double total = 0;
-  for(int L=0;L<labels.size();L++) 
-    total += Pr(v,L);
-
-  return total;
-}
-
 
 class SSE_match_pairs: public function {
   Tree T;
@@ -358,14 +228,6 @@ void do_setup(Arguments& args,vector<alignment>& alignments,alignment& A,Sequenc
   //----------- Load and link template A and T -----------------//
   load_A_and_T(args,A,T,false);
 
-  /*
-  Matrix D = DistanceMatrix(T);
-  vector<double> ls_b = FastLeastSquares(T,D);
-  for(int b=0;b<T.n_branches();b++) {
-    std::cerr<<T.branch(b).length()<<"  "<<ls_b[b]<<std::endl;
-  }
-  */
-    
   //------------ Try to load alignments -----------//
   int maxalignments = args.loadvalue("maxalignments",1000);
 
@@ -377,7 +239,7 @@ void do_setup(Arguments& args,vector<alignment>& alignments,alignment& A,Sequenc
   alphabets.push_back(A.get_alphabet());
   std::cerr<<"Loading alignments...";
   alignments = load_alignments(std::cin,tag,alphabets,maxalignments);
-  std::cerr<<"done."<<std::endl;
+  std::cerr<<"done. ("<<alignments.size()<<" alignments)"<<std::endl;
 
   //-------- Check compatability of estimate & samples-------//
   assert(A.size2() == T.n_leaves());
@@ -478,6 +340,8 @@ double get_column_probability(const vector<int>& column,
 }
 
 
+//FIXME - use rooted trees?  How?
+
 int main(int argc,char* argv[]) { 
   try {
     Arguments args;
@@ -489,7 +353,6 @@ int main(int argc,char* argv[]) {
     SequenceTree T;
     vector<alignment> alignments;
     do_setup(args,alignments,A,T);
-    cerr<<"Read "<<alignments.size()<<" alignments\n";
     const int n = T.n_leaves();
 
     //----------- Find root branch ---------//
@@ -531,9 +394,10 @@ int main(int argc,char* argv[]) {
     for(int c=0;c<A.length();c++) {
       vector<int> column = get_column(MA,c,n);
 
-      vector< vector<int> > labels;
+      // Get labels - should I instead get *counts*?
+      vector< vector<int> > labels(alignments.size());
       for(int i=0;i<alignments.size();i++)
-	labels.push_back(getlabels(alignments[i],column,column_indexes[i]));
+	labels[i] = getlabels(alignments[i],column,column_indexes[i]);
 
       //Get initial estimate using fast least squares
       vector<double> branch_lengths(T.n_branches());
@@ -541,8 +405,7 @@ int main(int argc,char* argv[]) {
       for(int b=0;b<branch_lengths.size();b++)
 	if (branch_lengths[b] < 0) branch_lengths[b] = 0;
 
-
-      // alignment_probability f(labels);
+      // Define an objective function for refining the initial estimates
       function * f = NULL;
       if (args["refine"] == "SSE")
 	f = new SSE_match_pairs(labels,T);
@@ -551,7 +414,7 @@ int main(int argc,char* argv[]) {
       else if (args["refine"] == "LeastSquares")
 	f = new LeastSquares(labels,T);
 
-      // refine initial estimate if called-for
+      // refine initial estimate if requested
       if (f) {
 	optimize::Vector x(T.n_branches());
 	for(int i=0;i<branch_lengths.size();i++)
