947d9cc1e1c84a3e6c16554e2f77b0655e218b7e
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-02-19 19:21:43 +0000

    * tools/alignment-indices: update to use boost::program_options  * GNUmakefile: add alignment-indices to list of tools to build

--- src/tools/alignment-indices.C
+++ src/tools/alignment-indices.C
@@ -1,41 +1,75 @@
 #include <iostream>
-#include "arguments.H"
 #include "alignment.H"
 #include "alignment-util.H"
 #include "setup.H"
 
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+using po::variables_map;
+
 using std::cout;
 using std::cerr;
 
-int main(int argc,char* argv[]) { 
-  Arguments args;
-  args.read(argc,argv);
+variables_map parse_cmd_line(int argc,char* argv[]) 
+{ 
+  using namespace po;
+
+  // named options
+  options_description all("Allowed options");
+  all.add_options()
+    ("help", "produce help message")
+    ("data-dir", value<string>()->default_value("Data"),"data directory")
+    ("align", value<string>(),"file with sequences and initial alignment")
+    ("alphabet",value<string>(),"set to 'Codons' to prefer codon alphabets")
+    ("with-stop","include stop codons in amino-acid alphabets")
+    ;
+
+  // positional options
+  positional_options_description p;
+  p.add("align", 1);
+  
+  variables_map args;     
+  store(command_line_parser(argc, argv).
+	    options(all).positional(p).run(), args);
+  // store(parse_command_line(argc, argv, desc), args);
+  notify(args);    
+
+  if (args.count("help")) {
+    cout<<"Usage: alignment-gild <alignment-file> <tree-file> ... [OPTIONS]\n";
+    cout<<all<<"\n";
+    exit(0);
+  }
+
+  return args;
+}
+
+
+int main(int argc,char* argv[]) 
+{ 
 
   try {
-    /* ----- Try to load alignment ------ */
+    //---------- Parse command line  -------//
+    variables_map args = parse_cmd_line(argc,argv);
 
-    if (not args.set("align")) 
-      throw myexception("Alignment file not specified! (align=<filename>)");
+    //------- Try to load alignment --------//
+    alignment A = load_A(args);
 
-    alignment A;
-    load_A(args,A);
-
-    alignment MA = M(A);
-
+    ublas::matrix<int> MA = M(A);
 
     //------- Write the sequence names ------//
-    for(int i=0;i<MA.size2();i++) {
-      cout<<MA.seq(i).name;
-      if (i==MA.size2()-1)
+    for(int i=0;i<A.size2();i++) {
+      cout<<A.seq(i).name;
+      if (i==A.size2()-1)
 	cout<<std::endl;
       else
 	cout<<" ";
     }
       
-    const alphabet& a = MA.get_alphabet();
+    const alphabet& a = A.get_alphabet();
 
     //------- Write the columns ------//
-    for(int c=0;c<MA.length();c++) {
+    for(int c=0;c<MA.size1();c++) {
       // write the indices
       for(int i=0;i<MA.size2();i++) {
 	if (MA(c,i) == -1)
