cd29746a4777cabe399a60df92cd836297ee0400
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-04-12 20:56:30 +0000

    * bali-phy.C: do_sampling( )    - revert remove of useless SPR node... why does this have an effect?

--- src/bali-phy.C
+++ src/bali-phy.C
@@ -94,6 +94,7 @@ void do_sampling(const variables_map& args,alignment& A,Parameters& P,long int m
   MoveAll tree_moves("tree");
   MoveAll topology_move("topology");
   MoveEach NNI_move("NNI");
+  MoveEach SPR_move("SPR");
 
   if (P.IModel().full_tree)
     NNI_move.add(1,MoveArgSingle("three_way_NNI:alignment:nodes:topology",
@@ -122,17 +123,18 @@ void do_sampling(const variables_map& args,alignment& A,Parameters& P,long int m
 
 
   if (P.IModel().full_tree)
-    topology_move.add(0.05,MoveArgSingle("SPR_and_A:topology:lengths:nodes:alignment:alignment_branch",
+    SPR_move.add(1,MoveArgSingle("SPR_and_A:topology:lengths:nodes:alignment:alignment_branch",
 				 sample_SPR,
 				 branches)
 		 );
   else
-    topology_move.add(0.05,MoveArgSingle("SPR:topology:lengths",
+    SPR_move.add(1,MoveArgSingle("SPR_and_A:topology:lengths",
 				 sample_SPR,
 				 branches)
 		 );
 
   topology_move.add(1,NNI_move,false);
+  topology_move.add(0.04,SPR_move);
   if (P.T.n_leaves() >3 and P.SModel().full_tree)
     tree_moves.add(1,topology_move);
   
