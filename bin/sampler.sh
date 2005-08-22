a8d115b836d749a8dd3a9bc1ea58c0e0524ea7b0
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-08-22 21:52:25 +0000

    * bin/sampler.sh: if the VERSION isn't specified, then use the highest version    that is available in ~/bin/bali-phy/.

--- bin/sampler.sh
+++ bin/sampler.sh
@@ -41,6 +41,23 @@ if [ ! -e Data ] ; then
     ln -sf ~/Devel/Sampler/Data
 fi
 
+#--------- Determine the Version --------#
+if [ "$VERSION" ] ; then
+    echo "Using specified version $VERSION."
+else
+    V=1
+    pushd ~/bin/bali-phy > /dev/null
+    for file in *-NDEBUG ; do
+	V2=${file%-NDEBUG}
+	if (( V2 > V )) 2>/dev/null ; then
+	    V=$V2;
+	fi
+    done
+    VERSION=$V
+    popd >/dev/null
+    echo "Using highest version $VERSION."
+fi
+exit
 #------ start the sampler with the specified args ----
 qsub=$(which qsub)
 if [ "$qsub" ] ; then
