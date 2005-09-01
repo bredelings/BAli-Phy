1eaf20a8678f1707c3c119d0fb1e07cd9749896c
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2005-09-01 00:01:03 +0000

    * bin/run.sh: output PID as well as JOB_ID

--- bin/run.sh
+++ bin/run.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 #$ -V -S /bin/sh -cwd
 
+# don't quit after 20 minutes! :P
 ulimit -t unlimited
+
+# get the arguments
 progname=$1
 shift
-echo working directory: $(pwd)
-echo command line: "exec $progname $@"
+
+# echo information to job_info file
 echo job id: $JOB_ID
-echo $JOB_ID > job_id
-hostname > job_node
+{  
+  echo "command: $progname $@"
+  echo "directory: $(pwd)"
+  if [ "$JOB_ID" ] ; then 
+    echo "job id: $JOB_ID"
+  fi
+  echo "hostname: $(hostname)"
+  echo "PID: $$"
+} > job_info
+
+# echo information to beginning of STDOUT
+echo working directory: $(pwd)
+echo command: "$progname $@"
+
+# exec the program
 exec $progname "$@"
