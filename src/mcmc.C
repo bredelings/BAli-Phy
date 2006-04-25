40340c0bb57d170af1024e4725297026de3345a4
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2006-04-25 23:10:07 +0000

    * mcmc.C:    + Sampler::go() - output alignment length and tree length.

--- src/mcmc.C
+++ src/mcmc.C
@@ -494,7 +494,8 @@ void Sampler::go(alignment& A,Parameters& P,int subsample,const int max,
   
   s_parameters<<"iter\t";
   s_parameters<<"prior\tlikelihood\tlogp\tbeta\t";
-  s_parameters<<P.header()<<endl;
+  s_parameters<<P.header();
+  s_parameters<<"\t|A|\t|T|"<<<<endl;
 
   //---------------- Run the MCMC chain -------------------//
   for(int iterations=0; iterations < max; iterations++) {
@@ -517,7 +518,8 @@ void Sampler::go(alignment& A,Parameters& P,int subsample,const int max,
 
       s_parameters<<iterations<<"\t";
       s_parameters<<prior<<"\t"<<likelihood<<"\t"<<Pr<<"\t"<<P.beta[0]<<"\t";
-      s_parameters<<P.state()<<endl;
+      s_parameters<<P.state();
+      s_parameters<<"\t"<<A.length()<<"\t"<<length(P.T)<<endl;
     }
 
     if (iterations%20 == 0) {
