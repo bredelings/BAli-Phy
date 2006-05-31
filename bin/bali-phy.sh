fc7d235f9c6dac3fc710050c59ad09b130fbe9df
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2006-05-31 21:11:04 +0000

    Simply SGE submissions yet further.
     * bin/bali-phy-sge, bin/bali-phy.sh
       + Set the job name with -N
       + Use a Here document for the submission script.

--- bin/bali-phy.sh
+++ bin/bali-phy.sh
@@ -1,50 +1,49 @@
 #!/bin/sh
 
-#### A script for running bali-phy on the Sun Grid Engine (SGE)
+### A script for running bali-phy on the Sun Grid Engine (SGE) ###
 
 # % VERSION=[version] bali-phy.sh <data.fasta> [optargs]
 
-# We assume that binaries are in ~/bin/bali-phy/$VERSION-NDEBUG
+# We assume that binaries are in ~/bin/bali-phy/$VERSION
 # (or ~/bin/bali-phy/$VERSION-DEBUG)
 
-#---- Are we debugging? ----
+#------------------------ qsub options: --------------------#
+# -V:         Give the job access to shell environment variables
+# -S /bin/sh: Use /bin/sh as the shell for the job.
+# -cwd:       Execute the job from the current working directory.
+# -N <name>   Specify job name
+
+QSUB_ARGS='-V -S /bin/sh -cwd -N bali-phy'
+
+#-------------------- Are we debugging? ---------------------#
 if [ "$DEBUG" ] ; then
     DEBUG="-DEBUG";
 fi
 
-#----------- Determine the Version ----------#
+#------------------ Locate the programs ---------------------#
+QSUB=$(type -p qsub)
+BALI_PHY=$(type -p bali-phy${DEBUG})
+
 if [ "$VERSION" ] ; then
     BALI_PHY=~/bin/bali-phy/${VERSION}${DEBUG}
-else
-    BALI_PHY=$(type -p bali-phy${DEBUG})
 fi
 
-QSUB=$(type -p qsub)
 #------ Run w/o SGE if not found ----
+if [ ! -x "$BALI_PHY" ] ; then
+   echo "bali-phy.sh: I can't find 'bali-phy' to run it!"
+   echo "bali-phy.sh: If it is installed, please add it to your $PATH"
+   exit 1
+fi
+
 if [ ! -x "$QSUB" ] ; then
    echo "Not using SGE";
    ($BALI_PHY "$@" &)
    exit 0
 fi
 
-if [ ! -x "$BALI_PHY" ] ; then
-   echo "bali-phy-sge: I can't find 'bali-phy' to run it!"
-   echo "bali-phy-sge: If it is installed, please add it to your $PATH"
-   exit 1
-fi
-
-#------ write the script
-RUN_SH=/tmp/bali-phy-sge-$$
-trap "rm -f $RUN_SH" 2 3 15
-
-echo >$RUN_SH '#!/bin/sh
-exec $@'
-
 #------ start the sampler
+qsub $QSUB_ARGS <<EOF 
+#!/bin/sh
+exec $BALI_PHY $@
+EOF
 
-QSUB_ARGS='-V -S /bin/sh -cwd'
-
-qsub $QSUB_ARGS $RUN_SH $BALI_PHY $@
-
-#------ clean up 
-rm -f $RUN_SH
