b936a5c59139332e33e618a5aaf400caecb46c58
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2007-05-09 21:27:21 +0000

    * configure.ac:    + define AM conditional HAVE_CAIRO based on --enable-cairo    + use PKG_CHECK_MODULES to check for cairo and set CAIRO_CFLAGS and      CAIRO_LDFLAGS    + substitute CAIRO_CFLAGS and CAIRO_LIBS
     * src/Makefile.am
       + use CAIRO_CFLAGS and CAIRO_LIBS instead of calling pkg-config manually
    
     * src/tools/draw-tree.C
       + load tree file in the requested format
       + construct an MF tree from full partitions in this file.

--- src/tools/draw-tree.C
+++ src/tools/draw-tree.C
@@ -1,11 +1,23 @@
 #include <cmath>
 #include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
 
 #include <cairo.h>
 #include <cairo-ps.h>
 #include <cairo-pdf.h>
 
-using std::string;
+#include <boost/program_options.hpp>
+
+#include "myexception.H"
+#include "sequencetree.H"
+#include "tree-dist.H"
+
+namespace po = boost::program_options;
+using po::variables_map;
+
+using namespace std;
 
 const double inch = 72.0;
 
@@ -96,11 +108,152 @@ void draw_to_pdf(const string& filename,void (*draw)(cairo_t*))
   draw_to_page(surface,draw);
 }
 
-int main ()
-{
-  draw_to_ps("tree.ps",draw_curvy_rectangle);
-  draw_to_pdf("tree.pdf",draw_curvy_rectangle);
+variables_map parse_cmd_line(int argc,char* argv[])
+{ 
+  using namespace po;
 
+  // named options
+  options_description input("Input options");
+  input.add_options()
+    ("help", "produce help message")
+    ("file",value<string>(),"predicates to examine")
+    ;
+  
+  options_description all("All options");
+  all.add(input);
+
+  // positional options
+  positional_options_description p;
+  p.add("file", -1);
+  
+  variables_map args;     
+  store(command_line_parser(argc, argv).
+	    options(all).positional(p).run(), args);
+  // store(parse_command_line(argc, argv, desc), args);
+  notify(args);    
+
+  if (args.count("help")) {
+    cout<<"Usage: draw-graph <file1>\n";
+    cout<<all<<"\n";
+    exit(0);
+  }
+
+  if (not args.count("file"))
+    throw myexception()<<"No file supplied.";
+
+  return args;
+}
+
+class MC_tree
+{
+public:
+  vector<Partition>  branches;
+  vector<double> branch_lengths;
+
+  vector<Partition> nodes;
+  vector<double> node_lengths;
+
+  vector<Partition> mini_branches;
+  vector<double> mini_branch_lengths;
+  vector<int> parent_branch_of_mini_branch;
+};
+
+/// FIXME - check partitions to see that names are all unique?
+
+MC_tree get_MC_tree(const string& filename)
+{
+  MC_tree MC;
+  std::ifstream file(filename.c_str());
+
+  if (not file)
+    throw myexception()<<"Can't open file '"<<filename<<"'";
+
+  string line;
+  while(file) {
+    while (getline(file,line) and not line.size());
+    if (not file) break;
+    vector<string> words = split(line,' ');
+
+    if (words.size() != 2)
+      throw myexception()<<"Don't understand line '"<<line<<"'";
+
+    double length = convertTo<double>(words[1]);
+
+    if (not getline(file,line))
+      throw myexception()<<"Missing partition at end of file!";
+
+    if (not line.size())
+      throw myexception()<<"Empty line in middle of file?";
+
+    Partition P(line);
+    line = "";
+    if (words[0] == "branch") {
+      MC.branches.push_back(P);
+      MC.branch_lengths.push_back(length);
+    }
+    else if (words[0] == "node") {
+      MC.nodes.push_back(P);
+      MC.node_lengths.push_back(length);
+    }
+    else if (words[0] == "mini-branch") {
+      MC.mini_branches.push_back(P);
+      MC.mini_branch_lengths.push_back(length);
+    }
+    else
+      throw myexception()<<"Don't understand word="<<words[0];;
+  }
+
+  cerr<<"Read "<<MC.branches.size()<<" partitions."<<endl;
+  return MC;
+}
+
+SequenceTree build_tree(const MC_tree& MC)
+{
+  vector<Partition> full_partitions;
+  for(int i=0;i<MC.branches.size();i++)
+    if (MC.branches[i].full())
+      full_partitions.push_back(MC.branches[i]);
+
+  cerr<<"Read "<<MC.branches.size()<<" partitions ... "<<full_partitions.size()<<" full partitions."<<endl;
+
+  SequenceTree MF = star_tree(full_partitions[0].names);
+  for(int i=0;i<full_partitions.size();i++) {
+    cerr<<"i = "<<i<<"    pi = "<<full_partitions[i]<<endl;
+    MF.induce_partition(full_partitions[i].group1);
+  }
+
+  //set the branch lengths!
+
+  cout<<MF.write(true)<<endl;
+
+  return MF;
+}
+
+
+int main(int argc,char* argv[]) 
+{
+  try {
+    //---------- Parse command line  -------//
+    variables_map args = parse_cmd_line(argc,argv);
+
+    //-------- Load MC Tree and lengths -----//
+    MC_tree MC = get_MC_tree(args["file"].as<string>());
+
+    SequenceTree MF = build_tree(MC);
+
+    // do we need a generic draw-to-cr-context object?
+
+    // lay out the tree
+
+    // draw the tree
+
+    draw_to_ps("tree.ps",draw_curvy_rectangle);
+    draw_to_pdf("tree.pdf",draw_curvy_rectangle);
+  }
+  catch (std::exception& e) {
+    cerr<<"Exception: "<<e.what()<<endl;
+    exit(1);
+  }
   return 0;
 }
 
