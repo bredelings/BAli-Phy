bb4cc2949cc147c55e09a150059dfb954ecd7657
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2007-08-02 03:09:43 +0000

    [NEW] Print out SVN revision+date.
     * src/version.C
       + include src/revision.H
       + Print out SVN revision+date in the VERSION line.
    
     * src/Makefile.am
       + distribution revision.H

--- src/version.C
+++ src/version.C
@@ -9,8 +9,20 @@
 #endif
 
 #include <iostream>
+#include "revision.H"
+#include <string>
 
-void print_version_info(std::ostream& file) 
+using namespace std;
+
+void print_version_info(ostream& file) 
 {
-  file<<"VERSION: "<<PACKAGE_VERSION<<"\nBUILD: "<<__DATE__<<" "<<__TIME__<<std::endl;
+  string svn_date = SVN_LAST_CHANGED_DATE;
+  svn_date = svn_date.substr(1,25);
+  string svn_rev = SVN_LAST_CHANGED_REV;
+  svn_rev = svn_rev.substr(1,svn_rev.size()-3);
+
+  file<<"VERSION: "<<PACKAGE_VERSION;
+  file<<"    ("<<svn_rev<<")";
+  file<<"    ("<<svn_date<<")\n";
+  file<<"BUILD: "<<__DATE__<<" "<<__TIME__<<endl;
 }
