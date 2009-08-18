ae741ba56889a14b6a0e833935d554bfc3cd5d08
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2009-08-18 23:24:56 +0000

    * tree.{H,C}    + virtual int {Rooted,}Tree::parse_with_names( ): new member functions.    + write(Tree&, names, bool): new function.

--- src/tree.C
+++ src/tree.C
@@ -1073,6 +1073,81 @@ int Tree::parse_no_names(const string& line)
   return root_->node;
 }
 
+// FIXME - don't we need to destroy the current tree?
+int Tree::parse_with_names(const string& line,const vector<string>& names)
+{
+  // destroy old tree structure
+  if (nodes_.size()) TreeView(nodes_[0]).destroy();
+
+  vector< vector<BranchNode*> > tree_stack(1);
+
+  const string delimiters = "(),:;";
+  const string whitespace = "\t\n ";
+
+  string prev;
+  string word;
+  for(int i=0;get_word(word,i,line,delimiters,whitespace);prev=word) 
+  {
+    //std::cerr<<"word = '"<<word<<"'    depth = "<<tree_stack.size()<<"   stack size = "<<tree_stack.back().size()<<std::endl;
+
+    if (word == ";") break;
+
+    //------ Process the data given the current state ------//
+    if (word == "(") {
+      tree_stack.push_back(vector<BranchNode*>());
+      if (not (prev == "(" or prev == "," or prev == ""))
+	throw myexception()<<"In tree file, found '(' in the middle of word \""<<prev<<"\"";
+    }
+    else if (word == ")") {
+      // We need at least 2 levels of trees
+      if (tree_stack.size() < 2)
+	throw myexception()<<"In tree file, too many end parenthesis.";
+
+      // merge the trees in the top level
+      BranchNode* BN = tree_stack.back()[0];
+      for(int i=1;i<tree_stack.back().size();i++)
+	TreeView::merge_nodes(BN,tree_stack.back()[i]);
+
+      // destroy the top level
+      tree_stack.pop_back();
+
+      // insert merged trees into the next level down
+      BN = ::add_node(BN);
+      BN->out->length = BN->length = -1;
+      tree_stack.back().push_back(BN);
+    }
+    else if (prev == "(" or prev == "," or prev == "") 
+    {
+      int leaf_index = find_index(names,word);
+
+      BranchNode* BN = new BranchNode(-1,leaf_index,-1);
+      BN->out = BN->next = BN->prev = BN;
+
+      BN = ::add_node(BN);
+      BN->out->length = BN->length = -1;
+      tree_stack.back().push_back(BN);
+    }
+    else if (prev == ":") {
+      BranchNode* BN = tree_stack.back().back();
+      BN->out->length = BN->length = convertTo<double>(word);
+    }
+  }
+
+
+  if (tree_stack.size() != 1)
+    throw myexception()<<"Attempted to read w/o enough left parenthesis";
+  if (tree_stack.back().size() != 1)
+    throw myexception()<<"Multiple trees on the same line";
+
+  BranchNode* remainder = tree_stack.back()[0];
+  BranchNode* root_ = TreeView::unlink_subtree(remainder->out);
+  TreeView(remainder).destroy();
+
+  reanalyze(root_);
+
+  return root_->node;
+}
+
 Tree::Tree(const BranchNode* BN) 
   :caches_valid(false)
 {
@@ -1244,6 +1319,15 @@ int RootedTree::parse_no_names(const string& s)
   return r;
 }
 
+int RootedTree::parse_with_names(const string& s,const vector<string>& names)
+{
+  int r = Tree::parse_with_names(s,names);
+
+  root_ = nodes_[r];
+
+  return r;
+}
+
 string write(const vector<string>& names, const_branchview b, bool print_lengths)
 {
   string output;
@@ -1274,6 +1358,20 @@ string write(const vector<string>& names, const_branchview b, bool print_lengths
 string write(const RootedTree& T, const vector<string>& names, bool print_lengths) 
 {
   vector<const_branchview> branches = sorted_neighbors(T.root());
+
+  string output = "(";
+  for(int i=0;i<branches.size();i++) {
+    output += write(names,branches[i],print_lengths);
+    if (i+1 < branches.size())
+      output += ',';
+  }
+  output += ");";
+  return output;
+}
+
+string write(const Tree& T, const vector<string>& names, bool print_lengths) 
+{
+  vector<const_branchview> branches = sorted_neighbors(T.directed_branch(0).target());
 
   string output = "(";
   for(int i=0;i<branches.size();i++) {
