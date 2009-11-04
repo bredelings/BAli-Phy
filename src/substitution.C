0ecef2e619ab09eaaed1bb5097ec80bd670c4b85
Benjamin Redelings I <benjamin_redelings@ncsu.edu>
2009-11-04 19:07:53 +0000

    * Add copyright headers.

--- src/substitution.C
+++ src/substitution.C
@@ -1,3 +1,22 @@
+/*
+   Copyright (C) 2004-2005,2007,2009 Benjamin Redelings
+
+This file is part of BAli-Phy.
+
+BAli-Phy is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
+
+BAli-Phy is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with BAli-Phy; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
 #include "substitution.H"
 #include "substitution-index.H"
 #include "rng.H"
