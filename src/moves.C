27cf620d3fa0983201cf7e18a3515db782a34f96
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2010-03-16 11:28:46 -0400

    Do NNI+A less often: its expensive!

--- src/moves.C
+++ src/moves.C
@@ -356,7 +356,7 @@ void walk_tree_sample_NNI_and_A(Parameters& P, MoveStats& Stats)
   for(int i=0;i<branches.size();i++) 
   {
     int b = branches[i];
-    if (myrandomf() < 0.025)
+    if (myrandomf() < 0.01)
       three_way_topology_and_alignment_sample(P,Stats,b);
     else
       if (myrandomf() < 0.95)
