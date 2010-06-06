4ea7201246c3cb25fad3520be3ba7db7012b91a4
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2010-06-05 22:19:58 -0400

    Make RS07 models have t=0.25 during training.

--- src/imodel.C
+++ src/imodel.C
@@ -57,6 +57,19 @@ namespace indel
   }
 }
 
+void IndelModel::set_training(bool b)
+{
+  in_training = b;
+}
+
+bool IndelModel::is_training() const
+{
+  return in_training;
+}
+
+IndelModel::~IndelModel() {}
+
+
 string i_parameter_name(int i,int n) {
   if (i>=n)
     throw myexception()<<"substitution model: refered to parameter "<<i<<" but there are only "<<n<<" parameters.";
@@ -152,9 +165,6 @@ efloat_t SimpleIndelModel::lengthp(int l) const
   return P;
 }
 
-IndelModel::~IndelModel() {}
-
-
 indel::PairHMM SimpleIndelModel::get_branch_HMM(double) const 
 {
   using namespace states;
@@ -163,8 +173,8 @@ indel::PairHMM SimpleIndelModel::get_branch_HMM(double) const
   double e       = exp(get_parameter_value(1));
   double t       = exp(get_parameter_value(2));
 
-  if (in_training)
-    delta = std::min(delta, 0.005);
+  if (is_training())
+    delta = std::min(delta,0.01);
 
   if (delta > 0.5)
     throw myexception()<<"indel model: we need (delta <= 0.5), but delta = "<<delta;
@@ -245,11 +255,6 @@ efloat_t SimpleIndelModel::prior() const
 
 string SimpleIndelModel::name() const {return "RS05";}
 
-void SimpleIndelModel::set_training(bool b)
-{
-  in_training = b;
-}
-
 SimpleIndelModel::SimpleIndelModel()
   :QE(Q1.size1(),Q1.size2())
 {
@@ -286,6 +291,9 @@ indel::PairHMM NewIndelModel::get_branch_HMM(double t) const
   if (not time_dependant)
     t = 1;
 
+  if (is_training())
+    t = 0.25;
+
   double rate    = exp(get_parameter_value(0));
   double e = exp(get_parameter_value(1));
 
@@ -294,8 +302,9 @@ indel::PairHMM NewIndelModel::get_branch_HMM(double t) const
   double mu = rate*t/(1.0-e);
   double P_indel = 1.0 - exp(-mu);
   double A = P_indel;
-  if (in_training)
-    A = std::min(A, 0.005);
+  if (is_training())
+    A = std::min(A,0.01);
+
   double delta = A/(1+A);
 
   if (t < -0.5)
@@ -341,11 +350,6 @@ indel::PairHMM NewIndelModel::get_branch_HMM(double t) const
   Q.start_pi(E)  = 0;
 
   return Q;
-}
-
-void NewIndelModel::set_training(bool b)
-{
-  in_training = b;
 }
 
 string NewIndelModel::name() const 
