f93be11857c0bb7f864b5664b473712c9e1e14f5
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2011-07-04 12:38:51 -0400

    Make the distributions use the right densities.

--- src/distribution-operations.C
+++ src/distribution-operations.C
@@ -1,22 +1,22 @@
 #include "distribution-operations.H"
 
-expression_ref exponential_dist = prob_density("Exponential",log_laplace_density());
+expression_ref exponential_dist = prob_density("Exponential", exponential_density());
 
-expression_ref gamma_dist       = prob_density("Gamma",log_laplace_density());
+expression_ref gamma_dist       = prob_density("Gamma", gamma_density());
 
-expression_ref beta_dist        = prob_density("Beta",log_laplace_density());
+expression_ref beta_dist        = prob_density("Beta", beta_density());
 
-expression_ref normal_dist      = prob_density("Normal",log_laplace_density());
+expression_ref normal_dist      = prob_density("Normal", normal_density());
 
-expression_ref log_normal_dist  = prob_density("LogNormal",log_laplace_density());
+expression_ref log_normal_dist  = prob_density("LogNormal", log_normal_density());
 
-expression_ref cauchy_dist      = prob_density("Cauchy",log_laplace_density());
+expression_ref cauchy_dist      = prob_density("Cauchy", cauchy_density());
 
-expression_ref dirichlet_dist   = prob_density("Dirichlet",log_laplace_density());
+expression_ref dirichlet_dist   = prob_density("Dirichlet", dirichlet_density());
 
-expression_ref laplace_dist     = prob_density("Laplace",log_laplace_density());
+expression_ref laplace_dist     = prob_density("Laplace", laplace_density());
 
-expression_ref log_laplace_dist = prob_density("LogLaplace",log_laplace_density());
+expression_ref log_laplace_dist = prob_density("LogLaplace", log_laplace_density());
 
-expression_ref uniform_dist     = prob_density("Uniform",log_laplace_density());
+expression_ref uniform_dist     = prob_density("Uniform", uniform_density());
 
