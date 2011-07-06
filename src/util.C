c999664bb2d3c3152171f2bd676f6f8ab6d623f7
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2011-07-06 14:02:56 -0400

    Add a demangler for GCC.

--- src/util.C
+++ src/util.C
@@ -382,3 +382,20 @@ void check_supplied_filenames(int n,const vector<string>& files, bool exact)
     cerr<<"Warning: ignoring "<<files.size()-n<<" extra filenames."<<endl;
 }
 
+#ifdef __GNUC__
+#include <cxxabi.h>
+string demangle(const string& s)
+{
+  int     status;
+  char* result_str = abi::__cxa_demangle(s.c_str(), 0, 0, &status);
+
+  string result = result_str;
+  free(result_str);
+  return result;
+}
+#else
+string demangle(const string& s)
+{
+  return s;
+}
+#endif
