937c0f7c8be1217b6cefb69d9e4ddb755c52d5c9
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2011-09-29 18:36:56 -0400

    Add handling of case expressions.

--- src/operations.C
+++ src/operations.C
@@ -29,7 +29,52 @@ shared_ptr<const Object> Case::operator()(OperationArgs& Args) const
   shared_ptr<const Object> obj = Args.evaluate(0);
   shared_ptr<const Object> alts = Args.reference(1);
 
-  std::abort();
+  vector<expression_ref> cases;
+  vector<expression_ref> results;
+  parse_alternatives(alts, cases, results);
+
+  shared_ptr<const expression> E = dynamic_pointer_cast<const expression>(obj);
+
+  expression_ref result;
+  for(int i=0;i<cases.size() and not result;i++)
+  {
+    // If its a dummy, then match it.
+    if (shared_ptr<const dummy> D2 = dynamic_pointer_cast<const dummy>(cases[i]))
+    {
+      result = results[i];
+
+      // Substitute the matched value into the expression if the dummy isn't "_";
+      if (D2->index >= 0)
+	result = substitute(result, cases[i], obj);
+    }
+    // If we are a 0-arg literal constant constructor, then match iff obj==cases[i]
+    else if (not E)
+    {
+      if (obj->compare(*cases[i]))
+	result = results[i];
+    }
+    // If we are an n-arg constructor, then match iff the case is an expression and the head matches.
+    else if (E)
+    {
+      if (shared_ptr<const expression> E2 = dynamic_pointer_cast<const expression>(cases[i]))
+      {
+	if (E->sub[0]->compare(*E2->sub[0]))
+	{
+	  assert(E->size() == E2->size());
+
+	  result = results[i];
+
+	  for(int j=1;j<E->size();j++)
+	  {
+	    if (not is_wildcard(E2->sub[j]))
+	      result = substitute(result, E2->sub[j], E->sub[j]);
+	  }
+	}
+      }
+    }
+  }
+
+  return result;
 }
 
 std::string Case::name() const {
