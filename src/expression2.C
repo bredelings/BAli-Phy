90cd1d1e0f047a1a769fd2a4ddc1f19673c28ea9
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-01-03 16:05:45 -0800

    Remove Context and Formula from expression2.C

--- src/expression2.C
+++ src/expression2.C
@@ -6,8 +6,6 @@
 #include "util.H"
 
 #include "formula.H"
-#include "context.H"
-#include "computation.H"
 #include "operation.H"
 #include "operations.H"
 #include "distribution-operations.H"
@@ -67,68 +65,20 @@ using std::endl;
  *    ------------
  */
 
-term_ref add_probability_expression(polymorphic_cow_ptr<Formula>& F)
-{
-  expression_ref query = distributed(_2,Tuple(prob_density(_,_1),_3));
-
-  typed_expression_ref<Log_Double> Pr;
-
-  // Check each expression in the Formula
-  for(int i=0;i<F->size();i++)
-  {
-    vector<expression_ref> results; 
-
-    // If its a probability expression, then...
-    if (find_match(query,(*F)[i],results))
-    {
-      // Extract the density operation
-      shared_ptr<const Operation> density_op = dynamic_pointer_cast<Operation>(results[0]);
-      if (not density_op) throw myexception()<<"Expression "<<i<<" does have an Op in the right place!";
-
-      // Create an expression for calculating the density of these random variables given their inputs
-      expression_ref density_func = lambda_expression( *density_op );
-      typed_expression_ref<Log_Double> Pr_i = density_func(results[1], results[2]);
-
-      // Extend the probability expression to include this term also.
-      // (FIXME: a balanced tree could save computation time)
-      if (not Pr)
-	Pr = Pr_i;
-      else
-	Pr = Pr_i * Pr;
-    }
-  }
-
-  // If this model has random variables... 
-  if (Pr)
-  {
-    F->add_expression(prob(Pr));
-
-    vector<int> results;
-    F->find_match_expression2(prob(_1),results);
-    return term_ref(results[0],F);
-  }
-  else
-    return term_ref();
-}
-
 int main()
 {
-  Formula f;
-  polymorphic_cow_ptr<Formula> F(f);
   //  term_ref x = F->add_state_node("X");
   expression_ref x = parameter("X");
   expression_ref y = parameter("Y");
   expression_ref z = parameter("Z");
   expression_ref w = parameter("W");
-  term_ref one = F->add_expression(Double(1));
+  expression_ref one = Double(1);
 
   typed_expression_ref<Double> X = x;
   typed_expression_ref<Double> Y = y;
   typed_expression_ref<Int> W = w;
   typed_expression_ref<Double> Z = z;
   typed_expression_ref<Double> One(1.0);
-
-  F->add_expression(Double(1));
 
   expression_ref mul = lambda_expression( Multiply<Double>() );
   expression_ref muli = lambda_expression( Multiply<Int>() );
@@ -177,40 +127,45 @@ int main()
   cout<<graph_normalize(case_if(Z>1.0,X*Y,W))<<"\n";
   cout<<let_float(graph_normalize(case_if(Z>1.0,X*Y,W)))<<"\n";
 
-  term_ref x_times_y_plus_one = F->add_expression( plus(mul(x)(y))(one) );
+  cout<<"Creating an Context...\n";
+  context CTX1;
+  CTX1.add_parameter("X");
+  CTX1.add_parameter("Y");
+  CTX1.add_parameter("Z");
+  CTX1.add_parameter("W");
 
-  term_ref z_gt_1 = F->add_expression(gt(z)(one));
+  expression_ref x_times_y_plus_one = plus(mul(x)(y))(one);
+  int x_times_y_plus_one_ = CTX1.add_compute_expression(x_times_y_plus_one);
 
-  term_ref z_gt = F->add_expression(gt(z));
+  expression_ref z_gt_1 = gt(z)(one);
 
-  term_ref x_plus_y = F->add_expression(plus(x)(y));
+  int z_gt = CTX1.add_compute_expression(gt(z));
 
-  term_ref w_2 = F->add_expression( muli(w)(w) );
+  int x_plus_y = CTX1.add_compute_expression(plus(x)(y));
 
-  term_ref cond = F->add_expression( If(z_gt_1, x_times_y_plus_one, w_2));
+  int w_2 = CTX1.add_compute_expression( muli(w)(w) );
+
+  int cond = CTX1.add_compute_expression( If(z_gt_1, x_times_y_plus_one, w_2));
 
   // this should be a dup and do nothing
-  F->add_expression( If( gt(z)(one) ) ( plus( mul(x)(y))(one) ) ( muli(w)(w) ) );
+  CTX1.add_compute_expression( If( gt(z)(one) ) ( plus( mul(x)(y))(one) ) ( muli(w)(w) ) );
   // -- using multiple arguments instead of one at a time.  This works up to 3 arguments
-  F->add_expression( If( gt(z, one) , plus( mul(x, y), one) , muli(w,w) ) );
+  CTX1.add_compute_expression( If( gt(z, one) , plus( mul(x, y), one) , muli(w,w) ) );
   // -- using automatic creation of operators based on typed references
-  F->add_expression( If( Z > One , X*Y+One , W*W ) );
+  CTX1.add_compute_expression( If( Z > One , X*Y+One , W*W ) );
   // -- can we create constants easily?
-  F->add_expression( If( Z > 1.0, X*Y+1.0, W*W ) );
+  CTX1.add_compute_expression( If( Z > 1.0, X*Y+1.0, W*W ) );
 
-  term_ref defv = F->add_expression(  default_value(parameter("X"), 2.0) );
-  term_ref list_x_y = F->add_expression(Cons(X,Cons(Y,ListEnd)));
-  term_ref tuple_x_y = F->add_expression(Tuple(X,Y));
+  int defv = CTX1.add_compute_expression(  default_value(parameter("X"), 2.0) );
+  int list_x_y = CTX1.add_compute_expression(Cons(X,Cons(Y,ListEnd)));
+  int tuple_x_y = CTX1.add_compute_expression(Tuple(X,Y));
 
-  term_ref prior_x_y = F->add_expression(distributed(parameter("X"),Tuple(exponential_dist,Y+One)));
-  term_ref prior_y_z = F->add_expression(distributed(parameter("Y"),Tuple(exponential_dist,Z+One)));
-  term_ref probability_expression = add_probability_expression(F);
-
-  cout<<"Creating an Context...\n";
-  Context CTX1(F);
+  int prior_x_y = CTX1.add_note(distributed(parameter("X"),Tuple(exponential_dist,Y+One)));
+  int prior_y_z = CTX1.add_note(distributed(parameter("Y"),Tuple(exponential_dist,Z+One)));
+  int probability_expression = add_probability_expression(CTX1);
 
   cout<<"Setting a few variables...\n";
-  //  CTX1.set_parameter_value("X",Double(2));
+  CTX1.set_parameter_value("X",Double(2));
   CTX1.set_parameter_value("Y",Double(3));
   CTX1.set_parameter_value("Z",Double(4));
   CTX1.set_parameter_value("W",Int(5));
@@ -220,13 +175,13 @@ int main()
 
   cout<<"\n\n";
   cout<<"Copying the context: values should be shared between CTX1 and CTX2.\n";
-  Context CTX2 = CTX1;
+  context CTX2 = CTX1;
 
   cout<<"CTX1 = \n"<<CTX1<<"\n";
 
   cout<<"\n\n";
   cout<<"Evaluating expressions in CTX1: results should be visible in CTX2 also.\n";
-  shared_ptr<const Object> result = CTX1.evaluate(x_times_y_plus_one);
+  shared_ptr<const Object> result = CTX1.evaluate(x_times_y_plus_one_);
   CTX1.evaluate(cond);
   CTX1.evaluate(defv);
   CTX1.evaluate(z_gt);
@@ -246,7 +201,7 @@ int main()
 
   cout<<"\n\n";
   cout<<"Evaluating X*Y+1.0 in CTX1: since X*Y is unchanged, old computation should be re-used.\n";
-  result = CTX1.evaluate(x_times_y_plus_one);
+  result = CTX1.evaluate(x_times_y_plus_one_);
 
   cout<<"\n\n";
   cout<<"Changing W in CTX2: If(Z>1,X*Y+1,W*W) is unaffected should remain valid.\n";
@@ -264,53 +219,6 @@ int main()
   // I guess the current framework could not evaluate X:Y to X:Y.  It would simply return value(X):value(Y).
   // I could introduce a QUOTE expression to prevent this... that sounds rather LISP-y.
 
-  cout<<" Adding function 'square' to CTX1.\n";
-  expression_ref square = lambda_expression(body_function("square",1));
-  CTX1.add_expression( defun( square(_1), true, mul(_1,_1)) );
-
-  cout<<" Adding function 'fmap' to CTX1.\n";
-  expression_ref fmap = lambda_expression(body_function("fmap",2));
-  CTX1.add_expression( defun( fmap(_,ListEnd), true, ListEnd) );
-  CTX1.add_expression( defun( fmap(_1,Cons(_2,_3)), true, Cons(_1(_2),fmap(_1,_3) )) );
-
-  cout<<" Adding function 'take' to CTX1.\n";
-  expression_ref take = lambda_expression(body_function("take",2));
-  CTX1.add_expression( defun( take(_,ListEnd), true, ListEnd) );
-  CTX1.add_expression( defun( take(0,_), true, ListEnd) );
-  CTX1.add_expression( defun( take(_1,Cons(_2,_3)), true, Cons(_2,take(minusi(_1,1),_3)) ) );
-
-  cout<<" Adding function 'repeat' to CTX1.\n";
-  expression_ref repeat = lambda_expression(body_function("repeat",1));
-  CTX1.add_expression( defun( repeat(_1), true, Cons(_1,repeat(_1)) ) );
-
-  cout<<" Adding function 'iterate' to CTX1.\n";
-  expression_ref iterate = lambda_expression(body_function("iterate",2));
-  CTX1.add_expression( defun( iterate(_1,_2), true, Cons(_2,iterate(_1,_1(_2))) ) );
-
-  cout<<" Adding function 'print' to CTX1.\n";
-  cout<<" Adding function 'print_list' to CTX1.\n";
-  expression_ref print = lambda_expression(body_function("print",1));
-  expression_ref print_list = lambda_expression(body_function("print_list",1));
-
-  CTX1.add_expression( defun( print_list(Cons(_1,ListEnd)), true, print(_1) ) );
-  CTX1.add_expression( defun( print_list(Cons(_1,_2)), true, concat(print(_1),concat(", ",print_list(_2))) ) );
-  CTX1.add_expression( defun( print(Cons(_1,_2)), true, concat("[",concat(print_list(Cons(_1,_2)),"]")) ) );
-  CTX1.add_expression( defun( print(_1), true, sys_print(_1) ) );
-
-  expression_ref v0 = dummy(0);
-  expression_ref v1 = dummy(1);
-  expression_ref v2 = dummy(2);
-  expression_ref v3 = dummy(3);
-  expression_ref v4 = dummy(4);
-
-  CTX1.add_expression( case_expression(false, dummy(0), Cons(dummy(1),ListEnd), dummy(1)) );
-  CTX1.add_expression( case_expression(true, dummy(0), Cons(dummy(1),ListEnd), dummy(1)) );
-  CTX1.add_expression( def_function(false, Tuple(v0, Cons(v1, v2)), Cons(v1, v3((typed_expression_ref<Double>(v0)-1.0),v2))) );
-  CTX1.add_expression( def_function(true, Tuple(v0, Cons(v1, v2)), Cons(v1, v3((typed_expression_ref<Double>(v0)-1.0),v2))) );
-
-  cout<<"\n CTX1 now contains this list of non-sub expressions:\n";
-  //  cout<<*CTX1.F<<"\n";
-
   for(int i=0;i<CTX1.n_expressions();i++)
   {
     expression_ref R = CTX1.get_expression(i);
@@ -320,73 +228,16 @@ int main()
     cout<<"     "<<N->print()<<"\n";
   }
 
-
-  cout<<"\n\n";
-  cout<<"Match test:\n";
-  expression_ref pattern = default_value(parameter("X"))(match(0));
-  expression_ref target = default_value(parameter("X"))(One);
-  vector< expression_ref > results;
-  cout<<"Checking if expression "<<target<<" matches query "<<pattern<<":\n";
-  if (find_match(pattern, target,results))
-  {
-    cout<<"match has size "<<results.size()<<"\n";
-    if (results.size())
-      cout<<"   _0 = "<<results[0]<<"\n";
-  }
-  else
-    cout<<"no match!";
-
-  expression_ref test = Tuple(One+One,One+One+One);
-  cout<<"\n";
-  cout<<"Eval test: does "<<test<<" match "<<Tuple(2.0,3.0)<<"?\n";
-  results.clear();
-  if (eval_match(CTX1,test,Tuple(2.0,3.0),results) )
-    cout<<"yes, result = "<<test<<"\n";
-
-
-  expression_ref test2 = Tuple(square(parameter("X")),One+One);
-  cout<<"\n";
-  cout<<"Eval test: does "<<test2<<" match "<<Tuple(9.0, 2.0)<<"?\n";
-  results.clear();
-  if (eval_match(CTX1, test2,Tuple(9.0, 2.0), results))
-    cout<<"yes, result = "<<test2<<"\n";
-
-  expression_ref test3 = fmap(square,Cons(parameter("X"),Cons(One+One,ListEnd)));
-  cout<<"\n";
-  cout<<"Eval test: "<<test3<<" = ";
-  test3 = eval(CTX1, test3);
-  cout<<test3<<"\n";
-
-  expression_ref test4 = take(3,repeat(X));
-  cout<<"\n";
-  cout<<"Eval test: "<<test4<<" = ";
-  test4 = eval(CTX1, test4);
-  cout<<test4<<"\n";
-
-  expression_ref test5 = take(3,fmap(square,repeat(X)));
-  cout<<"\n";
-  cout<<"Eval test: "<<test5<<" = ";
-  test5 = eval(CTX1, test5);
-  cout<<test5<<"\n";
-
-  // Here we hit the problem of substituting lambda expressions into lambda expressions: match name collision!
-  expression_ref test6 = take(3,fmap(square,iterate(plus(1.0),1.0)));
-  cout<<"\n";
-  cout<<"Eval test: "<<test6<<" = ";
-  test6 = eval(CTX1, test6);
-  cout<<test6<<"\n";
-
-  expression_ref test7 = print(take(3,fmap(square,iterate(plus(1.0),1.0))));
-  cout<<"\n";
-  cout<<"Eval test: "<<test7<<" = ";
-  test7 = eval(CTX1, test7);
-  cout<<test7<<"\n";
+  expression_ref v0 = dummy(0);
+  expression_ref v1 = dummy(1);
+  expression_ref v2 = dummy(2);
+  expression_ref v3 = dummy(3);
+  expression_ref v4 = dummy(4);
 
   // take - actually, this requires handling operators.
+  expression_ref take = dummy("take");
   expression_ref def_take;
   {  
-    take = dummy("take");
-
     vector<expression_ref> patterns;
     vector<expression_ref> bodies;
     patterns.push_back( Tuple(0, v1) );
@@ -407,9 +258,6 @@ int main()
 
     //       - lambda take.n.x.case n of {0->[],_->case x of {[] -> [],h:t->h:(take (n-3) t)}}
     def_take = def_function(true, patterns, bodies);
-
-    CTX1.add_expression( def_function(false, patterns, bodies) );
-    CTX1.add_expression( def_take );
   }
 
   expression_ref test_let_float = def_take;
@@ -418,9 +266,9 @@ int main()
   cout<<let_float(graph_normalize(test_let_float))<<"\n";
 
   // iterate
+  expression_ref iterate = dummy("iterate");
   expression_ref def_iterate;
   {  
-    iterate = dummy("iterate");
 
     vector<expression_ref> patterns;
     vector<expression_ref> bodies;
@@ -431,9 +279,9 @@ int main()
   }
 
   // fmap
+  expression_ref fmap = dummy("fmap");
   expression_ref def_fmap;
   {  
-    fmap = dummy("fmap");
 
     vector<expression_ref> patterns;
     vector<expression_ref> bodies;
