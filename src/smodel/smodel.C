7ae454b967ed935746c66b6419fbd792f16a93b3
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-01-06 19:34:33 -0500

    Remove old unused class ReversibleMarkovSuperModel.

--- src/smodel/smodel.C
+++ src/smodel/smodel.C
@@ -649,11 +649,6 @@ namespace substitution {
     recalc_all();
   }
 
-  /// Construct a reversible Markov model on alphabet 'a'
-  ReversibleMarkovSuperModel::ReversibleMarkovSuperModel(const ::Model& S1,const ::Model& R1)
-    :OpModel( Q_from_S_and_R(model_result_expression(*prefix_model(S1,"S")),model_result_expression(*prefix_model(R1,"R"))) )
-  { }
-
   SimpleReversibleMarkovModel::SimpleReversibleMarkovModel(const ::Model& E)
     :OpModel( Q_from_S_and_R( model_result_expression(*prefix_model(E,"S")), model_result_expression(*prefix_model(SimpleFrequencyModel(*get_alphabet(E)),"R") ) ) )
   { }
