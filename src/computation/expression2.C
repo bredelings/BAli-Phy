dd37e84b39af3e14dc965e565a0f9412ffcfbb78
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-02-16 12:12:59 -0500

    Test arrays.

--- src/computation/expression2.C
+++ src/computation/expression2.C
@@ -443,7 +443,11 @@ int main()
   cout<<"C.evaluate(3) = "<<C.evaluate(3)<<"\n";
 
 
-  expression_ref test18 = (getIndex,myArray,5);
+  expression_ref test18 = (getIndex,myArray,9);
   cout<<"Evaluating "<<test18<<std::endl;
-  cout<<"  result = "<<C.evaluate_expression( (getIndex,myArray,5) )<<std::endl;
+  cout<<"  result = "<<C.evaluate_expression( test18 )<<std::endl;
+  cout<<"  result = "<<C.evaluate_expression( test18 )<<std::endl;
+  cout<<"  result = "<<C.evaluate_expression( (bnds,myArray) )<<std::endl;
+  cout<<"  result = "<<C.evaluate_expression( myArray )<<std::endl;
+  cout<<"  result = "<<C.evaluate_expression( myArray )<<std::endl;
 }
