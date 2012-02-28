039dddb65101d5911f234d03137f62760c7801cc
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-02-28 15:55:44 -0500

    Convert some more things to T_.

--- src/substitution.C
+++ src/substitution.C
@@ -1766,8 +1766,8 @@ namespace substitution {
     data_partition P2 = P;
     P2.LC.invalidate_all();
     P2.invalidate_subA_index_all();
-    for(int i=0;i<P2.T->n_branches();i++)
-      P2.setlength(i,P2.T->branch(i).length());
+    for(int i=0;i<P2.T_->n_branches();i++)
+      P2.setlength(i,P2.T_->branch(i).length());
     efloat_t result2 = Pr(P2, P2.LC);
 
     if (std::abs(log(result) - log(result2))  > 1.0e-9) {
