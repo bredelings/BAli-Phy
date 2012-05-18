7baa22c24d8da5a1691dcf60e47b1ceb887e8c76
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-05-18 17:40:13 -0400

    Move gamma_quantile to probability.{H,C} and add log_normal_quantile as well.

--- src/probability.C
+++ src/probability.C
@@ -20,6 +20,7 @@ along with BAli-Phy; see the file COPYING.  If not see
 #include "probability.H"
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_sf.h>
+#include <gsl/gsl_cdf.h>
 #include <iostream>
 
 #include "logsum.H"
@@ -184,9 +185,152 @@ log_double_t normal_pdf(double x, double mu, double sigma)
   return Pr;
 }
 
+double normal_quantile(double p, double mu, double sigma)
+{
+  return mu+gsl_cdf_gaussian_Pinv(p,sigma);
+}
+
 log_double_t log_normal_pdf(double x, double mu, double sigma)
 {
   log_double_t Pr = 1;
   Pr.log() = normal_pdf(log(x),mu,sigma)-log(x);
   return Pr;
+}
+
+double log_normal_quantile(double p, double lmu, double lsigma)
+{
+  // don't go crazy
+  lsigma = minmax(lsigma, 1.0e-5, 1.0e5);
+
+  return gsl_cdf_lognormal_Pinv(p, lmu, lsigma);
+}
+
+static double pointChi2(double prob, double v)
+{
+  // Returns z so that Prob{x<z}=prob where x is Chi2 distributed with df
+  // = v
+  // RATNEST FORTRAN by
+  // Best DJ & Roberts DE (1975) The percentage points of the
+  // Chi2 distribution. Applied Statistics 24: 385-388. (AS91)
+
+  double e = 0.5e-6, aa = 0.6931471805, p = prob, g;
+  double xx, c, ch, a = 0, q = 0, p1 = 0, p2 = 0, t = 0, x = 0, b = 0, s1, s2, s3, s4, s5, s6;
+    
+  if (v <= 0)
+    throw myexception()<<"Arguments out of range: alpha = "<<a;
+
+  assert(p >= 0 and p <= 1);
+
+  if (p < 0.000002) 
+  {
+    std::cerr<<"Warning: can't handle p = "<<p<<" in gamma quantile: using 0.000002";
+
+    p = 0.000002;
+  }
+
+  if (p > 0.999998) 
+  {
+    std::cerr<<"Warning: can't handle p = "<<p<<" in gamma quantile: using 0.999998";
+
+    p = 0.999998;
+  }
+
+  g = gsl_sf_lngamma(v / 2);
+    
+  xx = v / 2;
+  c = xx - 1;
+  if (v < -1.24 * log(p)) {
+    ch = pow((p * xx * exp(g + xx * aa)), 1 / xx);
+    if (ch - e < 0) {
+      return ch;
+    }
+  } else {
+    if (v > 0.32) {
+      x = gsl_cdf_gaussian_Pinv(p,1);
+      p1 = 0.222222 / v;
+      ch = v * pow((x * sqrt(p1) + 1 - p1), 3.0);
+      if (ch > 2.2 * v + 6) {
+	ch = -2 * (log(1 - p) - c * log(.5 * ch) + g);
+      }
+    } else {
+      ch = 0.4;
+      a = log(1 - p);
+
+      do {
+	q = ch;
+	p1 = 1 + ch * (4.67 + ch);
+	p2 = ch * (6.73 + ch * (6.66 + ch));
+	t = -0.5 + (4.67 + 2 * ch) / p1
+	  - (6.73 + ch * (13.32 + 3 * ch)) / p2;
+	ch -= (1 - exp(a + g + .5 * ch + c * aa) * p2 / p1)
+	  / t;
+      } while (std::abs(q / ch - 1) - .01 > 0);
+    }
+  }
+  do {
+    q = ch;
+    p1 = 0.5 * ch;
+      
+    if ((t = gsl_sf_gamma_inc_P(xx, p1)) < 0)
+      throw myexception()<<"Arguments out of range: t < 0";
+
+    p2 = p - t;
+    t = p2 * exp(xx * aa + g + p1 - c * log(ch));
+    b = t / ch;
+    a = 0.5 * t - b * c;
+
+    s1 = (210 + a * (140 + a * (105 + a * (84 + a * (70 + 60 * a))))) / 420;
+    s2 = (420 + a * (735 + a * (966 + a * (1141 + 1278 * a)))) / 2520;
+    s3 = (210 + a * (462 + a * (707 + 932 * a))) / 2520;
+    s4 = (252 + a * (672 + 1182 * a) + c * (294 + a * (889 + 1740 * a))) / 5040;
+    s5 = (84 + 264 * a + c * (175 + 606 * a)) / 2520;
+    s6 = (120 + c * (346 + 127 * c)) / 5040;
+    ch += t
+      * (1 + 0.5 * t * s1 - b
+	 * c
+	 * (s1 - b
+	    * (s2 - b
+	       * (s3 - b
+		  * (s4 - b * (s5 - b * s6))))));
+  } while (std::abs(q / ch - 1) > e);
+    
+  assert(not std::isnan(ch) and std::isfinite(ch));
+  assert(ch >= 0);
+  return (ch);
+}
+
+double gamma_quantile_no_approx(double p, double a, double b)
+{
+  assert(a >= 0);
+  assert(b >= 0);
+
+  assert(p >= 0);
+  assert(p <= 1);
+
+  return 0.5 * b * pointChi2(p, 2.0* a);
+}
+
+double gamma_quantile(double p, double a, double b)
+{
+  assert(a >= 0);
+  assert(b >= 0);
+  assert(p >= 0);
+  assert(p <= 1);
+
+  if (a < 10000)
+    return gamma_quantile_no_approx(p,a,b);
+  else {
+    double M = a*b;
+    double V = a*b*b;
+
+    // Isn't this the same as log1p(1.0/a)?
+    double sigma2 =  log1p(V/(M*M));
+    double mu = log(M) - sigma2/2.0;
+    double sigma = sqrt(sigma2);
+
+    // don't go crazy
+    sigma = minmax(sigma, 1.0e-5, 1.0e5);
+
+    return gsl_cdf_lognormal_Pinv(p,mu,sigma);
+  }
 }
