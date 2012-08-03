5c86e5dd5b3594dd4f40dfd618e436f977e95520
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-08-03 12:40:20 -0400

    Add parser for XML to BUGS.C

--- src/BUGS.C
+++ src/BUGS.C
@@ -11,9 +11,12 @@ using std::string;
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/foreach.hpp>
 
 namespace qi = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
@@ -63,6 +66,70 @@ struct employee_parser : qi::grammar<Iterator, employee(), ascii::space_type>
     qi::rule<Iterator, employee(), ascii::space_type> start;
 };
 
+struct mini_xml;
+
+typedef
+    boost::variant<
+        boost::recursive_wrapper<mini_xml>
+      , std::string
+    >
+mini_xml_node;
+
+struct mini_xml
+{
+    std::string name;                           // tag name
+    std::vector<mini_xml_node> children;        // children
+};
+
+BOOST_FUSION_ADAPT_STRUCT(
+			  mini_xml,
+    (std::string, name)
+    (std::vector<mini_xml_node>, children)
+)
+
+template <typename Iterator>
+struct mini_xml_grammar : qi::grammar<Iterator, mini_xml(), ascii::space_type>
+{
+    mini_xml_grammar() : mini_xml_grammar::base_type(xml)
+    {
+        using qi::lit;
+        using qi::lexeme;
+        using ascii::char_;
+        using ascii::string;
+        using namespace qi::labels;
+
+        using phoenix::at_c;
+        using phoenix::push_back;
+
+        text = lexeme[+(char_ - '<')        [_val += _1]];
+        node = (xml | text)                 [_val = _1];
+
+        start_tag =
+                '<'
+            >>  !lit('/')
+            >>  lexeme[+(char_ - '>')       [_val += _1]]
+            >>  '>'
+        ;
+
+        end_tag =
+                "</"
+            >>  string(_r1)
+            >>  '>'
+        ;
+
+        xml =
+                start_tag                   [at_c<0>(_val) = _1]
+            >>  *node                       [push_back(at_c<1>(_val), _1)]
+            >>  end_tag(at_c<0>(_val))
+        ;
+    }
+
+    qi::rule<Iterator, mini_xml(), ascii::space_type> xml;
+    qi::rule<Iterator, mini_xml_node(), ascii::space_type> node;
+    qi::rule<Iterator, std::string(), ascii::space_type> text;
+    qi::rule<Iterator, std::string(), ascii::space_type> start_tag;
+    qi::rule<Iterator, void(std::string), ascii::space_type> end_tag;
+};
 
 
 ///////////////////////////////////////////////////////////////////////////////
