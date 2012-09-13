08476c867aa2e9d9780ab6a04c4ddb8e557516a4
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-09-13 18:06:30 -0400

    Add a negation operation.

--- src/computation/operations.C
+++ src/computation/operations.C
@@ -291,6 +291,22 @@ closure LExp_Op::operator()(OperationArgs& Args) const
 
 expression_ref LExp = lambda_expression( LExp_Op() );
 
+closure Negate::operator()(OperationArgs& Args) const
+{
+  object_ref x = Args.evaluate(0);
+  
+  if (object_ptr<const Double> xd = dynamic_pointer_cast<const Double>(x))
+    return -(*xd);
+  else if (object_ptr<const Int> xi = dynamic_pointer_cast<const Int>(x))
+    return -(*xi);
+  else if (object_ptr<const Log_Double> xld = dynamic_pointer_cast<const Log_Double>(x))
+    return -(*xld);
+  else if (object_ptr<const Char> xc = dynamic_pointer_cast<const Char>(x))
+    return -(*xc);
+  else
+    throw myexception()<<"Negate: object '"<<x->print()<<"' is not Double, Int, Log_Double, or Char'";
+}
+
 closure Add::operator()(OperationArgs& Args) const
 {
   object_ref x = Args.evaluate(0);
