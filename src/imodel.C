c1227e7d12090e9af2a2bbab8f8f77bdbfedd665
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-10-31 13:46:51 -0400

    Add new operation.

--- src/imodel.C
+++ src/imodel.C
@@ -567,6 +567,38 @@ indel::PairHMM RS07_branch_HMM_(double e, double D, double heat, bool in_trainin
   return Q;
 }
 
+closure pairwise_alignment_probability_from_counts::operator()(OperationArgs& Args) const
+{
+  const ublas::matrix<int>& counts = Args.evaluate_as<Box<ublas::matrix<int>>>(0)->t;
+  const indel::PairHMM& Q = *Args.evaluate_as<indel::PairHMM>(1);
+
+  using namespace A2;
+
+  efloat_t P=1;
+
+  // Account for S-? start probability
+  for(int i=0;i<Q.size2();i++)
+    if (counts(states::S,i))
+      P *= Q.start(i);
+
+  // Account for the mass of transitions
+  for(int i=0;i<3;i++)
+    for(int j=0;j<3;j++) {
+      efloat_t Qij = Q(i,j);
+      // FIXME - if we propose really bad indel parameters, we can get log(Q_ij) where Qij == 0
+      if (counts(i,j))
+	P *= pow(Qij,counts(i,j));
+    }
+  
+  // Account for ?-E end probability
+  if (not counts(states::S,states::E))
+    for(int i=0;i<Q.size1();i++)
+      if (counts(i,states::E))
+       P *= Q(i,states::E);
+
+  return Log_Double(P);  
+}
+
 closure get_transition_counts::operator()(OperationArgs& Args) const
 {
   const pairwise_alignment_t& A = *Args.evaluate_as<pairwise_alignment_t>(0);
