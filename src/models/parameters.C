a729875ecc5384dd718f4dc573164c55f3e1facf
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-11-29 20:02:57 -0500

    Write edgesBeforeEdge using list comprehensions.

--- src/models/parameters.C
+++ src/models/parameters.C
@@ -1241,22 +1241,9 @@ targetNode t edge = snd (nodesForEdge t edge);\
 edgeForNodes t (n1,n2) = head [b | b <- (edgesOutOfNode t n1), (targetNode t b)==n2];\
 reverseEdge t b = edgeForNodes (swap (nodesForEdge t b));\
 nodeDegree t n = length (edgesOutOfNode t n);\
-neighbors t n = fmap (targetNode t) (edgesOutOfNode t n)\
+neighbors t n = fmap (targetNode t) (edgesOutOfNode t n);\
+edgesBeforeEdge t b = case (nodesForEdge t b) of {(n1,n2) -> [edgeForNodes t (n,n1) | n <- neighbors t n1, n /= n2 ]}\
 }";
-
-  // edgeForNodes t (n1, n2) = (\[b] -> b) [b | b <- (edgesOutOfNode t n1), target t b == n2]
-  //  tree_program += Def( (var("edgeForNodes"),dummy("t"),Tuple(dummy("n1"),dummy("n2"))), 
-  //  		       parse_haskell_line("findFirst (\\n->(targetNode t n)==n2) (edgesOutOfNode t n1)")
-  //		       );
-
-  // edgesBeforeEdge t b = [edgeForNodes (n,n1) | let {(n1,n2) = nodesForEdge t b}, n <- neighbors t n1, n /= n2 ]
-  tree_program += Def( (var("edgesBeforeEdge"), v1, v2), case_expression((var("nodesForEdge"),v1,v2),Tuple(v3,v4),
-									  (var("concatMap"),
-									   v5^(If,(var("/="),v5,v4), (var("edgeForNodes"),v1,Tuple(v5,v3))&ListEnd, ListEnd),
-									   (var("neighbors"),v1,v3)
-									   )
-									  )
-		       );
 
   C += tree_program;
 
