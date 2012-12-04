6d3a0a2d816fe50be4eacdfacfb2fbaf35811f5b
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2012-12-03 19:45:04 -0500

    Add start on better relaxed rates model.

--- src/imodel/setup.C
+++ src/imodel/setup.C
@@ -66,8 +66,102 @@ formula_expression_ref get_imodel(string name, const SequenceTree& T)
     imodel.set_exp( Tuple(v1^(v2^(RS07BranchHMM, epsilon, rate*(var("!"),v1,v2), heat, training)), 
 			  v1^(lengthp,epsilon,v1)) );
   }
+  else if (name == "relaxed_rates1[RS07]")
+  {
+    /*
+     * DefineBuiltin RS07BranchHMM
+     * DefineBuiltin lengthp
+     * 
+     * meanIndelLenthMinus1 ~ Exponential(10.0)
+     * lambdaSigmaOverMu ~ LogLaplace(-3.0, 1.0)
+     * lambdaScale{$i} ~ Gamma(1.0/(LambdaSigmaOverMu*LambdaSigmaOverMu), LambdaSigmaOverMu*LambdaSigmaOverMu)
+     * logLambdaMean ~ Laplace(-4.0, 1.0)
+     * epsilon = meanIndelLengthMinus1/(1.0 + meanIndelLengthMinus1)
+     * main = let {lambdaMean = exp logLambdaMean, lambdaScales = listArray' lambdasList} in 
+     *            (\a b -> RS07BranchMHH epsilon (lambdaMean*(lambdaScales!b))*(a!b) Heat.beta IModels.training, 
+                   \a -> lengthp epsilon a)
+     */
+
+    expression_ref RS07BranchHMM = lambda_expression( RS07_branch_HMM() );
+    expression_ref lengthp = lambda_expression( RS07_lengthp() );
+    expression_ref meanIndelLengthMinus1 = def_parameter(imodel, "meanIndelLengthMinus1", 1.0, lower_bound(0),exponential_dist, 10.0);
+    expression_ref epsilon = meanIndelLengthMinus1/(1.0 + meanIndelLengthMinus1);
+
+    expression_ref lambda_sigma_over_mu = def_parameter(imodel,"lambdaSigmaOverMu", 0.1, lower_bound(0), log_laplace_dist, Tuple(-3.0, 1.0) );
+    expression_ref B = lambda_sigma_over_mu*lambda_sigma_over_mu;
+    expression_ref A = 1.0/B;
+
+    vector<expression_ref> branch_lambdas;
+    for(int b=0;b<T.n_branches();b++)
+      branch_lambdas.push_back(def_parameter(imodel, "lambdaScale"+convertToString(b), 1.0, nullptr, gamma_dist, Tuple(A, B)));
+    expression_ref lambdas_list = get_list(branch_lambdas);
+
+    expression_ref log_lambda_mean = def_parameter(imodel, "logLambdaMean", -4.0, nullptr, laplace_dist, Tuple(-4.0, 1.0));
+
+    expression_ref lambda_mean = (var("exp"), log_lambda_mean);
+    
+    expression_ref heat = parameter("Heat.beta");
+    expression_ref training = parameter("IModels.training");
+
+    // let lambdaMean = exp logLambdaMean, lambdaScales = arrayFromList lambdasList .... lambdaMean*lambdasList!b
+
+    imodel.set_exp( let_expression(v3, (var("exp"), log_lambda_mean),
+				   let_expression(v4, (var("listArray'"), lambdas_list),
+						  Tuple(v1^(v2^(RS07BranchHMM, epsilon, v3*(var("!"),v4,v2)*(var("!"),v1,v2), heat, training)), 
+							v1^(lengthp,epsilon,v1))
+						  )
+				   )
+		    );
+  }
   else if (name == "relaxed_rates[RS07]")
   {
+    /*
+     * DefineBuiltin RS07BranchHMM
+     * DefineBuiltin lengthp
+     * 
+     * meanIndelLenthMinus1 ~ Exponential(10.0)
+     * lambdaSigmaOverMu ~ LogLaplace(-3.0, 1.0)
+     * lambdaScale{$i} ~ Gamma(1.0/(LambdaSigmaOverMu*LambdaSigmaOverMu), LambdaSigmaOverMu*LambdaSigmaOverMu)
+     * logLambdaMean ~ Laplace(-4.0, 1.0)
+     * epsilon = meanIndelLengthMinus1/(1.0 + meanIndelLengthMinus1)
+     * main = let {lambdaMean = exp logLambdaMean, lambdaScales = listArray' lambdasList} in 
+     *            (\a b -> RS07BranchMHH epsilon (lambdaMean*(lambdaScales!b))*(a!b) Heat.beta IModels.training, 
+                   \a -> lengthp epsilon a)
+
+     */
+
+    /*
+     * iidDensity ((probDensity _ density _),args) l = foldl' (*) 1.0 (fmap (density args) l)
+     *
+     * iid (dist,args) = ((probDensity "i.i.d." (iidDensity dist args) (error "idd has no quantile")), () )
+     *
+     * logLambdaMeans = [logLambdaMean1, logLambdaMean2, logLambdaMean3, logLambdaMean4]
+     *
+     * logLambdaMean ~ Laplace(-4.0, 1.0)
+     * logLambdaMeans ~ iid ( normal (logLambdaMean, sigmaBetweenGroup))
+     *
+     * a = 10.0
+     * q1 ~ Beta(a, 1.0)
+     * q2 ~ Beta(a, 1.0)
+     * q3 ~ Beta(a, 1.0)
+     *
+     * p1 = q1
+     * p2 = (1.0-q1)*q2
+     * p3 = (1.0-q1)*(1.0-q2)*q3
+     * p4 = (1.0-q1)*(1.0-q2)*(1.0-q3)
+     *
+     * dists = fmap (\x -> Normal(x, sigmaWithinGroup)) [logLambdaMean1, logLambdaMean2, logLambdaMean3, logLambdaMean4]
+     * weights = [p1, p2, p3, p4]
+     *
+     * logLambdasList = logLambda1:logLambda2:logLambda3:logLambda4 .... logLambda{$B}
+     *
+     * logLambdasList ~ iid Mixture( zip weights dists )
+     *
+     * main = let {lambdaMean = exp logLambdaMean, logLambdaScales = listArray' logLambdasList} in 
+     *            (\a b -> RS07BranchMHH epsilon (exp logLambdaScales!b))*(a!b) Heat.beta IModels.training, 
+                   \a -> lengthp epsilon a)
+     *
+     */
     expression_ref RS07BranchHMM = lambda_expression( RS07_branch_HMM() );
     expression_ref lengthp = lambda_expression( RS07_lengthp() );
     expression_ref meanIndelLengthMinus1 = def_parameter(imodel, "meanIndelLengthMinus1", 1.0, lower_bound(0),exponential_dist, 10.0);
