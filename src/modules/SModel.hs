4a85676b7a4f87803a29b99388f15ee65eda8156
Benjamin Redelings <benjamin.redelings@duke.edu>
2012-12-20 22:19:18 -0800

    Move SModel functions to SModel.hs.

--- src/modules/SModel.hs
+++ src/modules/SModel.hs
@@ -1,2 +1,52 @@
 module SModel where 
-{}
+{  
+scale x (ReversibleMarkov a s q pi l t r) = ReversibleMarkov a s q pi l (x*t) (x*r);
+
+multiParameter f (DiscreteDistribution d) = MixtureModel (DiscreteDistribution (fmap2 f d));
+
+multiRate m d = multiParameter (\x->(scale x m)) d;
+
+rate (ReversibleMarkov a s q pi l t r) = r;
+rate (MixtureModel d) = average (fmap2 rate d);
+     
+qExp (ReversibleMarkov a s q pi l t r) = lExp l pi t;
+
+branchTransitionP (MixtureModel (DiscreteDistribution l)) t = let {r = rate (MixtureModel (DiscreteDistribution l))} 
+                                                              in map (\x -> qExp (scale (t/r) (snd x))) l;
+
+qFromSandR s (ReversibleFrequency a smap pi r) = let {q = getQ s r} 
+                                                 in ReversibleMarkov a smap q pi (getEigensystem q pi) 1.0 (getEquilibriumRate a smap q pi);
+
+nBaseModels (MixtureModel (DiscreteDistribution l)) = length l;
+nBaseModels (MixtureModels (m:ms)) = nBaseModels m;
+
+baseModel (MixtureModel (DiscreteDistribution l)) i = snd (l !! i);
+
+stateLetters (ReversibleMarkov _ smap _ _ _ _ _) = smap;
+stateLetters (F81 _ smap _ _ ) = smap;
+stateLetters (MixtureModel l) = stateLetters (baseModel (MixtureModel l) 0);
+stateLetters (MixtureModels (m:ms)) = stateLetters m;
+
+nStates m = sizeOfVectorUnsigned (stateLetters m);
+  
+getAlphabet (ReversibleMarkov a _ _ _ _ _ _) = a;
+getAlphabet (F81 a _ _ _) = a;
+getAlphabet (MixtureModel l) = getAlphabet (baseModel (MixtureModel l) 0);
+getAlphabet (MixtureModels (m:ms)) = getAlphabet m;
+
+frequencies (ReversibleMarkov _ _ _ pi _ _ _) = pi;
+frequencies (F81 _ _ _ pi) = pi;
+
+componentFrequencies (MixtureModel d)       i = frequencies (baseModel (MixtureModel d) i);
+componentFrequencies (MixtureModels (m:ms)) i = componentFrequencies m i;
+
+distribution (MixtureModel (DiscreteDistribution l)) = map fst l;
+distribution (MixtureModels (m:ms))                  = distribution m;
+
+getNthMixture (MixtureModels l) i = l !! i;
+
+unwrapMM (MixtureModel dd) = dd;
+
+mixMixtureModels l dd = MixtureModel (mixDiscreteDistributions l (map unwrapMM dd));
+
+}
--- src/smodel/functions.C
+++ src/smodel/functions.C
@@ -11,7 +11,6 @@ using std::string;
 Program SModel_Functions(const vector<string>& module_root_paths)
 {
   Program P("SModel");
-  P.import_module(module_root_paths,"Prelude",false);
 
   P.def_function("plusGWF", lambda_expression(substitution::Plus_gwF_Op()));
   P.def_function("lExp", lambda_expression(LExp_Op()));
@@ -27,52 +26,5 @@ Program SModel_Functions(const vector<string>& module_root_paths)
 
   // scale x (F81 a s a' pi)= (F81 a s a'*x pi) ??
   // scale x (MixtureModel (DiscreteDistribution l)) s= (MixtureModel (DiscreteDistribution (fmap2,scale(s),l))) ??
-  P += "{scale x (ReversibleMarkov a s q pi l t r) = ReversibleMarkov a s q pi l (x*t) (x*r)}";
-
-  P += "{multiParameter f (DiscreteDistribution d) = MixtureModel (DiscreteDistribution (fmap2 f d))}";
-
-  P += "{multiRate m d = multiParameter (\\x->(scale x m)) d}";
-
-  P += "{rate (ReversibleMarkov a s q pi l t r) = r;\
-         rate (MixtureModel d) = average (fmap2 rate d)}";
-     
-  P += "{qExp (ReversibleMarkov a s q pi l t r) = lExp l pi t}";
-
-  P += "{branchTransitionP (MixtureModel (DiscreteDistribution l)) t = let {r = rate (MixtureModel (DiscreteDistribution l))} in map (\\x -> qExp (scale (t/r) (snd x))) l}";
-
-  P += "{qFromSandR s (ReversibleFrequency a smap pi r) = let {q = getQ s r} in ReversibleMarkov a smap q pi (getEigensystem q pi) 1.0 (getEquilibriumRate a smap q pi)}";
-
-  P += "{nBaseModels (MixtureModel (DiscreteDistribution l)) = length l;\
-         nBaseModels (MixtureModels (m:ms)) = nBaseModels m}";
-
-  P += "{baseModel (MixtureModel (DiscreteDistribution l)) i = snd (l !! i)}";
-
-  P += "{stateLetters (ReversibleMarkov _ smap _ _ _ _ _) = smap;\
-         stateLetters (F81 _ smap _ _ ) = smap;\
-         stateLetters (MixtureModel l) = stateLetters (baseModel (MixtureModel l) 0);\
-         stateLetters (MixtureModels (m:ms)) = stateLetters m}";
-
-  P += "{nStates m = sizeOfVectorUnsigned (stateLetters m)}";
-  
-  P += "{getAlphabet (ReversibleMarkov a _ _ _ _ _ _) = a;\
-         getAlphabet (F81 a _ _ _) = a;\
-         getAlphabet (MixtureModel l) = getAlphabet (baseModel (MixtureModel l) 0);\
-         getAlphabet (MixtureModels (m:ms)) = getAlphabet m}";
-
-  P += "{frequencies (ReversibleMarkov _ _ _ pi _ _ _) = pi;\
-         frequencies (F81 _ _ _ pi) = pi}";
-
-  P += "{componentFrequencies (MixtureModel d)       i = frequencies (baseModel (MixtureModel d) i);\
-         componentFrequencies (MixtureModels (m:ms)) i = componentFrequencies m i}";
-
-  P += "{distribution (MixtureModel (DiscreteDistribution l)) = map fst l;\
-         distribution (MixtureModels (m:ms))                  = distribution m}";
-
-  P += "{getNthMixture (MixtureModels l) i = l !! i}";
-
-  P += "{unwrapMM (MixtureModel dd) = dd}";
-
-  P += "{mixMixtureModels l dd = MixtureModel (mixDiscreteDistributions l (map unwrapMM dd))}";
-
   return P;
 }
