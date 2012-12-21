1a11e15f68b7f712b92a43187c7617be13b95ceb
Benjamin Redelings <benjamin.redelings@duke.edu>
2012-12-20 22:35:02 -0800

    Use ($) to avoid parenthesis around do statement.

--- src/modules/Prelude.hs
+++ src/modules/Prelude.hs
@@ -159,7 +159,7 @@ setVectorIndexInt v i x = IOAction3 builtinSetVectorIndexInt v i x;
 copyListToVectorInt [] v i = return ();
 copyListToVectorInt (h:t) v i = do {setVectorIndexInt v i h; copyListToVectorInt t v (i+1)};
 
-listToVectorInt l = unsafePerformIO (do {v <- newVectorInt (length l); copyListToVectorInt l v 0; return v});
+listToVectorInt l = unsafePerformIO $ do {v <- newVectorInt (length l); copyListToVectorInt l v 0; return v};
 
 newString s = IOAction1 builtinNewString s;
 
@@ -168,21 +168,21 @@ setStringIndexInt v i x = IOAction3 builtinSetStringIndexInt v i x;
 copyListToString [] v i = return ();
 copyListToString (h:t) v i = do {setStringIndexInt v i h ; copyListToString t v (i+1)};
 
-listToString l = unsafePerformIO (do {v <- newString (length l); copyListToString l v 0; return v});
+listToString l = unsafePerformIO $ do {v <- newString (length l); copyListToString l v 0; return v};
 
 newVectorDouble s = IOAction1 builtinNewVectorDouble s;
 setVectorIndexDouble v i x = IOAction3 builtinSetVectorIndexDouble v i x;
 copyListToVectorDouble [] v i = return ();
 copyListToVectorDouble (h:t) v i = do { setVectorIndexDouble v i h ; copyListToVectorDouble t v (i+1)};
 
-listToVectorDouble l = unsafePerformIO (do { v <- newVectorDouble (length l); copyListToVectorDouble l v 0; return v});
+listToVectorDouble l = unsafePerformIO $ do { v <- newVectorDouble (length l); copyListToVectorDouble l v 0; return v};
 
 newVectorMatrix s = IOAction1 builtinNewVectorMatrix s;
 setVectorIndexMatrix v i x = IOAction3 builtinSetVectorIndexMatrix v i x;
 copyListToVectorMatrix [] v i = return ();
 copyListToVectorMatrix (h:t) v i = do { setVectorIndexMatrix v i h; copyListToVectorMatrix t v (i+1)};
 
-listToVectorMatrix l = unsafePerformIO (do { v <- newVectorMatrix (length l); copyListToVectorMatrix l v 0 ; return v});
+listToVectorMatrix l = unsafePerformIO $ do { v <- newVectorMatrix (length l); copyListToVectorMatrix l v 0 ; return v};
 
 error m = builtinError (listToString m);
 
