887978702ae4c0e2515e21d74c5deb9576d1272d
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-01-01 23:06:30 -0800

    Add notes for determining binary name on Mac.

--- src/bali-phy.C
+++ src/bali-phy.C
@@ -1244,14 +1244,23 @@ void set_foreground_branches(Parameters& P)
 fs::path find_exe_path(const fs::path& argv0)
 {
   /*
-    Mac OS X: _NSGetExecutablePath() (man 3 dyld)
     Linux: readlink /proc/self/exe
-    Solaris: getexecname()
     FreeBSD: sysctl CTL_KERN KERN_PROC KERN_PROC_PATHNAME -1
+
+    Mac OS X: _NSGetExecutablePath() (man 3 dyld)
+    Solaris: getexecname()
     BSD with procfs: readlink /proc/curproc/file
     Windows: GetModuleFileName() with hModule = NULL
   */
 
+  /* For Mac
+     char path[1024];
+     uint32_t size = sizeof(path);
+     if (_NSGetExecutablePath(path, &size) == 0)
+       printf("executable path is %s\n", path);
+     else
+       printf("buffer too small; need size %u\n", size);
+  */
   fs::path program_location;
 
   // This only works on Linux.
@@ -1260,7 +1269,7 @@ fs::path find_exe_path(const fs::path& argv0)
   // This only works on BSD with procfs.
   else if (fs::exists("/proc/curproc/file"))
     program_location = "/proc/curproc/file";
-  // Try argv[0]
+  // Try argv[0] - This *PROBABLY* works on windows.
   else if (fs::exists(argv0))
     program_location = argv0;
   // Search $PATH for argv[0]
