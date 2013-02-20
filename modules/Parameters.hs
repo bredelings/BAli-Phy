1f04ac05b14ebcca7ed68c94d15e1fe898fe6c5c
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-02-20 16:14:51 -0500

    Add new builtin get_modifiable_index.
    Also move parameter builtins to Parameters.hs.

--- modules/Parameters.hs
+++ modules/Parameters.hs
@@ -2,6 +2,11 @@ module Parameters where
 {
 import Range;
 
+builtin builtin_set_modifiable_value 3 "set_modifiable_value";
+builtin is_changeable 2 "is_changeable";
+builtin is_modifiable 2 "is_modifiable";
+builtin get_modifiable_index 2 "get_modifiable_index";
+
 set_modifiable_value token m v = IOAction3 builtin_set_modifiable_value token m v;
 set_parameter_value' token (p:ps) (v:vs) = do { set_parameter_value token p v; 
                                                 set_parameter_value token ps vs
@@ -16,7 +21,5 @@ findAtomic ps (ListRange rs) = concat $ zipWith findAtomic ps rs;
 findAtomic p r = [(p,r)];
 
 findBinary' p = [p2 | (p2,TrueFalseRange) <- findAtomic p];
-findBinary = listToVectorInt . (map getModifiableIndex).findBinary';
-
-getModifiableIndex x = x;
+findBinary = listToVectorInt . (map get_modifiable_index) . findBinary';
 }
