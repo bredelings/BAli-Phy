abd9ce369c871cc2b0b55a6a5a09dc179d01a72b
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-02-25 10:50:22 -0500

    Rename p2 to m.

--- modules/Parameters.hs
+++ modules/Parameters.hs
@@ -21,10 +21,10 @@ set_parameter_value token p v = if (is_modifiable token p)
 findAtomic ps (ListRange rs) = concat $ zipWith findAtomic ps rs;
 findAtomic p r = [(p,r)];
 
-findBinary t p r = list_to_vector [get_modifiable_index t p2 | (p2,TrueFalseRange) <- findAtomic p r];
+findBinary t p r = list_to_vector [get_modifiable_index t m | (m,TrueFalseRange) <- findAtomic p r];
 
-findReal t p r = list_to_vector [c_pair (get_modifiable_index t p2, getBounds (OpenInterval x y)) 
-                              | (p2,OpenInterval x y) <- findAtomic p r];
+findReal t p r = list_to_vector [c_pair (get_modifiable_index t m, getBounds (OpenInterval x y)) 
+                              | (m,OpenInterval x y) <- findAtomic p r];
 
 findInteger' p r = [p2 | (p2,IntegerInterval _ _) <- findAtomic p r];
 findInteger = listToVectorInt . (map get_modifiable_index) . findInteger';
