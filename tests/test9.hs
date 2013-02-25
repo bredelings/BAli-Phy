7b4bee38191e59eabe56879245b12dd49599f810
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-02-25 17:13:48 -0500

    Simply test9.hs now that real modifiables are discovered for MCMC.

--- tests/test9.hs
+++ tests/test9.hs
@@ -38,11 +38,8 @@ note sigmaOverMu5 ~ gamma(1.05,0.1);
 
   alpha = 1.0;
   
-note q1 ~ beta(1.0,alpha);
-note q2 ~ beta(1.0,alpha);
-note q3 ~ beta(1.0,alpha);
-note q4 ~ beta(1.0,alpha);
-note q5 ~ beta(1.0,alpha);
+note [q1,q2,q3,q4,q5] ~ iid (5, beta(1.0,alpha) );
+
   p1' = q1;
   p2' = (1.0-q1)*q2;
   p3' = (1.0-q1)*(1.0-q2)*q3;
@@ -63,40 +60,10 @@ note q5 ~ beta(1.0,alpha);
                        (p5, gamma (a5,b5))
                       ];
 
-note theta1 ~ thetaDist;
-note theta2 ~ thetaDist;
-note theta3 ~ thetaDist;
-note theta4 ~ thetaDist;
-note theta5 ~ thetaDist;
-note theta6 ~ thetaDist;
-note theta7 ~ thetaDist;
-note theta8 ~ thetaDist;
-note theta9 ~ thetaDist;
-note theta10 ~ thetaDist;
-note theta11 ~ thetaDist;
-note theta12 ~ thetaDist;
-note theta13 ~ thetaDist;
-note theta14 ~ thetaDist;
-note theta15 ~ thetaDist;
-note theta16 ~ thetaDist;
-note theta17 ~ thetaDist;
-note theta18 ~ thetaDist;
-note theta19 ~ thetaDist;
-note theta20 ~ thetaDist;
-note theta21 ~ thetaDist;
-note theta22 ~ thetaDist;
-note theta23 ~ thetaDist;
-note theta24 ~ thetaDist;
-note theta25 ~ thetaDist;
-note theta26 ~ thetaDist;
-note theta27 ~ thetaDist;
-note theta28 ~ thetaDist;
-note theta29 ~ thetaDist;
-note theta30 ~ thetaDist;
-note theta31 ~ thetaDist;
-note theta32 ~ thetaDist;
 note theta33 ~ thetaDist;
   thetas = [theta1,theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,theta10,theta11,theta12,theta13,theta14,theta15,theta16,theta17,theta18,theta19,theta20,theta21,theta22,theta23,theta24,theta25,theta26,theta27,theta28,theta29,theta30,theta31,theta32];
+  
+note [theta1,theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,theta10,theta11,theta12,theta13,theta14,theta15,theta16,theta17,theta18,theta19,theta20,theta21,theta22,theta23,theta24,theta25,theta26,theta27,theta28,theta29,theta30,theta31,theta32] ~ iid (32, thetaDist);
 
   d1 = getAFS filename;
 note data d1 ~ plate (length d1, \i -> afs (thetas!!i));
