13a31836572f0d5a772c07a7e67420c7f47eaa20
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-03-02 18:35:34 -0500

    Simplify test9.hs.

--- tests/test9.hs
+++ tests/test9.hs
@@ -20,11 +20,13 @@ note q ~ iid (5, beta(1.0,alpha) );
 
   normalize l = let {total = sum l} in map (\x-> x/total) l;
 
-  p' = [q!!0,
-   (1.0-q!!0)*q!!1,
-   (1.0-q!!0)*(1.0-q!!1)*q!!1,
-   (1.0-q!!0)*(1.0-q!!1)*(1.0-q!!2)*q!!3,
-   (1.0-q!!0)*(1.0-q!!1)*(1.0-q!!2)*(1.0-q!!3)*q!!4];
+  q' = map (\x->1.0-x) q;
+  left = map (\i->product (take i q')) [0..4];
+p' = [q!!0,
+      q'!!0*q!!1,
+      q'!!0*q'!!1*q!!1,
+      q'!!0*q'!!1*q'!!2*q!!3,
+      q'!!0*q'!!1*q'!!2*q'!!3*q!!4];
   
   p = normalize p';
   
