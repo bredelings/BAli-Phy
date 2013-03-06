4e0319bfa794737b005725e7f47624cde90f7782
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-03-06 18:24:14 -0500

    Remove default arguments to GTR parameters.

--- modules/GTR.hs
+++ modules/GTR.hs
@@ -3,11 +3,5 @@ module GTR where
   import SModel;
   import Distributions;
   note [ag,at,ac,gt,gc,tc] ~ dirichlet [2.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 2.0/8.0];
-  note ag := 2.0/8.0;
-  note at := 1.0/8.0;
-  note ac := 1.0/8.0;
-  note gt := 1.0/8.0;
-  note gc := 1.0/8.0;
-  note tc := 2.0/8.0;
   main nuca = gtr nuca ag at ac gt gc tc;
 }
