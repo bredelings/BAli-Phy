5b351c0a6eb1b8600cbcb93ba048e98e12ca775d
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-03-08 17:15:13 -0500

    Add node predicates and node lists to Tree.

--- modules/Tree.hs
+++ modules/Tree.hs
@@ -12,5 +12,12 @@ edgeForNodes t (n1,n2) = head [b | b <- (edgesOutOfNode t n1), (targetNode t b)=
 reverseEdge t b = edgeForNodes (swap (nodesForEdge t b));
 nodeDegree t n = length (edgesOutOfNode t n);
 neighbors t n = fmap (targetNode t) (edgesOutOfNode t n);
-edgesBeforeEdge t b = case (nodesForEdge t b) of {(n1,n2) -> [edgeForNodes t (n,n1) | n <- neighbors t n1, n /= n2 ]}
+edgesBeforeEdge t b = case (nodesForEdge t b) of {(n1,n2) -> [edgeForNodes t (n,n1) | n <- neighbors t n1, n /= n2 ]};
+
+is_leaf_node t n = (nodeDegree t n == 1);
+is_internal_node t n = not $ is_leaf_node t n;
+
+nodes t = [0..numNodes t - 1];
+leaf_nodes t = filter is_leaf_node (nodes t);
+internal_nodes t = filter is_internal_node (nodes t);
 }
