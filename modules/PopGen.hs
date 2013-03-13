e562024da87cc13a4fa447536284c9d6584f9a89
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-03-13 13:01:04 -0400

    Add new builtin to PopGen module.

--- modules/PopGen.hs
+++ modules/PopGen.hs
@@ -8,6 +8,7 @@ builtin allele_frequency_spectrum 1 "allele_frequency_spectrum" "popgen";
 builtin ewens_sampling_group_probability 2 "ewens_sampling_group_probability" "popgen";
 builtin ewens_sampling_probability 2 "ewens_sampling_probability" "popgen";
 builtin builtin_ewens_sampling_mixture_probability 3 "ewens_sampling_mixture_probability" "popgen";
+builtin builtin_ewens_diploid_probability 3 "ewens_diploid_probability" "popgen";
 
 read_phase_file = builtin_read_phase_file . listToString;
 
