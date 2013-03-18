fefa7b545173c4cd29da47b3c820bb0e300c4ca0
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-03-18 15:34:55 -0400

    Remove unused plugin log.C.

--- src/computation/builtins/log.C
+++
@@ -1,9 +0,0 @@
-#include "computation/computation.H"
-
-extern "C" closure builtin_function_log(OperationArgs& Args)
-{
-  object_ptr<const Double> x = Args.evaluate_as<Double>(0);
-  assert(*x > 0.0);
-
-  return new Double(log(*x));
-}
