8042002df2f437f38cad1afae4222b164c5973c9
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-04-22 18:16:12 -0400

    Add some more test files.

---
+++ tests/PartialSelfing/diploid.pure_herm.multi_theta.DC.hs
@@ -0,0 +1,54 @@
+module Test where
+{
+  import PopGen;
+  import Distributions;
+  get_observed_alleles file = map list_from_vector $ list_from_vector $ read_phase_file file;
+
+  filename = "/home/bredelings/Reports/Kmar/DC.phase1.infile";
+
+  n = 5;
+
+note mean ~ iid(n, gamma(0.5,0.5) );
+note sigmaOverMu ~ iid(n, gamma(1.05,0.1) );
+
+  alpha = 1.0;
+
+note p ~ dirichlet' (n, alpha/(intToDouble n));
+
+  data1 = get_observed_alleles filename;
+
+  n_loci = length data1;
+
+  n_individuals = (length (data1!!0))/2;
+
+note category ~ iid(n_loci, categorical p);
+
+note z ~ iid(n_loci, normal(0.0, 1.0));
+
+  safe_exp x = if (x < (-20.0)) then
+                 exp (-20.0);
+               else if (x > 20.0) then
+                 exp 20.0;
+               else
+                 exp x;
+
+  theta = [ mean!!k * safe_exp (z!!i * sigmaOverMu!!k) | i <- take n_loci [0..], let {k=category!!i}];
+
+  theta_effective = map (*(2.0-s)) theta;
+
+note theta_example ~ mixture [ (p!!i, logNormal(log(mean!!i),sigmaOverMu!!i)) | i <- take n [0..] ];
+
+note s ~ uniform(0.0, 1.0);
+
+note t' ~ iid(n_individuals, exponential (-1.0/log s));
+
+  t = map truncate t';
+
+note i ~ iid(n_loci, plate (n_individuals,\k->bernoulli (1.0-0.5**t!!k)) );
+
+note data data1 ~ plate (n_loci, \l -> afs2 (theta_effective!!l,i!!l));
+
+note MakeLogger p;
+note MakeLogger theta;
+note MakeLogger t;
+}
---
+++ tests/PartialSelfing/diploid.pure_herm.single_theta.NNKB3.hs
@@ -0,0 +1,30 @@
+module Test where
+{
+  import PopGen;
+  import Distributions;
+  get_observed_alleles file = map list_from_vector $ list_from_vector $ read_phase_file file;
+
+  filename = "/home/bredelings/Reports/Kmar/NNKB3.phase1.infile";
+
+  data1 = get_observed_alleles filename;
+
+  n_loci = length data1;
+
+  n_individuals = (length (data1!!0))/2;
+
+note theta ~ gamma(0.5,0.5);
+
+  theta_effective = theta*(2.0-s);
+
+note s ~ uniform(0.0, 1.0);
+
+note t' ~ iid(n_individuals, exponential (-1.0/log s));
+
+  t = map truncate t';
+
+note i ~ iid(n_loci, plate (n_individuals,\k->bernoulli (1.0-0.5**t!!k)) );
+
+note data data1 ~ plate (n_loci, \l -> afs2 (theta_effective,i!!l));
+
+note MakeLogger t;
+}
