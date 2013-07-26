37cf3be8119d52a6bb038848070b44762bb3d2d0
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2013-07-26 16:29:16 -0400

    Whitespace change for better font highlighting.

--- modules/GTR.hs
+++ modules/GTR.hs
@@ -2,6 +2,6 @@ module GTR where
 {
   import SModel;
   import Distributions;
-  note [ag,at,ac,gt,gc,tc] ~ dirichlet [2.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 2.0/8.0];
+note [ag,at,ac,gt,gc,tc] ~ dirichlet [2.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 2.0/8.0];
   main nuca = gtr nuca ag at ac gt gc tc;
 }
