3a3043b729a09f5eaeca62b1ca7210d923ad9c86
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-08-17 22:31:31 -0400

    Change "as" to "in" for submodels.

--- modules/F3x4.hs
+++ modules/F3x4.hs
@@ -3,9 +3,9 @@ module F3x4 where
   import Distributions;
   import SModel;
   import Alphabet;
-  import submodel PlusF as Site1;
-  import submodel PlusF as Site2;
-  import submodel PlusF as Site3;
+  import submodel PlusF in Site1;
+  import submodel PlusF in Site2;
+  import submodel PlusF in Site3;
   main T = ReversibleFrequency T (iotaUnsigned (alphabetSize T)) pi (plus_gwF T 1.0 pi) where 
       {pi1 = listToVectorDouble Site1.main,
        pi2 = listToVectorDouble Site2.main,
