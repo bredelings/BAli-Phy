4363a7c586bd2aa8b90c0c43450da049f0c887b3
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-12-10 14:35:33 -0500

    Separate new_modifiable and new_random_modifiable.

--- src/computation/builtins/Modifiables.C
+++ src/computation/builtins/Modifiables.C
@@ -18,6 +18,24 @@ extern "C" closure builtin_function_new_modifiable(OperationArgs& Args)
   int R1 = Args.allocate(std::move(C));
   M.access(R1).changeable = true;
 
+  // Return a reference to the new modifiable.
+  return {index_var(0),{R1}};
+}
+
+extern "C" closure builtin_function_new_random_modifiable(OperationArgs& Args)
+{
+  assert(not Args.evaluate_changeables());
+
+  reg_heap& M = Args.memory();
+
+  int D = Args.reg_for_slot(0);
+
+  // Allocate a reg, and fill it with a modifiable of the correct index
+  expression_ref E(new expression(modifiable(),{index_var(0)}));
+  closure C{E,{D}};
+  int R1 = Args.allocate(std::move(C));
+  M.access(R1).changeable = true;
+
   M.add_random_modifiable(R1);
 
   // Return a reference to the new modifiable.
