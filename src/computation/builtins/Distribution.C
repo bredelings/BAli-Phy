e819b712fdd92b64b62ff4369748b0ae3ec80257
Benjamin Redelings <benjamin.redelings@duke.edu>
2013-12-11 19:08:34 -0500

    Factor out list-walking.

--- src/computation/builtins/Distribution.C
+++ src/computation/builtins/Distribution.C
@@ -12,6 +12,58 @@ using std::vector;
 using std::string;
 using std::valarray;
 
+template<typename U, typename V>
+vector<U> get_vector_from_list(OperationArgs& Args, int slot)
+{
+  vector<U> z;
+  const closure* top = &Args.evaluate_slot_to_closure(slot);
+  while(top->exp->size())
+  {
+    assert(is_exactly(top->exp,":"));
+    assert(top->exp->size() == 2);
+
+    int element_index = assert_is_a<index_var>(top->exp->sub[0])->index;
+    int element_reg = top->lookup_in_env( element_index );
+
+    int next_index = assert_is_a<index_var>(top->exp->sub[1])->index;
+    int next_reg = top->lookup_in_env( next_index );
+
+    // Add the element to the list.
+    z.push_back( *convert<const V>(Args.evaluate_reg_to_object(element_reg)) );
+
+    // Move to the next element or end
+    top = &Args.evaluate_reg_to_closure(next_reg);
+  }
+  assert(is_exactly(top->exp,"[]"));
+  return z;
+}
+
+template<typename U, typename V>
+vector<U> get_vector_from_list_(OperationArgs& Args, int slot)
+{
+  vector<U> z;
+  const closure* top = &Args.evaluate_slot_to_closure_(slot);
+  while(top->exp->size())
+  {
+    assert(is_exactly(top->exp,":"));
+    assert(top->exp->size() == 2);
+
+    int element_index = assert_is_a<index_var>(top->exp->sub[0])->index;
+    int element_reg = top->lookup_in_env( element_index );
+
+    int next_index = assert_is_a<index_var>(top->exp->sub[1])->index;
+    int next_reg = top->lookup_in_env( next_index );
+
+    // Add the element to the list.
+    z.push_back( *convert<const V>(Args.evaluate_reg_to_object_(element_reg)) );
+
+    // Move to the next element or end
+    top = &Args.evaluate_reg_to_closure_(next_reg);
+  }
+  assert(is_exactly(top->exp,"[]"));
+  return z;
+}
+
 extern "C" closure builtin_function_exponential_density(OperationArgs& Args)
 {
   double mu = *Args.evaluate_as<Double>(0);
@@ -290,26 +342,7 @@ extern "C" closure builtin_function_CRP_density(OperationArgs& Args)
   int D = *Args.evaluate_as<Int>(2);
 
   //------------- 2. Get argument Z -----------------
-  vector<int> z;
-  const closure* top = &Args.evaluate_slot_to_closure(3);
-  while(top->exp->size())
-  {
-    assert(is_exactly(top->exp,":"));
-    assert(top->exp->size() == 2);
-
-    int element_index = assert_is_a<index_var>(top->exp->sub[0])->index;
-    int element_reg = top->lookup_in_env( element_index );
-
-    int next_index = assert_is_a<index_var>(top->exp->sub[1])->index;
-    int next_reg = top->lookup_in_env( next_index );
-
-    // Add the element to the list.
-    z.push_back( *convert<const Int>(Args.evaluate_reg_to_object(element_reg)) );
-
-    // Move to the next element or end
-    top = &Args.evaluate_reg_to_closure(next_reg);
-  }
-  assert(is_exactly(top->exp,"[]"));
+  vector<int> z = get_vector_from_list<int,Int>(Args,3);
 
   return object_ptr<Log_Double> (new Log_Double( ::CRP_pdf(alpha,N,D,z) ) );
 }
@@ -372,26 +405,7 @@ extern "C" closure builtin_function_sample_CRP(OperationArgs& Args)
 extern "C" closure builtin_function_sample_categorical(OperationArgs& Args)
 {
   //------------- 1. Get argument p -----------------
-  vector<double> z;
-  const closure* top = &Args.evaluate_slot_to_closure_(0);
-  while(top->exp->size())
-  {
-    assert(is_exactly(top->exp,":"));
-    assert(top->exp->size() == 2);
-
-    int element_index = assert_is_a<index_var>(top->exp->sub[0])->index;
-    int element_reg = top->lookup_in_env( element_index );
-
-    int next_index = assert_is_a<index_var>(top->exp->sub[1])->index;
-    int next_reg = top->lookup_in_env( next_index );
-
-    // Add the element to the list.
-    z.push_back( *convert<const Double>(Args.evaluate_reg_to_object_(element_reg)) );
-
-    // Move to the next element or end
-    top = &Args.evaluate_reg_to_closure(next_reg);
-  }
-  assert(is_exactly(top->exp,"[]"));
+  vector<double> z = get_vector_from_list_<double,Double>(Args,0);
 
   Int result = choose_scratch(z);
   return object_ptr<const Object>(result.clone());
