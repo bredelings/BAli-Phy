858ee01193b747a5e64cd567a90893daee27bd44
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-01-04 18:36:26 -0800

    Actually rewrite get_pins( ) to work without the sequence order.

--- src/alignment/alignment-constraint.C
+++ src/alignment/alignment-constraint.C
@@ -21,6 +21,7 @@ along with BAli-Phy; see the file COPYING.  If not see
 #include <iostream>
 #include <fstream>
 #include <utility>
+#include <set>
 #include "alignment/alignment-constraint.H"
 #include "alignment/alignment-util.H"
 #include "tree/tree-util.H"
@@ -28,6 +29,7 @@ along with BAli-Phy; see the file COPYING.  If not see
 #include "io.H"
 
 using std::string;
+using std::set;
 using std::ifstream;
 using std::vector;
 using std::valarray;
@@ -288,16 +290,18 @@ vector< vector<int> > get_pins(const ublas::matrix<int>& constraint,const alignm
     if (not (constrained(group1,constraint,i) and constrained(group2,constraint,i)))
       satisfied[i] = -1;
 
+  vector< vector<int> > impossible;
+  impossible.push_back(vector<int>(2,-1));
+
   // Mark and check each alignment column which is going to get pinned.
-  vector<int> pinned(A.length(),0);
+  vector<set<int>> x_constraints(seq1.size());
+  vector<set<int>> y_constraints(seq2.size());
   for(int i=0;i<satisfied.size();i++) 
   {
     int column = satisfied[i];
 
-    if (column != -1 and not pinned[column]) 
+    if (column != -1)
     {
-      pinned[column] = 1;
-    
       int x = find_index(seq1,column);
       int y = find_index(seq2,column);
 
@@ -306,11 +310,11 @@ vector< vector<int> > get_pins(const ublas::matrix<int>& constraint,const alignm
       // character is not present at the internal nodes that we have
       // access to.  Therefore, no alignment that we choose can satisfy
       // this constraint, so we must bail out.
-      if (x == -1 or y == -1) {
-	vector< vector<int> > impossible;
-	impossible.push_back(vector<int>(2,-1));
+      if (x == -1 or y == -1)
 	return impossible;
-      }    
+
+      x_constraints[x].insert(i);
+      y_constraints[y].insert(i);
     }
   }
 
@@ -319,27 +323,25 @@ vector< vector<int> > get_pins(const ublas::matrix<int>& constraint,const alignm
   vector<int>& X = pins[0];
   vector<int>& Y = pins[1];
 
-  /* TODO: Rewrite this to go through the pinned columns in order.
-           The order of x-columns with constraints must match the y-order.
-	   If two constraints occur in the same x-column, the same two constraints must occur in the y-column.
-	   I guess I could make a set<int> for each x-column and each y-column.
-	   I could then go through and find matching columns. */
+  /* TODO: Note that we cannot have pins like (x1,y1) (x1,y2)
+           because we can only pin matches, and the second one
+	   would be a gap. */
 
-  /*
-  for(int i=0;i<seq12.size();i++)
+  for(int x=0,y=0;;)
   {
-    int column = seq12[i];
+    for(;x<seq1.size() and x_constraints[x].empty();x++)
+      ;
+    for(;y<seq2.size() and y_constraints[y].empty();y++)
+      ;
 
-    if (not pinned[column]) continue;
-    
-    int x = find_index(seq1,column);
-    int y = find_index(seq2,column);
+    if (x == seq1.size() and y == seq2.size()) break;
+
+    if (x == seq1.size() or y == seq2.size()) return impossible;
+
+    if (x_constraints[x] != y_constraints[y]) return impossible;
 
     assert(x >=0 and x < seq1.size());
     assert(y >=0 and y < seq2.size());
-
-    if (x == -1 or y == -1)
-      throw myexception()<<"Did not already bail out on un-pinnable column?!?";
 
     X.push_back(x+1);
     Y.push_back(y+1);
@@ -349,7 +351,7 @@ vector< vector<int> > get_pins(const ublas::matrix<int>& constraint,const alignm
     if (Y.size() >= 2 and Y[pins.size()-2] > Y[pins.size()-1])
       throw myexception()<<"X pins not always increasing!";
   }
-  */
+
   return pins;
 }
 
