0aeaa162e48b9a178638dd9d5aca253be75912a2
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-01-31 09:22:14 -0500

    Make (evaluate -1 exp) evaluate unchangeably.

--- src/builtins/evaluate.C
+++ src/builtins/evaluate.C
@@ -6,18 +6,23 @@ using boost::dynamic_pointer_cast;
 
 extern "C" closure builtin_function_evaluate(OperationArgs& Args)
 {
-#ifndef NDEBUG
-  if (Args.evaluate_changeables())
-    throw myexception()<<"Calling builtin_function_evaluate( ) when evaluate_changeables=true";
-#endif
-
   auto& M = Args.memory();
 
   int c = *Args.evaluate_as<Int>(0);
 
+#ifndef NDEBUG
+  if (Args.evaluate_changeables() and c >= 0)
+    throw myexception()<<"Calling builtin_function_evaluate( ) when evaluate_changeables=true and c >= 0";
+#endif
+
   int R1 = Args.reg_for_slot(1);
 
-  int R2 = M.incremental_evaluate_in_context(R1, c);
+  int R2 = 0;
+
+  if (c < 0)
+    R2 = M.incremental_evaluate(R1, 0);
+  else
+    R2 = M.incremental_evaluate_in_context(R1, c);
 
   assert( R2 );
 
