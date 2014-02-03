7dd1ce1deca8e0c6f0757aa6d6dd9378da3f1082
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-02-03 15:40:31 -0500

    Add new type of model routine for rs07.

--- modules/RS07.hs
+++ modules/RS07.hs
@@ -13,4 +13,17 @@ note meanIndelLengthMinus1 ~ exponential 10.0;
   lambda = exp logLambda;
   
   main = (\d b heat training -> rs07_branch_HMM epsilon (lambda*d!b) heat training, \l -> rs07_lengthp epsilon l);
+
+  main2 x y = "RS07" @@  
+             (do { logLambda <- "logLambda" ~~ laplace (-4.0) (1.0/sqrt 2.0);
+                   let {lambda = exp logLambda};
+
+                   meanIndelLengthMinus1 <- exponential 10.0;
+                   let {epsilon = meanIndelLengthMinus1/(1.0 + meanIndelLengthMinus1)};
+
+                   Log "meanIndelLength" (meanIndelLengthMinus1+1);
+
+                   let {f1 d b heat training = rs07_branch_HMM epsilon (lambda*d!b) heat training};
+                   return (f1, rs07_lengthp epsilon)
+                 });
 }
