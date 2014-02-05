db4ec7b68aec9637cbfc485384ceeb0e7cb069d7
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-02-05 17:41:46 -0500

    Update TN.hs.

--- modules/TN.hs
+++ modules/TN.hs
@@ -3,12 +3,12 @@ module TN where
 import SModel;
 import Distributions;
 
-main nuca = Prefix "TN" 
+main = Prefix "TN" 
   (do {
      kappaPur <- logLaplace (log 2.0) 0.25 ;
      Log "kappaPur" kappaPur;
-     kappaPur <- logLaplace (log 2.0) 0.25 ;
+     kappaPyr <- logLaplace (log 2.0) 0.25 ;
      Log "kappaPyr" kappaPyr;
-     return $ tn nuca kappaPur kappaPyr
+     return $ \nuca -> tn nuca kappaPur kappaPyr
 });
 }
