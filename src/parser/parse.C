d52cfd5b7cce9f04bc10caac78b315bf9d0b8cba
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-02-10 12:40:12 -0500

    Expect something after the <-

--- src/parser/parse.C
+++ src/parser/parse.C
@@ -700,9 +700,9 @@ struct HParser : qi::grammar<Iterator, expression_ref()>
 
 	/*----- Section 3.14 -----*/
 	stmts = exp[push_back(_a,_1)] >> -tok.SemiColon >> tok.RightCurly [ _val = new_<expression>(AST_node("Stmts"), _a) ]
-	  | eps [clear(_a) ] >> stmt[push_back(_a,_1)] >> stmts[push_back(_a,_1)] >> eps [ _val = new_<expression>(AST_node("Stmts"), _a) ];
+	  | eps [clear(_a) ] >> stmt[push_back(_a,_1)] > stmts[push_back(_a,_1)] >> eps [ _val = new_<expression>(AST_node("Stmts"), _a) ];
 	stmt =  exp[push_back(_a,_1)] >> tok.SemiColon [ _val = new_<expression>(AST_node("SimpleStmt"), _a) ]
-	  | eps [clear(_a) ] >> pat[push_back(_a,_1)] >> tok.LeftArrow >> exp[push_back(_a,_1)] >> tok.SemiColon [ _val = new_<expression>(AST_node("PatStmt"), _a) ]
+	  | eps [clear(_a) ] >> pat[push_back(_a,_1)] >> tok.LeftArrow > exp[push_back(_a,_1)] >> tok.SemiColon [ _val = new_<expression>(AST_node("PatStmt"), _a) ]
 	  | eps [clear(_a) ] >> let_decls[push_back(_a,_1)] >> tok.SemiColon [ _val = new_<expression>(AST_node("LetStmt"), _a) ]
 	  | eps [clear(_a) ] >> tok.SemiColon [ _val = new_<expression>(AST_node("EmptyStmt"), _a) ];
 
