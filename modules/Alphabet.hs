dcaa302dc4bd94e9ca25fffdfcd80c2e67e31f49
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-02-18 17:28:08 -0500

    Rename builtin library alphabetSize.C to Alphabet.C

--- modules/Alphabet.hs
+++ modules/Alphabet.hs
@@ -1,8 +1,8 @@
 module Alphabet where
 {
 builtin getNucleotides 1 "getNucleotides";
-builtin alphabetSize 1 "alphabetSize";
-builtin builtin_alphabet_letters 1 "alphabet_letters" "alphabetSize";
+builtin alphabetSize 1 "alphabetSize" "Alphabet";
+builtin builtin_alphabet_letters 1 "alphabet_letters" "Alphabet";
 
 alphabet_letters a = map listFromString (list_from_vector (builtin_alphabet_letters a) )
 }
