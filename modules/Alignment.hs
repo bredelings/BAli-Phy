df85311cfbf74580454eebec96e6fbc9c9f41286
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-02-18 17:34:42 -0500

    Create Alignment.C by renaming.

--- modules/Alignment.hs
+++ modules/Alignment.hs
@@ -4,7 +4,7 @@ module Alignment where
   builtin pairwise_alignment_length1 1 "pairwise_alignment_length1";
   builtin pairwise_alignment_length2 1 "pairwise_alignment_length2";
   builtin transition_counts 1 "transition_counts";
-  builtin pairwise_alignment_probability_from_counts 2 "pairwise_alignment_probability_from_counts";
+  builtin pairwise_alignment_probability_from_counts 2 "pairwise_alignment_probability_from_counts" "Alignment";
   
   branch_hmm model distances heat training b = fst model distances b heat training;
   
