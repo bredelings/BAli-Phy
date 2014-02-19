d2e5b05137254c90758869b5b8b1aa33c09f8550
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-02-19 13:31:23 -0500

    Move other builtins into Pair.C

--- src/builtins/Pair.C
+++ src/builtins/Pair.C
@@ -8,3 +8,16 @@ extern "C" closure builtin_function_c_fst(OperationArgs& Args)
 {
   return Args.evaluate_as<OPair>(0)->first;
 }
+
+extern "C" closure builtin_function_c_snd(OperationArgs& Args)
+{
+  return Args.evaluate_as<OPair>(0)->second;
+}
+
+extern "C" closure builtin_function_c_pair(OperationArgs& Args)
+{
+  const object_ref fst = Args.evaluate(0);
+  const object_ref snd = Args.evaluate(1);
+
+  return OPair(Opair{fst,snd});
+}
