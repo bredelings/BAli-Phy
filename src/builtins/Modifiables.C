37801cddc6c363974631af0ff72f5e062d058988
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-02-19 13:48:49 -0500

    Move evaluate and trigger to Modifiables.C

--- src/builtins/Modifiables.C
+++ src/builtins/Modifiables.C
@@ -154,3 +154,41 @@ extern "C" closure builtin_function_register_probability(OperationArgs& Args)
 
   return constructor("()",0);
 }
+
+extern "C" closure builtin_function_evaluate(OperationArgs& Args)
+{
+  auto& M = Args.memory();
+
+  int c = *Args.evaluate_as<Int>(0);
+
+#ifndef NDEBUG
+  if (Args.evaluate_changeables() and c >= 0)
+    throw myexception()<<"Calling builtin_function_evaluate( ) when evaluate_changeables=true and c >= 0";
+#endif
+
+  int R1 = Args.reg_for_slot(1);
+
+  int R2 = 0;
+
+  if (c < 0)
+    R2 = M.incremental_evaluate(R1, 0);
+  else
+    R2 = M.incremental_evaluate_in_context(R1, c);
+
+  assert( R2 );
+
+  return {index_var(0),{R2}};
+}
+
+extern "C" closure builtin_function_trigger(OperationArgs& Args)
+{
+  int i = *Args.evaluate_as<Int>(0);
+
+  reg_heap& M = Args.memory();
+
+  int token = Args.current_token();
+
+  M.triggers(token).push_back(i);
+
+  return constructor("()",0);
+}
