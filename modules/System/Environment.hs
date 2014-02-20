aea8d338651ca7e9cd167303510462dd2c3effcc
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-02-20 11:27:06 -0500

    Fix System.Environment.

--- modules/System/Environment.hs
+++ modules/System/Environment.hs
@@ -1,6 +1,6 @@
 module Environment where
 {
-  builtin "builtin_getArgs" 1 "getArgs" "Environment";
+builtin builtin_getArgs 1 "getArgs" "Environment";
 
-  getArgs = map listFromString $ list_from_vector $ builtin_getArgs ();
+getArgs = map listFromString $ list_from_vector $ builtin_getArgs ()
 }
