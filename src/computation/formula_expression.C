cf8caccce4da0f5adbb95083480cf82939a19d9f
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-02-28 09:37:19 -0500

    Remove reference to bounds from formula_expression.C

--- src/computation/formula_expression.C
+++ src/computation/formula_expression.C
@@ -1,41 +1,7 @@
 #include "formula_expression.H"
-#include "bounds.H"
 #include "context.H"
 #include "computation/module.H"
 #include "parser/AST.H"
-
-/* 1. So a formula_expression_ref is basically a model.
-     It is a single evaluatable expression E with annotations (*notes*) N about its parameters.
-     Neither the notes, nor the expression should be changed.
-
-   2. Is E a note?
-     Well, we need it to be a note for the following reasons.
-     - A parameter might be "declared" in E.  Thus if E is removed, the number of parameters might change.
-     - When we add prefixes to variables, we need to modify both E and N.
-     - 
-
-   3. What kinds of notes might we need?
-      - default value [computable!]
-      - bounds [computable!]
-      - prior [computable!]
-      - loggers
-      - mcmc steps
-      - clamps?
-      Question: suppose we JUST have priors.  Is that still a model?  It has no compute expression.
-
-   4. We could alternatively decide that a model 
-
-   5. How about saying that a model includes ONLY those parameters mentioned in E?
-
-   6. Which parameters does a model contain?
-     - Currently, it contains any parameters mentioned anywhere.
-     - If the expression is reduced a parameters might be eliminated.
-       For example (@,\x->2,y) reduces to 2, which then no longer contains 'y'.
-
-   7. Q1: Do I want to separate the expression from the notes, again?
-      Q2: Can I combine subsets of notes incrementally without 
-          expression syntax for formula_expression_ref?
- */
 
 using std::vector;
 using std::set;
