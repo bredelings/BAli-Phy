a8855172cdd9d8fd0588f65119d389d7c3bed3c0
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-03-04 10:44:37 -0500

    Rewrite stats-select to preserve column contexts exactly.

--- src/tools/stats-select.C
+++ src/tools/stats-select.C
@@ -79,48 +79,11 @@ variables_map parse_cmd_line(int argc,char* argv[])
 }
 
 template <typename T>
-class table
-{
-  /// The list of column names
-  std::vector<std::string> names_;
-
-  /// List of data for each column
-  std::vector< std::vector<T> > data_;
-
-  /// Load data from a file
-  void load_file(std::istream&,int,int,int,const std::vector<std::string>&,const std::vector<std::string>&);
-public:
-  /// Access the column names
-  const std::vector<std::string>& names() const {return names_;}
-
-  /// Access the data for the i-th column
-  const std::vector<T>& column(int i) const {return data_[i];}
-
-  /// Add a row of data
-  void add_row(const std::vector<double>& row);
-
-  /// How many rows does the table contain?
-  int n_rows() const {return data_[0].size();}
-
-  /// How many columns does the table contain?
-  int n_columns() const {return names_.size();}
-
-  /// Remove burnin samples
-  void chop_first_rows(int n);
-
-  /// Load the table from a file
-  table(std::istream&,int,int,int,const std::vector<std::string>&,const std::vector<std::string>&);
-
-  /// Load the table from a file by name
-  table(const std::string&,int,int,int,const std::vector<std::string>&,const std::vector<std::string>&);
-};
-
-template <typename T>
 struct table_row_function
 {
   virtual table_row_function* clone() const =0;
 
-  virtual T operator()(const stats_table&, int row) const =0;
+  virtual T operator()(const Table<string>&, int row) const =0;
 
   string name;
 
@@ -129,78 +92,24 @@ struct table_row_function
   {}
 };
 
-struct select_column_function: public table_row_function<double>
-{
-  int index;
-
-  select_column_function* clone() const {return new select_column_function(*this);}
-
-  double operator()(const stats_table& t, int row) const
-  {
-    return t.column(index)[row];
-  }
-
-  select_column_function(const stats_table& t, const string& name)
-    :table_row_function<double>(name), index(t.find_column_index(name))
-  {
-    if (index == -1)
-      throw myexception()<<"Can't find column '"<<name<<" in table.";
-  }
-};
-
-struct sum_of_fields: public table_row_function<double>
-{
-  vector<int> indices;
-
-  sum_of_fields* clone() const {return new sum_of_fields(*this);}
-
-  double operator()(const stats_table&,int row) const;
-
-  sum_of_fields(const stats_table&, const string&);
-};
-
-double sum_of_fields::operator()(const stats_table& t,int row) const
-{
-  double sum = 0;
-  for(int i=0;i<indices.size();i++)
-    sum += t.column(indices[i])[row];
-  return sum;
-}
-
-sum_of_fields::sum_of_fields(const stats_table& t, const string& name)
-  :table_row_function<double>(name)
-{
-  vector<string> names = split(name,'+');
-
-  for(int i=0; i < names.size(); i++)
-  {
-    int index = t.find_column_index(names[i]);
-    
-    if (index == -1)
-      throw myexception()<<"Can't find column '"<<name<<" in table.";
-
-    indices.push_back(index);
-  }
-}
-
 struct key_value_condition: public table_row_function<bool>
 {
   int key_index;
-  double value;
+  string value;
 
   key_value_condition* clone() const {return new key_value_condition(*this);}
 
-  bool operator()(const stats_table&,int row) const;
+  bool operator()(const Table<string>&,int row) const;
 
-  key_value_condition(const stats_table&, const string&);
+  key_value_condition(const Table<string>&, const string&);
 };
 
-bool key_value_condition::operator()(const stats_table& t,int row) const
+bool key_value_condition::operator()(const Table<string>& t,int row) const
 {
   return (t.column(key_index)[row] == value);
 }
 
-key_value_condition::key_value_condition(const stats_table& t, const string& condition)
+key_value_condition::key_value_condition(const Table<string>& t, const string& condition)
   :table_row_function<bool>(condition)
 {
   vector<string> parse = split(condition,'=');
@@ -211,7 +120,7 @@ key_value_condition::key_value_condition(const stats_table& t, const string& con
   if (key_index == -1)
     throw myexception()<<"Can't find column '"<<parse[0]<<"' in table.";
 
-  value = convertTo<double>(parse[1]);
+  value = parse[1];
 }
 
 int main(int argc,char* argv[]) 
@@ -230,26 +139,8 @@ int main(int argc,char* argv[])
     if (not args.count("columns") or args.count("remove"))
       std::swap(remove,keep);
 
-    vector<string> add;
-    if (args.count("add"))
-      add = args["add"].as<vector<string> >();
+    Table<string> table(std::cin,0,1,-1,remove,keep);
 
-    stats_table table(std::cin,0,1,-1,remove,keep);
-
-    //------------ Parse column names ----------//
-    vector< owned_ptr<table_row_function<double> > > column_functions;
-
-    for(int i=0;i<table.n_columns();i++)
-    {
-      const string& name = table.names()[i];
-      column_functions.push_back(select_column_function(table, name));
-    }
-
-    if (add.size())
-    {
-      for(const auto& sum:add)
-	column_functions.push_back(sum_of_fields(table,sum));
-    }
 
     //----------- Parse conditions ------------//
     vector< owned_ptr<table_row_function<bool> > > conditions;
@@ -266,8 +157,8 @@ int main(int argc,char* argv[])
     if (not args.count("no-header"))
     {
       vector<string> headers;
-      for(int i=0;i<column_functions.size();i++)
-	headers.push_back(column_functions[i]->name);
+      for(int i=0;i<table.n_columns();i++)
+	headers.push_back(table.names()[i]);
 
       write_header(std::cout, headers);
     }
@@ -282,11 +173,15 @@ int main(int argc,char* argv[])
 	  ok = false;
       if (not ok) continue;
 
-      vector<double> values;
-      for(int i=0; i<column_functions.size(); i++)
-	values.push_back((*column_functions[i])(table,r));
-
-      std::cout<<join(values,"\t")<<"\n";
+      vector<string> values;
+      for(int i=0; i<table.n_columns(); i++)
+      {
+	std::cout<<table.column(i)[r];
+	if (i+1 == table.n_columns())
+	  std::cout<<"\n";
+	else
+	  std::cout<<"\t";
+      }
     }
   }
   catch (std::exception& e) {
