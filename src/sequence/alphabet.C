173e0e6356162a5bc8094e825a26e6b8d6985286
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-05-21 14:10:36 -0400

    OK, allow all ambiguity codes.

--- src/sequence/alphabet.C
+++ src/sequence/alphabet.C
@@ -320,13 +320,13 @@ Nucleotides::Nucleotides(const string& s, char c)
   insert_class("R","AG");     // puRine
   insert_class("W",t+"A");    // Weak
   insert_class("S","GC");     // Strong
-  // insert_class("K",t+"G"); // Ketone
-  // insert_class("M","AC");  // aMino
+  insert_class("K",t+"G"); // Ketone
+  insert_class("M","AC");  // aMino
 
-  // insert_class("B",t+"GC"); // not-A (B is after A)
-  // insert_class("D",t+"AG"); // not-C (D is after C)
-  // insert_class("H",t+"AC"); // not-G (H is after G)
-  // insert_class("V","GAC");  // not-T (V is after U)
+  insert_class("B",t+"GC"); // not-A (B is after A)
+  insert_class("D",t+"AG"); // not-C (D is after C)
+  insert_class("H",t+"AC"); // not-G (H is after G)
+  insert_class("V","GAC");  // not-T (V is after U)
 }
 
 DNA::DNA()
