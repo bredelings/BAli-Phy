8d22b2847f28fcfcbf64fbb9850f78117fd75866
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-07-02 11:52:50 -0400

    Remove new_computation_for_reg( ).

--- src/computation/graph_register.C
+++ src/computation/graph_register.C
@@ -51,7 +51,7 @@ using std::endl;
  */
 
 /*
- * 1. Make operations return a reg_address
+ * 1. [DONE] Merge new_computation_for_reg(t,r) with add_shared_computation(t,r)
  *
  * 2. Start recording created regs.
  *
@@ -1717,14 +1717,6 @@ int reg_heap::remove_shared_computation(int t, int r)
     return tokens[t].vm_relative.erase_value(r);
   else
     return tokens[t].vm_relative.set_value(r,-1);
-}
-
-int reg_heap::new_computation_for_reg(int t, int r) const
-{
-  int rc = computations.allocate();
-  computations[rc].source_token = t;
-  computations[rc].source_reg = r;
-  return rc;
 }
 
 int reg_heap::move_computation(int t1, int t2, int r)
