550d3f53dd99086cc27790de962a7f55882595de
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-07-08 12:46:10 -0400

    Move release_token( ) next to destroy_all_computations_in_token(t).

--- src/computation/graph_register.C
+++ src/computation/graph_register.C
@@ -1761,8 +1761,6 @@ void reg_heap::try_release_token(int t)
   // mark token for this context unused
   int parent = parent_token(t);
 
-  release_token(t);
-
   if (t == root_token)
     assert(not n_children);
 
@@ -1792,6 +1790,7 @@ void reg_heap::try_release_token(int t)
 
   // clear only the mappings that were actually updated here.
   destroy_all_computations_in_token(t);
+  release_token(t);
 
   // If we just released a terminal token, maybe it's parent is not terminal also.
   if (parent != -1)
