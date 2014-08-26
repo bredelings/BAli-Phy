92b148eb912f79aa2a82704378d8e6c6cf92f529
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-08-26 14:31:12 -0400

    Add builtins to read doubles from text files.

---
+++ modules/Data/ReadFile.hs
@@ -0,0 +1,9 @@
+module Data.ReadFile where
+{
+builtin builtin_read_file 1 "read_file" "Data";
+builtin string_to_double 1 "string_to_double" "Data";
+
+read_file filename = list_from_vector $ builtin_read_file filename;
+
+read_file_as_double filename = map string_to_double $ read_file filename;
+}
---
+++ src/builtins/Data.C
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+#include "computation/computation.H"
+#include "myexception.H"
+#include "io.H"
+
+using boost::dynamic_pointer_cast;
+using std::vector;
+using std::string;
+
+extern "C" closure builtin_function_read_file(OperationArgs& Args)
+{
+  const string& filename = *Args.evaluate_as<String>(0);
+
+  checked_ifstream text_file(filename,"text file");
+
+  object_ptr<OVector> v(new OVector);
+
+  string line;
+  while(portable_getline(text_file, line))
+    (*v).push_back(new String(line));
+
+  return v;
+}
+
+extern "C" closure builtin_function_string_to_double(OperationArgs& Args)
+{
+  object_ptr<const String> s = Args.evaluate_as<String>(0);
+  object_ptr<const Double> d = new Double(convertTo<double>(*s));
+}
