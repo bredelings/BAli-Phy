ef6e3ecfa5bdd27833e327c126ef9f7b2ee731f5
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-08-26 14:52:57 -0400

    Convert Haskell string to C++ String object.

--- modules/Data/ReadFile.hs
+++ modules/Data/ReadFile.hs
@@ -3,7 +3,7 @@ module Data.ReadFile where
 builtin builtin_read_file 1 "read_file" "Data";
 builtin string_to_double 1 "string_to_double" "Data";
 
-read_file filename = list_from_vector $ builtin_read_file filename;
+read_file filename = list_from_vector $ builtin_read_file (listToString filename);
 
 read_file_as_double filename = map string_to_double $ read_file filename;
 }
