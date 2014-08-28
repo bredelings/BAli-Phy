7531fa999bb47e86c76d331f414b23c35079b58e
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-08-27 20:58:16 -0400

    Rewrite airline fatalities example.

--- tests/AirlineFatalities.hs
+++ tests/AirlineFatalities.hs
@@ -3,17 +3,28 @@ import Distributions;
 
 fatalities = [24,25,31,31,22,21,26,20,16,22];
 
-iotaZip i [] = [];
-iotaZip i (x:xs) = (i,x):(iotaZip (i+1.0) xs);
+indices' i [] = [];
+indices' i (x:xs) = i:(indices' (i+1.0) xs);
+indices l = indices' 0.0 l;
+
+observe_list ys dists = sequence_ [Observe y dist | (y,dist) <- zip ys dists];
+
+safe_exp x = if (x < (-700.0)) then
+                exp(-700.0);
+             else if (x > 700.0) then
+                exp 700.0;
+             else
+                exp x;
 
 main = Prefix "Airline" $ do 
 {
-  alpha <- uniform (-2.0) 2.0;
+  alpha <- cauchy 0.0 1.0;
   Log "alpha" alpha;
 
-  beta <- uniform (-2.0) 2.0;
+  beta <- cauchy 0.0 1.0;
   Log "beta" beta;
 
-  sequence_ [Observe y (poisson mu) | (i,y) <- iotaZip 0.0 fatalities, let {mu = exp(alpha + beta*i)}];
+  observe_list fatalities [poisson $ safe_exp(alpha + beta*i) | i <- indices fatalities]
 };
+
 }
