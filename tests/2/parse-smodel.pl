b636e4b5873125784b4b3a88ede0cf46c83d6dd4
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2014-11-01 12:28:36 -0400

    Add some tests for smodel arguments.

---
+++ tests/2/parse-smodel.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use File::Temp qw(tempfile);
+
+my $verbose = 0;
+
+sub exec_show
+{
+    my $cmd = shift;
+#    print LOG "\ncommand:  $cmd\n\n";
+    print "*";
+    my ($tmp_fh,$tmp_filename) = tempfile();
+
+    my $result = `$cmd 2>$tmp_filename`;
+    if ($? != 0)
+    {
+        my $code = $?>>8;
+        my $message = `cat $tmp_filename`; 
+        print STDERR "Subcommand failed! (code $code)\n";
+#        print LOG    "Subcommand failed! (code $code)\n";
+
+        print STDERR "\n  command:  $cmd\n";
+
+        print STDERR "\n  message:  $message\n";
+#        print LOG    "\n  message:  $message\n";
+        exit($code);
+    }
+    elsif ($verbose)
+    {
+        print STDERR "\n\t$cmd\n\n";
+    }
+    close $tmp_fh;
+    return $result;
+}
+
+sub exec_result
+{
+    my $cmd = shift;
+#    print LOG "\ncommand:  $cmd\n\n";
+
+    print STDERR "\n\t$cmd\n\n" if ($verbose);
+
+    my $result = `$cmd`;
+    return $?;
+}
+
+sub show_only
+{
+    exec_show("bali-phy --show-only @_");
+}
+
+show_only("rna.fasta");
+
+show_only("dna.fasta");
+show_only("dna.fasta --smodel=HKY");
+show_only("dna.fasta --smodel=HKY+F");
+show_only("dna.fasta --smodel=HKY+gwF");
+show_only("dna.fasta --smodel=HKY+gwF+gamma");
+show_only("dna.fasta --smodel=HKY+gwF+gamma_inv");
+show_only("dna.fasta --smodel=HKY+gwF+gamma_inv[4]");
+
+show_only("aa.fasta --smodel WAG");
+show_only("aa.fasta --smodel WAG+F");
+
+show_only("codons.fasta --alphabet=Codons");
+show_only("codons.fasta --alphabet=Codons --smodel=M0+F1x4");
+show_only("codons.fasta --alphabet=Codons --smodel=M8");
+show_only("codons.fasta --alphabet=Codons --smodel=M8[,HKY,F61]");
+print "\n";
