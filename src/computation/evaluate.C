0e4083ec692cc7bc6ce822d245082679241b5ac6
Benjamin Redelings <benjamin.redelings@duke.edu>
2015-02-21 15:25:40 -0500

    Improve comments.

--- src/computation/evaluate.C
+++ src/computation/evaluate.C
@@ -342,9 +342,11 @@ std::pair<int,int> reg_heap::incremental_evaluate(int R)
 	    assert(old_rc > 0);
 	    assert(computation_stack.back() == old_rc);
 	    computation_stack.pop_back();
-	    // This saves 25% of CPU time:
+	    // Removing this takes an extra 25% of CPU time:
 	    computations.inc_version();
 	    computations.reclaim_used(old_rc);
+	    // Replacing it with takes only 18% more CPU time.
+	    //	    computations[old_rc].clear();
 	  }
 	  return {R, result};
 	}
