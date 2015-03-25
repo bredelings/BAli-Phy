bd80ee85b5989ffb50a9fe04ac2d0d72a33307ce
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2015-03-25 15:34:40 -0400

    Check parsing of M3 and M3_Test

--- tests/2/parse-smodel.pl
+++ tests/2/parse-smodel.pl
@@ -68,6 +68,8 @@ show_only("aa.fasta --smodel LG+DP[4]");
 show_only("codons.fasta --alphabet=Codons");
 show_only("codons.fasta --alphabet=Codons --smodel=M0+F1x4");
 show_only("codons.fasta --alphabet=Codons --smodel=M0[GTR]+F1x4");
+show_only("codons.fasta --alphabet=Codons --smodel=M3[4,GTR,MG94]");
+show_only("codons.fasta --alphabet=Codons --smodel=M3_Test");
 show_only("codons.fasta --alphabet=Codons --smodel=M8");
 show_only("codons.fasta --alphabet=Codons --smodel=M8[,HKY,F61]");
 
