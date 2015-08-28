561a60f9b2689888bc7269af6e978c057d304c08
Benjamin Redelings <benjamin.redelings@gmail.com>
2015-08-28 16:08:09 -0400

    Fix reverseEdge t b.

--- modules/Tree.hs
+++ modules/Tree.hs
@@ -9,7 +9,7 @@ nodesForEdge (Tree _ branchesArray _ _) edgeIndex = branchesArray ! edgeIndex;
 sourceNode t edge = fst (nodesForEdge t edge);
 targetNode t edge = snd (nodesForEdge t edge);
 edgeForNodes t (n1,n2) = head [b | b <- (edgesOutOfNode t n1), (targetNode t b)==n2];
-reverseEdge t b = edgeForNodes (swap (nodesForEdge t b));
+reverseEdge t b = edgeForNodes t (swap (nodesForEdge t b));
 nodeDegree t n = length (edgesOutOfNode t n);
 neighbors t n = fmap (targetNode t) (edgesOutOfNode t n);
 edgesBeforeEdge t b = case (nodesForEdge t b) of {(n1,n2) -> [edgeForNodes t (n,n1) | n <- neighbors t n1, n /= n2 ]};
