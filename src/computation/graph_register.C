3a837bbcb2ac54c9769d4900718b00bdc79c4813
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-07-25 14:00:55 -0400

    Fix: +-/== should go to --/+- not --/+=

--- src/computation/graph_register.C
+++ src/computation/graph_register.C
@@ -1666,8 +1666,20 @@ void reg_heap::invalidate_shared_regs(int t1, int t2)
   }
 
   for(int r:call_and_value_may_be_changed)
-    if (has_result_(t2,r) and result_for_reg_(t2,r).temp == -1)
+  {
+    int res2 = result_index_for_reg_(t2,r);
+    if (res2 <= 0 and result_index_for_reg_(t1,r) == 0)
+    {
+      // If we have +- in t2 and == in t1, then we want to end up with +- in t1 and -- in t2.
+      // Therefore we have to detect this case and set the result to - in t1.
+      assert(res2 < 0 or is_root_token(t2));
+      assert(step_index_for_reg_(t1,r) == 0);
+      assert(not is_root_token(t1));
+      tokens[t1].vm_result.set_value(r,-1);
+    }
+    else if (res2 > 0 and results[res2].temp == -1)
       value_may_be_changed.push_back(r);
+  }
 
   if (t2 == root_token)
     for(int r: value_may_be_changed)
