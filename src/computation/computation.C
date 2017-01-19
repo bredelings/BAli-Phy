8b69adadcbdbb296c78eaded5d9d46e0f58889ea
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-01-19 17:18:00 -0500

    Indent.

--- src/computation/computation.C
+++ src/computation/computation.C
@@ -4,9 +4,9 @@
 
 int OperationArgs::reg_for_slot(int slot) const
 {
-  int index = reference(slot).as_index_var();
+    int index = reference(slot).as_index_var();
 
-  return current_closure().lookup_in_env(index);
+    return current_closure().lookup_in_env(index);
 }
 
 int OperationArgs::n_args() const {return current_closure().exp.size();}
@@ -15,97 +15,97 @@ const expression_ref& OperationArgs::reference(int slot) const {return current_c
 
 const closure& OperationArgs::evaluate_slot_to_closure(int slot)
 {
-  return evaluate_reg_to_closure(reg_for_slot(slot));
+    return evaluate_reg_to_closure(reg_for_slot(slot));
 }
 
 const closure& OperationArgs::evaluate_slot_to_closure_(int slot)
 {
-  return evaluate_reg_to_closure_(reg_for_slot(slot));
+    return evaluate_reg_to_closure_(reg_for_slot(slot));
 }
 
 int OperationArgs::evaluate_slot_no_record(int slot)
 {
-  return evaluate_reg_no_record(reg_for_slot(slot));
+    return evaluate_reg_no_record(reg_for_slot(slot));
 }
 
 int OperationArgs::evaluate_slot_to_reg(int slot)
 {
-  return evaluate_reg_to_reg(reg_for_slot(slot));
+    return evaluate_reg_to_reg(reg_for_slot(slot));
 }
 
 const expression_ref& OperationArgs::evaluate_reg_to_object(int R2)
 {
-  const expression_ref& result = evaluate_reg_to_closure(R2).exp;
+    const expression_ref& result = evaluate_reg_to_closure(R2).exp;
 #ifndef NDEBUG
-  if (result.head().is_a<lambda2>())
-    throw myexception()<<"Evaluating lambda as object: "<<result.print();
+    if (result.head().is_a<lambda2>())
+	throw myexception()<<"Evaluating lambda as object: "<<result.print();
 #endif
-  return result;
+    return result;
 }
 
 const expression_ref& OperationArgs::evaluate_reg_to_object_(int R2)
 {
-  const expression_ref& result = evaluate_reg_to_closure_(R2).exp;
+    const expression_ref& result = evaluate_reg_to_closure_(R2).exp;
 #ifndef NDEBUG
-  if (result.head().is_a<lambda2>())
-    throw myexception()<<"Evaluating lambda as object: "<<result.print();
+    if (result.head().is_a<lambda2>())
+	throw myexception()<<"Evaluating lambda as object: "<<result.print();
 #endif
-  return result;
+    return result;
 }
 
 const expression_ref& OperationArgs::evaluate_slot_to_object(int slot)
 {
-  return evaluate_reg_to_object(reg_for_slot(slot));
+    return evaluate_reg_to_object(reg_for_slot(slot));
 }
 
 const expression_ref& OperationArgs::evaluate_slot_to_object_(int slot)
 {
-  return evaluate_reg_to_object_(reg_for_slot(slot));
+    return evaluate_reg_to_object_(reg_for_slot(slot));
 }
 
 const expression_ref& OperationArgs::evaluate(int slot)
 {
-  return evaluate_slot_to_object(slot);
+    return evaluate_slot_to_object(slot);
 }
 
 const expression_ref& OperationArgs::evaluate_(int slot)
 {
-  return evaluate_slot_to_object_(slot);
+    return evaluate_slot_to_object_(slot);
 }
 
 int OperationArgs::allocate(closure&& C)
 {
-  if (C.exp.head().type() == index_var_type)
-  {
-    int index = C.exp.as_index_var();
+    if (C.exp.head().type() == index_var_type)
+    {
+	int index = C.exp.as_index_var();
 
-    int r = C.lookup_in_env( index );
+	int r = C.lookup_in_env( index );
     
-    assert(M.is_used(r));
+	assert(M.is_used(r));
 
-    return r;
-  }
-  else
-  {
-    int r = allocate_reg();
-    M.set_C(r, std::move(C));
-    return r;
-  }
+	return r;
+    }
+    else
+    {
+	int r = allocate_reg();
+	M.set_C(r, std::move(C));
+	return r;
+    }
 }
 
 int OperationArgs::allocate_reg()
 {
-  int r = M.push_temp_head();
-  n_allocated++;
-  return r;
+    int r = M.push_temp_head();
+    n_allocated++;
+    return r;
 }
 
 OperationArgs::OperationArgs(reg_heap& m)
-  :M(m)
+    :M(m)
 { }
 
 OperationArgs::~OperationArgs()
 {
-  for(int i=0;i<n_allocated;i++)
-    M.pop_temp_head();
+    for(int i=0;i<n_allocated;i++)
+	M.pop_temp_head();
 }
