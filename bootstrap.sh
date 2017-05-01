8fa9f51d201b91921e0969339c751d9d0b761117
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-05-01 10:54:24 -0400

    Run glibtoolize on Darwin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+LIBTOOLIZE=libtoolize
+uname="$(uname)"
+echo "Running on $uname"
+case "$uname" in (*Darwin*) LIBTOOLIZE=glibtoolize ;; esac
+
 set -x
 aclocal -I m4
-libtoolize
+$LIBTOOLIZE
 aclocal -I m4 || exit
-libtoolize || exit
+$LIBTOOLIZE || exit
 autoheader || exit
 automake -a || exit
 autoconf
