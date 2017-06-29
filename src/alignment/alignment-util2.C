06bf9fa830131b2c9c73284b79bff626b5fa684b
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-06-29 10:41:56 -0400

    Speed up minimally_connect_leaf_characters( ) with TreeInterface.

--- src/alignment/alignment-util2.C
+++ src/alignment/alignment-util2.C
@@ -23,6 +23,7 @@
 /// \brief This file implements alignment utility functions.
 ///
 
+#include <boost/optional.hpp>
 #include "alignment-util.H"
 #include "alignment-util2.H"
 #include "util.H"
@@ -38,6 +39,7 @@ using std::endl;
 using std::istream;
 
 using boost::dynamic_bitset;
+using boost::optional;
 
 using boost::program_options::variables_map;
 
@@ -135,35 +137,104 @@ bool check_leaf_characters_minimally_connected(const alignment& A,const TreeInte
     return true;
 }
 
+optional<int> find_internal_node(const TreeInterface& t)
+{
+    for(int n=0;n<t.n_nodes();n++)
+	if (t.is_internal_node(n)) return n;
+
+    return boost::none;
+}
+
 /// Force internal node states are consistent by connecting leaf characters
 void minimally_connect_leaf_characters(alignment& A,const TreeInterface& t)
 {
     assert(A.n_sequences() == t.n_nodes());
 
-    auto partitions = get_partitions(t);
-    for(int column=0;column<A.length();column++)
-    {
-	// construct leaf presence/absence mask
-	dynamic_bitset<> present(t.n_nodes());
-	for(int i=0;i<t.n_leaves();i++)
-	    present[i] = not A.gap(column,i);
-    
-	// compute presence/absence for internal nodes
-	connect_all_characters(t, partitions, present);
+    auto root_ = find_internal_node(t);
+    if (not root_) return;
+    int root = *root_;
 
-	// put present characters into the alignment.
-	for(int i=t.n_leaves();i<t.n_nodes();i++) {
-	    if (present[i])
-		A.set_value(column,i, alphabet::not_gap );
-	    else
-		A.set_value(column,i, alphabet::gap );
+    vector<int> branches = t.all_branches_toward_node(root);
+
+    // Set the bitmasks for the leaf sequences
+    vector<dynamic_bitset<>> character_before_node(A.n_sequences(), dynamic_bitset<>(A.length()));
+
+    // 1. First determine whether there is any character before a branch.
+    for(int b: branches)
+    {
+	int n = t.source(b);
+	auto& mask = character_before_node[n];
+	auto before = t.branches_before(b);
+
+	if (before.empty())
+	{
+	    for(int c=0;c<A.length();c++)
+		if (A.character(c,n))
+		    mask.set(c);
+	}
+	else
+	{
+	    for(int bb: before)
+		mask |= character_before_node[t.source(bb)];
+	}
+	  
+    }
+
+    // 2. Get the list of internal nodes from the root toward the leaves
+    vector<int> nodes = {root};
+    for(int i=branches.size()-1;i>=0;i--)
+    {
+	int b = branches[i];
+	int n = t.source(b);
+	if (t.is_leaf_node(n)) continue;
+	nodes.push_back(n);
+    }
+
+    // 3. We are going to write over @character_before_node with @between_characters
+    auto& between_characters = character_before_node;
+
+    dynamic_bitset<> temp(A.length());
+    for(int n: nodes)
+    {
+	auto& mask = between_characters[n];
+
+	// One of these branches is going to be between_characters, and the rest will be character_before_node
+	auto before = t.neighbors(n);
+
+	// Consider all pairs of edges
+	for(int i=0;i<before.size();i++)
+	{
+	    int n1 = before[i];
+	    for(int j=0;j<i;j++)
+	    {
+		int n2 = before[j];
+		temp = between_characters[n1];
+		temp &= between_characters[n2];
+		mask |= temp;
+		// mask |= (between_characers[n1] & between_characters[n2]);
+	    }
 	}
     }
-    remove_empty_columns(A);
-}
+
+    // 4. Copy new bitmasks to alignment matrix
+    // FIXME -- ideally we could keep this as a bitmask...
+    for(int n: nodes)
+    {
+	auto& present = between_characters[n];
+
+	for(int column=0;column<A.length();column++)
+	{
+	    // put present characters into the alignment.
+	    if (present[column])
+		A.set_value(column, n, alphabet::not_gap );
+	    else
+		A.set_value(column, n, alphabet::gap );
+	}
+    }
+}    
 
 /// Check that internal node states are consistent
-void check_internal_nodes_connected(const alignment& A,const TreeInterface& t,const vector<int>& ignore) 
+void check_internal_nodes_connected(const alignment& A,const TreeInterface& t,const vector<int>& ignore)
 {
     // Only check if A in fact has internal node sequences.
     if (A.n_sequences() == t.n_leaves()) return;
