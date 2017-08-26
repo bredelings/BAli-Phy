b0e8d5494a12d069c9b1b454ab1d51c11bc21208
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-08-26 17:10:29 -0400

    Reindent.

--- src/computation/closure.C
+++ src/computation/closure.C
@@ -9,60 +9,60 @@ using std::string;
 
 void closure::clear()
 {
-  exp.clear();
-  Env.clear();
+    exp.clear();
+    Env.clear();
 }
 
 string closure::print() const
 {
-  string result = exp.print();
-  if (Env.size())
-    result += " {" + join(Env,", ") + "}";
-  return result;
+    string result = exp.print();
+    if (Env.size())
+	result += " {" + join(Env,", ") + "}";
+    return result;
 }
 
 closure get_trimmed(const closure& C)
 {
-  closure C2 = C;
-  return get_trimmed(std::move(C2));
+    closure C2 = C;
+    return get_trimmed(std::move(C2));
 }
 
 closure get_trimmed(closure&& C)
 {
-  if (C.exp.head().type() == trim_type)
-  {
-    expression_ref old = C.exp;
-    const vector<int>& keep = old.sub()[0].as_<Vector<int>>();
-
-    C.exp = C.exp.sub()[1];
-
-    // Since environments are indexed backwards
-    for(int i=0;i<keep.size();i++)
+    if (C.exp.head().type() == trim_type)
     {
-      int k = keep[keep.size()-1-i];
-      C.Env[i] = C.lookup_in_env(k);
-    }
-    C.Env.resize(keep.size());
+	expression_ref old = C.exp;
+	const vector<int>& keep = old.sub()[0].as_<Vector<int>>();
 
-    // Should this ever happen?
-    assert(not C.exp.head().is_a<Trim>());
-  }
+	C.exp = C.exp.sub()[1];
+
+	// Since environments are indexed backwards
+	for(int i=0;i<keep.size();i++)
+	{
+	    int k = keep[keep.size()-1-i];
+	    C.Env[i] = C.lookup_in_env(k);
+	}
+	C.Env.resize(keep.size());
+
+	// Should this ever happen?
+	assert(not C.exp.head().is_a<Trim>());
+    }
   
-  return std::move(C);
+    return std::move(C);
 }
 
 expression_ref deindexify(const closure& C)
 {
-  vector<expression_ref> variables;
-  for(int R: C.Env)
-    variables.push_back(reg_var(R));
+    vector<expression_ref> variables;
+    for(int R: C.Env)
+	variables.push_back(reg_var(R));
   
-  return deindexify(C.exp, variables);
+    return deindexify(C.exp, variables);
 }
 
 closure trim_unnormalize(closure C)
 {
-  C.exp = trim_unnormalize(C.exp);
-  return C;
+    C.exp = trim_unnormalize(C.exp);
+    return C;
 }
 
