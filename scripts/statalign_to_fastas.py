3f985be7f899239b80119c17d54d7a9d3230d6e2
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-09-01 10:54:03 -0400

    Add script for getting a list of alignments from statalign format.

---
+++ scripts/statalign_to_fastas.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+def get_alignments(infile):
+    alignment = None
+    last_sample = None
+    with open(infile, 'r') as infilestream:
+        for line in infilestream:
+            m = re.match("Sample (\d+)\s+Alignment:\s+(.*)$",line)
+            if m is not None:
+                current_sample = int(m.group(1))
+                a = m.group(2)
+                # when starting a new group, yield the last alignment and start a new one
+                if last_sample != current_sample:
+                    if alignment is not None:
+                        yield alignment
+                    alignment = []
+                    last_sample = current_sample
+                alignment.append(a)
+# Since we only yield an alignment when we start a new one,
+# the last one must be handled separately
+    yield alignment
+
+def print_alignment(alignment):
+    for line in alignment:
+        print(line)
+
+
+if __name__ == '__main__':
+    import codecs
+    import re
+    import sys
+    import os
+
+    infile = sys.argv[1]
+
+    for alignment in get_alignments(infile):
+        print_alignment(alignment)
+        print("\n\n")
