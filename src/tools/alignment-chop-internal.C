543493e1e362eced0a39d22804d15b3d4efefd43
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-10-20 16:26:36 -0400

    Reindent.

--- src/tools/alignment-chop-internal.C
+++ src/tools/alignment-chop-internal.C
@@ -1,21 +1,21 @@
 /*
-   Copyright (C) 2004-2006,2008 Benjamin Redelings
+  Copyright (C) 2004-2006,2008 Benjamin Redelings
 
-This file is part of BAli-Phy.
+  This file is part of BAli-Phy.
 
-BAli-Phy is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
-version.
+  BAli-Phy is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2, or (at your option) any later
+  version.
 
-BAli-Phy is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+  BAli-Phy is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for more details.
 
-You should have received a copy of the GNU General Public License
-along with BAli-Phy; see the file COPYING.  If not see
-<http://www.gnu.org/licenses/>.  */
+  You should have received a copy of the GNU General Public License
+  along with BAli-Phy; see the file COPYING.  If not see
+  <http://www.gnu.org/licenses/>.  */
 
 #include <iostream>
 #include <fstream>
@@ -42,56 +42,56 @@ using std::string;
 
 variables_map parse_cmd_line(int argc,char* argv[]) 
 { 
-  using namespace po;
+    using namespace po;
 
-  // named options
-  options_description all("Allowed options");
-  all.add_options()
-    ("help", "produce help message")
-    ("align", value<string>(),"file with sequences and initial alignment")
-    ("alphabet",value<string>(),"set to 'Codons' to prefer codon alphabets")
-    ;
+    // named options
+    options_description all("Allowed options");
+    all.add_options()
+	("help", "produce help message")
+	("align", value<string>(),"file with sequences and initial alignment")
+	("alphabet",value<string>(),"set to 'Codons' to prefer codon alphabets")
+	;
 
-  // positional options
-  positional_options_description p;
-  p.add("align", 1);
+    // positional options
+    positional_options_description p;
+    p.add("align", 1);
   
-  variables_map args;     
-  store(command_line_parser(argc, argv).
-	    options(all).positional(p).run(), args);
-  // store(parse_command_line(argc, argv, desc), args);
-  notify(args);    
+    variables_map args;     
+    store(command_line_parser(argc, argv).
+	  options(all).positional(p).run(), args);
+    // store(parse_command_line(argc, argv, desc), args);
+    notify(args);    
 
-  if (args.count("help")) {
-    cout<<"Usage: alignment-chop-internal <alignment-file> [OPTIONS]\n";
-    cout<<"Remove ancestral sequences from an alignment.\n\n";
-    cout<<all<<"\n";
-    exit(0);
-  }
+    if (args.count("help")) {
+	cout<<"Usage: alignment-chop-internal <alignment-file> [OPTIONS]\n";
+	cout<<"Remove ancestral sequences from an alignment.\n\n";
+	cout<<all<<"\n";
+	exit(0);
+    }
 
-  return args;
+    return args;
 }
 
 
 int main(int argc,char* argv[]) 
 { 
 
-  try {
-    //---------- Parse command line  -------//
-    variables_map args = parse_cmd_line(argc,argv);
+    try {
+	//---------- Parse command line  -------//
+	variables_map args = parse_cmd_line(argc,argv);
 
-    //------- Try to load alignment --------//
-    alignment A = load_A(args);
+	//------- Try to load alignment --------//
+	alignment A = load_A(args);
 
-    //------- Print out the alignment ------//
-    alignment A2 = chop_internal(A);
-    std::cout<<A2<<std::endl;
+	//------- Print out the alignment ------//
+	alignment A2 = chop_internal(A);
+	std::cout<<A2<<std::endl;
 
-  }
-  catch (std::exception& e) {
-    std::cerr<<"alignment-chop-internal: Error! "<<e.what()<<endl;
-    exit(1);
-  }
-  return 0;
+    }
+    catch (std::exception& e) {
+	std::cerr<<"alignment-chop-internal: Error! "<<e.what()<<endl;
+	exit(1);
+    }
+    return 0;
 
 }
