1d29bb63b8f2ac9ef3df5e8542d16a85b323d5f7
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-12-04 17:12:40 -0500

    Remove actual timer_stack class.

--- src/timer_stack.C
+++ src/timer_stack.C
@@ -30,9 +30,6 @@ along with BAli-Phy; see the file COPYING.  If not see
 using namespace std;
 using namespace boost::chrono;
 
-/// This timer stack is a global variable that is always available.
-timer_stack default_timer_stack;
-
 duration_t total_cpu_time()
 {
   return process_user_cpu_clock::now() - process_user_cpu_clock::time_point();
@@ -74,85 +71,3 @@ string duration_string(seconds t)
   return s;
 }
 
-timer_stack::container_t::iterator timer_stack::lookup_profile(const string& s)
-{
-  container_t::iterator record = total_times.find(s);
-  if (record == total_times.end()) {
-    total_times.insert(container_t::value_type(s,region_profile()));
-    record = total_times.find(s);
-    assert(record != total_times.end());
-  }
-  return record;
-}
-
-void timer_stack::credit_active_timers()
-{
-  assert(record_stack.size() == start_time_stack.size());
-
-  duration_t now = total_cpu_time();
-  for(int i=0;i<n_active_timers();i++)
-  {
-    duration_t elapsed = now - start_time_stack[i];
-    record_stack[i]->second.duration += elapsed;
-    start_time_stack[i] = now;
-  }
-}
-
-void timer_stack::push_timer(const string& s)
-{
-  start_time_stack.push_back( total_cpu_time() );
-  container_t::iterator record = lookup_profile(s);
-  record->second.n_calls++;
-  record_stack.push_back(record);
-}
-
-void timer_stack::pop_timer()
-{
-  if (record_stack.empty()) throw myexception()<<"Trying to remove a non-existent timer!";
-  duration_t start = start_time_stack.back();
-  start_time_stack.pop_back();
-
-  container_t::iterator record = record_stack.back();
-  record_stack.pop_back();
-
-  duration_t end = total_cpu_time();
-
-  record->second.duration += (end-start);
-}
-
-string timer_stack::report()
-{
-  credit_active_timers();
-
-  ostringstream o;
-
-  duration_t T = total_cpu_time();
-
-  vector<duration_t> times(total_times.size());
-  vector<container_t::iterator> records(total_times.size());
-  int j=0;
-  for(container_t::iterator i = total_times.begin();i != total_times.end();i++,j++) {
-    records[j] = i;
-    times[j] = i->second.duration;
-  }
-  vector<int> order = iota<int>(total_times.size());
-  sort(order.begin(), order.end(), sequence_order<duration_t>(times) );
-  std::reverse(order.begin(), order.end());
-
-  o.precision(3);
-  for(int r=0;r<records.size();r++)
-  {
-    container_t::iterator i = records[order[r]];
-    duration_t t = i->second.duration;
-
-    o<<setw(5)<<(t*100/T)<<"%"
-     <<"         "<<setw(6)<<t<<" sec"
-     <<"         "<<setw(8)<<i->second.n_calls
-     <<"         "<<i->first<<"\n";
-  }
-
-  if (total_times.empty())
-    o<<"   CPU time profiles: no data.\n";
-
-  return o.str();
-}
