5459087460ad3477ee299e0fedccc5e202d78dfd
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-13 12:54:23 -0400

    Fix/update Demo.hs

--- examples/models/Demo.hs
+++ examples/models/Demo.hs
@@ -6,40 +6,42 @@ import Distributions;
 -- (f x) is the distribution of the point after x.
 random_walk x0 0 _ = return [x0];
 random_walk x0 n f = do {
-                           x1 <- f x0;
+                           x1 <- sample $ f x0;
                            xs <- random_walk x1 (n-1) f;
                            return (x0:xs);
                         };
 
 main = do
 {
-  p <- beta 10.0 1.0;
-  Log "p" p;
+  p <- sample $ beta 10.0 1.0;
 
-  n <- geometric p;
-  Log "n" n;
+  n <- sample $ geometric p;
 
-  q <- cauchy 0.0 1.0;
-  Log "q" q;
+  q <- sample $ cauchy 0.0 1.0;
 
-  x <- iid 10 (normal 0.0 1.0);
-  Log "x" x;
+  x <- sample $ iid 10 (normal 0.0 1.0);
  
   -- Random array indices.
   -- You can't do this in BUGS: it makes a dynamic graph!
-  c <- iid 10 (categorical (replicate 10 0.1));
+  c <- sample $ iid 10 (categorical (replicate 10 0.1));
 
   let {w = [x!!(c!!i) | i <- [0..9]]};
-  Log "w" w;
 
   -- y[i] depends on x[i]
-  y <- list [normal (x!!i) 1.0 | i <- [0..9]];
-  Log "y" y;
+  y <- sample $ list [normal (x!!i) 1.0 | i <- [0..9]];
 
   -- Brownian-bridge-like
   z <- random_walk 0.0 10 (\mu -> normal mu 1.0);
-  Log "z" z;
 
   Observe 2.0 (normal (z!!10) 1.0);
+  return (Nothing,[
+           ("p",(Just p,[])),
+           ("n",(Just n,[])),
+           ("q",(Just q,[])),
+           ("x",(Just x,[])),
+           ("w",(Just w,[])),
+           ("y",(Just y,[])),
+           ("z",(Just z,[]))
+           ]);
 }
 }
