98bb3ff3636c1c0ab2dc68eba05185897e884ed7
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-13 12:15:42 -0400

    Update/fix Demo4.hs

--- examples/models/Demo4.hs
+++ examples/models/Demo4.hs
@@ -4,11 +4,11 @@ import Distributions;
 
 main = do
 {
-  xs <- iid 10 (normal 0.0 1.0);
+  xs <- sample $ iid 10 (normal 0.0 1.0);
 
-  categories <- iid 10 (categorical (replicate 10 0.1));
+  categories <- sample $ iid 10 (categorical (replicate 10 0.1));
 
   let {ys = [xs!!(categories!!i) | i <- [0..9]]};
-  Log "ys" ys;
+  return (Nothing,[("ys",(Just ys,[]))]);
 }
 }
