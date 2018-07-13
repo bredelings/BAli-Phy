7bbd0bf9068a82c5638220d0d36b94f02fe007cb
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-13 12:49:46 -0400

    Fix/update Demo5.hs

--- examples/models/Demo5.hs
+++ examples/models/Demo5.hs
@@ -4,7 +4,7 @@ import Distributions;
 
 random_walk x0 0 _ = return [x0];
 random_walk x0 n f = do {
-                           x1 <- f x0;
+                           x1 <- sample $ f x0;
                            xs <- random_walk x1 (n-1) f;
                            return (x0:xs);
                         };
@@ -12,8 +12,8 @@ random_walk x0 n f = do {
 main = do
 {
   zs <- random_walk 0.0 10 (\mu -> normal mu 1.0);
-  Log "zs" zs;
 
   Observe 2.0 (normal (zs!!10) 1.0);
+  return (Nothing,[("zs",(Just zs,[]))]);
 }
 }
