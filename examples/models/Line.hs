187ed40d1d22754ebec6b013af91f2b82c8d3ed2
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-13 13:00:13 -0400

    Fix / update Line.hs

--- examples/models/Line.hs
+++ examples/models/Line.hs
@@ -8,20 +8,21 @@ xs = read_file_as_double "xs";
 
 ys = read_file_as_double "ys";
 
-main = Prefix "Line" $ do 
+main = do
 {
-  b <- normal 0.0 1.0;
-  Log "b" b;
+  b <- sample $ normal 0.0 1.0;
 
-  m <- normal 0.0 1.0;
-  Log "m" m;
+  m <- sample $ normal 0.0 1.0;
 
-  s <- gamma 1.0 1.0;
-  Log "s" s;
+  s <- sample $ gamma 1.0 1.0;
 
   let {y_hat x = m*x + b};
 
   sequence_ [Observe y (normal y' s) | (x,y) <- zip xs ys, let {y' = y_hat x}];
+  return (Nothing,[
+           ("b",(Just b,[])),
+           ("m",(Just m,[])),
+           ("s",(Just s,[]))]);
 };
 
 }
