7edf91e23298e7dd3ab7722a9494d85fcbeef76a
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-13 11:38:02 -0400

    Fix/update the Airline.hs example.

--- examples/models/Airline.hs
+++ examples/models/Airline.hs
@@ -9,22 +9,16 @@ indices l = indices' 0.0 l;
 
 observe_list ys dists = sequence_ [Observe y dist | (y,dist) <- zip ys dists];
 
-safe_exp x = if (x < (-700.0)) then
-                exp(-700.0);
-             else if (x > 700.0) then
-                exp 700.0;
-             else
-                exp x;
-
-main = Prefix "Airline" $ do 
+main = do
 {
-  alpha <- cauchy 0.0 1.0;
-  Log "alpha" alpha;
+  alpha <- sample $ cauchy 0.0 1.0;
 
-  beta <- cauchy 0.0 1.0;
-  Log "beta" beta;
+  beta <- sample $ cauchy 0.0 1.0;
 
-  observe_list fatalities [poisson $ safe_exp(alpha + beta*i) | i <- indices fatalities]
+  observe_list fatalities [poisson $ safe_exp(alpha + beta*i) | i <- indices fatalities];
+  return (Nothing,[
+          ("alpha",(Just alpha,[])),
+          ("beta",(Just beta,[]))]);
 };
 
 }
