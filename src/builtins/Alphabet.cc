b2206ed9c9b5ee76f210885178752e5d076242e0
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-27 15:54:45 -0400

    Reindent Alphabet.cc

--- src/builtins/Alphabet.cc
+++ src/builtins/Alphabet.cc
@@ -5,30 +5,30 @@
 
 extern "C" closure builtin_function_alphabetSize(OperationArgs& Args)
 {
-  const alphabet& a = Args.evaluate(0).as_<alphabet>();
+    const alphabet& a = Args.evaluate(0).as_<alphabet>();
 
-  return {a.n_letters()};
+    return {a.n_letters()};
 }
 
 extern "C" closure builtin_function_alphabet_letters(OperationArgs& Args)
 {
-  const alphabet& a = Args.evaluate(0).as_<alphabet>();
+    const alphabet& a = Args.evaluate(0).as_<alphabet>();
 
-  auto v = new EVector;
-  for(int i=0;i<a.n_letters();i++)
-    v->push_back(String(a.letter(i)));
+    auto v = new EVector;
+    for(int i=0;i<a.n_letters();i++)
+	v->push_back(String(a.letter(i)));
   
-  return v;
+    return v;
 }
 
 extern "C" closure builtin_function_getNucleotides(OperationArgs& Args)
 {
-  return Args.evaluate(0).as_<Triplets>().getNucleotides();
+    return Args.evaluate(0).as_<Triplets>().getNucleotides();
 }
 
 extern "C" closure builtin_function_getAminoAcids(OperationArgs& Args)
 {
-  return Args.evaluate(0).as_<Codons>().getAminoAcids();
+    return Args.evaluate(0).as_<Codons>().getAminoAcids();
 }
 
 extern "C" closure builtin_function_triplets(OperationArgs& Args)
@@ -74,9 +74,9 @@ extern "C" closure builtin_function_aaWithStop(OperationArgs&)
 
 extern "C" closure builtin_function_translate(OperationArgs& Args)
 {
-  auto C = Args.evaluate(0);
-  int codon = Args.evaluate(1).as_int();
+    auto C = Args.evaluate(0);
+    int codon = Args.evaluate(1).as_int();
 
-  return {C.as_<Codons>().translate(codon)};
+    return {C.as_<Codons>().translate(codon)};
 }
 
