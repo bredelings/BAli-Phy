52df9b622a8a023161cee5d94f5b9369a50e32a0
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-30 20:44:11 -0400

    Update x3.cpp and rename to x3.cc.

--- src/parser/x3.cc
+++ src/parser/x3.cc
@@ -4,6 +4,7 @@
 #include <boost/spirit/home/x3.hpp>
 #include <boost/fusion/adapted/std_pair.hpp>
 #include <boost/fusion/adapted/std_tuple.hpp>
+#include "../util/ptree.H"
 
 namespace x3 = boost::spirit::x3;
 using std::string;
@@ -11,6 +12,7 @@ using std::pair;
 
 bool parse(const string& s)
 {
+    using x3::int_;
     using x3::double_;
     using x3::char_;
     using boost::spirit::x3::ascii::space;
@@ -25,18 +27,42 @@ bool parse(const string& s)
     auto varid = x3::rule<class varid,string>()
 	= small >> *(small | large | digit | char_("'"));
 
-    auto conid = x3::rule<class varid,string>()
+    auto conid = x3::rule<class conid,string>()
 	= large >> *(small | large | digit | char_("'"));
-    auto modid = x3::rule<class varid,string>()
+    auto modid = x3::rule<class modid,string>()
 	= +(conid>>char_("."));
 
-    auto QVarID = x3::rule<class varid,string>()
+    auto QVarID = x3::rule<class QVarID,string>()
 	= modid >> varid;
     string x;
     
     auto iter = s.begin();
     bool ok = x3::parse(iter, s.end(), QVarID, x);
 
+
+    auto string_literal = x3::rule<class string_literal,string>()
+	= '"' >> x3::lexeme[ *(~char_('"')|char_('\\')>>char_)] >> '"';
+
+    auto char_literal = x3::rule<class char_literal,char>()
+	= '\'' >> char_ >> '\'';
+
+    auto literal = int_ | double_ | string_literal | char_literal;
+    
+
+    x3::rule<class term, ptree> term = "term";
+
+    // term = | submodels       (term + term + term)
+    //        | function_call   (QVarId[term,term,term])
+    //        | double
+    //        | int
+    //        | char
+    //        | string
+    //        | tuple           (term,term,term)
+    //        | list            [term,term,term]
+    //        | parenthesized   (term)
+    //        | let_exp         (let {x = E, y= F,..} in G)
+    //        | lambda_exp      ( \varid -> term )
+    
     if (ok and iter == s.end())
     {
 	std::cout<<"read: "<<x<<"\n";
