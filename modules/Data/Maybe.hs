4c5a2a89c296f1e13effdb5145989d4dab0ad142
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-08-15 17:12:57 -0400

    Add more content to Data.Maybe.hs

---
+++ modules/Data/Maybe.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Data.Maybe where
+{
+import Data.Bool;
+import Data.List;
+
+data Maybe a = Just a | Nothing;
+
+maybe n _ Nothing  = n;
+maybe _ f (Just x) = f x;
+
+isJust Nothing = False;
+isJust _       = True;
+
+isNothing Nothing = True;
+isNothing _       = False;
+
+fromJust (Just x) = x;
+-- fromJust Nothing  = error "Maybe.fromJust: Nothing";
+
+fromMaybe d x = case x of {Nothing -> d; Just v -> v};
+
+maybeToList Nothing = [];
+maybeToList (Just x) = [x];
+
+listToMaybe [] = Nothing;
+listToMaybe (x:_) = Just x;
+-- listToMaybe = foldr (const . Just) Nothing;
+-- GHC uses this to fused via the foldr/build rule.
+
+-- catMaybes ls = [ x | Just x <- ls];
+
+mapMaybes _     [] = [];
+mapMaybes f (x:xs) = let {rs = mapMaybes f xs} in
+                     case f x of {Nothing -> rs;
+                                  Just r  -> r:rs};
+}
