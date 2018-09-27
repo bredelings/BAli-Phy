a803015917ec34f3e4fb16b8e59656a2d96733ee
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-09-26 21:12:09 -0400

    Implement push/pop_error_message( ).

--- src/parser/driver.cc
+++ src/parser/driver.cc
@@ -1,8 +1,11 @@
 #include "driver.hh"
 #include "parser.hh"
+#include "myexception.H"
 
 void driver::pop_context()
 {
+    if (contexts.empty())
+	throw myexception()<<"Trying to pop empty context!";
     contexts.pop_back();
 }
 
@@ -37,6 +40,20 @@ LayoutContext driver::get_offside(const yy::parser::location_type& loc)
 	return {1,false};
 }
 
+void driver::push_error_message(const std::pair<location_type,std::string>& e)
+{
+    std::cerr<<"Pushing error message '"<<e.second<<"' at "<<e.first<<"\n";
+    error_messages.push_back(e);
+}
+
+void driver::pop_error_message()
+{
+    std::cerr<<"Popping error message\n";
+    if (error_messages.empty())
+	throw myexception()<<"No message to pop!";
+    error_messages.pop_back();
+}
+
 driver::driver ()
     : trace_parsing (false), trace_scanning (false)
 {
@@ -52,6 +69,10 @@ driver::parse (const std::string &f)
   parser.set_debug_level (trace_parsing);
   int res = parser.parse ();
   scan_end ();
+  for(auto& e: error_messages)
+  {
+      std::cerr << e.first << ": " << e.second << '\n';
+  }
   return res;
 }
 
