bc8155ddbb0a0c67aa48525311bb72722243f371
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-02 16:00:44 -0400

    Rewrite list.H

--- src/computation/expression/list.cc
+++ src/computation/expression/list.cc
@@ -8,12 +8,17 @@ using std::string;
 
 expression_ref List() {return constructor("[]",0);}
 
+expression_ref cons(const expression_ref& head,const expression_ref& tail)
+{
+    return  constructor(":",2)+head+tail;
+}
+
 template<> expression_ref get_list<>(const vector<expression_ref>& v)
 {
     expression_ref E = List();
 
     for(int i=v.size()-1;i>=0;i--)
-	E = constructor(":",2)+v[i]+E;
+	E = cons(v[i],E);
 
     return E;
 }
