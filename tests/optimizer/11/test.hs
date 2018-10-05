51986eb10858d9dbb6e69fd177d93a4bd880d809
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-04 20:56:42 -0400

    Remove {;}

--- tests/optimizer/11/test.hs
+++ tests/optimizer/11/test.hs
@@ -1,6 +1,5 @@
-module Test where {
-f r x = if null x then r else tail x;
-g x r = if null x then r else tail x;
-h x y = fst (if x then (1,y) else (y,3));
-q y x = fst (if x then (1,y) else (y,3));
-}
+module Test where
+f r x = if null x then r else tail x
+g x r = if null x then r else tail x
+h x y = fst (if x then (1,y) else (y,3))
+q y x = fst (if x then (1,y) else (y,3))
