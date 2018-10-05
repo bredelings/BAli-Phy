f90a91e8a491d01269526861a321b2f1f19143c9
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-05 16:48:59 -0400

    Fix typo added to Distributions.hs

--- modules/Distributions.hs
+++ modules/Distributions.hs
@@ -233,7 +233,7 @@ extendDiscreteDistribution d p x = mix [p, 1.0-p] [certainly x, d]
 
 average l = foldl' (\x y->(x+(fst y)*(snd y))) 0.0 l
 
-uniformGrid n = [( 1.0/n', (2.0*i'+1.0)/(2.0*n') ) | i <- take n [0..], let n' = intToDouble, let i'=intToDouble i]
+uniformGrid n = [( 1.0/n', (2.0*i'+1.0)/(2.0*n') ) | i <- take n [0..], let n' = intToDouble n, let i'=intToDouble i]
 
 uniformDiscretize dist n = fmap2 (quantile dist) (uniformGrid n)
 
