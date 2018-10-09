1367f34f508fcb40f6d34b846553082181059f02
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-09 08:58:29 -0400

    Add a desugar_state to correctly get fresh variables.

--- src/parser/desugar.cc
+++ src/parser/desugar.cc
@@ -46,6 +46,24 @@ int max_index(const expression_ref& x)
     return index;
 }
 
+struct desugar_state
+{
+    const Module& m;
+
+    int var_index;
+
+    var get_fresh_wildcard() { return var(-var_index++);}
+    var get_fresh_var() { return var(var_index++);}
+    var get_fresh_var(const string& name) {return var(name,var_index++);}
+
+    vector<expression_ref> parse_fundecls(const vector<expression_ref>& v);
+    expression_ref desugar(const expression_ref& E);
+
+    desugar_state(const Module& m_)
+	:m(m_),var_index(1+max_index(m.topdecls))
+    {}
+};
+
 bool is_irrefutable_pat(const expression_ref& E)
 {
     return E.head().is_a<var>();
@@ -69,7 +87,7 @@ expression_ref get_body(const expression_ref& decl)
     return rhs.sub()[0];
 }
 
-vector<expression_ref> parse_fundecls(const vector<expression_ref>& v)
+vector<expression_ref> desugar_state::parse_fundecls(const vector<expression_ref>& v)
 {
     // Now we go through and translate groups of FunDecls.
     vector<expression_ref> decls;
@@ -89,6 +107,7 @@ vector<expression_ref> parse_fundecls(const vector<expression_ref>& v)
 	// If its just a variable with no args, don't call def_function because ... its complicated?
 	if (lhs.is_a<var>())
 	{
+	    assert(is_AST(decl.sub()[1],"rhs"));
 	    decls.push_back(decl.head() + lhs + decl.sub()[1].sub()[0]);
 	    continue;
 	}
@@ -104,7 +123,7 @@ vector<expression_ref> parse_fundecls(const vector<expression_ref>& v)
 
 	vector<vector<expression_ref> > patterns;
 	vector<expression_ref> bodies;
-	string name = f.as_<var>().name;
+	auto fvar = f.as_<var>();
 	patterns.push_back( get_patterns(decl) );
 	bodies.push_back( get_body(decl) );
 
@@ -115,15 +134,15 @@ vector<expression_ref> parse_fundecls(const vector<expression_ref>& v)
 	    auto& j_f   = j_lhs.head();
 	    if (j_f.is_a<constructor>()) break;
 
-	    if (j_f.as_<var>().name != name) break;
+	    if (j_f.as_<var>() != fvar) break;
 
 	    patterns.push_back( get_patterns(v[j]) );
 	    bodies.push_back( get_body(v[j]) );
 
 	    if (patterns.back().size() != patterns.front().size())
-		throw myexception()<<"Function '"<<name<<"' has different numbers of arguments!";
+		throw myexception()<<"Function '"<<fvar<<"' has different numbers of arguments!";
 	}
-	decls.push_back(AST_node("Decl") + var(name) + def_function(patterns,bodies) );
+	decls.push_back(AST_node("Decl") + fvar + def_function(patterns,bodies) );
 
 	// skip the other bindings for this function
 	i += (patterns.size()-1);
@@ -131,12 +150,7 @@ vector<expression_ref> parse_fundecls(const vector<expression_ref>& v)
     return decls;
 }
 
-expression_ref get_fresh_id(const string& s, const expression_ref& /* E */)
-{
-    return var(s);
-}
-
-expression_ref desugar(const Module& m, const expression_ref& E)
+expression_ref desugar_state::desugar(const expression_ref& E)
 {
     vector<expression_ref> v;
     if (E.is_expression())
@@ -151,7 +165,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	{
 	    // translate each individual decl
 	    for(auto& e: v)
-		e = desugar(m, e);
+		e = desugar(e);
 
 	    // Convert fundecls to normal decls
 	    vector<expression_ref> decls = parse_fundecls(v);
@@ -174,7 +188,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 
             // Replace bound vars in (a) the patterns and (b) the body
 	    for(auto& e: v)
-		e = desugar(m, e);
+		e = desugar(e);
 
 	    return expression_ref{E.head(),v};
 	}
@@ -186,7 +200,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 		assert(is_AST(decls,"Decls"));
 		expression_ref E2 = AST_node("Let") + decls + E.sub()[0];
 		E2 = AST_node("rhs") + E2;
-		return desugar(m,E2);
+		return desugar(E2);
 	    }
 	    else
 	    { }      // Fall through and let the standard case handle this.
@@ -234,7 +248,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 		    else
 		    {
 			// Problem: "ok" needs to be a fresh variable.
-			expression_ref ok = get_fresh_id("ok",E);
+			expression_ref ok = get_fresh_var("ok");
 			expression_ref lhs1 = ok + p;
 			expression_ref rhs1 = AST_node("rhs") + E2;
 			expression_ref decl1 = AST_node("Decl") + lhs1 + rhs1;
@@ -254,7 +268,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 		else
 		    std::abort();
 	    }
-	    return desugar(m,E2);
+	    return desugar(E2);
 	}
 	else if (n.type == "Lambda")
 	{
@@ -265,7 +279,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	    v.pop_back();
 
 	    // 2. Desugar the body, binding vars mentioned in the lambda patterns.
-	    body = desugar(m, body);
+	    body = desugar(body);
 
 	    return def_function({v},{body}); 
 	}
@@ -283,7 +297,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	    if (stmts.size() == 1) {
 		auto& stmt = stmts.front();
 		assert(is_AST(stmt, "SimpleQual"));
-		return desugar(m,stmt.sub()[0]);
+		return desugar(stmt.sub()[0]);
 	    }
 
 	    auto first = stmts[0];
@@ -313,7 +327,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 		}
 		else
 		{
-		    expression_ref ok = get_fresh_id("ok",E);
+		    expression_ref ok = get_fresh_var("ok");
 		    expression_ref lhs1 = ok + p;
 		    expression_ref rhs1 = AST_node("rhs") + do_stmts;
 		    expression_ref decl1 = AST_node("Decl") + lhs1 + rhs1;
@@ -341,40 +355,23 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	    else
 		std::abort();
 
-	    return desugar(m,result);
+	    return desugar(result);
 	}
 	else if (n.type == "If")
 	{
 	    for(auto& e: v)
-		e = desugar(m, e);
+		e = desugar(e);
 
 	    return case_expression(v[0],true,v[1],v[2]);
 	}
 	else if (n.type == "LeftSection")
 	{
-	    // FIXME... the infixexp needs to parse the same as if it was parenthesized.
-	    // FIXME... probably we need to do a disambiguation on the infix expression. (infixexp op x)
-	    std::set<var> free_vars;
-	    for(auto& e: v) {
-		e = desugar(m, e);
-		add(free_vars, get_free_indices(e));
-	    }
 	    return apply_expression(v[1],v[0]);
 	}
 	else if (n.type == "RightSection")
 	{
-	    // FIXME... probably we need to do a disambiguation on the infix expression. (x op infixexp)
-	    // FIXME... the infixexp needs to parse the same as if it was parenthesized.
-	    std::set<var> free_vars;
-	    for(auto& e: v) {
-		e = desugar(m, e);
-		add(free_vars, get_free_indices(e));
-	    }
-	    int safe_var_index = 0;
-	    if (not free_vars.empty())
-		safe_var_index = max_index(free_vars)+1;
-	    var vsafe(safe_var_index);
-	    return lambda_quantify(vsafe,apply_expression(apply_expression(v[0],vsafe),v[1]));
+	    auto x = get_fresh_var();
+	    return lambda_quantify(x,apply_expression(apply_expression(v[0],x),v[1]));
 	}
 	else if (n.type == "Let")
 	{
@@ -408,36 +405,36 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	    }
 
 	    // parse the decls and bind declared names internally to the decls.
-	    v[0] = desugar(m, v[0]);
+	    v[0] = desugar(v[0]);
 
 	    vector<pair<var,expression_ref>> decls;
 
-	    // find the bound var names + construct arguments to let_obj()
+	    // find the bound vars + construct arguments to let_obj()
 	    for(const auto& decl: v[0].sub())
 	    {
 		if (is_AST(decl,"EmptyDecl")) continue;
 
-		var x = decl.sub()[0].as_<var>().name;
+		var x = decl.sub()[0].as_<var>();
 		auto E = decl.sub()[1];
 
 		decls.push_back({x,E});
 	    }
 
 	    // finally desugar let-body, now that we know the bound vars.
-	    body = desugar(m, body);
+	    body = desugar(body);
 
 	    // construct the new let expression.
 	    return let_expression(decls, body);
 	}
 	else if (n.type == "Case")
 	{
-	    expression_ref case_obj = desugar(m, v[0]);
+	    expression_ref case_obj = desugar(v[0]);
 	    vector<expression_ref> alts = v[1].sub();
 	    vector<expression_ref> patterns;
 	    vector<expression_ref> bodies;
 	    for(const auto& alt: alts)
 	    {
-		patterns.push_back(desugar(m, alt.sub()[0]) );
+		patterns.push_back(desugar(alt.sub()[0]) );
 
 		// Handle where-clause.
 		assert(alt.size() == 2 or alt.size() == 3);
@@ -452,7 +449,7 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 		    body = AST_node("Let") + alt.sub()[2] + body;
 		}
 
-		bodies.push_back(desugar(m, body) );
+		bodies.push_back(desugar(body) );
 	    }
 	    return case_expression(case_obj, patterns, bodies);
 	}
@@ -461,23 +458,29 @@ expression_ref desugar(const Module& m, const expression_ref& E)
 	    expression_ref E2 = var("Prelude.enumFrom");
 	    for(auto& e: v)
 		E2 = {E2, e};
-	    return desugar(m, E2);
+	    return desugar(E2);
 	}
 	else if (n.type == "enumFromTo")
 	{
 	    expression_ref E2 = var("Prelude.enumFromTo");
 	    for(auto& e: v)
 		E2 = {E2, e};
-	    return desugar(m, E2);
+	    return desugar(E2);
 	}
     }
 
     for(auto& e: v)
-	e = desugar(m, e);
+	e = desugar(e);
     if (E.size())
 	return expression_ref{E.head(),v};
     else
 	return E;
+}
+
+expression_ref desugar(const Module& m, const expression_ref& E)
+{
+    desugar_state ds(m);
+    return ds.desugar(E);
 }
 
 bool is_all_space(const string& line)
