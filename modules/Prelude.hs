92eb27a4e4d3ee40e4d8c7776063955354b8faa4
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-15 17:20:28 -0400

    Fix slow_list_to_vector l in Prelude

--- modules/Prelude.hs
+++ modules/Prelude.hs
@@ -107,7 +107,8 @@ copy_list_to_vector (h:t) v i = do set_vector_index v i h
                                    copy_list_to_vector t v (i+1)
 
 slow_list_to_vector l = runST $ do v <- new_vector (length l)
-                                   copy_list_to_vector l v 0 return v
+                                   copy_list_to_vector l v 0
+                                   return v
 
 list_to_vector = builtin_vector_from_list
 
