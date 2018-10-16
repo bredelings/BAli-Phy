c5e67d90e0b025c144f9b82c547a013b0f33a4f4
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-15 23:45:18 -0400

    Rearrange optimizer/14 test.

--- tests/optimizer/14/test.hs
+++ tests/optimizer/14/test.hs
@@ -1,9 +1,13 @@
-module Test where {
 -- straightforward code for
 -- f (x:xs) (y:ys) = xs++ys
 -- f (x:xs) []     = [x]
 -- f _      _      = error "f"
 f a b = let {fail1 = let {fail2 = error "f"} in case a of {[]->fail2; x:xs-> case b of {[]->[x]; y:ys -> fail2}}} in
-        case a of {_ -> fail1; x:xs  -> case b of {_ -> fail1; y:ys -> xs++ys}};
+        case a of {x:xs  -> case b of {y:ys -> xs++ys;_ -> fail1};_ -> fail1};
 
-}
+f2 (x:xs) (y:ys) = xs++ys
+f2 (x:xs) []     = [x]
+f2 _      _      = error "f"
+
+f3 (x:xs) (y:ys) = xs++ys
+f3 (x:xs) []     = [x]
