288cef7a8161b03f9d9558095f9e2a8153e88533
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-30 07:37:39 -0400

    Add test of lazy sampling.

---
+++ tests/prob_prog/lazy_sample/sample.hs
@@ -0,0 +1,10 @@
+import Distributions
+
+model = do
+  x <- sample $ normal 0.0 1.0
+  ys <- Lazy $ sample $ list (repeat $ normal 0.0 1.0)
+  return $ (x*x):(take 10 ys)
+
+main = do
+  y <- run_random (error "No alphabet!") False model
+  putStrLn $ show y
