51147d0313ca0722807cacae57c4e7128094ba1c
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-11-04 22:27:57 -0500

    Try and fix windows compilation.

--- src/parser/lexer.cc
+++ src/parser/lexer.cc
@@ -2304,7 +2304,7 @@ static void yy_load_buffer_state  (void)
 
 /* %if-c-only */
 
-        b->yy_is_interactive = file ? (isatty( fileno(file) ) > 0) : 0;
+        b->yy_is_interactive = 0;
     
 /* %endif */
 /* %if-c++-only */
