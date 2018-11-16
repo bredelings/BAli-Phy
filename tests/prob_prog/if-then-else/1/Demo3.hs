4f6ebb70d35a8400ab622a19437b4566e6b1ad89
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-11-15 20:42:52 -0500

    Reformat test.

--- tests/prob_prog/if-then-else/1/Demo3.hs
+++ tests/prob_prog/if-then-else/1/Demo3.hs
@@ -1,15 +1,11 @@
 module Demo3 where
-{
-import Distributions;
+
+import Distributions
 
 main = do
-{
-  i <- sample $ bernoulli 0.5;
-  y <- sample $ normal 0.0 1.0;
-  z <- sample $ exponential 0.1;
-  let {x = if (i==1) then y else z};
--- How are we going to log things from stochastic procedures?
--- One way is to return a pair of (value, loggers).
-  return $ (Nothing,[("x",(Just x,[]))]);
-}
-}
+  i <- sample $ bernoulli 0.5
+  y <- sample $ normal 0.0 1.0
+  z <- sample $ exponential 0.1
+  let x = if i==1 then y else z
+  return $ log_all [x %% "x"]
+
