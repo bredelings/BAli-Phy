6d953d8b68a3ca31894ff2b2e529695e742de6cb
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-11-16 11:46:20 -0500

    Add two more PPL tests.

---
+++ tests/prob_prog/regression/LinearRegression.hs
@@ -0,0 +1,23 @@
+module Line where
+
+import Data.ReadFile
+import Distributions
+import System.Environment
+
+xs = read_file_as_double "xs"
+
+ys = read_file_as_double "ys"
+
+main = do
+
+  b <- sample $ normal 0.0 1.0
+
+  a <- sample $ normal 0.0 1.0
+
+  s <- sample $ exponential 1.0
+
+  let f x = b*x + a
+
+  sequence_ [observe y (normal mu_y s) | (x,y) <- zip xs ys, let mu_y = f x]
+
+  return $ log_all [b %% "b", a %% "a", s %% "s"]
