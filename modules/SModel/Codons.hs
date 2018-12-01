f0d811fde9eee14cac6dbdbd178c8f4f27bea6ca
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-01 17:26:09 -0500

    mg94_ext: put submodel argument last.

--- modules/SModel/Codons.hs
+++ modules/SModel/Codons.hs
@@ -31,9 +31,9 @@ gy94_ext  sym w pi a = gtr a (m0 a sym w) pi
 
 gy94  k w pi a = gy94_ext  sym w pi a where sym = hky85_sym k $ getNucleotides a
 
-mg94_ext a q w = dNdS w (x3 a q)
-mg94k a k pi w  = mg94_ext a (hky85 k pi nuc_a) w where nuc_a = getNucleotides a
-mg94  a   pi w  = mg94_ext a (f81     pi nuc_a) w where nuc_a = getNucleotides a
+mg94_ext a w q = q & x3 a & dNdS w
+mg94k a k pi w  = hky85 k pi nuc_a & mg94_ext a w where nuc_a = getNucleotides a
+mg94  a   pi w  = f81     pi nuc_a & mg94_ext a w where nuc_a = getNucleotides a
 
 x3x3 a (ReversibleMarkov _ _ q_1 pi_1 _ _ _) (ReversibleMarkov _ _ q_2 pi_2 _ _ _) (ReversibleMarkov _ _ q_3 pi_3 _ _ _) =
     let smap = simple_smap a
