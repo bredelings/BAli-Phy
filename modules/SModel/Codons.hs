ec84c383ef2607b1ca1d119b29f8a6eaf0331f5f
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-01 17:15:43 -0500

    Make submodel last argument of x3.

--- modules/SModel/Codons.hs
+++ modules/SModel/Codons.hs
@@ -31,18 +31,18 @@ gy94_ext  sym w pi a = gtr a (m0 a sym w) pi
 
 gy94  k w pi a = gy94_ext  sym w pi a where sym = hky85_sym k $ getNucleotides a
 
-mg94_ext a q w = dNdS (x3 q a) w
+mg94_ext a q w = dNdS (x3 a q) w
 mg94k a k pi w  = mg94_ext a (hky85 k pi nuc_a) w where nuc_a = getNucleotides a
 mg94  a   pi w  = mg94_ext a (f81     pi nuc_a) w where nuc_a = getNucleotides a
 
-x3x3 (ReversibleMarkov _ _ q_1 pi_1 _ _ _) (ReversibleMarkov _ _ q_2 pi_2 _ _ _) (ReversibleMarkov _ _ q_3 pi_3 _ _ _) a =
+x3x3 a (ReversibleMarkov _ _ q_1 pi_1 _ _ _) (ReversibleMarkov _ _ q_2 pi_2 _ _ _) (ReversibleMarkov _ _ q_3 pi_3 _ _ _) =
     let smap = simple_smap a
         q = singlet_to_triplet_rates a q_1 q_2 q_3
         pi = f3x4_frequencies_builtin a pi_1 pi_2 pi_3
     in reversible_markov a smap q pi
 
 x3_sym a s = singlet_to_triplet_rates a s s s
-x3 q a = x3x3 q q q a
+x3 a q = x3x3 a q q q
 
 -- maybe this should be t*(q %*% dNdS_matrix) in order to avoid losing scaling factors?  Probably this doesn't matter at the moment.
 dNdS (ReversibleMarkov a s q pi l t r) omega = reversible_markov a s q2 pi where q2 = q %*% dNdS_matrix a omega
