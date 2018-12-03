c78ab9abdcf4673aa8b9a1420e37fd32141a79c8
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-03 18:52:11 -0500

    Remove VectorInt functions.

--- modules/Foreign/Vector.hs
+++ modules/Foreign/Vector.hs
@@ -10,10 +10,8 @@ builtin get_vector_index 2 "get_vector_index" "Vector"
 builtin vector_size 1 "vector_size" "Vector"
 builtin sizeOfVectorUnsigned 1 "sizeOfVectorUnsigned" "Vector"
 builtin sizeOfVectorInt 1 "sizeOfVectorInt" "Vector"
-builtin getVectorIntElement 2 "getVectorIntElement" "Vector"
 builtin getStringElement 2 "getStringElement" "Vector"
 builtin sizeOfString 1 "sizeOfString" "Vector"
-builtin builtinNewVectorInt 1 "NewVectorInt" "Vector"
 builtin builtinSetVectorIndexInt 3 "SetVectorIndexInt" "Vector"
 builtin builtinNewString 1 "NewString" "Vector"
 builtin builtinSetStringIndexInt 3 "SetStringIndex" "Vector"
@@ -24,10 +22,6 @@ builtin builtinSetVectorIndexMatrix 3 "SetVectorIndexMatrix" "Vector"
 list_from_vector' v s i = if (i<s) then (get_vector_index v i):list_from_vector' v s (i+1) else []
 
 list_from_vector v = list_from_vector' v (vector_size v) 0
-
-listFromVectorInt' v s i = if (i<s) then (getVectorIntElement v i):listFromVectorInt' v s (i+1) else []
-
-listFromVectorInt v = listFromVectorInt' v (sizeOfVectorInt v) 0
 
 listFromString' v s i = if (i<s) then (getStringElement v i):listFromString' v s (i+1) else []
 
