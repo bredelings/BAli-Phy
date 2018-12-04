78e6c750c5314672cad72b2f05a011f3acee8b25
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-03 19:00:36 -0500

    Go back to using `join` instead of `seq`.

--- modules/Prelude.hs
+++ modules/Prelude.hs
@@ -97,7 +97,7 @@ unsafePerformIO (IOAction3 x y z w) = x y z w
 unsafePerformIO (IOAction4 x y z w u) = x y z w u
 unsafePerformIO (LazyIO f) = unsafePerformIO f
 unsafePerformIO (IOAndPass (LazyIO f) g) = let x = unsafePerformIO f in unsafePerformIO (g x)
-unsafePerformIO (IOAndPass f g) = let x = unsafePerformIO f in x `seq` unsafePerformIO (g x)
+unsafePerformIO (IOAndPass f g) = let x = unsafePerformIO f in x `join` unsafePerformIO (g x)
 unsafePerformIO (MFix f) = let x = unsafePerformIO (f x) in x
 unsafePerformIO (IOReturn x) = x
 
