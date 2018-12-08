4f6d6cea1a4bb71ebc5fa37b9b04daeff538597e
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-08 09:58:19 -0500

    Refactor cut-range tool.

--- src/tools/cut-range.cc
+++ src/tools/cut-range.cc
@@ -26,27 +26,38 @@
 #include "io.H"
 
 #include <boost/program_options.hpp>
+#include <boost/optional.hpp>
 
 namespace po = boost::program_options;
 using po::variables_map;
 
 using std::string;
+using std::vector;
 using std::cout;
+using boost::optional;
 
 variables_map parse_cmd_line(int argc,char* argv[]) 
 { 
     using namespace po;
 
     // named options
-    options_description all("Allowed options");
-    all.add_options()
+    options_description invisible("Invisible options");
+    invisible.add_options()
+	("filenames", value<vector<string>>(),"Filenames to read")
+	;
+
+    options_description visible("All options");
+    visible.add_options()
 	("help,h", "produce help message")
 	("key", value<string>()->default_value("iterations"),"cut based on values of <key>=value")
 	("skip",value<int>(),"the number of samples to skip")
-	("size",value<int>(),"maximum number of samples to use")
 	("until",value<int>(),"last sample to use")
+	("size",value<int>(),"maximum number of samples to use")
 	("verbose","Output more log messages on stderr.")
 	;
+
+    options_description all("All options");
+    all.add(invisible).add(visible);
 
     variables_map args;     
     store(parse_command_line(argc, argv, all), args);
@@ -61,72 +72,83 @@ variables_map parse_cmd_line(int argc,char* argv[])
 
     if (args.count("verbose")) log_verbose = 1;
 
+    if (not args.count("key")) 
+	throw myexception()<<"argument 'key' not set.";
+
     return args;
 }
 
+
+void cut_range(std::istream& in, std::ostream& out,
+	       const string& key, optional<int> min, optional<int> max)
+{
+    string pattern = key + " = ";
+
+    string line;
+
+    bool in_interval = not min;
+    while(portable_getline(in,line))
+    {
+	// look for the pattern
+	int where = line.find(pattern);
+
+	// if no pattern, then
+	if (where != -1)
+	{
+	    // move PAST the pattern
+	    where += pattern.size();
+	    double value = convertTo<double>(line.substr(where));
+	    in_interval = true;
+	    if (min and value <= *min)
+		in_interval = false;
+
+	    if (max and value > *max)
+		return;
+
+	    //    std::cerr<<line<<std::endl;
+	    //    std::cerr<<"where = "<<where<<std::endl;
+	    //    std::cerr<<value<<std::endl;
+	}
+
+	if (in_interval)
+	    out<<line<<std::endl;
+    }
+}
 
 
 
 
 // assumptions: the value of 'key' is increasing
 
-int main(int argc,char* argv[]) {
+int main(int argc,char* argv[])
+{
     try {
 
 	//---------- Parse command line  -------//
 	variables_map args = parse_cmd_line(argc,argv);
 
-	if (not args.count("key")) 
-	    throw myexception()<<"argument 'key' not set.";
+	auto key = args["key"].as<string>();
+	optional<int> min;
+	if (args.count("skip"))
+	    min = args["skip"].as<int>();
+
+	optional<int> max;
+	if (args.count("until"))
+	    max = args["until"].as<int>();
+
+	optional<int> size;
+	if (args.count("size"))
+	    size = args["size"].as<int>();
+
 	if (args.count("size") and args.count("until"))
 	    throw myexception()<<"cannot set both arguments 'size' and 'until'.";
+	else if (size)
+	    max = (min?*min:0) + *size;
 
-	bool is_min = args.count("skip");
-	double min = 0;
-	if (is_min) min = args["skip"].as<int>();
+	if (min and max and *max < *min)
+	    throw myexception()<<"error: maximum value ("<<*max<<") < minimum value ("<<*min<<")";
 
-	bool is_max = args.count("size") or args.count("until");
-	double max = min;
-	if (args.count("size"))
-	    max = min + args["size"].as<int>();
-
-	if (args.count("until"))
-	    max = std::max(max,(double)args["until"].as<int>());
-	if (is_min and is_max and max < min)
-	    throw myexception()<<"error: maximum value < minimum value";
-
-	string pattern = args["key"].as<string>() + " = ";
-
-	string line;
-
-	bool in_interval = not is_min;
-	while(portable_getline(std::cin,line)) {
-
-	    // look for the pattern
-	    int where = line.find(pattern);
-
-	    // if no pattern, then
-	    if (where != -1) {
-
-		// move PAST the pattern
-		where += pattern.size();
-		double value = convertTo<double>(line.substr(where));
-		in_interval = true;
-		if (is_min and value <= min)
-		    in_interval = false;
-		if (is_max and value > max) {
-		    in_interval = false;
-		    exit(0);
-		}
-
-		//    std::cerr<<line<<std::endl;
-		//    std::cerr<<"where = "<<where<<std::endl;
-		//    std::cerr<<value<<std::endl;
-	    }
-
-	    if (in_interval)
-		std::cout<<line<<std::endl;
-	}
+	cut_range(std::cin, std::cout, key, min, max);
     }
     catch (std::exception& e) {
 	std::cerr<<"cut-range: Error! "<<e.what()<<std::endl;
