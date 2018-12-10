2dd8b0363976767e56f1586a92c38fd2d07fab48
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-12-09 22:49:46 -0500

    Add JSON data structure and printing function.

--- modules/Distributions.hs
+++ modules/Distributions.hs
@@ -113,6 +113,19 @@ do_log prefix model = do
       create_sub_loggers prefix (snd result)
       return (fst result)
 
+
+-- Hmm... it doesn't look like we can have a JSON object, just JSON representation, because a JSON object would have to have existential type fields.
+data JSON = JSONArray [JSON] | JSONObject [(String,JSON)] | JSONDouble Double | JSONInt Int | JSONBool Bool | JSONString String | JSONNull
+
+json_to_string (JSONArray x) = "["++intercalate "," (map json_to_string x) ++ "]"
+json_to_string (JSONObject x) = "{"++ intercalate "," ["\""++key++"\": "++json_to_string value | (key,value) <- x] ++ "}"
+json_to_string (JSONDouble x) = show x
+json_to_string (JSONInt x) = show x
+json_to_string (JSONBool True) = "true"
+json_to_string (JSONBool False) = "false"
+json_to_string (JSONString x) = "\""++x++"\""
+json_to_string (JSONNull) = "null"
+
 -- Define some helper functions
 no_quantile name = error ("Distribution '"++name++"' has no quantile function")
 
