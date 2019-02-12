eab36e02e75ecaf8b82ebda5d1f914205eb1c7cf
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-02-11 23:06:19 -0500

    Add mut_sel_aa model modifier.

--- modules/SModel.hs
+++ modules/SModel.hs
@@ -179,19 +179,27 @@ mut_sel' w' q0 = mut_sel w q0 where
     w = get_ordered_elements (alphabet_letters a) w' "fitnesses"
     a = getAlphabet q0
 
+mut_sel_aa ws q@(ReversibleMarkov codon_a _ _ _ _ _ _) = mut_sel (aa_to_codon codon_a ws) q
+
+mut_sel_aa' ws' q0 = mut_sel ws q0 where
+    ws = get_ordered_elements (alphabet_letters amino_alphabet) ws' "fitnesses"
+    codon_alphabet = getAlphabet q0
+    amino_alphabet = getAminoAcids codon_alphabet
+
 fMutSel codon_a codon_w omega nuc_model = nuc_model & x3 codon_a & dNdS omega & mut_sel codon_w
 
 fMutSel' codon_a codon_ws' omega nuc_model = fMutSel codon_a codon_ws omega nuc_model
     where codon_ws = get_ordered_elements (alphabet_letters codon_a) codon_ws' "fitnesses"
 
+aa_to_codon codon_a xs = [xs_array!aa | codon <- codons, let aa = translate codon_a codon]
+    where xs_array = listArray' xs
+          codons = take n_letters [0..]
+          n_letters = alphabetSize codon_a
+
 -- \#1->let {w' = listAray' #1} in \#2 #3->fMutSel #0 codon_w #2 #3
 -- The whole body of the function is let-floated up in round 2, and w' is eliminated.
-fMutSel0 a w omega nuc_q  = fMutSel a codon_w omega nuc_q
-    where codon_w = [w'!aa| codon <- codons,let aa = translate a codon]
-          w' = listArray' w
-          codons = take n_letters [0..]
-          n_letters = alphabetSize a
---replicate n_letters (1.0/intToDouble n_letters)
+fMutSel0 codon_a aa_w omega nuc_q  = fMutSel codon_a codon_w omega nuc_q
+    where codon_w = aa_to_codon codon_a aa_w
 
 fMutSel0' codon_a amino_ws' omega nuc_model = fMutSel0 codon_a amino_ws omega nuc_model
                                                where amino_ws = get_ordered_elements (alphabet_letters amino_a) amino_ws' "fitnesses"
