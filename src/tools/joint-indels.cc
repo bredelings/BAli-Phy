89b8b0bd7c93aa009af5982c51fd6fac9bcb8150
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-03-07 10:54:26 -0500

    Allow extracting ancestral sequences for a certain node.

--- src/tools/joint-indels.cc
+++ src/tools/joint-indels.cc
@@ -25,12 +25,16 @@
 #include "alignment/alignment.H"
 
 #include "util/string/split.H"
-
+#include "tree/tree-util.H"
 #include "alignment/alignment-util.H"
 #include "distance-methods.H"
 #include "joint-A-T.H"
 #include "partition.H"
 #include "dp/A2_states.H"
+#include <optional>
+#include <map>
+
+#include <boost/dynamic_bitset.hpp>
 
 extern int log_verbose;
 
@@ -42,13 +46,120 @@ using std::ifstream;
 using std::vector;
 using std::string;
 using std::endl;
+using std::optional;
+using std::map;
+using boost::dynamic_bitset;
 //using namespace optimize;
 
 namespace po = boost::program_options;
 using po::variables_map;
 using namespace A2;
 
-void run_analysis(const variables_map& args) {
+// OK, what does a complete conflict analysis look like?
+// For summaries, we want to map branches in the sampled tree
+//  to either branches or nodes of the consensus tree.
+// A branch can then map to a branch (aligned), a node (extends), or
+//  nothing (conflict)
+
+// For extracting sequences corresponding to a node, we want to
+// map nodes on the query tree to nodes in the sampled tree.
+
+map<dynamic_bitset<>,int> get_splits(const Tree& T)
+{
+    map<dynamic_bitset<>,int> split_to_branch;
+
+    for(int b=0;b<2*T.n_branches();b++)
+    {
+	auto s = branch_partition(T,b);
+	split_to_branch.insert({s,b});
+	int b2 = T.directed_branch(b).reverse();
+	split_to_branch.insert({~s,b2});
+    }
+    return split_to_branch;
+}
+
+
+vector<optional<int>> get_partial_branches_map(const Tree& Q, const Tree& T)
+{
+    auto Q_splits = get_splits(Q);
+    auto T_splits = get_splits(T);
+
+    vector<optional<int>> Q_to_T_branches(2*Q.n_branches());
+
+    for(int b=0;b<Q_to_T_branches.size();b++)
+    {
+	auto qs = branch_partition(Q,b);
+	if (T_splits.count(qs))
+	    Q_to_T_branches[b] = T_splits[qs];
+    }
+    return Q_to_T_branches;
+}
+
+
+optional<int> get_corresponding_node(int q_node, const Tree& Q, const Tree& T, const vector<optional<int>>& Q_to_T_branches)
+{
+    optional<int> t_node;
+    for(auto q_branch=Q.node(q_node).branches_out(); q_branch; q_branch++)
+    {
+	auto t_branch = Q_to_T_branches[*q_branch];
+	if (not t_branch) return {};
+
+	int t_node2 = (int)T.directed_branch(*t_branch).source();
+	if (not t_node)
+	    t_node = t_node2;
+	else if (t_node2 != *t_node)
+	    return {};
+    }
+    return *t_node;
+}
+
+
+vector<optional<int>> get_nodes_map(const Tree& Q, const Tree& T)
+{
+    // For trees with no branches, we can't use branch correspondence.
+    if (Q.n_nodes() == 1) return { { 0 } };
+
+    vector<optional<int>> Q_to_T_nodes(Q.n_nodes());
+
+    auto Q_to_T_branches = get_partial_branches_map(Q,T);
+
+    for(int q_node=0; q_node<Q.n_nodes(); q_node++)
+	Q_to_T_nodes[q_node] = get_corresponding_node(q_node, Q, T, Q_to_T_branches);
+
+    return Q_to_T_nodes;
+}
+
+void extract_sequence(const variables_map& args, const joint_A_T& J)
+{
+    auto Q = load_tree_from_file(args["extract-sequences"].as<string>());
+    remap_T_leaf_indices(Q, J.T[0].get_leaf_labels());
+    for(int i=0; i<J.size();i++)
+    {
+	auto& A = J.A[i];
+	auto& T = J.T[i];
+	auto Q_to_T_nodes = get_nodes_map(Q,T);
+
+	for(int q_node=Q.n_leaves(); q_node < Q.n_nodes(); q_node++)
+	{
+	    auto t_node = Q_to_T_nodes[q_node];
+	    if (not t_node) continue;
+
+	    string q_node_name = Q.get_label(q_node);
+	    if (q_node_name.empty()) continue;
+
+	    string t_node_name = T.get_label(*t_node);
+	    std::cerr<<"Mapped "<<q_node_name<<" -> "<<t_node_name<<"\n";
+	    std::cout<<">"<<q_node_name<<"\n";
+	    auto& a = A.get_alphabet();
+	    for(int i=0;i<A.length();i++)
+		std::cout<<a.lookup(A(i,*t_node));
+	    std::cout<<"\n";
+	}
+	std::cout<<"\n\n";
+    }
+}
+
+void run_analysis(const variables_map& args, const joint_A_T& J) {
 
     // Handle Partition name
     if (not args.count("partition"))
@@ -56,14 +167,6 @@ void run_analysis(const variables_map& args) {
     vector<string> pnames = split(args["partition"].as<string>(),':');
 
     //--------------------- Load (A,T) ------------------------//
-    if (log_verbose) cerr<<"joint-indels: Loading alignments and trees...\n";
-    joint_A_T J = get_joint_A_T(args,true);
-
-    if (J.size() == 0) 
-	throw myexception()<<"No (A,T) read in!";
-    else
-	if (log_verbose) cerr<<"joint-indels: Loaded "<<J.size()<<" (A,T) pairs."<<endl;;
-
     std::cout << "Iter\tPart\tLen\tIndels" << endl;
   
     int consistentsamples = 0;
@@ -146,6 +249,7 @@ variables_map parse_cmd_line(int argc,char* argv[])
 	("partition", value<string>(), "find indels along internal branch that bi-partitions given taxa (<taxa1>:<taxa2>:...)")
 	("alphabet",value<string>(),"set to 'Codons' to prefer codon alphabets")
 	("verbose","Output more log messages on stderr.")
+	("extract-sequences",value<string>(),"Extract sequences corresponding to tree")
 	;
 
     options_description all("All options");
@@ -179,7 +283,19 @@ int main(int argc,char* argv[]) {
 	//Arguments args;
 	//args.read(argc,argv);
 	//args.print(cerr);
-	run_analysis(args);
+
+	if (log_verbose) cerr<<"joint-indels: Loading alignments and trees...\n";
+	joint_A_T J = get_joint_A_T(args,true);
+
+	if (J.size() == 0)
+	    throw myexception()<<"No (A,T) read in!";
+	else
+	    if (log_verbose) cerr<<"joint-indels: Loaded "<<J.size()<<" (A,T) pairs."<<endl;;
+
+	if (args.count("extract-sequences"))
+	    extract_sequence(args,J);
+	else
+	    run_analysis(args,J);
     }
     catch (std::exception& e) {
 	cerr<<"joint-indels: Error! "<<e.what()<<endl;
