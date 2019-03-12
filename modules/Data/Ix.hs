e5ac003c646e8bedf165ccba00e8b37e8968a2c4
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-03-12 08:20:09 -0400

    Fix Ix module.

--- modules/Data/Ix.hs
+++ modules/Data/Ix.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Data.Ix where
 
+import Compiler.Num
+import Data.Ord
+import Data.Bool
+
 range     (start,end) = [start..end]
 index     (start,end) val = val - start
-inRange   (start,end) val = start <= val && val <= end
+inRange   (start,end) val = (start <= val) && (val <= end)
 rangeSize (start,end) = end - start + 1
