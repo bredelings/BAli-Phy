00a0726c80688ed6e6c452d717bbe7d249c086b8
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-03-26 10:17:21 -0400

    Make extract-ancestors work with groups.

--- src/tools/extract-ancestors.cc
+++ src/tools/extract-ancestors.cc
@@ -23,7 +23,11 @@
 #include <vector>
 #include "util/myexception.H"
 #include "util/log-level.H"
+#include "util/string/strip.H"
+#include "util/string/split.H"
+#include "util/io.H"
 #include "alignment/alignment.H"
+#include "partition.H"
 
 #include "util/string/split.H"
 #include "tree/tree-util.H"
@@ -36,10 +40,12 @@
 #include <map>
 
 #include <boost/dynamic_bitset.hpp>
+#include "range/v3/all.hpp"
 
 using std::cin;
 using std::cout;
 using std::cerr;
+using std::pair;
 using std::istream;
 using std::ifstream;
 using std::vector;
@@ -95,6 +101,8 @@ vector<optional<int>> get_partial_branches_map(const Tree& Q, const Tree& T)
 }
 
 
+// Check that all corresponding branches in T point to a single node in T.
+// This should rule out cases where the Q node is a polytomy, but T is bifurcating.
 optional<int> get_corresponding_node(int q_node, const Tree& Q, const Tree& T, const vector<optional<int>>& Q_to_T_branches)
 {
     optional<int> t_node;
@@ -128,57 +136,177 @@ vector<optional<int>> get_nodes_map(const Tree& Q, const Tree& T)
     return Q_to_T_nodes;
 }
 
+vector<pair<string,dynamic_bitset<>>> load_groups_from_file(const string& filename, const vector<string>& leaf_names)
+{
+    namespace view = ranges::view;
+    vector<pair<string,dynamic_bitset<>>> groups;
+
+    istream_or_ifstream groups_file(std::cin,"-",filename,"groups file");
+
+    for(auto& line_: read_lines(groups_file))
+    {
+        auto line = lstrip(rstrip(line_," \t")," \t");
+        if (not line.size()) continue;
+        if (line[0] == '#') continue;
+
+        auto words = resplit(line,R"([ \t]+)");
+        if (words.size() < 3 or words[1] != "=")
+        {
+            throw myexception()<<"In groups file '"<<filename<<"':\n  Badly formed line:\n    |"<<line_;
+        }
+
+        auto name = words[0];
+        auto taxon_names = words | view::drop(2);
+        dynamic_bitset<> group = group_from_names(leaf_names, taxon_names);
+        groups.push_back({name,group});
+    }
+
+    return groups;
+}
+
+map<int,int> find_and_name_nodes(const SequenceTree& T,const vector<pair<string,dynamic_bitset<>>>& groups)
+{
+    // 1. Make something we can use to quickly look up groups
+    map<dynamic_bitset<>,int> partitions;
+    for(int b=0;b<T.n_branches()*2;b++)
+        partitions.insert({branch_partition(T,b),b});
+
+    // 2. Look up each group
+    map<int,int> node_map;
+    for(int g=0;g<groups.size();g++)
+    {
+        auto& [name,group] = groups[g];
+        auto it = partitions.find(group);
+        if (it != partitions.end())
+        {
+            int b = it->second;
+            int node = T.directed_branch(b).target();
+            node_map.insert({g,node});
+        }
+    }
+    return node_map;
+}
+
+map<int,int> find_and_name_nodes(const SequenceTree& T,const SequenceTree& Q)
+{
+    map<int,int> node_map;
+    auto Q_to_T_nodes = get_nodes_map(Q,T);
+
+    if (log_verbose > 1)
+    {
+        std::cerr<<Q.write(false)<<"\n";
+        std::cerr<<T.write(false)<<"\n";
+    }
+
+    for(int q_node=Q.n_leaves(); q_node < Q.n_nodes(); q_node++)
+    {
+        string q_node_name = Q.get_label(q_node);
+        if (q_node_name.empty()) continue;
+
+        auto t_node = Q_to_T_nodes[q_node];
+        if (not t_node) continue;
+
+        string t_node_name = T.get_label(*t_node);
+        if (log_verbose > 0)
+            std::cerr<<"Mapped "<<q_node_name<<" -> "<<t_node_name<<"\n";
+        node_map.insert({q_node,*t_node});
+    }
+
+    return node_map;
+}
+
 void extract_sequence(const variables_map& args, const joint_A_T& J)
 {
     bool show_leaves = args["show-leaves"].as<bool>();
 
     bool require_all_nodes = args["all-nodes"].as<bool>();
 
-    auto Q = load_tree_from_file(args["ancestors"].as<string>());
+    vector<int> node_counts;
+    optional<SequenceTree> Q;
+    if (args.count("nodes"))
+    {
+        Q = load_tree_from_file(args["nodes"].as<string>());
+        remap_T_leaf_indices(*Q, J.leaf_names());
+        node_counts.resize(Q->n_nodes());
+    }
 
-    remap_T_leaf_indices(Q, J.leaf_names());
+    vector<int> group_counts;
+    optional<vector<pair<string,dynamic_bitset<>>>> groups;
+    if (args.count("groups"))
+    {
+        groups = load_groups_from_file(args["groups"].as<string>(), J.leaf_names());
+        group_counts.resize(groups->size());
+    }
 
-    vector<int> node_counts(Q.n_nodes());
+
 
     for(int i=0; i<J.size();i++)
     {
 	auto& A = J.A[i];
 	auto& T = J.T[i];
-	auto Q_to_T_nodes = get_nodes_map(Q,T);
 
-	if (log_verbose > 1)
-	{
-	    std::cerr<<Q.write(false)<<"\n";
-	    std::cerr<<T.write(false)<<"\n";
-	}
-	int q_node0 = (show_leaves)?0:Q.n_leaves();
-	for(int q_node=q_node0; q_node < Q.n_nodes(); q_node++)
-	{
-	    auto t_node = Q_to_T_nodes[q_node];
-	    if (not t_node) continue;
+        map<string,int> internal_nodes;
 
-	    string q_node_name = Q.get_label(q_node);
-	    if (q_node_name.empty()) continue;
+        // Add ancestral nodes from the tree.
+        if (Q)
+        {
+            for(auto& [q_node,t_node]: find_and_name_nodes(T, *Q))
+            {
+                node_counts[q_node]++;
+                auto name = Q->get_labels()[q_node];
+                internal_nodes.insert({name, t_node});
+            }
+        }
 
-	    node_counts[q_node]++;
+        // Add ancestral nodes from groups
+        if (groups)
+        {
+            for(auto& [g,t_node]: find_and_name_nodes(T,*groups))
+            {
+                group_counts[g]++;
+                auto& [name,_] = (*groups)[g];
+                internal_nodes.insert({name, t_node});
+            }
+        }
 
-	    string t_node_name = T.get_label(*t_node);
-	    if (log_verbose > 0)
-		std::cerr<<"Mapped "<<q_node_name<<" -> "<<t_node_name<<"\n";
-	    std::cout<<">"<<q_node_name<<"\n";
+        if (show_leaves)
+            for(int node=0;node<J.leaf_names().size();node++)
+            {
+                std::cout<<">"<<J.leaf_names()[node]<<"\n";
+                auto& a = A.get_alphabet();
+                for(int i=0;i<A.length();i++)
+                    std::cout<<a.lookup(A(i,node));
+                std::cout<<"\n";
+            }
+        for(auto& [name,node]: internal_nodes)
+        {
+	    std::cout<<">"<<name<<"\n";
 	    auto& a = A.get_alphabet();
 	    for(int i=0;i<A.length();i++)
-		std::cout<<a.lookup(A(i,*t_node));
+		std::cout<<a.lookup(A(i,node));
 	    std::cout<<"\n";
-	}
-	std::cout<<"\n\n";
+        }
+        std::cout<<"\n\n";
     }
-    for(int q_node=Q.n_leaves(); q_node<Q.n_nodes(); q_node++)
-    {
-	auto q_node_name = Q.get_label(q_node);
-	if (q_node_name.empty()) continue;
 
-	std::cerr<<"Node '"<<q_node_name<<"': present in "<<node_counts[q_node]<<"/"<<J.size()<<" = "<<double(node_counts[q_node])/J.size()*100<<"% of samples.\n";
+    if (Q)
+    {
+        for(int q_node=Q->n_leaves(); q_node<Q->n_nodes(); q_node++)
+        {
+            auto q_node_name = Q->get_label(q_node);
+            if (q_node_name.empty()) continue;
+
+            std::cerr<<"Node '"<<q_node_name<<"': present in "<<node_counts[q_node]<<"/"<<J.size()<<" = "<<double(node_counts[q_node])/J.size()*100<<"% of samples.\n";
+        }
+    }
+    if (groups)
+    {
+        for(int i=0;i<groups->size();i++)
+        {
+            auto& [name,_] = (*groups)[i];
+
+            std::cerr<<"Group '"<<name<<"': present in "<<group_counts[i]<<"/"<<J.size()<<" = "<<double(group_counts[i])/J.size()*100<<"% of samples.\n";
+        }
     }
 }
 
@@ -191,7 +319,6 @@ variables_map parse_cmd_line(int argc,char* argv[])
     invisible.add_options()
 	("alignments", value<string>(),"File of alignment samples")
 	("trees", value<string>(), "File of corresponding tree samples")
-	("ancestors",value<string>(),"Newick tree with labelled ancestors")
 	;
 
     // named options
@@ -203,6 +330,8 @@ variables_map parse_cmd_line(int argc,char* argv[])
 	("subsample,x",value<unsigned>()->default_value(10),"factor by which to sub-sample trees")
 	("show-leaves,L",value<bool>()->default_value(true),"Include leaf sequences in filtered alignments")
 	("all-nodes,A",value<bool>()->default_value(false),"Only show alignments with ALL the labeled internal nodes")
+	("nodes",value<string>(),"Newick tree with labelled ancestors")
+	("groups",value<string>(),"File with named groups")
 	;
 
     options_description all("All options");
@@ -212,7 +341,6 @@ variables_map parse_cmd_line(int argc,char* argv[])
     positional_options_description p;
     p.add("alignments", 1);
     p.add("trees", 1);
-    p.add("ancestors", 1);
   
     variables_map args;     
     store(command_line_parser(argc, argv).
@@ -222,10 +350,11 @@ variables_map parse_cmd_line(int argc,char* argv[])
 
     if (args.count("help")) {
 	cout<<"Construct alignments with internal sequences for labeled nodes in query tree.\n\n";
-	cout<<"Usage: extract-ancestors <alignments file> <trees file> <query tree>[OPTIONS]\n";
+	cout<<"Usage: extract-ancestors <alignments file> <trees file> [--nodes=<query tree>] [--groups=<groups file>] [OPTIONS]\n";
 	cout<<visible<<"\n";
 	cout<<"Examples:\n\n";
-	cout<<"   % extract-ancestors C1.P1.fastas C1.trees query.tree\n\n";
+	cout<<"   % extract-ancestors C1.P1.fastas C1.trees --nodes=query.tree\n\n";
+	cout<<"   % extract-ancestors C1.P1.fastas C1.trees --groups=groups.txt\n\n";
 	exit(0);
     }
 
