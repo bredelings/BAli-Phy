33a391640f7f47c1fdb50dc97fb8f618d95210e5
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-03-26 10:36:31 -0400

    Reindent.

--- src/tools/joint-A-T.cc
+++ src/tools/joint-A-T.cc
@@ -1,21 +1,21 @@
 /*
-   Copyright (C) 2005,2008,2010 Benjamin Redelings
+  Copyright (C) 2005,2008,2010 Benjamin Redelings
 
-This file is part of BAli-Phy.
+  This file is part of BAli-Phy.
 
-BAli-Phy is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
-version.
+  BAli-Phy is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2, or (at your option) any later
+  version.
 
-BAli-Phy is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+  BAli-Phy is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for more details.
 
-You should have received a copy of the GNU General Public License
-along with BAli-Phy; see the file COPYING.  If not see
-<http://www.gnu.org/licenses/>.  */
+  You should have received a copy of the GNU General Public License
+  along with BAli-Phy; see the file COPYING.  If not see
+  <http://www.gnu.org/licenses/>.  */
 
 #include "joint-A-T.H"
 
@@ -44,39 +44,39 @@ const std::vector<std::string>& joint_A_T::leaf_names() const
 
 joint_A_T::joint_A_T(const vector<alignment>& A,const vector<SequenceTree>& T,bool internal)
 {
-  unsigned s = std::min(A.size(),T.size());
-  if (s != A.size())
-    std::cerr<<"joint-A-T: Warning! only using "<<s<<"/"<<A.size()<<" alignments to match number of trees."<<endl;
+    unsigned s = std::min(A.size(),T.size());
+    if (s != A.size())
+        std::cerr<<"joint-A-T: Warning! only using "<<s<<"/"<<A.size()<<" alignments to match number of trees."<<endl;
 
-  if (s != T.size())
-    std::cerr<<"joint-A-T: Warning! only using "<<s<<"/"<<T.size()<<" trees to match number of alignments."<<endl;
+    if (s != T.size())
+        std::cerr<<"joint-A-T: Warning! only using "<<s<<"/"<<T.size()<<" trees to match number of alignments."<<endl;
 
-  for(int i=0;i<s;i++)
-      push_back({A[i],T[i]});
+    for(int i=0;i<s;i++)
+        push_back({A[i],T[i]});
 
-  if (s == 0) return;
+    if (s == 0) return;
 
-  leaf_names_ = T[0].get_leaf_labels();
+    leaf_names_ = T[0].get_leaf_labels();
   
-  for(int i=0;i<size();i++)
-  {
-    remap_T_leaf_indices((*this)[i].second, leaf_names_);
-    link((*this)[i].first, (*this)[i].second, true);
-    link((*this)[i].first, (*this)[i].second, internal);
-  }
+    for(int i=0;i<size();i++)
+    {
+        remap_T_leaf_indices((*this)[i].second, leaf_names_);
+        link((*this)[i].first, (*this)[i].second, true);
+        link((*this)[i].first, (*this)[i].second, internal);
+    }
 }
 
 
 joint_A_T get_joint_A_T(const variables_map& args,bool internal)
 {
-  checked_ifstream a_file(args["alignments"].as<string>(), "alignment samples file");
+    checked_ifstream a_file(args["alignments"].as<string>(), "alignment samples file");
 
-  checked_ifstream t_file(args["trees"].as<string>(), "tree samples file");
+    checked_ifstream t_file(args["trees"].as<string>(), "tree samples file");
 
-  unsigned subsample = args["subsample"].as<unsigned>();
+    unsigned subsample = args["subsample"].as<unsigned>();
 
-  vector<alignment> A = load_alignments(a_file, get_alphabet_name(args));
-  vector<SequenceTree> T = load_trees(t_file, 0, subsample);
+    vector<alignment> A = load_alignments(a_file, get_alphabet_name(args));
+    vector<SequenceTree> T = load_trees(t_file, 0, subsample);
 
-  return joint_A_T(A,T,internal);
+    return joint_A_T(A,T,internal);
 }
