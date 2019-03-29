4efa4d9540f8eff35f3f2236eecf1bb6f1b9153c
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-03-29 18:59:41 -0400

    Avoid a non-terminating computation to determine the sampling rate.

--- modules/Probability/Distribution/List.hs
+++ modules/Probability/Distribution/List.hs
@@ -6,11 +6,12 @@ list_densities (d:ds) (x:xs) = densities d x ++ list_densities ds xs
 list_densities [] []         = []
 list_densities _  _          = [doubleToLogDouble 0.0]
 
+-- This cannot handle infinite lists because it uses the total number of distributions to determine the rate.
 list dists = Distribution (list_densities dists) (no_quantile "list") do_sample (ListRange (map distRange dists))
              where do_sample = SamplingRate (1.0/sqrt (intToDouble $ length dists)) $ mapM sample dists
 
 iid n dist = Distribution (list_densities (replicate n dist)) (no_quantile "iid") iid_sample (ListRange $ take n $ repeat $ distRange dist) where
-    iid_sample = do xs <- sample $ list (repeat dist)
+    iid_sample = do xs <- SamplingRate (1.0/sqrt (intToDouble n)) $ mapM sample (repeat dist)
                     return $ take n xs
 
 plate n dist_f = list $ map dist_f [0..n-1]
