4d799cdd0ea289d3b41bd63ed18e21bb77914ecb
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-04-27 14:55:26 -0400

    Update Text for unpack_cpp_string

--- modules/Data/Text.hs
+++ modules/Data/Text.hs
@@ -2,14 +2,13 @@ module Data.Text where
 
 -- Note that this module is really just a hacky implementation of a boxed c++ std::string
 
-import Foreign.Vector
+import Foreign.String
 
-data CppString
 data Text = Text CppString
 
 builtin builtin_pack 1 "pack" "Text"
 
 pack = builtin_pack . list_to_vector
 
-unpack (Text s) = listFromString s
+unpack (Text s) = unpack_cpp_string s
 
