4099ba5afdd965560a5010f9ead7299da8cc5080
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-05-02 20:39:37 -0400

    Fix categorical density.

--- modules/Probability/Distribution/Categorical.hs
+++ modules/Probability/Distribution/Categorical.hs
@@ -4,5 +4,6 @@ import Probability.Random
 
 builtin builtin_sample_categorical 1 "sample_categorical" "Distribution"
 sample_categorical ps = RandomStructure modifiable $ liftIO (IOAction1 builtin_sample_categorical ps)
-categorical ps = Distribution (make_densities $ qs!) (no_quantile "categorical") (sample_categorical ps) (integer_between 0 (length ps - 1))
+-- FIXME: is `make_densities $ qs !` supposed to parse as `make_densities (qs!)`?
+categorical ps = Distribution (make_densities (qs!)) (no_quantile "categorical") (sample_categorical ps) (integer_between 0 (length ps - 1))
                 where qs = listArray' $ map doubleToLogDouble ps
