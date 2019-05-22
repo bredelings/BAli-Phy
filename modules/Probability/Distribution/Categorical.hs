28e0bec20d21696d4aae3bef9950a84c81824369
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-05-22 10:14:58 +0100

    Automatically include categorical move.

--- modules/Probability/Distribution/Categorical.hs
+++ modules/Probability/Distribution/Categorical.hs
@@ -1,9 +1,11 @@
-module Probability.Distribution.Categorical where
+module Probability.Distribution.Categorical (categorical) where
 
 import Probability.Random
+import MCMC
 
+categorical_effect = \x pdf rate -> add_move (\c -> gibbs_sample_categorical x 4 c)
 builtin builtin_sample_categorical 1 "sample_categorical" "Distribution"
-sample_categorical ps = RandomStructure do_nothing modifiable $ liftIO (IOAction1 builtin_sample_categorical ps)
+sample_categorical ps = RandomStructure categorical_effect modifiable $ liftIO (IOAction1 builtin_sample_categorical ps)
 -- FIXME: is `make_densities $ qs !` supposed to parse as `make_densities (qs!)`?
 categorical ps = Distribution (make_densities (qs!)) (no_quantile "categorical") (sample_categorical ps) (integer_between 0 (length ps - 1))
                 where qs = listArray' $ map doubleToLogDouble ps
