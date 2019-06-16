c50e412865a79b7e6d8c04167d8cec45659a522f
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-06-15 21:53:17 -0400

    Check that the number of rvs does not change during the loop.

--- src/computation/machine/sums.cc
+++ src/computation/machine/sums.cc
@@ -60,13 +60,18 @@ log_double_t reg_heap::prior_for_context(int c)
 
     double log_pr = 0.0;
     double C = 0.0;
+
     /*
-     * NOTE: random_variables_ can be moved during the loop, because of insertions.
-     *       Therefore, we cannot use a range-for.
+     * NOTE: Evaluating a PDF should NOT cause the creation of any new
+     *       random variables, since all the parameters should be forced by the sampling
+     *       operation.
+     *
+     *       This ensures that a range-for is OK. (Range-for's assume that begin() and end() do not change).
      */
-    for(int i=0; i<random_variables_.size(); i++)
+    int num_rvs = random_variables_.size();
+
+    for(int r: random_variables_)
     {
-        int r = random_variables_[i];
 	int r_pdf = (*this)[r].reg_for_slot(1);
 
         /* For now, let's require priors to exist in all contexts.
@@ -78,6 +83,8 @@ log_double_t reg_heap::prior_for_context(int c)
         assert(reg_exists(r_pdf));
 
 	const auto& x = get_reg_value_in_context(r_pdf, c);
+        assert(random_variables_.size() == num_rvs);
+
 	log_double_t X = x.as_log_double();
 
 	double t;
@@ -97,6 +104,16 @@ log_double_t reg_heap::prior_for_context(int c)
 	}
 	log_pr = t;
     }
+
+    /*
+     * NOTE: We need to check evaluation of PDF doesn't create any new random variables_.
+     *       Insertion of random_variables can caused random_variables_ to be moved during the loop,
+     *          which does not work for a range-based for loop, since the start
+     *       It only works for `int r = random_variables_[i]`.
+     */
+    if (random_variables_.size() != num_rvs)
+        throw myexception()<<"The number of random variables changes while evaluating PDF!";
+
     log_double_t Pr;
     Pr.log() = log_pr;
     return Pr;
