f24f5f16e2c1b3cd4c4e7915a31371685dc2e194
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-06-19 18:23:58 -0400

    Fix sampling from categorical.

--- modules/Probability/Distribution/Categorical.hs
+++ modules/Probability/Distribution/Categorical.hs
@@ -3,9 +3,9 @@ module Probability.Distribution.Categorical (categorical) where
 import Probability.Random
 import MCMC
 
-categorical_effect x = add_move (\c -> gibbs_sample_categorical x 4 c)
+categorical_effect n x = add_move (\c -> gibbs_sample_categorical x n c)
 builtin builtin_sample_categorical 1 "sample_categorical" "Distribution"
-sample_categorical ps = RandomStructure categorical_effect modifiable $ liftIO (IOAction1 builtin_sample_categorical ps)
+sample_categorical ps = RandomStructure (categorical_effect (length ps)) modifiable $ liftIO (IOAction1 builtin_sample_categorical ps)
 -- FIXME: is `make_densities $ qs !` supposed to parse as `make_densities (qs!)`?
 categorical ps = Distribution (make_densities (qs!)) (no_quantile "categorical") (sample_categorical ps) (integer_between 0 (length ps - 1))
                 where qs = listArray' $ map doubleToLogDouble ps
