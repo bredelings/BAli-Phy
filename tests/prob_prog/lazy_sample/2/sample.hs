d870ec8a58704c913256b1f60edeac6143ee0b31
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-07-09 16:34:01 -0400

    Add lazy_sample/2 test.

---
+++ tests/prob_prog/lazy_sample/2/sample.hs
@@ -0,0 +1,11 @@
+import Probability
+
+model = do
+  x <- sample $ normal 0.0 1.0
+  ys <- sample $ list (repeat $ normal 0.0 1.0)
+  return $ (x*x):(take 10 ys)
+
+main = do
+  zs <- random $ model
+  observe (normal (zs!!2) 1.0) 10.0
+  return $ log_all [ zs %% "zs"]
