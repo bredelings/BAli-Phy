dce702abb18ecb125283e519a815e193833e8bcc
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-08-05 19:09:19 -0400

    move model into its own function

--- tests/prob_prog/regression/LinearRegression.hs
+++ tests/prob_prog/regression/LinearRegression.hs
@@ -1,21 +1,27 @@
 module Line where
 
-import Data.ReadFile
 import Probability
+import Data.ReadFile
 import System.Environment
 
 xs = read_file_as_double "xs"
 
 ys = read_file_as_double "ys"
 
+sample_model = do
+
+  b <- sample $ normal 0.0 1.0
+
+  a <- sample $ normal 0.0 1.0
+
+  s <- sample $ exponential 1.0
+
+  return (a,b,s)
+
 main = do
 
-  b <- random $ sample $ normal 0.0 1.0
-
-  a <- random $ sample $ normal 0.0 1.0
-
-  s <- random $ sample $ exponential 1.0
-
+  (a,b,s) <- random $ sample_model
+  
   let f x = b*x + a
 
   sequence_ [observe (normal mu_y s) y | (x,y) <- zip xs ys, let mu_y = f x]
