5a1a57718b4243ee6839c5a3a4e21f4797ea26f4
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 09:08:08 -0400

    Convert Laplace to IOAction

--- modules/Probability/Distribution/Laplace.hs
+++ modules/Probability/Distribution/Laplace.hs
@@ -3,6 +3,6 @@ module Probability.Distribution.Laplace where
 import Probability.Random
 
 builtin laplace_density 3 "laplace_density" "Distribution"
-builtin builtin_sample_laplace 2 "sample_laplace" "Distribution"
-sample_laplace m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction2 builtin_sample_laplace m s)
+builtin builtin_sample_laplace 3 "sample_laplace" "Distribution"
+sample_laplace m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_laplace m s state)))
 laplace m s = Distribution (make_densities $ laplace_density m s) () (sample_laplace m s) realLine
