bf70ed2027ea588c9f3f41a94970e3a0d534182d
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 10:17:08 -0400

    Add comment.

--- modules/Compiler/ST.hs
+++ modules/Compiler/ST.hs
@@ -12,6 +12,7 @@ unsafeInterleaveST x = LazyIO x
 runST' (IOAction f) = snd (f 0)
 runST' (LazyIO f) = runST' f
 runST' (IOAndPass (LazyIO f) g) = let x = runST' f in runST' (g x)
+-- probably we can get this effect by rerunning the whole computation if either the state or result changes. using join.
 runST' (IOAndPass f g) = let x = runST' f in x `join` runST' (g x)
 runST' (MFix f) = let x = runST' (f x) in x
 runST' (IOReturn x) = x
