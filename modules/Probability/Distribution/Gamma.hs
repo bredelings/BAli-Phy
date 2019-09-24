94ec07e3c3ef41e96b7a4c7d13c6ed2faac7d986
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 09:05:28 -0400

    Convert Gamma to IOAction

--- modules/Probability/Distribution/Gamma.hs
+++ modules/Probability/Distribution/Gamma.hs
@@ -4,7 +4,7 @@ import Probability.Random
 
 builtin shifted_gamma_density 4 "shifted_gamma_density" "Distribution"
 builtin shifted_gamma_quantile 4 "shifted_gamma_quantile" "Distribution"
-builtin builtin_sample_shifted_gamma 3 "sample_shifted_gamma" "Distribution"
-sample_shifted_gamma a b shift = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction3 builtin_sample_shifted_gamma a b shift)
+builtin builtin_sample_shifted_gamma 4 "sample_shifted_gamma" "Distribution"
+sample_shifted_gamma a b shift = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_shifted_gamma a b shift s)))
 shifted_gamma a b shift = Distribution (make_densities $ shifted_gamma_density a b shift) (shifted_gamma_quantile a b shift) (sample_shifted_gamma a b shift) (above shift)
 gamma a b = shifted_gamma a b 0.0
