026fae3a261cd979781439e90718b9f00893787a
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 08:44:38 -0400

    Convert Bernoulli to IOAction

--- modules/Probability/Distribution/Bernoulli.hs
+++ modules/Probability/Distribution/Bernoulli.hs
@@ -2,8 +2,8 @@ module Probability.Distribution.Bernoulli where
 
 import Probability.Random
 
-builtin builtin_sample_bernoulli 1 "sample_bernoulli" "Distribution"
-sample_bernoulli p = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction1 builtin_sample_bernoulli p)
+builtin builtin_sample_bernoulli 2 "sample_bernoulli" "Distribution"
+sample_bernoulli p = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_bernoulli p s)))
 bernoulli_density2 p q 1 = (doubleToLogDouble p)
 bernoulli_density2 p q 0 = (doubleToLogDouble q)
 bernoulli2 p q = Distribution (make_densities $ bernoulli_density2 p q) (no_quantile "bernoulli") (sample_bernoulli p) (integer_between 0 1)
