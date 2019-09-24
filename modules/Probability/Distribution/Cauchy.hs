831df0e5354c3438339c03f16c2f0b9140229834
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 08:57:57 -0400

    Convert Cauchy to IOAction

--- modules/Probability/Distribution/Cauchy.hs
+++ modules/Probability/Distribution/Cauchy.hs
@@ -3,6 +3,6 @@ module Probability.Distribution.Cauchy where
 import Probability.Random
 
 builtin cauchy_density 3 "cauchy_density" "Distribution"
-builtin builtin_sample_cauchy 2 "sample_cauchy" "Distribution"
-sample_cauchy m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction2 builtin_sample_cauchy m s)
+builtin builtin_sample_cauchy 3 "sample_cauchy" "Distribution"
+sample_cauchy m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_cauchy m s state)))
 cauchy m s = Distribution (make_densities $ cauchy_density m s) () (sample_cauchy m s) realLine
