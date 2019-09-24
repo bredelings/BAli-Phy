12f87768fc7f5be8f7ac08bfc02bf6a58f71b88c
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 08:53:54 -0400

    Convert Binomial to IOAction

--- modules/Probability/Distribution/Binomial.hs
+++ modules/Probability/Distribution/Binomial.hs
@@ -3,6 +3,6 @@ module Probability.Distribution.Binomial where
 import Probability.Random
 
 builtin binomial_density 3 "binomial_density" "Distribution"
-builtin builtin_sample_binomial 2 "sample_binomial" "Distribution"
-sample_binomial n p = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction2 builtin_sample_binomial n p)
+builtin builtin_sample_binomial 3 "sample_binomial" "Distribution"
+sample_binomial n p = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_binomial n p s)))
 binomial n p = Distribution (make_densities $ binomial_density n p) (no_quantile "binomial") (sample_binomial n p) (integer_between 0 n)
