3474c4feba9c744e9c4eee3ceffd58b4021bcf9f
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-09-24 08:53:06 -0400

    Convert Beta to IOAction

--- modules/Probability/Distribution/Beta.hs
+++ modules/Probability/Distribution/Beta.hs
@@ -4,6 +4,6 @@ import Probability.Random
 
 builtin beta_density 3 "beta_density" "Distribution"
 builtin beta_quantile 3 "beta_quantile" "Distribution"
-builtin builtin_sample_beta 2 "sample_beta" "Distribution"
-sample_beta a b = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction2 builtin_sample_beta a b)
+builtin builtin_sample_beta 3 "sample_beta" "Distribution"
+sample_beta a b = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_beta a b s)))
 beta a b = Distribution (make_densities $ beta_density a b) (beta_quantile a b) (sample_beta a b) (between 0.0 1.0)
