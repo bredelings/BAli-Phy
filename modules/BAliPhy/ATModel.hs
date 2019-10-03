ee4366107f7c16e2ab2f2c2110693029e414c808
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-10-02 22:39:40 -0400

    Convert BAliPhy.ATModel to use record syntax.

--- modules/BAliPhy/ATModel.hs
+++ modules/BAliPhy/ATModel.hs
@@ -11,33 +11,33 @@ import SModel
 --                                      what we really need is transition probabilities for each partition.
 
 data IndelModel = IndelModel
-data ATModel = ATModel Tree [MixtureModels] [IndelModel] [Double] [Double] [Int] [Partition]
-tree               (ATModel t _  _ _ _ _ _  ) = t
-smodels            (ATModel _ ss _ _ _ _ _  ) = ss
-imodels            (ATModel _ _ is _ _ _ _  ) = is
-scales             (ATModel _ _ _ rs _ _ _  ) = rs
-branch_lengths     (ATModel _ _ _ _ ls _ _  ) = ls
-branch_categories  (ATModel _ _ _ _ _ cs _  ) = cs
-partitions         (ATModel _ _ _ _ _  _ ps ) = ps
+data ATModel = ATModel {
+      tree:: Tree ,
+      smodels :: [MixtureModels],
+      imodels :: [IndelModel],
+      scales :: [Double],
+      branch_lengths :: [Double],
+      branch_categories :: [Int],
+      partitions :: [Partition]
+    }
 
 data ConditionalLikelihoodVector
 data Matrix
 data LeafSequence
-data ATModelExport = ATModelExport ATModel [Array Int Matrix] [Array Int EVector] [Array Int ConditionalLikelihoodVector] [Double] [Array Int LeafSequence] Bool Double Bool Int EVector
-
--- we need to add field declarations!
-
-get_atmodel                  (ATModelExport at _  _  _  _  _  _ _ _ _ _ ) = at
-get_all_transition_ps        (ATModelExport _  ps _  _  _  _  _ _ _ _ _ ) = ps
-get_all_cond_likes           (ATModelExport _  _  cl _  _  _  _ _ _ _ _ ) = cl
-get_all_ancestral_sequences  (ATModelExport _  _  _  as _  _  _ _ _ _ _ ) = as
-get_all_likelihoods          (ATModelExport _  _  _  _  ls _  _ _ _ _ _ ) = ls
-leaf_sequences               (ATModelExport _  _  _  _  _  ls _ _ _ _ _ ) = ls
-imodel_training              (ATModelExport _  _  _  _  _  _  t _ _ _ _ ) = t
-heat                         (ATModelExport _  _  _  _  _  _  _ h _ _ _ ) = h
-variable_alignment           (ATModelExport _  _  _  _  _  _  _ _ v _ _ ) = v
-subst_root                   (ATModelExport _  _  _  _  _  _  _ _ _ r _ ) = r
-sequence_names               (ATModelExport _  _  _  _  _  _  _ _ _ _ ns) = ns
+data ATModelExport = ATModelExport
+    {
+      get_atmodel :: ATModel,
+      get_all_transition_ps :: [Array Int Matrix],
+      get_all_cond_likes :: [Array Int EVector],
+      get_all_ancestral_sequences :: [Array Int ConditionalLikelihoodVector],
+      get_all_likelihoods :: [Double],
+      leaf_sequences :: [Array Int LeafSequence],
+      imodel_training :: Bool,
+      heat :: Double,
+      variable_alignment :: Bool,
+      subst_root :: Int,
+      sequence_names :: EVector
+    }
 
 observe_partition_type_0 partition compressed_alignment leaf_sequences column_counts alphabet branch_lengths subst_root = (transition_ps, cls, ancestral_sequences, likelihood)
     where tree = DP.get_tree partition
