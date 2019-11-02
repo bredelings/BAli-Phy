110002da293df1048f501442e2d542f8c5e4b3d7
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-11-02 16:43:13 -0400

    Add slice sampling moves for continuous variables.

--- haskell/Probability/Distribution/Laplace.hs
+++ haskell/Probability/Distribution/Laplace.hs
@@ -1,8 +1,12 @@
 module Probability.Distribution.Laplace where
 
 import Probability.Random
+import MCMC
 
 builtin laplace_density 3 "laplace_density" "Distribution"
 builtin builtin_sample_laplace 3 "sample_laplace" "Distribution"
-sample_laplace m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_laplace m s state)))
+
+laplace_effect x = add_move (\c -> slice_sample_real_random_variable x c)
+sample_laplace m s = RandomStructure laplace_effect modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_laplace m s state)))
+
 laplace m s = Distribution (make_densities $ laplace_density m s) () (sample_laplace m s) realLine
--- haskell/Probability/Distribution/Normal.hs
+++ haskell/Probability/Distribution/Normal.hs
@@ -1,10 +1,14 @@
 module Probability.Distribution.Normal where
 
 import Probability.Random
+import MCMC
 
 builtin normal_density 3 "normal_density" "Distribution"
 builtin normal_quantile 3 "normal_quantile" "Distribution"
 builtin builtin_sample_normal 3 "sample_normal" "Distribution"
-sample_normal m s = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\state -> (state, builtin_sample_normal m s state)))
+
+normal_effect x = add_move (\c -> slice_sample_real_random_variable x c)
+sample_normal m s = RandomStructure normal_effect modifiable_structure $ liftIO (IOAction (\state -> (state, builtin_sample_normal m s state)))
+
 normal m s = Distribution (make_densities $ normal_density m s) (normal_quantile m s) (sample_normal m s) realLine
 
