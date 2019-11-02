25116722050b5b65752740c77420eb3856db9394
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-11-02 16:43:13 -0400

    Add slice sampling moves for continuous variables.

--- haskell/Probability/Distribution/Uniform.hs
+++ haskell/Probability/Distribution/Uniform.hs
@@ -1,13 +1,19 @@
 module Probability.Distribution.Uniform where
 
 import Probability.Random
+import MCMC
 
 builtin uniform_density 3 "uniform_density" "Distribution"
 builtin builtin_sample_uniform 3 "sample_uniform" "Distribution"
-sample_uniform l u = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform l u s)))
+
+uniform_effect x = add_move (\c -> slice_sample_real_random_variable x c)
+sample_uniform l u = RandomStructure uniform_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform l u s)))
+
 uniform l u = Distribution (make_densities $ uniform_density l u) () (sample_uniform l u) (between l u)
 
 builtin uniform_int_density 3 "uniform_int_density" "Distribution"
 builtin builtin_sample_uniform_int 3 "sample_uniform_int" "Distribution"
+
 sample_uniform_int l u = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform_int l u s)))
+
 uniform_int l u = Distribution (make_densities $ uniform_int_density l u) () (sample_uniform_int l u) (integer_between l u)
