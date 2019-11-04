45e882dd091cc3f4255c4808f94630df019a486e
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-11-04 12:05:17 -0500

    Use the integer slice move for discrete distributions.

--- haskell/Probability/Distribution/Geometric.hs
+++ haskell/Probability/Distribution/Geometric.hs
@@ -1,10 +1,15 @@
 module Probability.Distribution.Geometric where
 
 import Probability.Random
+import MCMC
 
 builtin geometric_density 3 "geometric_density" "Distribution"
 builtin builtin_sample_geometric 2 "sample_geometric" "Distribution"
-sample_geometric p_success = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_geometric p_success s)))
+
+geometric_effect x = add_move (\c -> slice_sample_integer_random_variable x c)
+
+sample_geometric p_success = RandomStructure geometric_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_geometric p_success s)))
+
 geometric2 p_fail p_success = Distribution (make_densities $ geometric_density p_fail p_success) (no_quantile "geometric") (sample_geometric p_success) (integer_above 0)
 
 geometric p = geometric2 (1.0-p) p
--- haskell/Probability/Distribution/Poisson.hs
+++ haskell/Probability/Distribution/Poisson.hs
@@ -1,8 +1,13 @@
 module Probability.Distribution.Poisson where
 
 import Probability.Random
+import MCMC
 
 builtin poisson_density 2 "poisson_density" "Distribution"
 builtin builtin_sample_poisson 2 "sample_poisson" "Distribution"
-sample_poisson mu = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s -> (s, builtin_sample_poisson mu s)))
+
+poisson_effect x = add_move (\c -> slice_sample_integer_random_variable x c)
+
+sample_poisson mu = RandomStructure poisson_effect modifiable_structure $ liftIO (IOAction (\s -> (s, builtin_sample_poisson mu s)))
+
 poisson mu = Distribution (make_densities $ poisson_density mu) (no_quantile "Poisson") (sample_poisson mu) (integer_above 0)
