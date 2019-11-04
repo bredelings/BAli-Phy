7ac8ef648286e66da5569e79f62aec6c8c60e023
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-11-04 12:05:17 -0500

    Use the integer slice move for discrete distributions.

--- haskell/Probability/Distribution/Uniform.hs
+++ haskell/Probability/Distribution/Uniform.hs
@@ -14,6 +14,7 @@ uniform l u = Distribution (make_densities $ uniform_density l u) () (sample_uni
 builtin uniform_int_density 3 "uniform_int_density" "Distribution"
 builtin builtin_sample_uniform_int 3 "sample_uniform_int" "Distribution"
 
-sample_uniform_int l u = RandomStructure do_nothing modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform_int l u s)))
+uniform_int_effect x = add_move (\c -> slice_sample_integer_random_variable x c)
+sample_uniform_int l u = RandomStructure uniform_int_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform_int l u s)))
 
 uniform_int l u = Distribution (make_densities $ uniform_int_density l u) () (sample_uniform_int l u) (integer_between l u)
