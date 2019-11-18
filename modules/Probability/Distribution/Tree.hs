b57a8c743af5257509c702cd8527e0a5575cefe6
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-11-18 07:49:35 -0500

    Simplify random_tree_edges.

--- modules/Probability/Distribution/Tree.hs
+++ modules/Probability/Distribution/Tree.hs
@@ -16,15 +16,18 @@ remove_one [] = error "Cannot remove one from empty list"
 remove_one list = do i <- sample $ uniform_int 0 (length list-1)
                      return $ pick_index i list
 
+remove_n 0 list = return ([],list)
+remove_n n list = do (x,list_minus_1) <- remove_one list
+                     (xs,list_minus_n) <- remove_n (n-1) list_minus_1
+                     return ((x:xs),list_minus_n)
+
 -- choose 2 leaves, connect them to an internal node, and put that internal node on the list of leaves
 -- This is I think gives more weight to more balanced trees?
 random_tree_edges [l1] _         = return []
 random_tree_edges [l1,l2] _      = return [(l1,l2)]
-random_tree_edges leaves internal = do (l1,leaves')  <- remove_one leaves
-                                       (l2,leaves'') <- remove_one leaves'
-                                       let (i:internal') = internal
-                                       other_edges <- random_tree_edges (i:leaves'') internal'
-                                       return $ [(l1,i),(l2,i)]++other_edges
+random_tree_edges leaves (i:is) = do ([l1,l2],leaves') <- remove_n 2 leaves
+                                     other_edges <- random_tree_edges (i:leaves') is
+                                     return $ [(l1,i),(l2,i)]++other_edges
 
 -- Create a tree of size n-1, choose an edge at random, and insert the next leaf there.
 uniform_topology_edges [l1]    _     = return []
@@ -33,6 +36,7 @@ uniform_topology_edges (l:ls) (i:is) = do es1 <- uniform_topology_edges ls is
                                           ((x,y),es2) <- remove_one es1
                                           return $ [(l,i),(x,i),(i,y)] ++ es2
 
+-- We could rewrite uniform_topology_edges to automatically flip and sort the branches with leaf branches first.
 random_tree 1 = return $ Tree (listArray' [[]]) (listArray' []) 1 0
 random_tree n = do let num_nodes = 2*n-2
                    edges <- uniform_topology_edges [0..n-1] [n..num_nodes-1]
