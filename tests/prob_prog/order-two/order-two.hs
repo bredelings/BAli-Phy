486bfebebfd38ee866ad7775175b6801b76e557c
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-12-04 08:10:19 -0500

    simplify order 2 random walk.

--- tests/prob_prog/order-two/order-two.hs
+++ tests/prob_prog/order-two/order-two.hs
@@ -1,16 +1,14 @@
 import           Probability
 
-random_walk n x1 x2
-    | n < 2 = error ("Random walk must have at least 2 points")
-    | n == 2 = return [x1, x2]
-    | otherwise = do
-        dx <- normal 0.0 1.0
-        let x3 = dx - x1 + (2.0 * x2)
-        xs <- random_walk (n - 1) x2 x3
-        return (x1 : xs)
+random_walk x1 x2 = do
+    dx <- normal 0.0 1.0
+    let x3 = dx - x1 + (2.0 * x2)
+    xs <- random_walk x2 x3
+    return (x1 : xs)
 
 main = random $ do
-    x1 <- normal 0.0 1.0
-    x2 <- normal x1 (sqrt $ 1.0 / 3.0)
-    xs <- random_walk 100 x1 x2
+    x1   <- normal 0.0 1.0
+    x2   <- normal x1 (sqrt $ 1.0 / 3.0)
+    walk <- random_walk x1 x2
+    let xs = take 100 walk
     return ["x" %=% xs]
