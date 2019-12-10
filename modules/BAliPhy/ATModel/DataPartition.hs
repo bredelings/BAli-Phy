453654b647b523a8a95ea6319e6cbf97e59fb3ae
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-12-10 13:50:42 -0500

    Use record syntax for Partition type.

--- modules/BAliPhy/ATModel/DataPartition.hs
+++ modules/BAliPhy/ATModel/DataPartition.hs
@@ -8,11 +8,12 @@ data IModel
 data SModel
 data HMM
 data PairwiseAlignment
-data Partition = Partition SModel (Maybe IModel) Double Tree AlignmentOnTree (Maybe (Array Int HMM))
 
-smodel              (Partition s _ _ _  _  _ ) = s
-imodel              (Partition _ i _ _  _  _ ) = i
-scale               (Partition _ _ s _  _  _ ) = s
-get_tree            (Partition _ _ _ t  _  _ ) = t
-get_alignment       (Partition _ _ _ _  as _ ) = as
-hmms                (Partition _ _ _ _  _  hs) = hs
+data Partition = Partition {
+      smodel :: SModel,
+      imodel :: Maybe IModel,
+      scale  :: Double,
+      get_tree :: Tree,
+      get_alignment :: AlignmentOnTree,
+      hmms :: Maybe (Array Int HMM)
+    }
