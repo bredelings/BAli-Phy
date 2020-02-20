bbb96d34381a95cdf96fe11512a4fe3bb13c3f33
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-02-20 18:36:27 -0500

    Add test of the Li & Stephens model.

---
+++ tests/prob_prog/popgen/li_stephens/LiStephens2003_1.hs
@@ -0,0 +1,25 @@
+module LiStephens2003 where
+
+import           Bio.Alignment
+import           Alphabet
+import           Probability
+import           PopGen
+import           System.Environment
+
+filename = getArgs !! 0
+
+sequence_data = load_alignment dna filename
+
+model = do
+
+    rho       <- log_laplace 0.01 1.0
+
+    return rho
+
+main = do
+
+    rho <- random $ model
+
+    observe (li_stephens_2003 rho) sequence_data
+
+    return ["rho" %=% rho ]
