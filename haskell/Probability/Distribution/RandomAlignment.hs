f6ae519a113ef82b46b624a2c9c5470c48c79b77
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-04 09:16:11 -0700

    Reformat Distribution/{RandomAlignment.hs,Tree.hs}

--- haskell/Probability/Distribution/RandomAlignment.hs
+++ haskell/Probability/Distribution/RandomAlignment.hs
@@ -1,63 +1,65 @@
 module Probability.Distribution.RandomAlignment where
 
-import Probability.Random
-import Tree
-import Bio.Alignment
+import           Probability.Random
+import           Tree
+import           Bio.Alignment
 
 modifiable_alignment a@(AlignmentOnTree tree n_seqs ls as) | numNodes tree < 2 = a
-modifiable_alignment   (AlignmentOnTree tree n_seqs ls as) | otherwise         = AlignmentOnTree tree n_seqs ls' as'
-    where as' = mkArray (numElements as) (\i -> modifiable $ (as!i))
-          ls' = listArray' [ seqlength as' tree node | node <- [0..numNodes tree - 1]]
-
---FIXME: I should make this only trigger if you start looking at the VALUES of the pairwise alignments!
---FIXME: Maybe I should also reduce this to just a list of pairwise alignments?
-modifiable_alignment_structure value effect = let a = modifiable_alignment value
-                                                  triggered_a = effect `seq` a
-                                              in (a, triggered_a)
+modifiable_alignment (AlignmentOnTree tree n_seqs ls as) | otherwise           = AlignmentOnTree tree n_seqs ls' as'
+  where
+    as' = mkArray (numElements as) (\i -> modifiable $ (as ! i))
+    ls' = listArray' [ seqlength as' tree node | node <- [0 .. numNodes tree - 1] ]
 
 -- Compare to unaligned_alignments_on_tree in parameters.cc
-unaligned_alignments_on_tree t ls = [ make_a' b | b <- [0..2*numBranches t-1]]
-    where make_a' b = let b' = reverseEdge t b in
-                      if (b > b') then
-                          flip_alignment $ make_a b'
-                      else
-                          make_a b
-          make_a b = let n1 = sourceNode t b
-                         n2 = targetNode t b
-                         l1 = if n1 < numElements ls then ls!n1 else 0
-                         l2 = if n2 < numElements ls then ls!n2 else 0
-                     in unaligned_pairwise_alignment l1 l2
+unaligned_alignments_on_tree t ls = [ make_a' b | b <- [0 .. 2 * numBranches t - 1] ]
+  where
+    make_a' b = let b' = reverseEdge t b in if (b > b') then flip_alignment $ make_a b' else make_a b
+    make_a b =
+        let n1 = sourceNode t b
+            n2 = targetNode t b
+            l1 = if n1 < numElements ls then ls ! n1 else 0
+            l2 = if n2 < numElements ls then ls ! n2 else 0
+        in  unaligned_pairwise_alignment l1 l2
 
-deepseq_array array obj = go (numElements array-1) array obj
-    where go i array obj | i == -1   = obj
-                         | otherwise = (array!i) `seq` (go (i-1) array obj)
+deepseq_array array obj = go (numElements array - 1) array obj
+  where
+    go i array obj | i == -1   = obj
+                   | otherwise = (array ! i) `seq` (go (i - 1) array obj)
 
 -- Here we want to (i) force the tree, (ii) force the hmms, and (iii) match parameters.cc:unaligned_alignments_on_tree 
 sample_alignment tree hmms tip_lengths = return (hmms `deepseq_array` (AlignmentOnTree tree n_nodes ls as))
-    where n_leaves = numElements tip_lengths
-          ls  = listArray' $ [ if node < n_leaves then tip_lengths!node else seqlength as tree node | node <- [0..n_nodes-1] ]
-          as  = listArray' $ unaligned_alignments_on_tree tree tip_lengths
-          n_nodes = numNodes tree
+  where
+    n_leaves = numElements tip_lengths
+    ls       = listArray' $ [ if node < n_leaves then tip_lengths ! node else seqlength as tree node | node <- [0 .. n_nodes - 1] ]
+    as       = listArray' $ unaligned_alignments_on_tree tree tip_lengths
+    n_nodes  = numNodes tree
 
 product' = foldl' (*) (doubleToLogDouble 1.0)
 
 builtin transition_counts 1 "transition_counts" "Alignment"
 builtin pairwise_alignment_probability_from_counts 2 "pairwise_alignment_probability_from_counts" "Alignment"
-alignment_branch_pr a hmms b = pairwise_alignment_probability_from_counts (transition_counts (a!b)) (hmms!b)
+alignment_branch_pr a hmms b = pairwise_alignment_probability_from_counts (transition_counts (a ! b)) (hmms ! b)
 
-alignment_pr_top as tree hmms = product' $ map (alignment_branch_pr as hmms) [0..numBranches tree - 1]
-alignment_pr_bot as tree (_,lengthp) = (product' $ map (lengthp . seqlength as tree) (internal_nodes tree))^2
-alignment_pr1 length (_,lengthp) = lengthp length
+alignment_pr_top as tree hmms = product' $ map (alignment_branch_pr as hmms) [0 .. numBranches tree - 1]
+alignment_pr_bot as tree (_, lengthp) = (product' $ map (lengthp . seqlength as tree) (internal_nodes tree)) ^ 2
+alignment_pr1 length (_, lengthp) = lengthp length
 
-alignment_pr (AlignmentOnTree tree n_seqs ls as) hmms model | numNodes tree < 1  = error "Tree only has "++numNodes++" nodes."
-                                                            | numNodes tree == 1 = alignment_pr1 (ls!0) model
-                                                            | otherwise          = (alignment_pr_top as tree hmms)/(alignment_pr_bot as tree model)
+alignment_pr (AlignmentOnTree tree n_seqs ls as) hmms model | numNodes tree < 1 = error "Tree only has " ++ numNodes ++ " nodes."
+                                                            | numNodes tree == 1 = alignment_pr1 (ls ! 0) model
+                                                            | otherwise = (alignment_pr_top as tree hmms) / (alignment_pr_bot as tree model)
 
-alignment_pr' alignment hmms model var_a = if var_a then
-                                               alignment_pr alignment hmms model
-                                           else
-                                               doubleToLogDouble 1.0
+alignment_pr' alignment hmms model var_a = if var_a then alignment_pr alignment hmms model else doubleToLogDouble 1.0
 
-random_alignment tree hmms model tip_lengths var_a = Distribution (\a -> [alignment_pr' a hmms model var_a]) (no_quantile "random_alignment") (RandomStructure do_nothing modifiable_alignment_structure do_sample) ()
+--FIXME: I should make this only trigger if you start looking at the VALUES of the pairwise alignments!
+--FIXME: Maybe I should also reduce this to just a list of pairwise alignments?
+triggered_modifiable_alignment value effect =
+    let a           = modifiable_alignment value
+        triggered_a = effect `seq` a
+    in  (a, triggered_a)
+
+random_alignment tree hmms model tip_lengths var_a = Distribution (\a -> [alignment_pr' a hmms model var_a])
+                                                                  (no_quantile "random_alignment")
+                                                                  (RandomStructure do_nothing triggered_modifiable_alignment do_sample)
+                                                                  ()
     where do_sample = sample_alignment tree hmms tip_lengths
 
--- haskell/Probability/Distribution/Tree.hs
+++ haskell/Probability/Distribution/Tree.hs
@@ -1,66 +1,74 @@
 module Probability.Distribution.Tree where
 
-import Tree
-import Probability.Random
-import Probability.Distribution.Uniform
+import           Tree
+import           Probability.Random
+import           Probability.Distribution.Uniform
 
-xrange start end | start < end = start:xrange (start+1) end
+xrange start end | start < end = start : xrange (start + 1) end
                  | otherwise   = []
 
-pick_index 0 (h:t) = (h,t)
-pick_index 0 [] = error "Trying to pick from empty list!"
-pick_index i (h:t) = let (x, t2) = pick_index (i-1) t
-                     in (x, h:t2)
+pick_index 0 (h : t) = (h, t)
+pick_index 0 []      = error "Trying to pick from empty list!"
+pick_index i (h : t) = let (x, t2) = pick_index (i - 1) t in (x, h : t2)
 
-remove_one [] = error "Cannot remove one from empty list"
-remove_one list = do i <- uniform_int 0 (length list-1)
-                     return $ pick_index i list
+remove_one []   = error "Cannot remove one from empty list"
+remove_one list = do
+    i <- uniform_int 0 (length list - 1)
+    return $ pick_index i list
 
-remove_n 0 list = return ([],list)
-remove_n n list = do (x,list_minus_1) <- remove_one list
-                     (xs,list_minus_n) <- remove_n (n-1) list_minus_1
-                     return ((x:xs),list_minus_n)
+remove_n 0 list = return ([], list)
+remove_n n list = do
+    (x , list_minus_1) <- remove_one list
+    (xs, list_minus_n) <- remove_n (n - 1) list_minus_1
+    return ((x : xs), list_minus_n)
 
 -- choose 2 leaves, connect them to an internal node, and put that internal node on the list of leaves
 -- This is I think gives more weight to more balanced trees?
-random_tree_edges [l1] _         = return []
-random_tree_edges [l1,l2] _      = return [(l1,l2)]
-random_tree_edges leaves (i:is) = do ([l1,l2],leaves') <- remove_n 2 leaves
-                                     other_edges <- random_tree_edges (i:leaves') is
-                                     return $ [(l1,i),(l2,i)]++other_edges
+random_tree_edges [l1]     _        = return []
+random_tree_edges [l1, l2] _        = return [(l1, l2)]
+random_tree_edges leaves   (i : is) = do
+    ([l1, l2], leaves') <- remove_n 2 leaves
+    other_edges         <- random_tree_edges (i : leaves') is
+    return $ [(l1, i), (l2, i)] ++ other_edges
 
 -- Create a tree of size n-1, choose an edge at random, and insert the next leaf there.
-uniform_topology_edges [l1]    _     = return []
-uniform_topology_edges [l1,l2] _     = return [(l1,l2)]
-uniform_topology_edges (l:ls) (i:is) = do es1 <- uniform_topology_edges ls is
-                                          ((x,y),es2) <- remove_one es1
-                                          return $ [(l,i),(x,i),(i,y)] ++ es2
+uniform_topology_edges [l1]     _        = return []
+uniform_topology_edges [l1, l2] _        = return [(l1, l2)]
+uniform_topology_edges (l : ls) (i : is) = do
+    es1           <- uniform_topology_edges ls is
+    ((x, y), es2) <- remove_one es1
+    return $ [(l, i), (x, i), (i, y)] ++ es2
 
 -- We could rewrite uniform_topology_edges to automatically flip and sort the branches with leaf branches first.
 random_tree 1 = return $ Tree (listArray' [[]]) (listArray' []) 1 0
-random_tree n = do let num_nodes = 2*n-2
-                   edges <- uniform_topology_edges [0..n-1] [n..num_nodes-1]
-                   -- This flipping is suppose flip edges from (internal,leaf) -> (leaf, internal)
-                   let maybe_flip (x,y) | (y<x)     = (y,x)
-                                        | otherwise = (x,y)
-                   -- Then the sorting is supposed order edges like (0,_), (1,_), (2,_)
-                   -- in order to assign leaf branches the names 0..n-1
-                   let sorted_edges = sortOn fst $ map maybe_flip edges
-                   -- The number of edges should be 2*n-3, unchangably.
-                   let Tree es ns nn nb = tree_from_edges num_nodes sorted_edges
-                   return (Tree es ns nn (nn-1))
+random_tree n = do
+    let num_nodes = 2 * n - 2
+    edges <- uniform_topology_edges [0 .. n - 1] [n .. num_nodes - 1]
+    -- This flipping is suppose flip edges from (internal,leaf) -> (leaf, internal)
+    let maybe_flip (x, y) | (y < x)   = (y, x)
+                          | otherwise = (x, y)
+    -- Then the sorting is supposed order edges like (0,_), (1,_), (2,_)
+    -- in order to assign leaf branches the names 0..n-1
+    let sorted_edges     = sortOn fst $ map maybe_flip edges
+    -- The number of edges should be 2*n-3, unchangably.
+    let Tree es ns nn nb = tree_from_edges num_nodes sorted_edges
+    return (Tree es ns nn (nn - 1))
 
 
-modifiable_tree mod tree = Tree (listArray' nodes) (listArray' branches) (numNodes tree) (numBranches tree) where
-    nodes =    [ map mod (edgesOutOfNode tree n) | n <- xrange 0 (numNodes tree) ]
-    branches = [ (mod s, mod i, mod t, mod r) | b <- xrange 0 (numBranches tree * 2), let (s,i,t,r) = nodesForEdge tree b]
+modifiable_tree mod tree = Tree (listArray' nodes) (listArray' branches) (numNodes tree) (numBranches tree)  where
+    nodes    = [ map mod (edgesOutOfNode tree n) | n <- xrange 0 (numNodes tree) ]
+    branches = [ (mod s, mod i, mod t, mod r) | b <- xrange 0 (numBranches tree * 2), let (s, i, t, r) = nodesForEdge tree b ]
 
 uniform_topology_pr 1 = doubleToLogDouble 1.0
 uniform_topology_pr 2 = doubleToLogDouble 1.0
-uniform_topology_pr n = uniform_topology_pr (n-1) / (doubleToLogDouble $ intToDouble $ 2*n-5)
+uniform_topology_pr n = uniform_topology_pr (n - 1) / (doubleToLogDouble $ intToDouble $ 2 * n - 5)
 
-triggered_modifiable_tree value rand_var = let tree = modifiable_tree modifiable value
-                                               triggered_tree = modifiable_tree (rand_var `seq`) tree
-                                           in (tree, triggered_tree)
+triggered_modifiable_tree value effect =
+    let tree           = modifiable_tree modifiable value
+        triggered_tree = modifiable_tree (effect `seq`) tree
+    in  (tree, triggered_tree)
 
-uniform_topology n = Distribution (\tree-> [uniform_topology_pr n]) (no_quantile "uniform_topology") (RandomStructure do_nothing triggered_modifiable_tree (random_tree n)) (TreeRange n)
+uniform_topology n = Distribution (\tree -> [uniform_topology_pr n])
+                                  (no_quantile "uniform_topology")
+                                  (RandomStructure do_nothing triggered_modifiable_tree (random_tree n))
+                                  (TreeRange n)
