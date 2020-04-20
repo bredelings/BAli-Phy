e90d51f21073085ea2571111be1eb2f3bd8affa5
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-20 15:46:54 -0700

    Pass the bounds into slice_sample_real_random_variable.

--- haskell/Probability/Distribution/Uniform.hs
+++ haskell/Probability/Distribution/Uniform.hs
@@ -6,10 +6,11 @@ import MCMC
 builtin uniform_density 3 "uniform_density" "Distribution"
 builtin builtin_sample_uniform 3 "sample_uniform" "Distribution"
 
-uniform_effect x = add_move (\c -> slice_sample_real_random_variable x c)
-sample_uniform l u = RandomStructure uniform_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform l u s)))
+uniform_bounds l u = between l u
+uniform_effect l u x = x `seq` bnds `seq` add_move (\c -> slice_sample_real_random_variable x bnds c) where bnds = c_range $ uniform_bounds l u
+sample_uniform l u = RandomStructure (uniform_effect l u) modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform l u s)))
 
-uniform l u = Distribution (make_densities $ uniform_density l u) () (sample_uniform l u) (between l u)
+uniform l u = Distribution (make_densities $ uniform_density l u) () (sample_uniform l u) (uniform_bounds l u)
 
 builtin uniform_int_density 3 "uniform_int_density" "Distribution"
 builtin builtin_sample_uniform_int 3 "sample_uniform_int" "Distribution"
