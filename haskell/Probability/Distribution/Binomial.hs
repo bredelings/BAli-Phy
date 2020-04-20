6cd0cf6efada50d00787f38f5efc0e67bad53d56
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-20 15:57:12 -0700

    Pass integer bounds as arguments to slice sampling.

--- haskell/Probability/Distribution/Binomial.hs
+++ haskell/Probability/Distribution/Binomial.hs
@@ -6,8 +6,10 @@ import MCMC
 builtin binomial_density 3 "binomial_density" "Distribution"
 builtin builtin_sample_binomial 3 "sample_binomial" "Distribution"
 
-binomial_effect x = add_move (\c -> slice_sample_integer_random_variable x c)
+binomial_bounds n = integer_between 0 n
 
-sample_binomial n p = RandomStructure binomial_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_binomial n p s)))
+binomial_effect n x = x `seq` bnds `seq` add_move (\c -> slice_sample_integer_random_variable x bnds c) where bnds = c_range $ binomial_bounds n
 
-binomial n p = Distribution (make_densities $ binomial_density n p) (no_quantile "binomial") (sample_binomial n p) (integer_between 0 n)
+sample_binomial n p = RandomStructure (binomial_effect n) modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_binomial n p s)))
+
+binomial n p = Distribution (make_densities $ binomial_density n p) (no_quantile "binomial") (sample_binomial n p) (binomial_bounds n)
