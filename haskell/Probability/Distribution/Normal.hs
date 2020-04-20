4d7d060e7e97a4687b9390472cbd5551f1d77e82
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-20 15:46:54 -0700

    Pass the bounds into slice_sample_real_random_variable.

--- haskell/Probability/Distribution/Normal.hs
+++ haskell/Probability/Distribution/Normal.hs
@@ -7,8 +7,9 @@ builtin normal_density 3 "normal_density" "Distribution"
 builtin normal_quantile 3 "normal_quantile" "Distribution"
 builtin builtin_sample_normal 3 "sample_normal" "Distribution"
 
-normal_effect x = add_move (\c -> slice_sample_real_random_variable x c)
+normal_bounds = realLine
+normal_effect x = x `seq` bnds `seq` add_move (\c -> slice_sample_real_random_variable x bnds c) where bnds = c_range normal_bounds
 sample_normal m s = RandomStructure normal_effect modifiable_structure $ liftIO (IOAction (\state -> (state, builtin_sample_normal m s state)))
 
-normal m s = Distribution (make_densities $ normal_density m s) (normal_quantile m s) (sample_normal m s) realLine
+normal m s = Distribution (make_densities $ normal_density m s) (normal_quantile m s) (sample_normal m s) normal_bounds
 
