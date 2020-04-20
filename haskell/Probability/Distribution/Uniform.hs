d50e97eeaeb95a208ef0f1d1a046a09c2a9b0152
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-20 15:57:12 -0700

    Pass integer bounds as arguments to slice sampling.

--- haskell/Probability/Distribution/Uniform.hs
+++ haskell/Probability/Distribution/Uniform.hs
@@ -15,7 +15,8 @@ uniform l u = Distribution (make_densities $ uniform_density l u) () (sample_uni
 builtin uniform_int_density 3 "uniform_int_density" "Distribution"
 builtin builtin_sample_uniform_int 3 "sample_uniform_int" "Distribution"
 
-uniform_int_effect x = add_move (\c -> slice_sample_integer_random_variable x c)
-sample_uniform_int l u = RandomStructure uniform_int_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform_int l u s)))
+uniform_int_bounds l u = integer_between l u
+uniform_int_effect l u x = x `seq` bnds `seq` add_move (\c -> slice_sample_integer_random_variable x bnds c) where bnds = c_range $ uniform_int_bounds l u
+sample_uniform_int l u = RandomStructure (uniform_int_effect l u) modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_uniform_int l u s)))
 
 uniform_int l u = Distribution (make_densities $ uniform_int_density l u) () (sample_uniform_int l u) (integer_between l u)
