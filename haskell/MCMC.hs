d0a1bda08cc3233f2974adc0278703c788378357
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-21 07:18:37 -0700

    Don't propose -1 for the geometric.

--- haskell/MCMC.hs
+++ haskell/MCMC.hs
@@ -1,6 +1,7 @@
 module MCMC where
 
 import Foreign.Pair
+import Range
 
 builtin register_transition_kernel 2 "register_transition_kernel" "MCMC"
 
@@ -11,8 +12,10 @@ gibbs_sample_categorical x n c = IOAction (pair_from_c . builtin_gibbs_sample_ca
 builtin builtin_discrete_uniform_avoid_mh 5 "discrete_uniform_avoid_mh" "MCMC"
 discrete_uniform_avoid_mh x low high c = IOAction (pair_from_c . builtin_discrete_uniform_avoid_mh x low high c)
 
-builtin builtin_inc_dec_mh 3 "inc_dec_mh" "MCMC"
-inc_dec_mh x c = IOAction (pair_from_c . builtin_inc_dec_mh x c)
+-- It would be nice if we could (i) seq x and bounds HERE, and (ii) convert range to bounds HERE.
+-- But the seq needs to be done during changeable execution, and we execute the IO unchangeably.
+builtin builtin_inc_dec_mh 4 "inc_dec_mh" "MCMC"
+inc_dec_mh x bnds c = IOAction (pair_from_c . builtin_inc_dec_mh x bnds c)
 
 builtin builtin_slice_sample_real_random_variable 4 "slice_sample_real_random_variable" "MCMC"
 slice_sample_real_random_variable x bnds c = IOAction (pair_from_c . builtin_slice_sample_real_random_variable x bnds c)
