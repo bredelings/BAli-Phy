9387d665a113a5febf5b1b3f16f4c6aed4a27a2f
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-22 18:11:20 -0700

    Temporarily disable rates for 'independent' -- this chokes on infinite lists.

--- haskell/Probability/Distribution/List.hs
+++ haskell/Probability/Distribution/List.hs
@@ -7,9 +7,12 @@ independent_densities [] []         = []
 independent_densities _  _          = [doubleToLogDouble 0.0]
 
 -- This cannot handle infinite lists because it uses the total number of distributions to determine the rate.
-independent dists = Distribution (independent_densities dists) (no_quantile "independent") do_sample (ListRange (map distRange dists))
-             where do_sample = SamplingRate (1.0/sqrt (intToDouble $ length dists)) $ sequence dists
+independent dists = Distribution (independent_densities dists) (no_quantile "independent") (sequence dists) (ListRange (map distRange dists))
+--  If we try and put a sampling rate on this that depends on the length, then we can't handle infinitely long lists.
+--  If we tried to use the sqrt of the number of variables in the list that are instantiated, then that might actually work.
 
+-- If we could make 'independent' work, we could then define `iid n dist = do xs <- independent (repeat dist) ; return $ take n xs`
+--  or something like that.
 iid n dist = Distribution (independent_densities (replicate n dist)) (no_quantile "iid") iid_sample (ListRange $ take n $ repeat $ distRange dist) where
     iid_sample = do xs <- SamplingRate (1.0/sqrt (intToDouble n)) $ sequence (repeat dist)
                     return $ take n xs
