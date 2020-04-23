ff4c64bd94fc35f4b69528b6138283a58fd913a7
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-23 09:37:30 -0700

    Define dirichlet' so that it doesn't throw away all the state when n changes.

--- haskell/Probability/Distribution/Dirichlet.hs
+++ haskell/Probability/Distribution/Dirichlet.hs
@@ -2,14 +2,25 @@ module Probability.Distribution.Dirichlet where
 
 import Probability.Random
 import Probability.Distribution.Gamma
+import Probability.Distribution.List
 
 builtin builtin_dirichlet_density 2 "dirichlet_density" "Distribution"
 dirichlet_density as ps = builtin_dirichlet_density (list_to_vector as) (list_to_vector ps)
+
+-- The `dirichlet` does not handle cases where the number of as changes in a ungraceful way: all entries are resampled!
 sample_dirichlet as = SamplingRate (1.0/sqrt(intToDouble $ length as)) $ do vs <- mapM (\a-> gamma a 1.0) as
                                                                             return $ map (/(sum vs)) vs
+
 dirichlet as = Distribution (make_densities $ dirichlet_density as) (no_quantile "dirichlet") (sample_dirichlet as) (Simplex (length as) 1.0)
 
-dirichlet' n a = dirichlet (replicate n a)
+-- The `v_dirichlet` takes an infinite list of as, and a number of them to keep.
+sample_v_dirichlet n as = SamplingRate (1.0/sqrt(intToDouble n)) $ do vs <- independent [ gamma a 1.0 | a <- as ]
+                                                                      let ws = take n vs
+                                                                      return $ map (/(sum ws)) ws
+
+v_dirichlet n as = Distribution (make_densities $ dirichlet_density (take n as)) (no_quantile "v_dirichlet") (sample_v_dirichlet n as) (Simplex n 1.0)
+
+dirichlet' n a = v_dirichlet n (repeat a)
 
 sample_dirichlet_on items as = do ps <- sample_dirichlet as
                                   return $ zip items ps
