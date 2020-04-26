35f7de5b4460f3d8af45fd77e77d86da97d88810
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-25 19:59:14 -0700

    Add prog_force_counts and vm_force_count.

--- src/computation/machine/tokens.cc
+++ src/computation/machine/tokens.cc
@@ -85,6 +85,8 @@ void reg_heap::destroy_all_computations_in_token(int t)
     tokens[t].vm_result.clear();
 
     tokens[t].vm_force.clear();
+
+    tokens[t].vm_force_count.clear();
 }
 
 void reg_heap::release_tip_token(int t)
@@ -170,6 +172,7 @@ void reg_heap::release_tip_token(int t)
     assert(tokens[t].vm_step.empty());
     assert(tokens[t].vm_result.empty());
     assert(tokens[t].vm_force.empty());
+    assert(tokens[t].vm_force_count.empty());
 }
 
 // Given parent -> t1 -> t2 -> XXX, make t2 a child of parent instead of t1.
@@ -247,6 +250,17 @@ void reg_heap::merge_split_mappings(const vector<int>& knuckle_tokens)
         assert(tokens[t].type != token_type::set);
         tokens[t].type = token_type::merged;
     }
+
+    load_map(tokens[child_token].vm_force_count, prog_temp);
+    for(int t: knuckle_tokens)
+    {
+        assert(token_is_used(t));
+        assert(not tokens[t].is_referenced());
+        assert(tokens[t].children.size() == 1);
+
+        merge_split_mapping_(tokens[t].vm_force_count, tokens[child_token].vm_force_count, prog_temp);
+    }
+    unload_map(tokens[child_token].vm_force_count, prog_temp);
 
     load_map(tokens[child_token].vm_force, prog_temp);
     for(int t: knuckle_tokens)
