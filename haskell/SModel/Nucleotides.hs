94e3389cf568e1de66a89eb567d3cb4586eb685d
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-06-16 10:48:23 -0700

    Put the alphabet argument first.

--- haskell/SModel/Nucleotides.hs
+++ haskell/SModel/Nucleotides.hs
@@ -2,10 +2,10 @@ module SModel.Nucleotides (module SModel.Nucleotides) where
 
 import SModel.ReversibleMarkov
 
-tn93_sym k1 k2 a = gtr_sym [1.0, k1, 1.0, 1.0, k2, 1.0] a
-hky85_sym k a = tn93_sym k k a
+tn93_sym a k1 k2 = gtr_sym [1.0, k1, 1.0, 1.0, k2, 1.0] a
+hky85_sym a k = tn93_sym a k k
 
-k80 kappa a     = gtr a (hky85_sym kappa a) (uniform_frequencies a)
-hky85 k    pi a = gtr a (hky85_sym k a) pi
-tn93 k1 k2 pi a = gtr a (tn93_sym k1 k2 a) pi
+k80 a kappa     = gtr a (hky85_sym a kappa) (uniform_frequencies a)
+hky85 a k    pi = gtr a (hky85_sym a k) pi
+tn93 a k1 k2 pi = gtr a (tn93_sym a k1 k2) pi
 
