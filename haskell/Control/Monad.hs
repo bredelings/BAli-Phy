45204195edf01d44d3fed58026a72d7ceb8ca32a
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-07-20 22:02:27 -0700

    Fill out Control.Monad a bit.

--- haskell/Control/Monad.hs
+++ haskell/Control/Monad.hs
@@ -4,6 +4,8 @@ module Control.Monad where
 import Compiler.Base
 import Data.List
 import Data.Function
+import Data.Ord
+import Compiler.Num
 
 mapM f = sequence . map f
 
@@ -25,14 +27,41 @@ sequence_ (a:as) = do a
 
 (=<<) = flip (>>=)
 
+infixr 1 <=<, >=>
+
 f >=> g = \x -> do y <- f x
                    g y
 
 f <=< g = flip (>=>)
 
+forever as = do as
+                forever as
+
 void f = do x <- f
             return ()
 --
+
+-- join is implemented in Compiler.base
+
+replicateM n a | n <= 0    = return []
+               | otherwise = do x <- a
+                                xs <- replicateM (n-1) a
+                                return (x:xs)
+
+replicateM_ n a | n <= 0     = return ()
+                | otherwise  = do a
+                                  replicateM_ (n-1) a
+
+--
+
+--guard
+
+--when
+
+--unless
+
+---
+
 
 liftM f a = do x <- a
                return $ f x
@@ -44,11 +73,20 @@ liftM2 f a b = do x <- a
 liftM3 f a b c = do x <- a
                     y <- b
                     z <- c
-                    return $ f x y c
+                    return $ f x y z
 
--- LiftM4
+liftM4 f a b c d = do x <- a
+                      y <- b
+                      z <- c
+                      w <- d
+                      return $ f x y z w
 
--- LiftM5
+liftM5 f a b c d e = do x <- a
+                        y <- b
+                        z <- c
+                        w <- d
+                        u <- e
+                        return $ f x y z w u
 
 ap mf mx = do f <- mf
               x <- mx
