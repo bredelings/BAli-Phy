e5ef778b68dcbb28a89ac5bf0482413f5c647595
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-08-12 17:31:47 -0700

    Simplify LDA test.

--- tests/prob_prog/examples.1/LDA/LDA.hs
+++ tests/prob_prog/examples.1/LDA/LDA.hs
@@ -6,28 +6,22 @@ vocabulary = ["bear", "wolf", "python", "prolog"];
 
 ntopics = 2
 
-docs = [
-  ("doc1", words "bear wolf bear wolf bear wolf python wolf bear wolf"),
-  ("doc2", words "python prolog python prolog python prolog python prolog python prolog"),
-  ("doc3", words "bear wolf bear wolf bear wolf bear wolf bear wolf"),
-  ("doc4", words "python prolog python prolog python prolog python prolog python prolog"),
-  ("doc5", words "bear wolf bear python bear wolf bear wolf bear wolf")
-  ]
+docs = map words [
+        "bear wolf bear wolf bear wolf python wolf bear wolf",
+        "python prolog python prolog python prolog python prolog python prolog",
+        "bear wolf bear wolf bear wolf bear wolf bear wolf",
+        "python prolog python prolog python prolog python prolog python prolog",
+        "bear wolf bear python bear wolf bear wolf bear wolf"]
 
-docs_words = map snd docs
-
-doc_lengths = map length docs_words
+doc_lengths = map length docs
 
 word_frequencies_dist = symmetric_dirichlet_on vocabulary 1.0
 
-sample_topics_for_doc nwords = do
-  topic_frequencies <- symmetric_dirichlet ntopics 1.0
-  topics <- iid nwords $ categorical topic_frequencies
-  return (topics, topic_frequencies)
-
 word_dist_for_doc word_frequencies_for_topic nwords = do
-  (topics_for_doc_words, topic_frequencies) <- sample_topics_for_doc nwords
-  return (independent [ categorical_on $ word_frequencies_for_topic!!topic | topic <- topics_for_doc_words ], topic_frequencies)
+  topic_frequencies <- symmetric_dirichlet ntopics 1.0
+  topics  <- iid nwords $ categorical topic_frequencies
+  let word_dist = independent [ categorical_on $ word_frequencies_for_topic!!topic | topic <- topics ]
+  return (word_dist, topic_frequencies)
 
 model = do
   word_frequencies_for_topic <- iid ntopics word_frequencies_dist
@@ -42,5 +36,5 @@ model = do
 
 main = do
   (word_dists, loggers) <- random $ model
-  observe (independent word_dists) docs_words
+  observe (independent word_dists) docs
   return loggers
