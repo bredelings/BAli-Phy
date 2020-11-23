c44fe5851b77e666f13b8ca7518d76261209fda4
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-11-23 12:28:45 -0800

    Add new observe test with random variables.

---
+++ tests/prob_prog/observe/2/Main.hs
@@ -0,0 +1,8 @@
+import           Probability.Random
+import           Probability.Distribution.Normal
+
+main = do
+    x <- random $ normal 0.0 1.0
+    y <- random $ normal x   1.0
+    observe (normal y 1.0) 1.0
+    return []
