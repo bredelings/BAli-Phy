5686763d748bec7a119088e69d8f1a8bb5100cec
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-09 22:05:28 -0800

    Add new test-cases where we want to avoid resampling.

---
+++ tests/prob_prog/no-resample/1/Main.hs
@@ -0,0 +1,19 @@
+import           Probability
+
+my_iid n dist =
+    let body = do
+            x  <- dist
+            xs <- my_iid (n - 1) dist
+            return (x : xs)
+    in  if n == 0 then return [] else body
+
+model = do
+    n  <- geometric 0.25
+    xs <- my_iid n (normal 0.0 1.0)
+    let loggers = ["n" %=% n, "xs" %=% xs]
+    return (sum xs, loggers)
+
+main = do
+    (total, loggers) <- random $ model
+    observe (normal total 1.0) 20.0
+    return loggers
