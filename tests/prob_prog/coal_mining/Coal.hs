44373e75159a023f649a0c95f29879cd9c6f106d
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-12 21:29:47 -0800

    Extend Coal.hs to also observe from the piecewise constant poisson processes.

--- tests/prob_prog/coal_mining/Coal.hs
+++ tests/prob_prog/coal_mining/Coal.hs
@@ -3,7 +3,8 @@ import Probability
 import           Data.ReadFile
 
 times = read_file_as_double "coal-times.dat"
-    
+
+-- This feels pretty hacky
 truncated min max dist = do x <- dist
                             if ok x then
                                 return x
@@ -12,16 +13,21 @@ truncated min max dist = do x <- dist
     where
       ok x = x >= min && x <= max
 
+-- When we get polymorphism, maybe we can create an temporary array as a view on xs'
+-- I presume that using n directly is more efficient that using [length xs']
 orderedSample n dist = do
   xs' <- iid n dist
   let xs = sort xs'
   return $ listArray n xs
 
+get_intervals (r:[]) (t:[]) tend = [(r,t,tend)]
+get_intervals (r:rs) (t1:ts@(t2:_)) tend = (r,t1,t2):get_intervals rs ts tend
+
 prior t1 t2 = do
-  let lam = 3.0; nMax = 30; a = 1.0; b = 200.0
+  let lam = 3.0; nMax = 30; a = 1.0; b = 365.0/200.0
 
   -- n change points, n+1 intervals
-  n <- truncated 1 nMax (poisson lam)
+  n <- poisson lam
 
   -- even numbered order statistics over the interval [t1,t2]
   s' <- orderedSample (2*n+1) (uniform t1 t2)
@@ -30,11 +36,15 @@ prior t1 t2 = do
   -- n+1 rates
   g <- iid (n+1) (gamma a b)
 
-  return (n, s, g)
+  let intervals = get_intervals g s t2
+
+  return (n, s, g, intervals)
 
 main = do
-  (n, s, g) <- random $ prior 1851.0 1963.0
-  
-  return [ "n" %=% n, "s" %=% s, "g" %=% g]
+  (n, s, g, intervals) <- random $ prior 1851.0 1963.0
+
+  observe (poisson_processes intervals) times
+
+  return [ "n" %=% n, "s" %=% s, "g" %=% g, "intervals" %=% intervals]
 
 
