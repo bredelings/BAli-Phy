dd15888a9608fd82eee96e371f573480792af9a9
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-15 09:57:03 -0800

    Simplify example 1.

--- tests/prob_prog/1/Main.hs
+++ tests/prob_prog/1/Main.hs
@@ -8,22 +8,29 @@ xs = read_file_as_double "xs"
 
 n_points = length xs
 
-main = do
+prior = do
 
-  means <- random $ independent $ repeat $ normal 0.0 10.0
-  sigmas <- random $ independent $ repeat $ exponential 1.0
+  n_clusters <- (1+) `liftM` geometric 0.33
+
+  means <- independent $ repeat $ normal 0.0 10.0
+  sigmas <- independent $ repeat $ exponential 1.0
   let all_dists = [normal mean sigma | (mean,sigma) <- zip means sigmas]
-
-  n_clusters' <- random $ geometric 0.33
-  let n_clusters = 1+n_clusters'
 
   let dists = take n_clusters all_dists
 
-  ps <- random $ symmetric_dirichlet n_clusters 0.5
+  ps <- symmetric_dirichlet n_clusters 0.5
+
+  let loggers = ["n_clusters" %=% n_clusters,
+                 "weights" %=% ps,
+                 "mean" %=% take n_clusters means,
+                 "sigma" %=% take n_clusters sigmas]
+
+  return (ps, dists, loggers)
+
+main = do
+
+  (ps, dists, loggers) <- random $ prior
 
   observe (iid n_points (mixture ps dists)) xs
 
-  return ["n_clusters" %=% n_clusters,
-          "weights" %=% ps,
-          "mean" %=% take n_clusters means,
-          "sigma" %=% take n_clusters sigmas]
+  return loggers
