c3d9624e2631a9ea6ad78845c53ae84ebe99d093
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-19 12:05:43 -0800

    Add Data.Frame

---
+++ haskell/Data/Frame.hs
@@ -0,0 +1,12 @@
+module Data.Frame ( csv_to_frame, readTable )
+    where
+
+import qualified Data.Map as Map
+import Data.CSV
+
+-- This gives the csv file as a list of rows
+csv_to_frame :: [ [ String ] ] -> Map String [String]
+
+csv_to_frame (header:rows) = Map.fromList [(field_name, map (!!i) rows) | (i,field_name) <- zip [0..] header]
+
+readTable = csv_to_frame . read_csv
