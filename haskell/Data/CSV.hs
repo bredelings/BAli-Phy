dfcd05e4df459b143dac1ee54ee250d61368aa82
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-19 11:50:09 -0800

    Add read_csv function.

---
+++ haskell/Data/CSV.hs
@@ -0,0 +1,15 @@
+module Data.CSV ( read_csv )
+    where
+
+import Foreign.String
+import Foreign.Vector
+
+map_vector_to_list f = map f . vector_to_list
+
+builtin_read_csv :: CppString -> EVector (EVector CppString)
+builtin builtin_read_csv 1 "read_csv" "Data"
+
+-- This gives the csv file as a list of rows
+read_csv :: String -> [ [ String ] ]
+read_csv file = builtin_read_csv (pack_cpp_string file)
+-- read_csv file = map_vector_to_list (map_vector_to_list unpack_cpp_string) $ builtin_read_csv (pack_cpp_string file)
--- src/builtins/Data.cc
+++ src/builtins/Data.cc
@@ -4,10 +4,34 @@
 #include "computation/machine/args.H"
 #include "util/myexception.H"
 #include "util/io.H"
+#include "util/string/split.H"
 
 using boost::dynamic_pointer_cast;
 using std::vector;
 using std::string;
+
+extern "C" closure builtin_function_read_csv(OperationArgs& Args)
+{
+  const string filename = Args.evaluate(0).as_<String>();
+
+  checked_ifstream text_file(filename,"csv file");
+
+  EVector vec_all_lines;
+
+  string line;
+  while(portable_getline(text_file, line))
+  {
+      // This is probably not very smart -- ignores quoting, etc.
+      // See https://tools.ietf.org/html/rfc4180
+      // Probably I should write an actual parser.
+      EVector vec_line;
+      for(auto field: split(line, ','))
+          vec_line.push_back(String(field));
+      vec_all_lines.push_back(vec_line);
+  }
+
+  return vec_all_lines;
+}
 
 extern "C" closure builtin_function_read_file_lines(OperationArgs& Args)
 {
