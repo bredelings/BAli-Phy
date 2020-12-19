1166200f3ab7f1631ef2019c569dc4c5e76c4197
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-19 12:07:42 -0800

    Haskellize string_to_double

--- haskell/Data/ReadFile.hs
+++ haskell/Data/ReadFile.hs
@@ -3,11 +3,12 @@ module Data.ReadFile where
 import Foreign.String
 
 builtin builtin_read_file_lines 1 "read_file_lines" "Data"
-builtin string_to_double 1 "string_to_double" "Data"
-builtin builtin_string_to_int 1 "string_to_int" "Data"
+builtin cpp_string_to_double 1 "string_to_double" "Data"
+builtin cpp_string_to_int 1 "string_to_int" "Data"
 
 read_file_lines filename = list_from_vector $ builtin_read_file_lines (list_to_string filename)
 
-read_file_as_double filename = map string_to_double $ read_file_lines filename
+read_file_as_double filename = map cpp_string_to_double $ read_file_lines filename
 
-string_to_int = builtin_string_to_int . pack_cpp_string
+string_to_int    = cpp_string_to_int    . pack_cpp_string
+string_to_double = cpp_string_to_double . pack_cpp_string
