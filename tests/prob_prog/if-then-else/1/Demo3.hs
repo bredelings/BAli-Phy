8e5253dfac1a76ab6cdc1a193554cb2e395c4c61
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-31 23:35:32 -0800

    Convert remaining tests.

--- tests/prob_prog/if-then-else/1/Demo3.hs
+++ tests/prob_prog/if-then-else/1/Demo3.hs
@@ -2,10 +2,12 @@ module Demo3 where
 
 import           Probability
 
-main = sample $ do
+model = sample $ do
     i <- bernoulli 0.5
     y <- normal 0.0 1.0
     z <- exponential 0.1
     let x = if i == 1 then y else z
     return ["x" %=% x]
 
+main = do
+  mcmc model
