d3a286ffe34251f2f83644d95246aab44e65e744
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-31 23:35:32 -0800

    Convert remaining tests.

--- tests/prob_prog/order-two/order-two.hs
+++ tests/prob_prog/order-two/order-two.hs
@@ -6,9 +6,12 @@ random_walk x1 x2 = do
     xs <- random_walk x2 x3
     return (x1 : xs)
 
-main = sample $ do
+model = sample $ do
     x1   <- normal 0.0 1.0
     x2   <- normal x1 (sqrt $ 1.0 / 3.0)
     walk <- random_walk x1 x2
     let xs = take 100 walk
     return ["x" %=% xs]
+
+main = do
+  mcmc model
