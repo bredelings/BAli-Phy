fa22e1b9bf68014390f05302e179525791cc9fb7
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-31 23:33:12 -0800

    Convert more tests.

--- tests/prob_prog/examples.1/LDA/LDA.hs
+++ tests/prob_prog/examples.1/LDA/LDA.hs
@@ -6,15 +6,6 @@ vocabulary = ["bear", "wolf", "python", "prolog"];
 
 ntopics = 2
 
-docs = map words [
-        "bear wolf bear wolf bear wolf python wolf bear wolf",
-        "python prolog python prolog python prolog python prolog python prolog",
-        "bear wolf bear wolf bear wolf bear wolf bear wolf",
-        "python prolog python prolog python prolog python prolog python prolog",
-        "bear wolf bear python bear wolf bear wolf bear wolf"]
-
-doc_lengths = map length docs
-
 word_frequencies_dist = symmetric_dirichlet_on vocabulary 1.0
 
 word_dist_for_doc word_frequencies_for_topic nwords = do
@@ -23,7 +14,7 @@ word_dist_for_doc word_frequencies_for_topic nwords = do
   let word_dist = independent [ categorical_on $ word_frequencies_for_topic!!topic | topic <- topics ]
   return (word_dist, topic_frequencies)
 
-model = do
+prior doc_lengths = do
   word_frequencies_for_topic <- iid ntopics word_frequencies_dist
       
   (word_dists, topic_frequencies) <- unzip `liftM` independent [word_dist_for_doc word_frequencies_for_topic nwords | nwords <- doc_lengths]
@@ -34,7 +25,20 @@ model = do
   return (word_dists, loggers)
 
 
-main = do
-  (word_dists, loggers) <- sample $ model
+observe_data docs = do
+  let doc_lengths = map length docs
+  (word_dists, loggers) <- sample $ prior doc_lengths
   docs ~> independent word_dists
   return loggers
+
+
+docs = map words [
+        "bear wolf bear wolf bear wolf python wolf bear wolf",
+        "python prolog python prolog python prolog python prolog python prolog",
+        "bear wolf bear wolf bear wolf bear wolf bear wolf",
+        "python prolog python prolog python prolog python prolog python prolog",
+        "bear wolf bear python bear wolf bear wolf bear wolf"]
+
+main = do
+  let model = observe_data docs
+  mcmc model
