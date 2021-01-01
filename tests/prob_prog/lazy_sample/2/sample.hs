3a524f6de3cdab651146baddbab58e191bb15cf0
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-31 22:58:24 -0800

    Convert lazy_sample/{2,3} to explicit mcmc

--- tests/prob_prog/lazy_sample/2/sample.hs
+++ tests/prob_prog/lazy_sample/2/sample.hs
@@ -1,11 +1,15 @@
 import           Probability
 
-model = do
+prior = do
     x  <- normal 0.0 1.0
     ys <- independent (repeat $ normal 0.0 1.0)
     return $ (x * x) : (take 10 ys)
 
-main = do
-    zs <- sample $ model
-    10.0 ~> normal (zs !! 2) 1.0
+observe_data z' = do
+    zs <- sample $ prior
+    z' ~> normal (zs !! 2) 1.0
     return ["zs" %=% zs]
+
+main = do
+  let model = observe_data 10.0
+  mcmc model
