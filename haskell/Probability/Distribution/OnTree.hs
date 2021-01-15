6b54717b9c9201cc1eb3d440d64cd69265f34335
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-01-15 11:02:50 -0800

    Replace (tree,ds) with (BranchLengthTree tree ds)

--- haskell/Probability/Distribution/OnTree.hs
+++ haskell/Probability/Distribution/OnTree.hs
@@ -11,10 +11,11 @@ import Bio.Sequence -- for sequence_to_indices
 --        This needs to be after weighted_frequency_matrix.
 --        Because we have no polymorphism, wfm needs to be defined after MixtureModel and MixtureModels.
 
-subst_like_on_tree (topology,ds) root as smodel seqs = substitution_likelihood topology root seqs' as alphabet ps f smap
-    where taxa = get_labels topology
+subst_like_on_tree tree root as smodel seqs = substitution_likelihood tree root seqs' as alphabet ps f smap
+    where taxa = get_labels tree
+          ds = Tree.branch_lengths tree
           f = weighted_frequency_matrix smodel
-          ps = transition_p_index (SingleBranchLengthModel topology ds smodel)
+          ps = transition_p_index (SingleBranchLengthModel tree ds smodel)
           seqs' = listArray' $ map (sequence_to_indices alphabet) $ reorder_sequences taxa seqs
           alphabet = getAlphabet smodel
           smap = get_smap smodel
@@ -22,9 +23,10 @@ subst_like_on_tree (topology,ds) root as smodel seqs = substitution_likelihood t
 ctmc_on_tree tree root as smodel =
     Distribution (\seqs -> [subst_like_on_tree tree root as smodel seqs]) (no_quantile "ctmc_on_tree") () ()
 
-subst_likelihood_fixed_A (topology,lengths) smodel sequences = likelihood
-    where (_,_,_,likelihood) = observe_partition_type_1 topology lengths smodel sequences subst_root
-          subst_root = numNodes topology - 1
+subst_likelihood_fixed_A tree smodel sequences = likelihood
+    where (_,_,_,likelihood) = observe_partition_type_1 tree lengths smodel sequences subst_root
+          lengths = Tree.branch_lengths tree
+          subst_root = numNodes tree - 1
 
 ctmc_on_tree_fixed_A tree smodel =
     Distribution (\seqs -> [subst_likelihood_fixed_A tree smodel seqs]) (no_quantile "ctmc_on_tree_fixed_A") () ()
