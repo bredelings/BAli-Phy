23fb24428bcc43b06154db17e243b4ed9244c478
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-01-14 21:50:23 -0800

    Reorganize infer_tree/1

--- tests/prob_prog/infer_tree/1/Main.hs
+++ tests/prob_prog/infer_tree/1/Main.hs
@@ -9,7 +9,7 @@ import           Probability.Distribution.OnTree
 
 -- issues: 1. likelihood seems wrong - -1300 vs -700.
 --         2. no topology moves included.
-sample_imodel topology = do
+imodel_prior topology = do
     logLambda   <- log_laplace (-4.0) 0.707
     mean_length <- (1.0+) <$> exponential 10.0
 
@@ -18,13 +18,13 @@ sample_imodel topology = do
 
     return (imodel, loggers)
 
-sample_smodel = do
-    pi     <- dirichlet_on ["A", "C", "G", "T"] [1.0, 1.0, 1.0, 1.0]
+smodel_prior = do
+    freqs  <- symmetric_dirichlet_on ["A", "C", "G", "T"] 1.0
     kappa1 <- log_normal 0.0 1.0
     kappa2 <- log_normal 0.0 1.0
 
-    let tn93_model = tn93 dna kappa1 kappa2 (frequencies_from_dict dna pi)
-    let loggers = ["kappa1" %=% kappa1, "kappa2" %=% kappa2, "pi" %=% pi]
+    let tn93_model = tn93 dna kappa1 kappa2 (frequencies_from_dict dna freqs)
+    let loggers    = ["kappa1" %=% kappa1, "kappa2" %=% kappa2, "frequencies" %=% freqs]
 
     return (tn93_model, loggers)
 
@@ -38,27 +38,36 @@ sample_alignment (topology,ds) imodel tip_seq_lengths = do
     alignment_on_tree <- random_alignment topology hmms imodel tip_seq_lengths
     return $ Bio.Alignment.pairwise_alignments alignment_on_tree
 
-prior taxa tip_seq_lengths = do
-
+tree_prior taxa = do
     topology <- uniform_labelled_topology taxa
 
     let b           = numBranches topology
-        root        = targetNode topology 0
 
     times                     <- iid b (gamma 0.5 (2.0 / intToDouble b))
 
     scale                     <- gamma 0.5 2.0
 
     let distances = map (scale*) times
-        tree      = branch_length_tree topology distances
 
-    (smodel, smodel_loggers) <- sample_smodel
+    let loggers = ["scale" %=% scale]
 
-    (imodel, imodel_loggers) <- sample_imodel tree
+    return (branch_length_tree topology distances, loggers)
+
+prior taxa tip_seq_lengths = do
+
+    (tree,tree_loggers)       <- tree_prior taxa
+
+    let (topology,_) = tree
+
+    let root        = targetNode topology 0
+
+    (smodel, smodel_loggers) <- smodel_prior
+
+    (imodel, imodel_loggers) <- imodel_prior topology
 
     as                       <- Main.sample_alignment tree imodel tip_seq_lengths
 
-    let loggers = ["topology" %=% write_newick tree, "scale" %=% scale, "tn93" %>% smodel_loggers, "rs07" %>% imodel_loggers]
+    let loggers = ["tree" %=% write_newick tree, "tree" %>% tree_loggers, "tn93" %>% smodel_loggers, "rs07" %>% imodel_loggers]
 
     return (ctmc_on_tree tree root as smodel, loggers)
 
