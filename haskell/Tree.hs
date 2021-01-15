6cde6dd7be88ed99cb6339c1433f41103f7b6635
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-01-15 10:25:21 -0800

    Add BranchLengthTree and NodeHeightTree.

--- haskell/Tree.hs
+++ haskell/Tree.hs
@@ -7,21 +7,48 @@ data RootedTree = RootedTree Tree Int (Array Int Bool)
 
 data LabelledTree = LabelledTree Tree [String]
 
+-- Here the Tree/RootedTree are _classes_ not objects...
+-- Should we make LabelledTree,LabelledBranchLengthTree,LabelledNodeHeighTree
+--  and have them implement get_labels?
+-- It looks like add_labels would be a bit more complicated...
+data BranchLengthTree = BranchLengthTree Tree (Array Double)
+
+data NodeHeightTree   = NodeHeightTree RootedTree (Array Double)
+
 edgesOutOfNode (Tree nodesArray _ _ _) node = nodesArray ! node
-edgesOutOfNode (RootedTree t _ _) node = edgesOutOfNode t node
-edgesOutOfNode (LabelledTree t _) node = edgesOutOfNode t node
+edgesOutOfNode (RootedTree t _ _) node      = edgesOutOfNode t node
+edgesOutOfNode (LabelledTree t _) node      = edgesOutOfNode t node
+edgesOutOfNode (BranchLengthTree t _) node  = edgesOutOfNode t node
+edgesOutOfNode (NodeHeightTree t _) node    = edgesOutOfNode t node
 
 nodesForEdge (Tree _ branchesArray _ _) edgeIndex = branchesArray ! edgeIndex
-nodesForEdge (RootedTree t _ _) edgeIndex = nodesForEdge t edgeIndex
-nodesForEdge (LabelledTree t _) edgeIndex = nodesForEdge t edgeIndex
+nodesForEdge (RootedTree t _ _) edgeIndex         = nodesForEdge t edgeIndex
+nodesForEdge (LabelledTree t _) edgeIndex         = nodesForEdge t edgeIndex
+nodesForEdge (BranchLengthTree t _) edgeIndex     = nodesForEdge t edgeIndex
+nodesForEdge (NodeHeightTree t _) edgeIndex       = nodesForEdge t edgeIndex
 
-numNodes (Tree _ _ n _) = n
-numNodes (RootedTree t _ _) = numNodes t
-numNodes (LabelledTree t _) = numNodes t
+numNodes (Tree _ _ n _)         = n
+numNodes (RootedTree t _ _)     = numNodes t
+numNodes (LabelledTree t _)     = numNodes t
+numNodes (BranchLengthTree t _) = numNodes t
+numNodes (NodeHeightTree t _)   = numNodes t
 
-numBranches (Tree _ _ _ n) = n
-numBranches (RootedTree t _ _) = numBranches t
-numBranches (LabelledTree t _) = numBranches t
+numBranches (Tree _ _ _ n)         = n
+numBranches (RootedTree t _ _)     = numBranches t
+numBranches (LabelledTree t _)     = numBranches t
+numBranches (BranchLengthTree t _) = numBranches t
+numBranches (NodeHeightTree t _)   = numBranches t
+
+branch_lengths   (BranchLengthTree _ ds) = ds
+branch_lengths t@(NodeHeightTree _ hs)   = mkArray (numBranches t) (\b -> branch_length t b)
+
+branch_length (BranchLengthTree _ ds) b = ds!b
+branch_length (NodeHeightTree t   hs) b = abs (hs!source - hs!target)
+    where source = sourceNode t b
+          target = targetNode t b
+
+scale_branch_lengths factor (BranchLengthTree t ds) = (BranchLengthTree t ds')
+    where ds' = arrayMap (factor*) ds
 
 -- Given that this is a tree, would numNodes t - numBranches t + 2 work for n_leaves >=3?
 numLeaves t = length $ leaf_nodes t
@@ -32,10 +59,20 @@ root (LabelledTree t _) = root t
 remove_root (RootedTree t _ _) = t
 remove_root (LabelledTree t labels) = LabelledTree (remove_root t) labels
 
+get_labels (Tree _ _ _ _)          = error "get_labels: trying to get labels from an unlabelled tree!"
+get_labels (RootedTree _ _ _)      = error "get_labels: trying to get labels from an unlabelled tree!"
 get_labels (LabelledTree _ labels) = labels
-add_labels t labels = LabelledTree t labels
+get_labels (BranchLengthTree t _)  = get_labels t
+get_labels (NodeHeightTree t _)    = get_labels t
+
+add_labels t@(Tree _ _ _ _)      labels   = LabelledTree t labels
+add_labels rt@(RootedTree _ _ _) labels   = LabelledTree rt labels
+add_labels (LabelledTree _ _)    labels   = error "add_labels: trying to add labels to an already-labelled tree!"
+add_labels (BranchLengthTree t ds) labels = BranchLengthTree (add_labels t labels) ds
+add_labels (NodeHeightTree t hs) labels   = NodeHeightTree (add_labels t labels) hs
 
 add_root (LabelledTree t labels) r = LabelledTree (add_root t r) labels
+add_root (BranchLengthTree t ds) r = BranchLengthTree (add_root t r) ds
 add_root t r = rt
     where check_away_from_root b = (sourceNode rt b == root rt) || (or $ map (away_from_root rt) (edgesBeforeEdge rt b))
           nb = numBranches t * 2
@@ -43,15 +80,16 @@ add_root t r = rt
 
 make_rooted tree = add_root tree (numNodes tree - 1)
 
-away_from_root (RootedTree t r arr) b = arr!b
-away_from_root (LabelledTree t _) b = away_from_root t b
+away_from_root (RootedTree t r arr    ) b = arr!b
+away_from_root (LabelledTree t _      ) b = away_from_root t b
+away_from_root (BranchLengthTree t _  ) b = away_from_root t b
+away_from_root (NodeHeightTree   t _  ) b = away_from_root t b
+away_from_root (Tree _ _ _ _          ) b = error "away_from_root: unrooted tree!"
 
 toward_root    rt b = not $ away_from_root rt b
 
-parentBranch (LabelledTree t _) n = parentBranch t n
 parentBranch rooted_tree n = listToMaybe [b | b <- edgesOutOfNode rooted_tree n, toward_root rooted_tree b]
 
-parentNode (LabelledTree t _) n = parentNode t n
 parentNode rooted_tree n = case parentBranch rooted_tree n of Just b  -> Just $ targetNode rooted_tree b
                                                               Nothing -> Nothing
 
