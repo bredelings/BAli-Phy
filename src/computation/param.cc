40086522980a988488d34488313c0b722f9fdc01
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-04-24 14:42:40 -0400

    Add context_ptr class.

--- src/computation/param.cc
+++ src/computation/param.cc
@@ -5,6 +5,7 @@
 #include "computation/expression/modifiable.H"
 #include "computation/expression/list.H"
 #include "computation/context.H"
+#include "computation/machine/graph_register.H"
 
 #include "util/log-level.H"
 
@@ -111,7 +112,7 @@ param get_param(context_ref& C, const expression_ref& E, eval_strategy s)
     }
 }
 
-vector<param> get_params_from_list_(context_ref& C, const expression_ref& structure, std::optional<int> check_size)
+vector<param> get_params_from_list_(context_ref& C, const expression_ref& structure, optional<int> check_size)
 {
     if (log_verbose >= 3)
         std::cerr<<"structure = "<<structure<<"\n\n";
@@ -128,14 +129,14 @@ vector<param> get_params_from_list_(context_ref& C, const expression_ref& struct
     return params;
 }
 
-vector<param> get_params_from_list(context_ref& C, const expression_ref& list, std::optional<int> check_size)
+vector<param> get_params_from_list(context_ref& C, const expression_ref& list, optional<int> check_size)
 {
     expression_ref structure = C.evaluate_expression({var("Parameters.maybe_modifiable_structure"), list});
 
     return get_params_from_list_(C, structure, check_size);
 }
 
-vector<param> get_params_from_array_(context_ref& C, const expression_ref& structure, std::optional<int> check_size)
+vector<param> get_params_from_array_(context_ref& C, const expression_ref& structure, optional<int> check_size)
 {
     vector<param> params;
 
@@ -151,10 +152,67 @@ vector<param> get_params_from_array_(context_ref& C, const expression_ref& struc
     return params;
 }
 
-vector<param> get_params_from_array(context_ref& C, const expression_ref& array, std::optional<int> check_size)
+vector<param> get_params_from_array(context_ref& C, const expression_ref& array, optional<int> check_size)
 {
     expression_ref structure = C.evaluate_expression({var("Parameters.maybe_modifiable_structure"), array});
 
     return get_params_from_array_(C, structure, check_size);
+}
+
+context_ptr context_ptr::operator[](int i) const
+{
+    int r = C.memory()->closure_at(reg).reg_for_slot(i);
+    return {C, r};
+}
+
+context_ptr context_ptr::result() const
+{
+    auto cp2 = *this;
+    cp2.move_to_result();
+    return cp2;
+}
+
+optional<context_ptr> context_ptr::modifiable() const
+{
+    auto cp2 = *this;
+    if (cp2.move_to_modifiable())
+        return cp2;
+    else
+        return {};
+}
+
+void context_ptr::move_to_result()
+{
+    reg = C.memory()->value_for_reg(reg);
+    assert(reg != -1);
+}
+
+bool context_ptr::move_to_modifiable()
+{
+    auto mod_reg = C.find_modifiable_reg(reg);
+    if (mod_reg)
+    {
+        reg = *mod_reg;
+        return true;
+    }
+    else
+        return false;
+}
+
+int context_ptr::size() const
+{
+    return C.memory()->expression_at(reg).size();
+}
+
+expression_ref context_ptr::head() const
+{
+    return C.memory()->expression_at(reg).head();
+}
+
+context_ptr::context_ptr(const context_ref& c, int i)
+    :C(c)
+{
+    auto [r1,_] = C.incremental_evaluate(i);
+    reg = r1;
 }
 
