27448c4eb35ca2692b370620d158157b6de26db2
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-05-11 09:49:37 -0400

    Correctly prefix Map.Map

--- haskell/Data/Frame.hs
+++ haskell/Data/Frame.hs
@@ -5,7 +5,7 @@ import qualified Data.Map as Map
 import Data.CSV
 
 -- This gives the csv file as a list of rows
-csv_to_frame :: [ [ String ] ] -> Map String [String]
+csv_to_frame :: [ [ String ] ] -> Map.Map String [String]
 
 csv_to_frame (header:rows) = Map.fromList [(field_name, map (!!i) rows) | (i,field_name) <- zip [0..] header]
 
