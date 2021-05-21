f71557bca3107c1d84c1399ef4b9a001d193c21e
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-05-21 13:35:33 -0400

    Add note on Partition.get_imodel

--- haskell/BAliPhy/ATModel/DataPartition.hs
+++ haskell/BAliPhy/ATModel/DataPartition.hs
@@ -11,7 +11,12 @@ data PairwiseAlignment
 
 data Partition = Partition {
       get_smodel :: SModel,
+
+-- FIXME: Should we delete the imodel, or register it as an in_edge?
+-- We only need the imodel for computing sequence length probabilities.
+-- And we only use them in other_prior and A{3,5}::correction to check stuff.
       get_imodel :: Maybe IModel,
+
       get_tree :: BranchLengthTree,
       get_alignment :: AlignmentOnTree,
       get_hmms :: Maybe (Array Int HMM)
