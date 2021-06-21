dac22b820d7b19e8ecde5981eb8e090de1a8bf49
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-06-21 18:53:06 -0400

    Annotate inputs to laplace distribution.

--- haskell/Probability/Distribution/Laplace.hs
+++ haskell/Probability/Distribution/Laplace.hs
@@ -10,4 +10,9 @@ laplace_bounds = realLine
 laplace_effect x = add_move $ slice_sample_real_random_variable x laplace_bounds
 sample_laplace m s = RandomStructure laplace_effect modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_laplace m s state)))
 
-laplace m s = Distribution "laplace" (make_densities $ laplace_density m s) () (sample_laplace m s) laplace_bounds
+annotated_laplace_density m' s' x = do
+  m <- in_edge "m" m'
+  s <- in_edge "s" s'
+  return [laplace_density m s x]
+
+laplace m s = Distribution "laplace" (annotated_laplace_density m s) () (sample_laplace m s) laplace_bounds
