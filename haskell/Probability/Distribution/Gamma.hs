aadd7c49202f72b4e8e3d9bb2cb8382d4b792554
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-06-21 18:52:54 -0400

    Split gamma and shifted_gamma.

--- haskell/Probability/Distribution/Gamma.hs
+++ haskell/Probability/Distribution/Gamma.hs
@@ -11,5 +11,25 @@ gamma_bounds shift = above shift
 gamma_effect shift x = add_move $ slice_sample_real_random_variable x (gamma_bounds shift)
 sample_shifted_gamma a b shift = RandomStructure (gamma_effect shift) modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_shifted_gamma a b shift s)))
 
-shifted_gamma a b shift = Distribution "gamma" (make_densities $ shifted_gamma_density a b shift) (shifted_gamma_quantile a b shift) (sample_shifted_gamma a b shift) (gamma_bounds shift)
-gamma a b = shifted_gamma a b 0.0
+annotated_shifted_gamma_density a' b' shift' x = do
+  a <- in_edge "a" a'
+  b <- in_edge "b" b'
+  shift <- in_edge "shift" shift'
+  return [shifted_gamma_density a b shift x]
+
+shifted_gamma a b shift = Distribution "shifted_gamma" (annotated_shifted_gamma_density a b shift) (shifted_gamma_quantile a b shift) (sample_shifted_gamma a b shift) (gamma_bounds shift)
+
+
+---------------------
+
+gamma_density a b = shifted_gamma_density a b 0.0
+gamma_quantile a b = shifted_gamma_quantile a b 0.0
+sample_gamma a b = sample_shifted_gamma a b 0.0
+
+annotated_gamma_density a' b' x = do
+  a <- in_edge "a" a'
+  b <- in_edge "b" b'
+  return [gamma_density a b x]
+
+gamma a b = Distribution "gamma" (annotated_gamma_density a b) (gamma_quantile a b) (sample_gamma a b) (gamma_bounds 0.0)
+
