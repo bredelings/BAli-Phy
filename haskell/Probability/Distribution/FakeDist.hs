5d7ce4e068af56a6ef80633862dc84caebd50110
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-06-28 21:30:29 -0400

    Register in-edges and properties for fixed-alignment partitions also.

--- haskell/Probability/Distribution/FakeDist.hs
+++ haskell/Probability/Distribution/FakeDist.hs
@@ -4,8 +4,6 @@ import Probability.Random
 
 fake_sample_error = error "sampling from fake_dist is not allowed.  You can only observe it."
 fake_range_error = error "fake_dist has no range.  You can only observe it."
-fake_dist likelihood = Distribution "fake_dist" (\x-> return [likelihood]) (no_quantile "fake_dist") fake_sample_error fake_range_error
-
 
 annotated_fake_dist_0_pr tree' alignment' smodel' subst_root transition_ps cls ancestral_sequences likelihood sequences = do
   tree <- in_edge "tree" tree'
@@ -19,3 +17,15 @@ annotated_fake_dist_0_pr tree' alignment' smodel' subst_root transition_ps cls a
 
 fake_dist_0 tree alignment smodel subst_root transition_ps cls ancestral_sequences likelihood =
     Distribution "fake_dist_0" (annotated_fake_dist_0_pr tree alignment smodel subst_root transition_ps cls ancestral_sequences likelihood) (no_quantile "fake_dist_0") fake_sample_error fake_range_error
+
+annotated_fake_dist_1_pr tree' smodel' subst_root transition_ps cls ancestral_sequences likelihood sequences = do
+  tree <- in_edge "tree" tree'
+  smodel <- in_edge "smodel" smodel'
+  property "subst_root" subst_root
+  property "transition_ps" transition_ps
+  property "cond_likes" cls
+  property "anc_seqs" ancestral_sequences
+  return $ tree `seq` smodel `seq` [likelihood]
+
+fake_dist_1 tree smodel subst_root transition_ps cls ancestral_sequences likelihood =
+    Distribution "fake_dist_1" (annotated_fake_dist_1_pr tree smodel subst_root transition_ps cls ancestral_sequences likelihood) (no_quantile "fake_dist_1") fake_sample_error fake_range_error
--- src/models/parameters.cc
+++ src/models/parameters.cc
@@ -1745,7 +1745,7 @@ std::string generate_atmodel_program(int n_sequences,
             program.let(Tuple(transition_ps, cls_var, ancestral_sequences_var, likelihood_var),
                         {var("observe_partition_type_1"), tree, smodel,sequence_data_var, subst_root_var});
 
-            program.perform({var("~>"),sequence_data_var,{var("fake_dist"),likelihood_var}});
+            program.perform({var("~>"),sequence_data_var,{var("fake_dist_1"),tree, smodel, subst_root_var, transition_ps, cls_var, ancestral_sequences_var, likelihood_var}});
         }
         else
             std::abort();
