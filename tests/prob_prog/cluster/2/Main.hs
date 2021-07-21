7072917111f5068333f1bd8834a26066cd53c682
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-21 08:32:12 -0700

    Update cluster/2 test.

--- tests/prob_prog/cluster/2/Main.hs
+++ tests/prob_prog/cluster/2/Main.hs
@@ -8,36 +8,23 @@ cluster_dist = do
   sigma <- exponential 1.0
   return (mean, sigma)
 
-prior = do
+model xs = do
 
   n <- (1+) <$> geometric 0.33
 
   clusters <- iid n cluster_dist
 
-  let dists = [normal mean sigma | (mean,sigma) <- clusters]
-
   ps <- symmetric_dirichlet n 0.5
 
-  let loggers = ["n_clusters" %=% n,
-                 "weights" %=% ps,
-                 "clusters" %=% clusters]
-
-  return (ps, dists, loggers)
-
-observe_data xs = do
-
-  (ps, dists, loggers) <- sample $ prior
-
   let n_points = length xs
+      dists = [normal mean sigma | (mean,sigma) <- clusters]
 
   xs ~> iid n_points (mixture ps dists)
 
-  return loggers
+  return ["n_clusters" %=% n, "weights" %=% ps, "clusters" %=% clusters]
 
 main = do
   xtable <- readTable "x.csv"
   let xs = xtable $$ ("x", AsDouble)
 
-  let model = observe_data xs
-
-  mcmc model
+  mcmc $ model xs
