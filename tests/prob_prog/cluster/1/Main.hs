bf3401695c46663967df5adf259f1b2e5c73025c
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-21 08:31:02 -0700

    Update cluster/1 test.

--- tests/prob_prog/cluster/1/Main.hs
+++ tests/prob_prog/cluster/1/Main.hs
@@ -7,23 +7,17 @@ cluster_dist = do
   let sigma = 1.0/prec
   return (mean,sigma)
 
-prior n_points = do
+model xs = do
+
+  let n_points = length xs
 
   alpha <- gamma 0.5 10.0
 
   params <- dp n_points alpha cluster_dist
 
-  let loggers = ["alpha" %=% alpha, "params" %=% params]
-
-  return (alpha, params, loggers)
-
-observe_data xs = do
-
-  let n_points = length xs
-
-  (alpha, params, loggers) <- sample $ prior n_points
-
   xs ~> independent [normal mean sigma | (mean,sigma) <- params]
+
+  let loggers = ["alpha" %=% alpha, "params" %=% params]
 
   return loggers
 
@@ -34,7 +28,5 @@ main = do
 
   let xs = xtable $$ ("x", AsDouble)
 
-  let model = observe_data xs
-
-  mcmc model
+  mcmc $ model xs
 
