0544e6eb7400ba2a966eda7d665ed90753b2e1c8
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-23 14:14:10 -0700

    Put MFix back into the strict monad for now.

--- haskell/Probability/Random.hs
+++ haskell/Probability/Random.hs
@@ -89,6 +89,7 @@ run_strict (SamplingRate _ a) = run_strict a
 -- These are the lazily executed parts of the strict monad.
 run_strict dist@(Distribution _ _ _ _ _) = run_lazy dist
 run_strict e@(WithTKEffect _ _) = run_lazy e
+run_strict (MFix f) = mfix (run_lazy . f)
 run_strict (Lazy r) = unsafeInterleaveIO $ run_lazy r
 
 
@@ -131,6 +132,7 @@ run_strict' rate (SamplingRate rate2 a) = run_strict' (rate*rate2) a
 -- These are the lazily executed parts of the strict monad.
 run_strict' rate dist@(Distribution _ _ _ _ _) = run_lazy' rate dist
 run_strict' rate e@(WithTKEffect _ _) = run_lazy' rate e
+run_strict' rate (MFix f) = mfix (run_lazy' rate . f)
 run_strict' rate (Lazy r) = unsafeInterleaveIO $ run_lazy' rate r
 
 -- 1. Could we somehow implement slice sampling windows for non-contingent variables?
