1b5c2c388d54082e3399e599c8e31f35634ee73f
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-22 20:16:56 -0700

    Don't run distributions inside unsafeInterleaveIO in the strict monad.
    It is fragile, since adding (1+) <$> ... would defeat it, for example.
    
    We always force the results of operations in the strict monad, unless
    they are marked "lazy".

--- haskell/Probability/Random.hs
+++ haskell/Probability/Random.hs
@@ -87,7 +87,7 @@ run_strict (PerformTKEffect _) = return ()
 run_strict (AddMove _) = return ()
 run_strict (SamplingRate _ a) = run_strict a
 -- These are the lazily executed parts of the strict monad.
-run_strict dist@(Distribution _ _ _ _ _) = unsafeInterleaveIO $ run_lazy dist
+run_strict dist@(Distribution _ _ _ _ _) = run_lazy dist
 run_strict (Lazy r) = unsafeInterleaveIO $ run_lazy r
 
 
@@ -128,7 +128,7 @@ run_strict' rate (PerformTKEffect e) = run_tk_effects rate e
 run_strict' rate (AddMove m) = register_transition_kernel rate m
 run_strict' rate (SamplingRate rate2 a) = run_strict' (rate*rate2) a
 -- These are the lazily executed parts of the strict monad.
-run_strict' rate dist@(Distribution _ _ _ _ _) = unsafeInterleaveIO $ run_lazy' rate dist
+run_strict' rate dist@(Distribution _ _ _ _ _) = run_lazy' rate dist
 run_strict' rate (Lazy r) = unsafeInterleaveIO $ run_lazy' rate r
 
 -- 1. Could we somehow implement slice sampling windows for non-contingent variables?
