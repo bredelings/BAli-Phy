8e208f4958acfe7c935239f887896360b439abdf
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-31 01:05:04 -0700

    Simplify infer_tree/3/Main.hs

--- tests/prob_prog/infer_tree/3/Main.hs
+++ tests/prob_prog/infer_tree/3/Main.hs
@@ -19,31 +19,27 @@ smodel_prior codons = do
     let mut_sel_model w = gtr' sym pi nucleotides & SModel.x3 codons & dNdS w & mut_sel' ws
         m3_model = mut_sel_model & SModel.m3 ps omegas
 
-    let loggers = ["gtr:sym" %=% sym, "gtr:pi" %=% pi,
+    let loggers = ["gtr:sym" %=% sym,
+                   "gtr:pi" %=% pi,
                    "mut_sel:2ns" %=% ws,
-                   "m3:ps" %=% ps, "m3:omegas" %=% omegas]
+                   "m3:ps" %=% ps,
+                   "m3:omegas" %=% omegas]
 
     return (m3_model, loggers)
 
-tree_prior taxa = do
-
-    topology <- uniform_labelled_topology taxa
-    let b = numBranches topology
-    times <- iid b (gamma 0.5 (2.0 / intToDouble b))
-    scale <- gamma 0.5 2.0
-    let tree      = scale_branch_lengths scale $ branch_length_tree topology times
-
-    let loggers   = ["tree" %=% write_newick tree, "scale" %=% scale]
-    return (tree, loggers)
+branch_length_dist topology b = gamma 0.5 (2.0 / intToDouble n) where n = numBranches topology
 
 model seq_data = do
+
     let taxa = map sequence_name seq_data
 
-    (tree  , tree_loggers) <- tree_prior taxa
+    scale <- gamma 0.5 2.0
+
+    tree <- scale_branch_lengths scale <$> uniform_labelled_tree taxa branch_length_dist
 
     (smodel, sloggers    ) <- smodel_prior (codons dna standard_code)
 
-    let loggers = tree_loggers ++ ["S1" %>% sloggers]
+    let loggers = ["tree" %=% write_newick tree, "scale" %=% scale, "S1" %>% sloggers]
 
     seq_data ~> ctmc_on_tree_fixed_A tree smodel
 
