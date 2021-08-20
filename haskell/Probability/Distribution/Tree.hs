991c1d0bdd65fb6e180803620c771bb38488b8ce
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-08-20 12:53:33 -0400

    Add tree and alignment moves for uniform_labelled_tree

--- haskell/Probability/Distribution/Tree.hs
+++ haskell/Probability/Distribution/Tree.hs
@@ -79,6 +79,7 @@ triggered_modifiable_tree = triggered_modifiable_structure modifiable_cayley_tre
 uniform_topology_effect tree = do
 --  add_move $ walk_tree_sample_NNI_unsafe tree  -- probably we should ensure that the probability of the alignment is zero if pairwise alignments don't match?
   add_move $ walk_tree_sample_alignments tree  -- maybe this should be elsewhere?
+                                               -- if this were elsewhere then we would have to walk the whole tree for each partition... which might not be terrible...
   add_move $ walk_tree_sample_NNI tree         -- does this handle situations with no data partitions?
 
 uniform_topology n = Distribution "uniform_topology"
@@ -91,13 +92,39 @@ uniform_labelled_topology taxa = do
   topology <- uniform_topology (length taxa)
   return $ add_labels taxa topology
 
+add_alignment_moves tree = do
+  SamplingRate 1.0 $ add_move $ walk_tree_sample_alignments tree
+  SamplingRate 0.1 $ add_move $ realign_from_tips tree
+
+add_SPR_moves tree = do
+  SamplingRate 1.0 $ add_move $ sample_SPR_all tree
+  SamplingRate 0.5 $ add_move $ sample_SPR_flat tree
+  SamplingRate 0.5 $ add_move $ sample_SPR_nodes tree
+
+add_topology_moves tree = do
+  SamplingRate 1.0 $ add_SPR_moves tree
+
+
+add_length_moves tree = do
+  SamplingRate 1.0 $ add_move $ walk_tree_sample_branch_lengths tree
+
+add_tree_moves tree = do
+  SamplingRate 1.0 $ add_length_moves tree
+  SamplingRate 1.0 $ add_move $ walk_tree_sample_NNI_and_branch_lengths tree
+  SamplingRate 2.0 $ add_move $ walk_tree_sample_NNI tree  -- if alignment is fixed this is really cheap -- increase weight?
+  SamplingRate 0.5 $ add_move $ walk_tree_sample_NNI_and_A tree
+
+add_tree_alignment_moves tree = do
+  SamplingRate 2.0 $ add_tree_moves tree
+  SamplingRate 1.0 $ add_alignment_moves tree
+
 uniform_labelled_tree taxa branch_lengths_dist = do
-  -- These lines should be under SamplingRate 0.0
-  topology <- uniform_labelled_topology taxa
+  -- These lines should be under SamplingRate 0.0 -- but then the polytomy trees won't work
+  topology <- SamplingRate 0.0 $ uniform_labelled_topology taxa
+  -- Q. How can we do walk_tree and then run the MCMC kernels that affect a given branch?
   branch_lengths <- independent [branch_lengths_dist topology b | b <- [0..numBranches topology-1]]
-  -- Add MCMC moves here!
-  -- How can we do walk_tree and then run the MCMC kernels that affect a given branch?
-  return $ branch_length_tree topology branch_lengths
+  let tree = branch_length_tree topology branch_lengths
+  return tree `with_tk_effect` add_tree_alignment_moves
 
 ----
 -- choose 2 leaves, connect them to an internal node, and put that internal node on the list of leaves
