b4c2bd452f68bd8859422c016ca91ed5bf87338a
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-09-28 23:25:22 -0400

    Make a Matrix type.

--- haskell/Data/Matrix.hs
+++ haskell/Data/Matrix.hs
@@ -2,6 +2,8 @@ module Data.Matrix where
 
 import Foreign.Vector
 
+data Matrix
+
 fromLists xss = fromVectors $ list_to_vector $ map list_to_vector xss
 
 builtin scaleMatrix 2 "scaleMatrix" "SModel"
