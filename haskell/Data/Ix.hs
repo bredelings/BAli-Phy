906bb1681db5dc50a782f2083c19795756b618dc
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-10-11 12:36:35 -0400

    Declare class Ix

--- haskell/Data/Ix.hs
+++ haskell/Data/Ix.hs
@@ -9,3 +9,5 @@ range     (start,end) = [start..end]
 index     (start,end) val = val - start
 inRange   (start,end) val = (start <= val) && (val <= end)
 rangeSize (start,end) = end - start + 1
+
+class Ord a => Ix a
