e33e2b866529a2383ef22f744ca09b9cc8dbfd64
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-11-29 17:11:39 -0500

    Start adding typechecking infrastructure.

---
+++ src/computation/typecheck/typecheck.cc
@@ -0,0 +1,132 @@
+#include "typecheck.H"
+#include <range/v3/all.hpp>
+#include "parser/haskell.H"
+
+#include "immer/map.hpp" // for immer::map
+
+namespace views = ranges::views;
+
+typedef immer::map<Haskell::TypeVar,expression_ref> substitution_t;
+
+namespace std
+{
+    template <>
+    class hash < Haskell::TypeVar >{
+    public :
+        size_t operator()(const Haskell::TypeVar &x) const
+        {
+            size_t h = std::hash<std::string>()(unloc(x.name));
+            if (x.index)
+                size_t h = std::hash<int>()(*x.index) ^ h;
+            return  h ;
+        }
+    };
+}
+
+expression_ref apply_subst(const substitution_t& s, const Haskell::Type& t)
+{
+    if (auto tv = t.to<Haskell::TypeVar>())
+    {
+        if (auto t2 = s.find(*tv))
+            return *t2;
+        else
+            return t;
+    }
+    else if (auto p_forall = t.to<Haskell::ForallType>())
+    {
+        auto forall = *p_forall;
+
+        auto s2 = s;
+        for(auto& tv: forall.type_var_binders)
+            s2 = s2.erase(tv);
+
+        forall.type =  apply_subst(s2, forall.type);
+        return forall;
+    }
+    else if (auto p_app = t.to<Haskell::TypeApp>())
+    {
+        auto app = *p_app;
+        app.head = apply_subst(s, app.head);
+        app.arg  = apply_subst(s, app.arg);
+        return app;
+    }
+//    else if (is_type_con(t))
+//        return t;
+
+    std::abort();
+}
+
+// This should yield a substitution that is equivalent to apply FIRST s1 and THEN s2,
+// like f . g
+substitution_t compose(substitution_t s2, substitution_t s1)
+{
+    if (s2.empty()) return s1;
+
+    auto s3 = s2;
+    for(auto& [tv,e]: s1)
+        s3 = s3.insert({tv,apply_subst(s2,e)});
+    return s3;
+}
+
+bool occurs_check(const Haskell::TypeVar& tv, const expression_ref& t)
+{
+    if (auto x = t.to<Haskell::TypeVar>())
+        return tv == *x;
+    else if (auto f = t.to<Haskell::ForallType>())
+    {
+        for(auto& x: f->type_var_binders)
+            if (x == tv)
+                return false;
+        return occurs_check(tv, f->type);
+    }
+    else if (auto p_app = t.to<Haskell::TypeApp>())
+        return occurs_check(tv, p_app->head) or occurs_check(tv, p_app->arg);
+//    else if (is_type_con(t))
+//        return false;
+    else
+    {
+        throw myexception()<<"types do not unify!";
+    }
+}
+
+// Is there a better way to implement this?
+substitution_t unify(const expression_ref& t1, const expression_ref& t2)
+{
+    if (t1.is_a<Haskell::TypeApp>() and t2.is_a<Haskell::TypeApp>())
+    {
+        auto& app1 = t1.as_<Haskell::TypeApp>();
+        auto& app2 = t2.as_<Haskell::TypeApp>();
+
+        auto s1 = unify(app1.head, app2.head);
+        auto s2 = unify(apply_subst(s1, app1.arg), apply_subst(s1, app2.arg));
+        return compose(s2,s1);
+    }
+    else if (auto tv1 = t1.to<Haskell::TypeVar>())
+    {
+        substitution_t s;
+        if (t1 == t2)
+            return s;
+        if (occurs_check(*tv1, t2))
+            throw myexception()<<"Occurs check: cannot construct infinite type: "<<*tv1<<" ~ "<<t2<<"\n";
+        return s.insert({*tv1, t2});
+    }
+    else if (auto tv2 = t2.to<Haskell::TypeVar>())
+    {
+        substitution_t s;
+        if (t1 == t2)
+            return s;
+        if (occurs_check(*tv2,t1))
+            throw myexception()<<"Occurs check: cannot construct infinite type: "<<*tv2<<" ~ "<<t1<<"\n";
+        return s.insert({*tv2, t1});
+    }
+//    else if (is_type_con(t1) and
+//             is_type_con(t2) and
+//             t1.as_<type_con>() == t2.as_<type_con>())
+//    {
+//        return {};
+//    }
+    else
+    {
+        throw myexception()<<"types do not unify!";
+    }
+}
