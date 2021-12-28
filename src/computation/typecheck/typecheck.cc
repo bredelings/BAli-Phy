59e94591b17d21392d668cdd600ecd98b94f3db5
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-12-28 08:48:38 -0800

    Move kind checking inside the type checker.

--- src/computation/typecheck/typecheck.cc
+++ src/computation/typecheck/typecheck.cc
@@ -1,4 +1,5 @@
 #include "typecheck.H"
+#include "kindcheck.H"
 
 #include <set>
 
@@ -10,9 +11,12 @@
 
 #include "util/set.H"
 
+#include "util/graph.H" // for get_ordered_strong_components( )
+
 #include "computation/expression/apply.H"
 #include "computation/expression/tuple.H" // for is_tuple_name( )
 #include "computation/operation.H" // for is_non_apply_op( )
+#include "computation/module.H"    // for is_qualified_symbol( ), get_module_name( )
 
 namespace views = ranges::views;
 
@@ -22,6 +26,7 @@ using std::map;
 using std::set;
 using std::pair;
 using std::shared_ptr;
+using std::tuple;
 
 typedef immer::map<Hs::TypeVar, Hs::Type> substitution_t;
 
@@ -53,6 +58,297 @@ namespace std
             return  h ;
         }
     };
+}
+
+set<string> free_type_names_from_type(const Haskell::Type& type)
+{
+    set<string> tvars;
+    if (auto tc = type.to<Haskell::TypeCon>())
+    {
+        auto& name = unloc(tc->name);
+        if (is_qualified_symbol(name))
+            tvars.insert(name);
+    }
+    else if (type.is_a<Haskell::TypeApp>())
+    {
+        auto& app = type.as_<Haskell::TypeApp>();
+        add(tvars, free_type_names_from_type(app.head));
+        add(tvars, free_type_names_from_type(app.arg));
+    }
+    else if (type.is_a<Haskell::TupleType>())
+    {
+        auto& tuple = type.as_<Haskell::TupleType>();
+        for(auto element_type: tuple.element_types)
+            add(tvars, free_type_names_from_type(element_type));
+    }
+    else if (type.is_a<Haskell::ListType>())
+    {
+        auto& list = type.as_<Haskell::ListType>();
+        return free_type_names_from_type(list.element_type);
+    }
+    else if (auto forall = type.to<Haskell::ForallType>())
+    {
+        return free_type_names_from_type(forall->type);
+    }
+    return tvars;
+}
+
+set<string> free_type_vars_from_type(const Haskell::Type& type)
+{
+    set<string> tvars;
+    if (auto tv = type.to<Haskell::TypeVar>())
+    {
+        auto& name = unloc(tv->name);
+        tvars.insert(name);
+    }
+    else if (type.is_a<Haskell::TypeApp>())
+    {
+        auto& app = type.as_<Haskell::TypeApp>();
+        add(tvars, free_type_vars_from_type(app.head));
+        add(tvars, free_type_vars_from_type(app.arg));
+    }
+    else if (type.is_a<Haskell::TupleType>())
+    {
+        auto& tuple = type.as_<Haskell::TupleType>();
+        for(auto element_type: tuple.element_types)
+            add(tvars, free_type_vars_from_type(element_type));
+    }
+    else if (type.is_a<Haskell::ListType>())
+    {
+        auto& list = type.as_<Haskell::ListType>();
+        return free_type_vars_from_type(list.element_type);
+    }
+    else if (auto forall = type.to<Haskell::ForallType>())
+    {
+        tvars = free_type_vars_from_type(forall->type);
+        for(auto& type_var: forall->type_var_binders)
+        {
+            tvars.erase(unloc(type_var.name));
+        }
+    }
+    return tvars;
+}
+
+set<string> free_type_vars(const Haskell::Context& context)
+{
+    set<string> tvars;
+    for(auto& constraint: context.constraints)
+        add(tvars, free_type_vars_from_type(constraint));
+    return tvars;
+}
+
+set<string> free_type_names(const Haskell::Context& context)
+{
+    set<string> tvars;
+    for(auto& constraint: context.constraints)
+        add(tvars, free_type_names_from_type(constraint));
+    return tvars;
+}
+
+set<string> free_type_names(const Haskell::ClassDecl& class_decl)
+{
+    // QUESTION: We are ignoring default methods here -- should we?
+    set<string> tvars;
+    add(tvars, free_type_names(class_decl.context));
+    if (class_decl.decls)
+    {
+        for(auto& decl: unloc(*class_decl.decls))
+        {
+            if (decl.is_a<Haskell::SignatureDecl>())
+            {
+                auto& T = decl.as_<Haskell::SignatureDecl>();
+                add(tvars, free_type_names_from_type(T.type));
+            }
+        }
+    }
+    return tvars;
+}
+
+set<string> free_type_names(const Haskell::DataOrNewtypeDecl& type_decl)
+{
+    set<string> tvars;
+    add(tvars, free_type_names(type_decl.context));
+    for(auto& constr: type_decl.constructors)
+    {
+        if (constr.context)
+            add(tvars, free_type_names(*constr.context));
+
+        if (constr.is_record_constructor())
+        {
+            auto& field_decls = std::get<1>(constr.fields).field_decls;
+            for(auto& field: field_decls)
+                add(tvars, free_type_names_from_type(field.type));
+        }
+        else
+        {
+            auto& types = std::get<0>(constr.fields);
+            for(auto& type: types)
+                add(tvars, free_type_names_from_type(type));
+        }
+    }
+    return tvars;
+}
+
+set<string> free_type_names(const Haskell::TypeSynonymDecl& synonym_decl)
+{
+    return free_type_names_from_type(unloc(synonym_decl.rhs_type));
+}
+
+set<string> free_type_names(const Haskell::InstanceDecl& instance_decl)
+{
+    set<string> tvars;
+    add(tvars, free_type_names(instance_decl.context));
+    tvars.insert(instance_decl.name);
+    for(auto& type_arg: instance_decl.type_args)
+        add(tvars, free_type_names_from_type(type_arg));
+    return tvars;
+
+}
+
+set<string> from_this_module(const string& mod_name, set<string> names)
+{
+    set<string> ok_names;
+    for(auto& name: names)
+        if (get_module_name(name) == mod_name)
+            ok_names.insert(name);
+    return ok_names;
+}
+
+vector<vector<expression_ref>> find_type_groups(const Module& m, const vector<expression_ref>& initial_class_and_type_decls)
+{
+    // 1. Collection type and instance declarations
+
+    // [(name,names-we-depend-on)]  No instances.
+    map<string,set<string>> referenced_types;
+
+    map<string, expression_ref> decl_for_type;
+
+    vector<tuple<Haskell::InstanceDecl,set<string>>> instance_decls;
+
+    for(auto& decl: initial_class_and_type_decls)
+    {
+        if (decl.is_a<Haskell::ClassDecl>())
+        {
+            auto& class_decl = decl.as_<Haskell::ClassDecl>();
+            referenced_types[class_decl.name] = free_type_names(class_decl);
+            decl_for_type[class_decl.name] = decl;
+        }
+        else if (decl.is_a<Haskell::DataOrNewtypeDecl>())
+        {
+            auto& type_decl = decl.as_<Haskell::DataOrNewtypeDecl>();
+            referenced_types[type_decl.name] = free_type_names(type_decl);
+            decl_for_type[type_decl.name] = decl;
+        }
+        else if (decl.is_a<Haskell::TypeSynonymDecl>())
+        {
+            auto& type_decl = decl.as_<Haskell::TypeSynonymDecl>();
+            referenced_types[type_decl.name] = free_type_names(type_decl);
+            decl_for_type[type_decl.name] = decl;
+        }
+        else if (decl.is_a<Haskell::InstanceDecl>())
+        {
+            auto& instance_decl = decl.as_<Haskell::InstanceDecl>();
+            instance_decls.push_back({instance_decl, free_type_names(instance_decl)});
+        }
+        else
+            std::abort();
+    }
+
+    // 2. Get order list of mutually dependent TYPE declarations
+
+    // Input to the dependency analysis is a list of
+    // * (declaration, name, names that this declaration depends on)
+
+    for(auto& [type, referenced_types]: referenced_types)
+        referenced_types = from_this_module(m.name, referenced_types);
+
+    auto ordered_name_groups = get_ordered_strong_components(referenced_types);
+
+    auto type_decl_groups = map_groups( ordered_name_groups, decl_for_type );
+
+    // 3. Compute kinds for type/class constructors.
+    for(auto& type_decl_group: type_decl_groups)
+    {
+        kindchecker_state K(m);
+        for(auto& [name,ka]: K.infer_kinds(type_decl_group))
+        {
+            auto& [arity,k] = ka;
+            auto tinfo = m.lookup_resolved_type(name);
+            tinfo.arity = arity;
+            tinfo.k     = k;
+        }
+    }
+
+
+    // 4. Compute types and kinds for data constructors and class methods?
+    for(auto& type_decl_group: type_decl_groups)
+    {
+        kindchecker_state K(m);
+        for(auto& _: K.infer_child_types(type_decl_group))
+        {
+//            auto& [arity,k] = ka;
+//            auto& tinfo = types.at(name);
+//            tinfo.arity = arity;
+//            tinfo.k     = k;
+        }
+    }
+
+    // FIXME: Handle instances.
+
+    // Instances: an instance is a function from dictionaries a dictionaries.
+    //    instance (A a, B b) => A (b a) is a function of the form \dict_A_a dict_B_b -> dict_A_(b_a)
+
+    // Q: How are instances grouped?
+    // A: Each instance needs to be at-or-after all the types/classes referenced,
+    //    Do instances depend on other instances?  Maybe this is check in the context...
+    //    e.g. instance Eq a => Eq [a] where
+
+    // See equivalents in GHC Rename/Module.hs
+    // We are trying to find strongly connected components of
+    //  types, type classes, and instances.
+
+    // Shouldn't instances be AFTER everything?
+    // * We only have type class instances (ClsInstDecl), but GHC
+    //   also has data family instances and type family instances.
+
+    // GHC looks at types and classes first, then adds instances to the SCCs later.
+
+
+    // 5. Compute types for functions.
+
+    //   Does the type-checker need to augment all bound variables with their type?
+
+    //   Does the type-checker need to add type lambdas?
+
+    //   Does the type-checker need to specify type arguments to type lambdas?
+
+    //   So, let, lambda, and case would need to specify the type
+
+    // 6. Compute types for class default methods?
+
+    // Q: How are default method declarations handled here?
+    //    Do they affect type class resolution?
+    //    Do we need to do more work on them when handling value decls?
+    // A: I think default methods do not affect the type.
+
+    // See function `rnTyClDecls`, which calls `depAnalTyClDecls`.
+    // * Dependency analysis on values can be done by name, since instances are not included.
+    // * Code is in GHC.Data.Graph.Directed.
+
+    // I don't think we need to look up "parents" during typechecking unless we are promoting data constructors
+    // to types or kinds.
+
+    // For values, each value can have a body decl, a fixity decl, and a signature decl.
+    // So we can't use the decl itself as the key -- we have to use something like the name.
+
+    // It looks like GHC rename extracts the "free variables" from everything.
+    // For example: rnSrcInstDecl operates on ClsInstD, which wraps ClsInstDecl from Hs/Decl.hs
+
+    // FreeVars = OccEnv ID.  See Core/Opt/OccurAnal.hs.
+
+    // Looks like code for determining inlining
+
+    return type_decl_groups;
 }
 
 expression_ref apply_subst(const substitution_t& s, const Hs::Type& t);
@@ -1305,7 +1601,7 @@ Hs::Type remove_top_level_foralls(Hs::Type t)
     return t;
 }
 
-void typecheck(const Hs::ModuleDecls& M)
+void typecheck(const Module& m, const Hs::ModuleDecls& M)
 {
     // 1. Check the module's type declarations, and derives a Type Environment TE_T:(TCE_T, CVE_T)
     //    OK, so datatypes produce a
@@ -1332,6 +1628,8 @@ void typecheck(const Hs::ModuleDecls& M)
 
     //
 
+    auto class_and_type_decls = find_type_groups(m, M.type_decls);
+    
     typechecker_state state;
     global_value_env env0;
 
