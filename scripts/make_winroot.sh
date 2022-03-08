4bf08bca026dc1c41c64631015158e1144cbf147
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-03-08 17:35:41 -0500

    Update scripts/make_winroot.sh

--- scripts/make_winroot.sh
+++ scripts/make_winroot.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# parse command line arguments
+while echo $1 | grep ^- > /dev/null; do
+    # intercept help while parsing "-key value" pairs
+    if [ "$1" = "--help" ] || [ "$1" = "-h" ]
+    then
+        echo 'Command line options are:
+-h                              : print this help and exit.
+-download        <true|false>   : download MINGW packages.
+
+Example:
+  ./make_winroot.sh -help true'
+        exit
+    fi
+
+    # parse pairs
+    eval $( echo $1 | sed 's/-//g' | tr -d '\012')=$2
+    shift
+    shift
+done
+
 SYSROOT=$HOME/win_root
 
 # 1. Make sysroot
@@ -12,15 +32,17 @@ mkdir -p "${SYSROOT}/bin"
 CROSSNAME=win64-cross.txt
 echo
 echo "2. Writing cross file to '${CROSSNAME}'"
+
 cat > "${CROSSNAME}" <<EOF
 [binaries]
 c = ['ccache','/usr/bin/x86_64-w64-mingw32-gcc-posix']
 cpp = ['ccache','/usr/bin/x86_64-w64-mingw32-g++-posix']
 ar = '/usr/bin/x86_64-w64-mingw32-ar'
 strip = '/usr/bin/x86_64-w64-mingw32-strip'
+pkgconfig = '/usr/bin/pkg-config'
 exe_wrapper = 'wine64' # A command used to run generated executables.
 
-# We need these compiler args to find BOOST, which doesn't use pkg-config
+# why do we still need these? shouldn't they get added automatically if we find boost?
 [built-in options]
 c_args = ['-I${SYSROOT}/mingw64/include']
 c_link_args = ['-L${SYSROOT}/mingw64/lib']
@@ -30,7 +52,8 @@ cpp_link_args = ['-L${SYSROOT}/mingw64/lib']
 
 [properties]
 sys_root = '${SYSROOT}'
-pkg_config_libdir = '${SYSROOT}/mingw64/lib/pkgconfig' 
+pkg_config_libdir = '${SYSROOT}/mingw64/lib/pkgconfig'
+boost_root='${SYSROOT}/mingw64'
 
 [host_machine]
 system = 'windows'
@@ -39,22 +62,52 @@ cpu = 'x86_64'
 endian = 'little'
 EOF
 
-# 3. Download boost
-echo
-echo "3. Installing boost to ${SYSROOT}"
-echo
+# 3. Download packages
+if [ "$download" = "false" ] ; then
+    echo
+    echo "(3.) Skipping installation of packages to ${SYSROOT}."
+    echo
+else
+
+    echo
+    echo "3. Installing packages to ${SYSROOT}"
+    echo
 cd ${SYSROOT}
-PKGS="boost-1.75.0-2 libwinpthread-git-9.0.0.6090.ad98746a-1"
-# We are going to build out own boost, for now.
-# Do we need libwinpthread?  There's a system version in /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll
+
+# Note that the use of gcc-posix and g++-posix means that we need
+# *-posix/libgcc_s_seh-1.dll and *-posix2/libstdc++-6.dll instead
+# of the *-win32/ versions.
+PKGS="boost-1.75.0-2
+libwinpthread-git-9.0.0.6090.ad98746a-1
+"
+
+# We are going to build our own boost, for now.
+# We're going to use the /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll instead of downloading winpthread.
 PKGS=""
+
 for PKG in ${PKGS} ; do
     FILE=mingw-w64-x86_64-${PKG}-any.pkg.tar.zst
-    rm -f ${FILE}
-    wget --no-verbose --show-progress http://repo.msys2.org/mingw/x86_64/${FILE}
-    tar -I zstd -xf ${FILE}
-    rm ${FILE}
+    if [ -e "${FILE}" ] ; then
+        echo "   ${PKG} already downloaded and installed."
+    else
+        URL="http://repo.msys2.org/mingw/x86_64/${FILE}"
+        if ! wget --no-verbose --show-progress "${URL}" ; then
+            echo "Failed to download ${URL}"
+            exit
+        fi
+        if tar -I zstd -xf ${FILE} ; then
+            echo "${PKG} installed"
+        else
+            rm ${FILE}
+            echo "Failed to install ${PKG}"
+            echo
+            echo "Perhaps the decompression program zstd is not installed?"
+            echo "Try 'sudo apt install zstd'."
+            exit
+        fi
+    fi
 done
+fi
 
 echo
 echo "Done."
