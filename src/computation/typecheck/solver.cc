16badc2919eb0e711bcb59ffc4b68df23e72c9b4
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-07 16:21:58 -0400

    Move some code to solver.cc

---
+++ src/computation/typecheck/solver.cc
@@ -0,0 +1,363 @@
+#include "typecheck.H"
+#include "kindcheck.H"
+
+#include "util/set.H"
+
+#include <range/v3/all.hpp>
+
+using std::string;
+using std::vector;
+using std::map;
+using std::set;
+using std::pair;
+using std::optional;
+using std::tuple;
+
+namespace views = ranges::views;
+
+pair<local_instance_env, map<Hs::TypeVar, local_instance_env>>
+ambiguities(const set<Hs::TypeVar>& tvs1, const set<Hs::TypeVar>& tvs2, local_instance_env lie)
+{
+    // The input lie MUST be substituted to find its free type vars!
+    // lie = apply_current_subst(lie);
+    auto ambiguous_tvs = free_type_variables(lie) - tvs1 - tvs2;
+
+    // 1. Record the constraints WITH ambiguous type vars, by type var
+    map<Hs::TypeVar,local_instance_env> ambiguities;
+    for(auto& ambiguous_tv: ambiguous_tvs)
+    {
+        local_instance_env lie_for_tv;
+        for(auto& [dvar,constraint]: lie)
+        {
+            if (free_type_variables(constraint).count(ambiguous_tv))
+                lie_for_tv = lie_for_tv.insert({dvar,constraint});
+        }
+        if (not lie_for_tv.empty())
+            ambiguities.insert({ambiguous_tv, lie_for_tv});
+    }
+
+    // 2. Find the constraints WITHOUT ambiguous type vars
+    local_instance_env unambiguous_preds;
+
+    for(auto& [dvar, constraint]: lie)
+    {
+        auto ftvs = free_type_variables(constraint);
+        if (not intersects(ftvs, ambiguous_tvs))
+            unambiguous_preds = unambiguous_preds.insert({dvar, constraint});
+    }
+
+    return {unambiguous_preds, ambiguities};
+}
+
+
+bool type_is_hnf(const Hs::Type& type)
+{
+    auto [head,args] = decompose_type_apps(type);
+
+    if (head.is_a<Hs::TypeVar>())
+        return true;
+    else if (head.is_a<Hs::TypeCon>())
+        return false;
+    else if (head.is_a<Hs::ListType>())
+        return false;
+    else if (head.is_a<Hs::TupleType>())
+        return false;
+    else
+        std::abort();
+}
+
+// OK:     K a, K (a b), K (a [b]), etc. OK
+// NOT OK: K [a], K (a,b), etc. NOT OK.
+// Question: for multiparameter type classes, how about i.e. `K Int a`?
+bool constraint_is_hnf(const Hs::Type& constraint)
+{
+    auto [class_con, args] = decompose_type_apps(constraint);
+    for(auto& arg: args)
+        if (not type_is_hnf(arg))
+            return false;
+    return true;
+}
+
+
+// 1. An instance looks like (forall as.Q1(as) => Q2(as))
+// 2. We have some constraints Q3.
+// 3. We instantiate the instance with substitutions [as->gs].
+// 4. We'd like to check if Q3 ~ [as->gs]Q2, where only the variables gs are allowed to be unified.
+// 5. If we give the unification variables gs a higher level, can we guarantee that only
+//    gs will be constrained?
+// 6. Actually, I don't think so... Suppose that the instance is (Eq Int) and the constraint is
+//    Eq a.
+// 7. Unless we actually FORBID unification of variables at any higher level, then this won't work.
+// 8. Simply forbidding substitution to a deeper depth won't cut it.
+
+optional<pair<Hs::Var,vector<Hs::Type>>> typechecker_state::lookup_instance(const Hs::Type& constraint)
+{
+    for(auto& [name, type]: gie)
+    {
+        auto [_, instance_constraints, instance_head] = instantiate(type);
+
+        // Skip if this is not an instance.
+        if (constraint_is_hnf(instance_head)) continue;
+
+        auto s = ::maybe_match(instance_head, constraint);
+
+        // This instance doesn't match.
+        if (not s) continue;
+
+        for(auto& instance_constraint: instance_constraints)
+            instance_constraint = apply_subst(*s, instance_constraint);
+
+        auto dfun = Hs::Var({noloc, name});
+
+        return {{dfun, instance_constraints}};
+    }
+    return {};
+}
+
+pair<Hs::Binds,local_instance_env> typechecker_state::toHnf(const string& name, const Hs::Type& constraint)
+{
+    Hs::Binds binds;
+    local_instance_env lie;
+    if (constraint_is_hnf(constraint))
+    {
+        lie = lie.insert({name, constraint});
+    }
+    else
+    {
+        local_instance_env lie2;
+
+        // 1. find the (single) matching instance or fail.
+        // The instance will be of the form
+        //     dfun :: forall  a1 a2 a3. (constraint, constraint, constraint) => K (T b1 b2 b3)
+        // We need to substitute into this definition to make K (T b1 b2 b3) == constraint.
+        auto instance = lookup_instance(constraint);
+        if (not instance)
+            throw myexception()<<"No instance for '"<<constraint<<"'";
+
+        auto [dfun,new_constraints] = *instance;
+
+        // 2. We need to make up new dvar names for all the input constraints.
+        // Then we would add to the decls:
+        //     dvar_orig = dfun dvar_new1 dvar_new2 dvar_new3
+        // And we would get a NEW lie:
+        //     dvar_new1 :: constraint1
+        //     dvar_new2 :: constraint2
+        //     dvar_new3 :: constraint3
+        expression_ref rhs = dfun;
+        for(auto& new_constraint: new_constraints)
+        {
+            auto dvar = fresh_var("dvar", false);
+            lie2 = lie2.insert({unloc(dvar.name), new_constraint});
+            rhs = {rhs,dvar};
+        }
+
+        Hs::Var dvar({noloc, name});
+
+        Hs::Decls decls;
+        decls.push_back(Hs::simple_decl(dvar, rhs));
+
+        // 3. Finally, we may need to further simplify the new LIE
+        auto [binds2, lie3] = toHnfs(lie2);
+
+        lie = lie3;
+        binds = binds2;
+        binds.push_back(decls);
+    }
+    return {binds,lie};
+}
+
+pair<Hs::Binds, local_instance_env>
+typechecker_state::toHnfs(const local_instance_env& lie_in)
+{
+    Hs::Binds binds_out;
+    local_instance_env lie_out;
+    for(auto& [name, constraint]: lie_in)
+    {
+        auto [binds2, lie2] = toHnf(name, constraint);
+        for(auto& bind: binds2)
+            binds_out.push_back(bind);
+        lie_out += lie2;
+    }
+    return {binds_out, lie_out};
+}
+
+// FIXME: there should be a `const` way of getting these.
+// FIXME: instantiate is not constant though.
+// FIXME: we shouldn't need fresh type vars if the type is unambiguous though.
+vector<pair<string, Hs::Type>> typechecker_state::superclass_constraints(const Hs::Type& constraint)
+{
+    vector<pair<string, Hs::Type>> constraints;
+
+    for(auto& [name, type]: gie)
+    {
+        // Klass a => Superklass a
+        auto [_, class_constraints, superclass_constraint] = instantiate(type, false);
+
+        // Skip if this is not a method of extracting superclass dictionaries
+        if (not constraint_is_hnf(superclass_constraint)) continue;
+
+        assert(class_constraints.size() == 1);
+
+        auto class_constraint = class_constraints[0];
+        auto s = ::maybe_match(class_constraint, constraint);
+
+        // The premise doesn't match the current class;
+        if (not s) continue;
+
+        superclass_constraint = apply_subst(*s, superclass_constraint);
+
+        constraints.push_back( { name, superclass_constraint } );
+    }
+
+    return constraints;
+}
+
+// We are trying to eliminate the *first* argument.
+optional<vector<string>> typechecker_state::is_superclass_of(const Hs::Type& constraint1, const Hs::Type& constraint2)
+{
+    vector<string> extractors;
+    if (same_type(constraint1, constraint2))
+        return extractors;
+    else
+    {
+        // dvar1 :: constraint1 => dvar3 :: constraint3 => dvar2 :: constraint2
+        for(auto& [name, constraint3]: superclass_constraints(constraint2))
+        {
+            if (auto extractors2 = is_superclass_of(constraint1, constraint3))
+            {
+                extractors = std::move(*extractors2);
+                extractors.push_back(name);
+                return extractors;
+            }
+        }
+        return {};
+    }
+}
+
+optional<Hs::Binds> typechecker_state::entails_by_superclass(const pair<string, Hs::Type>& to_keep, const pair<string, Hs::Type>& to_remove)
+{
+    auto& [dvar_to_keep_name, constraint_to_keep] = to_keep;
+    auto& [dvar_to_remove_name, constraint_to_remove] = to_remove;
+
+    if (auto extractors = is_superclass_of(constraint_to_remove, constraint_to_keep))
+    {
+        Hs::Var dvar_to_keep({noloc, dvar_to_keep_name});
+        Hs::Var dvar_to_remove({noloc, dvar_to_remove_name});
+
+        Hs::Exp dict_exp = dvar_to_keep;
+        for(auto& extractor: *extractors | views::reverse)
+        {
+            Hs::Var get_dict({noloc, extractor});
+            dict_exp = {get_dict, dict_exp};
+        }
+
+        Hs::Decls decls;
+        // dvar_to_remove = extractor[n] extractor[n-1] ... extractor[0] dvar_to_keep
+        decls.push_back( Hs::simple_decl(dvar_to_remove, dict_exp) );
+        Hs::Binds binds;
+        binds.push_back(decls);
+        return binds;
+    }
+    else
+        return {};
+}
+
+// How does this relate to simplifying constraints?
+optional<Hs::Binds> typechecker_state::entails(const local_instance_env& lie1, const local_instance_env& lie2)
+{
+    Hs::Binds binds;
+    for(auto& constraint: lie2)
+    {
+        auto binds1 = entails(lie1, constraint);
+        if (not binds1)
+            return {};
+        ranges::insert(binds, binds.begin(), *binds1);
+    }
+    return binds;
+}
+
+pair<Hs::Binds, local_instance_env> typechecker_state::simplify(const local_instance_env& lie)
+{
+    Hs::Binds binds_out;
+    local_instance_env lie_out;
+    vector<pair<string,Hs::Type>> lie_vec;
+    for(auto& entry: lie)
+       lie_vec.push_back(entry);
+    vector<pair<string,Hs::Type>> checked;
+
+    for(int i=0;i<lie_vec.size();i++)
+    {
+        auto& pred = lie_vec[i];
+        auto preds = views::concat(lie_vec | views::drop(i+1), checked);
+        if (auto new_binds = entails(preds, pred))
+            ranges::insert(binds_out, binds_out.begin(), *new_binds);
+        else
+            checked.push_back(lie_vec[i]);
+    }
+
+    for(auto& var_equals_constraint: checked)
+        lie_out = lie_out.insert(var_equals_constraint);
+
+    return {binds_out, lie_out};
+}
+
+pair<Hs::Binds, local_instance_env> typechecker_state::reduce(const local_instance_env& lie)
+{
+    auto [binds1, lie1] = toHnfs(lie);
+
+    auto [binds2, lie2] = simplify(lie1);
+
+    auto binds = binds2;
+    for(auto& bind: binds1)
+        binds.push_back(bind);
+
+    return {binds, lie2};
+}
+
+Hs::Binds typechecker_state::reduce_current_lie()
+{
+    auto& lie = current_lie();
+
+    lie = apply_current_subst( lie );
+
+    auto [binds, new_lie] = reduce( lie );
+
+    lie = new_lie;
+
+    return binds;
+}
+
+
+tuple<substitution_t, Hs::Binds, local_instance_env>
+typechecker_state::default_preds( const set<Hs::TypeVar>& fixed_tvs,
+                                  const set<Hs::TypeVar>& referenced_tvs,
+                                  const local_instance_env& lie)
+{
+    substitution_t s;
+    Hs::Binds binds;
+    auto [unambiguous_preds, ambiguous_preds_by_var] = ambiguities(fixed_tvs, referenced_tvs, lie);
+
+    for(auto& [tv, preds]: ambiguous_preds_by_var)
+    {
+        auto result = candidates(tv, preds);
+
+        if (not result)
+        {
+            auto e = myexception()<<"Ambiguous type variable '"<<tv<<"' in classes: ";
+            for(auto& [dvar,constraint]: preds)
+                e<<constraint<<" ";
+            throw e;
+        }
+        auto& [s1, binds1] = *result;
+
+        auto tmp = combine(s, s1);
+        assert(tmp);
+        s = *tmp; // These constraints should be on separate variables, and should not interact.
+
+        // Each binds should be independent of the others, so order should not matter.
+        ranges::insert(binds, binds.end(), binds1);
+    }
+
+    return {s, binds, unambiguous_preds};
+}
+
