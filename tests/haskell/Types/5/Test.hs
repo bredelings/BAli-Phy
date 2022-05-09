492dc8bf3924e21dd1e728125ef3a95f045bd94c
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-09 11:03:05 -0400

    Fix instance for Eq (Maybe a)

--- tests/haskell/Types/5/Test.hs
+++ tests/haskell/Types/5/Test.hs
@@ -30,7 +30,7 @@ instance Eq a => Eq [a] where
     []     == []     = True
     _      == _      = False
 
-instance Eq (Maybe a) where
+instance Eq a => Eq (Maybe a) where
     Just x  == Just y  = x == y
     Nothing == Nothing = True
     _       == _       = False
