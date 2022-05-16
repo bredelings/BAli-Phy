473ccf84cf3068f183e9fd41c80fbe29f5d69124
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-16 18:24:08 -0400

    Don't define a "tuple" variable for a single entry.

--- src/computation/parser/desugar.cc
+++ src/computation/parser/desugar.cc
@@ -159,8 +159,6 @@ CDecls desugar_state::desugar_decls(const Haskell::Decls& v)
             const int N = gb->bind_infos.size();
             assert(N >= 1);
 
-            auto tup = get_fresh_var("tup");
-
             // tup = \dict1 dict2 ... dictn -> let dict_binds in let {x_inner[1]=..;...;x_inner[n]=..} in (x_inner[1],x_inner[2],...x_inner[n])
             expression_ref tup_body = let_expression ( desugar_decls(gb->dict_binds),
                                       let_expression ( desugar_decls(gb->body),
@@ -175,9 +173,7 @@ CDecls desugar_state::desugar_decls(const Haskell::Decls& v)
                     var x_outer = make_var(info.outer_id);
                     var x_inner = make_var(info.inner_id);
 
-                    expression_ref extractor = tup;
-                    for(auto& dict_var: tup_dict_vars)
-                        extractor = {extractor, dict_var};
+                    expression_ref extractor = tup_body;
 
                     vector<var> dict_vars = make_vars(info.dict_args);
 
@@ -186,11 +182,11 @@ CDecls desugar_state::desugar_decls(const Haskell::Decls& v)
 
                     decls.push_back({x_outer, extractor});
                 }
-
-                decls.push_back({tup, tup_lambda});
             }
             else
             {
+                auto tup = get_fresh_var("tup");
+
                 // x_outer[i] = \info.dict_args => let info.binds in case (tup dict1 .. dictn) of (_,_,x_inner[i],_,_) -> x_inner[i]
                 int i=0;
                 for(auto& [name, info]: gb->bind_infos)
