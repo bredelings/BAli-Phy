e9593aaf489ebaf662b880a66c00174d0fcb53fe
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-24 12:34:51 -0400

    Add types to builtins in Foreign.Pair

--- haskell/Foreign/Pair.hs
+++ haskell/Foreign/Pair.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Foreign.Pair where
 
-data EPair = EPair
+data EPair a b = EPair a b
 
+c_fst :: EPair a b -> a
 builtin c_fst 1 "c_fst" "Pair"
+
+c_snd :: EPair a b -> b
 builtin c_snd 1 "c_snd" "Pair"
+
+c_pair :: a -> b -> EPair a b
 builtin c_pair 2 "c_pair" "Pair"
 
+pair_from_c :: EPair a b -> (a,b)
 pair_from_c p = (c_fst p, c_snd p)
 
