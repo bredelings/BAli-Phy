4a1ef2bac53b5e147e040a79f6443fcc9eee3e39
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-24 17:54:04 -0400

    Add some type signatures.

--- haskell/Compiler/Num.hs
+++ haskell/Compiler/Num.hs
@@ -3,10 +3,14 @@ module Compiler.Num where
 
 infixl 7 *
 infixl 6 +, -
+
+(+),(-),(*) :: a -> a -> a
+
 builtin + 2 "add" "Prelude"
 builtin - 2 "subtract" "Prelude"
 builtin * 2 "multiply" "Prelude"
 
+negate, abs :: a -> a
 builtin negate 1 "negate" "Prelude"
 builtin abs    1 "abs"    "Prelude"
 
--- haskell/Foreign/String.hs
+++ haskell/Foreign/String.hs
@@ -5,7 +5,10 @@ import Compiler.Num -- for (-)
 
 data CPPString
 
+getStringElement :: CPPString -> Int -> Char
 builtin getStringElement 2 "getStringElement" "Vector"
+
+sizeOfString :: CPPString -> Int
 builtin sizeOfString 1 "sizeOfString" "Vector"
 
 unpack_cpp_string s = [getStringElement s i | i <- [0..sizeOfString s - 1]]
