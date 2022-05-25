ad7322d535c85f400f5440be69f132b33cbb23e7
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-25 16:29:29 -0400

    Switch to new implementation of unpack_cpp_string

--- haskell/Foreign/String.hs
+++ haskell/Foreign/String.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Foreign.String where
 
-import Compiler.Num -- for (-)
-
 data CPPString
 
 getStringElement :: CPPString -> Int -> Char
@@ -11,6 +9,18 @@ builtin getStringElement 2 "getStringElement" "Vector"
 sizeOfString :: CPPString -> Int
 builtin sizeOfString 1 "sizeOfString" "Vector"
 
-unpack_cpp_string s = [getStringElement s i | i <- [0..sizeOfString s - 1]]
 listFromString s = unpack_cpp_string s
 
+-- This is an attempt to avoid importing anything.
+-- If we import something, that something might already reference Foreign.String.unpack_cpp_string
+increment_int :: Int -> Int
+builtin increment_int 1 "increment_int" "Prelude"
+
+subtract_int' :: Int -> Int -> Int
+builtin subtract_int' 2 "subtract_int" "Prelude"
+
+unpack_cpp_string string = go string 0# where
+    size = sizeOfString string
+    go string i = case subtract_int' i size of
+                    0# -> []
+                    _ -> getStringElement string i : go string (increment_int i)
