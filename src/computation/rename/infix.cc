f74930376fd6edc1da17f616445f6f2455cb584d
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-25 22:53:18 -0400

    Move infix handling into rename/infix.cc

---
+++ src/computation/rename/infix.cc
@@ -0,0 +1,105 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <deque>
+
+#include "rename.H"
+#include "computation/module.H"
+#include "util/set.H"
+
+using std::string;
+using std::vector;
+using std::pair;
+using std::set;
+using std::optional;
+using std::deque;
+
+expression_ref infix_parse(const Module& m, const symbol_info& op1, const expression_ref& E1, deque<expression_ref>& T);
+
+/// Expression is of the form ... op1 [E1 ...]. Get right operand of op1.
+expression_ref infix_parse_neg(const Module& m, const symbol_info& op1, deque<expression_ref>& T)
+{
+    assert(not T.empty());
+
+    expression_ref E1 = T.front();
+    T.pop_front();
+
+    // We are starting with a Neg
+    if (E1.head().is_a<Hs::Neg>())
+    {
+	if (op1.fixity.precedence >= 6) throw myexception()<<"Cannot parse '"<<op1.name<<"' -";
+
+	E1 = infix_parse_neg(m, symbol_info("-",variable_symbol, {}, 2,{left_fix,6}), T);
+
+	return infix_parse(m, op1, {Hs::Var({noloc,"negate"}),E1}, T);
+    }
+    // If E1 is not a neg, E1 should be an expression, and the next thing should be an Op.
+    else
+	return infix_parse(m, op1, E1, T);
+}
+
+// FIXME: just return the fixity
+symbol_info get_op_sym(const Module& m, const expression_ref& O)
+{
+    symbol_info op_sym;
+    string name;
+    if (auto v = O.to<Haskell::Var>())
+        name = unloc(v->name);
+    else if (auto c = O.to<Haskell::Con>())
+        name = unloc(c->name);
+    else
+        std::abort();
+
+    if (m.is_declared( name ) )
+	op_sym = m.get_operator( name );
+    else
+    {
+	// FIXME: if this name is simply never declared, we should warn here.
+	op_sym.name = name;
+	op_sym.fixity = {left_fix,9};
+    }
+
+    return op_sym;
+}
+
+// FIXME: "h:t!!0 = h" gives an error that says that the arity of ":" is wrong.
+// We get ":" "h" "!!" "t" 0 as a pattern...
+//   This seems to be a result of the hack in unapply
+
+/// Expression is of the form ... op1 E1 [op2 ...]. Get right operand of op1.
+expression_ref infix_parse(const Module& m, const symbol_info& op1, const expression_ref& E1, deque<expression_ref>& T)
+{
+    if (T.empty())
+	return E1;
+
+    expression_ref op2_E = T.front();
+    symbol_info op2 = get_op_sym(m, op2_E);
+
+    // illegal expressions
+    if (op1.fixity.precedence == op2.fixity.precedence and (op1.fixity.fixity != op2.fixity.fixity or op1.fixity.fixity == non_fix))
+	throw myexception()<<"Must use parenthesis to order operators '"<<op1.name<<"' and '"<<op2.name<<"'";
+
+    // left association: ... op1 E1) op2 ...
+    if (op1.fixity.precedence > op2.fixity.precedence or (op1.fixity.precedence == op2.fixity.precedence and op1.fixity.fixity == left_fix))
+	return E1;
+
+    // right association: .. op1 (E1 op2 {...E3...}) ...
+    else
+    {
+	T.pop_front();
+	expression_ref E3 = infix_parse_neg(m, op2, T);
+
+	expression_ref E1_op2_E3 = {op2_E, E1, E3};
+
+	return infix_parse(m, op1, E1_op2_E3, T);
+    }
+}
+
+expression_ref desugar_infix(const Module& m, const vector<expression_ref>& T)
+{
+    deque<expression_ref> T2;
+    T2.insert(T2.begin(), T.begin(), T.end());
+
+    return infix_parse_neg(m, {"",variable_symbol,{}, 2,{non_fix,-1}}, T2);
+}
+
