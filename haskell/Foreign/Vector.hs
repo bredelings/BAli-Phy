d410aead6f5589ce5cc70cb583ce2ba78ecd1404
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-27 11:05:26 -0400

    Make Foreign.Vector more standalone.

--- haskell/Foreign/Vector.hs
+++ haskell/Foreign/Vector.hs
@@ -1,28 +1,33 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Foreign.Vector where
 
-import Compiler.Base
-import Compiler.Num
-import Data.Ord
 import Foreign.String
 
 data EVector a
 
+get_vector_index :: EVector a -> Int -> a
 builtin get_vector_index 2 "get_vector_index" "Vector"
+
+vector_size :: EVector a -> Int
 builtin vector_size 1 "vector_size" "Vector"
 
--- list_to_vector:: [a] -> EVector
+clist_to_vector :: CList a -> EVector a
 builtin clist_to_vector 1 "clist_to_vector" "Vector"
 
+data EMatrix a
+fromVectors :: EVector (EVector a) -> EMatrix a
 builtin fromVectors 1 "fromVectors" "Vector"
 
-list_from_vector_of_size v n = go 0 n where
-    go _ 0 = []
-    go i s = get_vector_index v i:go (i+1) (s-1)
+list_from_vector :: EVector a -> [a]
+list_from_vector vec = go vec 0# where
+    size = vector_size vec
+    go vec i = case subtract_int' i size of
+                 0# -> []
+                 _ -> get_vector_index vec i : go vec (increment_int i)
 
-list_from_vector v = list_from_vector_of_size v (vector_size v)
 vector_to_list = list_from_vector
 
+list_to_vector :: [a] -> EVector a
 list_to_vector x = clist_to_vector (list_to_CList x)
 
 pack_cpp_string = list_to_string
