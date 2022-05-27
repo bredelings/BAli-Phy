d8a6413caf3d7b99bda0f5194469148df64f6768
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-27 14:42:28 -0400

    Add signatures for Foreign.Introspection

--- haskell/Foreign/Introspection.hs
+++ haskell/Foreign/Introspection.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Foreign.Introspection where
 
+get_arg :: a -> Int -> b
 builtin get_arg    2 "get_arg"    "Prelude"
+
+get_n_args :: a -> Int
 builtin get_n_args 1 "get_n_args"  "Prelude"
+
+-- foreign import bpcall1 "Prelude:get_arg" get_arg :: a -> Int -> b
+
+-- foreign import bpcall1 "Prelude:get_n_args" get_n_args :: a -> Int
