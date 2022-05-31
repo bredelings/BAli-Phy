6290dbd494cb3b184731a3fc0b492743fd12ac89
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-05-30 22:40:42 -0400

    Fix up Prelude.

--- haskell/Prelude.hs
+++ haskell/Prelude.hs
@@ -46,15 +46,15 @@ import Foreign.Pair
 import Foreign.Vector
 import Foreign.String
 
-foreign import bpcall "Prelude:putStrLn" builtin_putStrLn :: () -> () -> ()
+foreign import bpcall "Prelude:putStrLn" builtin_putStrLn :: CPPString -> Int -> EPair Int ()
 
-foreign import bpcall "Prelude:is_char" is_char :: () -> ()
-foreign import bpcall "Prelude:is_double" is_double :: () -> ()
-foreign import bpcall "Prelude:is_int" is_int :: () -> ()
+foreign import bpcall "Prelude:is_char" is_char :: a -> Bool
+foreign import bpcall "Prelude:is_double" is_double :: a -> Bool
+foreign import bpcall "Prelude:is_int" is_int :: a -> Bool
 
-foreign import bpcall "Prelude:show" builtin_show :: () -> ()
-foreign import bpcall "Prelude:read_int" builtin_read_int :: () -> ()
-foreign import bpcall "Prelude:read_double" builtin_read_double :: () -> ()
+foreign import bpcall "Prelude:show" builtin_show :: a -> CPPString
+foreign import bpcall "Prelude:read_int" builtin_read_int :: CPPString -> Int
+foreign import bpcall "Prelude:read_double" builtin_read_double :: CPPString -> Double
 
 undefined = error "Prelude.undefined"
 
@@ -66,26 +66,43 @@ zip' = zipWith' (,)
 
 putStrLn line = IOAction (pair_from_c . builtin_putStrLn (list_to_string line))
 
-show () = "()"
-show (x,y) = "(" ++ show x ++ "," ++ show y ++ ")"
-show (x,y,z) = "(" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ")"
-show (x,y,z,w) = "(" ++ show x ++ "," ++ show y ++ "," ++ show z ++ "," ++ show w ++ ")"
-show [] = "[]"
-show (x:y) = "["++show x++show' y++"]" where show' [] = ""
-                                             show' (x:y) = ","++show x++show' y
-show (Just x) = "Just "++show x
-show Nothing = "Nothing"
-show True = "True"
-show False = "False"
-show x     = listFromString $ builtin_show x
+class Show a where
+    show :: a -> [Char]
 
-read_int [] = error "Can't convert empty string to int."
-read_int (h:t) = builtin_read_int (list_to_string (h:t))
-read_int s = builtin_read_int s
+instance Show () where
+    show _ = "()"
 
-read_double [] = error "Can't convert empty string to double."
-read_double (h:t) = builtin_read_double (list_to_string (h:t))
-read_double s = builtin_read_double s
+instance (Show a, Show b) => Show (a,b) where 
+    show (x,y) = "(" ++ show x ++ "," ++ show y ++ ")"
 
-foreign import bpcall "Data:readFile" builtin_readFile :: () -> ()
+instance (Show a, Show b, Show c) => Show (a,b,c) where 
+    show (x,y,z) = "(" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ")"
+
+instance (Show a, Show b, Show c, Show d) => Show (a,b,c,d) where 
+    show (x,y,z,w) = "(" ++ show x ++ "," ++ show y ++ "," ++ show z ++ "," ++ show w ++ ")"
+
+instance Show a => Show [a]where
+    show [] = "[]"
+    show (x:y) = "["++show x++show' y++"]" where show' [] = ""
+                                                 show' (x:y) = ","++show x++show' y
+instance Show a => Show (Maybe a) where
+    show (Just x) = "Just "++show x
+    show Nothing = "Nothing"
+
+instance Show Bool where                   
+    show True = "True"
+    show False = "False"
+
+class Read a where
+    read :: [Char] -> a
+
+instance Read Int where
+    read [] = error "Can't convert empty string to int."
+    read s = builtin_read_int (list_to_string s)
+
+instance Read Double where
+    read [] = error "Can't convert empty string to double."
+    read s = builtin_read_double (list_to_string s)
+
+foreign import bpcall "Data:readFile" builtin_readFile :: CPPString -> CPPString
 readFile filename = IOAction (\s -> (s,listFromString $ builtin_readFile $ list_to_string $ filename))
