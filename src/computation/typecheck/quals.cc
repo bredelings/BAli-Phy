3413361d5bf379ca6f0bec9b997d4527a95f53ad
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-02 18:38:03 -0400

    Avoid unused var warning.

--- src/computation/typecheck/quals.cc
+++ src/computation/typecheck/quals.cc
@@ -215,7 +215,7 @@ typechecker_state::infer_stmts_type(const global_value_env& env, int i, vector<H
         stmt = LQ;
         return stmts_type;
     }
-    else if (auto rq = stmt.to<Hs::RecStmt>())
+    else if (stmt.to<Hs::RecStmt>())
     {
         /*
          * See "Recursive binding groups" in https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/recursive_do.html
