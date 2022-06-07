6a8ae164991f8f0f0264172eaad917831bcddee2
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 09:12:18 -0400

    Add types for builtins in Probability.Distribution.Uniform

--- haskell/Probability/Distribution/Uniform.hs
+++ haskell/Probability/Distribution/Uniform.hs
@@ -3,8 +3,8 @@ module Probability.Distribution.Uniform where
 import Probability.Random
 import MCMC
 
-foreign import bpcall "Distribution:uniform_density" uniform_density :: () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_uniform" builtin_sample_uniform :: () -> () -> () -> ()
+foreign import bpcall "Distribution:" uniform_density :: Double -> Double -> Double -> LogDouble
+foreign import bpcall "Distribution:sample_uniform" builtin_sample_uniform :: Double -> Double -> Int -> Double
 
 uniform_bounds l u = between l u
 uniform_effect l u x = add_move $ slice_sample_real_random_variable x (uniform_bounds l u)
@@ -12,8 +12,8 @@ sample_uniform l u = RandomStructure (uniform_effect l u) modifiable_structure $
 
 uniform l u = Distribution "uniform" (make_densities $ uniform_density l u) () (sample_uniform l u) (uniform_bounds l u)
 
-foreign import bpcall "Distribution:uniform_int_density" uniform_int_density :: () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_uniform_int" builtin_sample_uniform_int :: () -> () -> () -> ()
+foreign import bpcall "Distribution:uniform_int_density" uniform_int_density :: Int -> Int -> Int -> LogDouble
+foreign import bpcall "Distribution:sample_uniform_int" builtin_sample_uniform_int :: Int -> Int -> Int -> Int
 
 uniform_int_bounds l u = integer_between l u
 uniform_int_effect l u x = add_move $ slice_sample_integer_random_variable x (uniform_int_bounds l u)
