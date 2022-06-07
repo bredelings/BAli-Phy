5b33fac84a29a44955170a1408227381af32d262
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 12:08:54 -0400

    Fix up Cauchy.hs

--- haskell/Probability/Distribution/Cauchy.hs
+++ haskell/Probability/Distribution/Cauchy.hs
@@ -5,20 +5,27 @@ import MCMC
 
 ---- cauchy
 
-foreign import bpcall "Distribution:cauchy_density" cauchy_density :: () -> () -> () -> ()
-foreign import bpcall "Distribution:cauchy_quantile" cauchy_quantile :: () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_cauchy" builtin_sample_cauchy :: () -> () -> () -> ()
+foreign import bpcall "Distribution:cauchy_density" cauchy_density :: Double -> Double -> Double -> LogDouble
+foreign import bpcall "Distribution:cauchy_quantile" cauchy_quantile :: Double -> Double -> Double -> Double
+foreign import bpcall "Distribution:sample_cauchy" builtin_sample_cauchy :: Double -> Double -> RealWorld -> Double
 
 cauchy_bounds = realLine
 cauchy_effect x = add_move $ slice_sample_real_random_variable x cauchy_bounds
 sample_cauchy m s = RandomStructure cauchy_effect modifiable_structure $ liftIO (IOAction (\state->(state,builtin_sample_cauchy m s state)))
 
-cauchy m s = Distribution "cauchy" (make_densities $ cauchy_density m s) (cauchy_quantile m s) (sample_cauchy m s) cauchy_bounds
+class HasCauchy d where
+    cauchy :: Double -> Double -> d Double
+
+instance HasCauchy Distribution where
+    cauchy m s = Distribution "cauchy" (make_densities $ cauchy_density m s) (cauchy_quantile m s) (sample_cauchy m s) cauchy_bounds
+
+instance HasCauchy Random where
+    cauchy m s = RanDistribution (cauchy m s)
 
 ---- half_cauchy
 
 half_cauchy_density m s x | x < m     = doubleToLogDouble 0.0
-                          | otherwise = 2.0*cauchy_density m s x
+                          | otherwise = doubleToLogDouble 2.0 * cauchy_density m s x
 
 sample_half_cauchy m s = do
   x <- cauchy m s
