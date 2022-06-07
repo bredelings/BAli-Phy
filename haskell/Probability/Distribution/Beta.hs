dbb0517f1ac48bdb244bb014212698e135a828f2
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 10:04:41 -0400

    Add types for Beta builtins

--- haskell/Probability/Distribution/Beta.hs
+++ haskell/Probability/Distribution/Beta.hs
@@ -3,9 +3,9 @@ module Probability.Distribution.Beta where
 import Probability.Random
 import MCMC
 
-foreign import bpcall "Distribution:beta_density" beta_density :: () -> () -> () -> ()
-foreign import bpcall "Distribution:beta_quantile" beta_quantile :: () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_beta" builtin_sample_beta :: () -> () -> () -> ()
+foreign import bpcall "Distribution:beta_density" beta_density :: Double -> Double -> Double -> LogDouble
+foreign import bpcall "Distribution:beta_quantile" beta_quantile :: Double -> Double -> Double -> Double
+foreign import bpcall "Distribution:sample_beta" builtin_sample_beta :: Double -> Double -> RealWorld -> Double
 
 beta_bounds = between 0.0 1.0
 beta_effect x = add_move $ slice_sample_real_random_variable x beta_bounds
