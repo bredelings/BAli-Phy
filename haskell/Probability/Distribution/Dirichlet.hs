ed6cffcf1ba8374c0761bceda1f692bbdefad26b
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 13:29:24 -0400

    Fix up Dirichlet.hs

--- haskell/Probability/Distribution/Dirichlet.hs
+++ haskell/Probability/Distribution/Dirichlet.hs
@@ -4,19 +4,21 @@ import Probability.Random
 import Probability.Distribution.Gamma
 import Probability.Distribution.List
 
-foreign import bpcall "Distribution:dirichlet_density" builtin_dirichlet_density :: () -> () -> ()
+foreign import bpcall "Distribution:dirichlet_density" builtin_dirichlet_density :: EVector Double -> EVector Double -> LogDouble
 dirichlet_density as ps = builtin_dirichlet_density (list_to_vector as) (list_to_vector ps)
 
 -- The `dirichlet` does not handle cases where the number of as changes in a ungraceful way: all entries are resampled!
-sample_dirichlet as = SamplingRate (1.0/sqrt(intToDouble $ length as)) $ do vs <- mapM (\a-> gamma a 1.0) as
-                                                                            return $ map (/(sum vs)) vs
+sample_dirichlet as = RanSamplingRate (1.0/sqrt(intToDouble $ length as)) $ do vs <- mapM (\a-> gamma a 1.0) as
+                                                                               return $ map (/(sum vs)) vs
 
 dirichlet as = Distribution "dirichlet" (make_densities $ dirichlet_density as) (no_quantile "dirichlet") (sample_dirichlet as) (Simplex (length as) 1.0)
 
+-- independent is a Distribution ... so shouldn't this fail because of a lack of a monad instance?
+
 -- The `v_dirichlet` takes an infinite list of as, and a number of them to keep.
-sample_v_dirichlet n as = SamplingRate (1.0/sqrt(intToDouble n)) $ do vs <- independent [ gamma a 1.0 | a <- as ]
-                                                                      let ws = take n vs
-                                                                      return $ map (/(sum ws)) ws
+sample_v_dirichlet n as = RanSamplingRate (1.0/sqrt(intToDouble n)) $ do vs <- independent [ gamma a 1.0 | a <- as ]
+                                                                         let ws = take n vs
+                                                                         return $ map (/(sum ws)) ws
 
 v_dirichlet n as = Distribution "v_dirichlet" (make_densities $ dirichlet_density (take n as)) (no_quantile "v_dirichlet") (sample_v_dirichlet n as) (Simplex n 1.0)
 
