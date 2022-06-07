469b087cad4d655cea496a73eb48cdf1b3456474
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 12:10:52 -0400

    Fix up Gamma.hs

--- haskell/Probability/Distribution/Gamma.hs
+++ haskell/Probability/Distribution/Gamma.hs
@@ -3,9 +3,9 @@ module Probability.Distribution.Gamma where
 import Probability.Random
 import MCMC
 
-foreign import bpcall "Distribution:shifted_gamma_density" shifted_gamma_density :: () -> () -> () -> () -> ()
-foreign import bpcall "Distribution:shifted_gamma_quantile" shifted_gamma_quantile :: () -> () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_shifted_gamma" builtin_sample_shifted_gamma :: () -> () -> () -> () -> ()
+foreign import bpcall "Distribution:shifted_gamma_density" shifted_gamma_density :: Double -> Double -> Double -> Double -> LogDouble
+foreign import bpcall "Distribution:shifted_gamma_quantile" shifted_gamma_quantile :: Double -> Double -> Double -> Double -> Double
+foreign import bpcall "Distribution:sample_shifted_gamma" builtin_sample_shifted_gamma :: Double -> Double -> Double -> RealWorld -> Double
 
 gamma_bounds shift = above shift
 gamma_effect shift x = add_move $ slice_sample_real_random_variable x (gamma_bounds shift)
