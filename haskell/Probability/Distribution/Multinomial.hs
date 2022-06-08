5c7bd48ebd0591843fb7b2f3a4ed3be00803e736
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 21:47:52 -0400

    Fix up Multinomial.hs

--- haskell/Probability/Distribution/Multinomial.hs
+++ haskell/Probability/Distribution/Multinomial.hs
@@ -4,7 +4,7 @@ import Probability.Random
 import MCMC
 import Probability.Distribution.Binomial
 
-foreign import bpcall "Distribution:multinomial_density" builtin_multinomial_density :: () -> () -> () -> ()
+foreign import bpcall "Distribution:multinomial_density" builtin_multinomial_density :: Int -> EVector Double -> EVector Int -> LogDouble
 
 multinomial_density n ps ks = builtin_multinomial_density n ps' ks'
     where ps' = list_to_vector ps
@@ -17,10 +17,10 @@ sample_multinomial n (p:ps) = do
   ms <- sample_multinomial (n-m) (normalize ps)
   return (m:ms)
 
-multinomial n ps = Distribution "multinomial" (make_densities $ multinomial_density n ps) (no_quantile "multinomial") (sample_multinomial n ps) ()
+multinomial n ps = Distribution "multinomial" (make_densities $ multinomial_density n ps) (no_quantile "multinomial") (sample_multinomial n ps) NoRange
 
 -- Not sure this is the same as sorting discrete uniforms.
-random_composition' sum n_groups = multinomial sum (replicate n_groups (1.0/intToDouble n_groups))
+random_composition' sum n_groups = RanDistribution $ multinomial sum (replicate n_groups (1.0/intToDouble n_groups))
 
 -- Not sure this is the same as conditioning on not having any zeros.
 random_composition sum n_groups | sum < n_groups = error "random_composition: sum < n_groups!"
