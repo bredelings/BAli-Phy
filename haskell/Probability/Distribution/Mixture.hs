2090d69d2ca712e89bbd6afa72449964a435e3f2
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 21:47:59 -0400

    Fix up Mixture

--- haskell/Probability/Distribution/Mixture.hs
+++ haskell/Probability/Distribution/Mixture.hs
@@ -3,14 +3,13 @@ module Probability.Distribution.Mixture where
 import Probability.Random
 import Probability.Distribution.Categorical
 
-mixtureRange _ (d:ds) = distRange
 mixture_density (p:ps) (dist:dists) x = (doubleToLogDouble p)*(density dist x) + (mixture_density ps dists x)
 mixture_density [] [] _ = (doubleToLogDouble 0.0)
-sample_mixture weights dists = do cat <- categorical weights
-                                  dists!!cat
+sample_mixture weights dists = do cat <- RanDistribution (categorical weights)
+                                  RanDistribution (dists!!cat)
 
 mixture weights dists = Distribution "mixture"
                                      (make_densities $ mixture_density weights dists)
                                      (no_quantile "mixture")
                                      (sample_mixture weights dists)
-                                     (mixtureRange weights dists)
+                                     NoRange
