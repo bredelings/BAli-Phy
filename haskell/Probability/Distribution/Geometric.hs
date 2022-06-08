80d1929608a6ef3a5f2d98a881f86575510fe0f1
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-07 21:33:12 -0400

    Fix up Geometric.hs

--- haskell/Probability/Distribution/Geometric.hs
+++ haskell/Probability/Distribution/Geometric.hs
@@ -4,15 +4,14 @@ import Probability.Random
 import MCMC
 import Range
 
-foreign import bpcall "Distribution:geometric_density" geometric_density :: () -> () -> () -> ()
-foreign import bpcall "Distribution:sample_geometric" builtin_sample_geometric :: () -> () -> ()
+foreign import bpcall "Distribution:geometric_density" geometric_density :: Double -> Double -> Int -> LogDouble
+foreign import bpcall "Distribution:sample_geometric" builtin_sample_geometric :: Double -> RealWorld -> Int
 
 geometric_bounds = integer_above 0
 
 geometric_effect x = do
-  let bnds = getIntegerBounds geometric_bounds
-  add_move $ slice_sample_integer_random_variable x bnds
-  add_move $ inc_dec_mh x bnds
+  add_move $ slice_sample_integer_random_variable x geometric_bounds
+  add_move $ inc_dec_mh x geometric_bounds
 
 sample_geometric p_success = RandomStructure geometric_effect modifiable_structure $ liftIO (IOAction (\s->(s,builtin_sample_geometric p_success s)))
 
