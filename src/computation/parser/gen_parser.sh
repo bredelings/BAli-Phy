948f0d0b692489647c348a73e98e7f1ba0c986ad
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-20 16:34:14 -0400

    Use reflex to build the parser in gen_parser.sh

--- src/computation/parser/gen_parser.sh
+++ src/computation/parser/gen_parser.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rm -f parser.hh parser.cc lexer.cc
-flex -o lexer.cc lexer.l
+#flex -o lexer.cc lexer.l
+
 bison parser.y --defines=parser.hh --output=parser.cc
+reflex -y lexer.l --header-file --bison-complete −−bison-locations −−namespace=yy -d --exception
