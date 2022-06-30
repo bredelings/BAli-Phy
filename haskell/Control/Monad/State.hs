f47699d6a8c30c04c443438e2c3bcb85c80fbd0b
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-30 19:56:25 -0400

    Update Control.Monad.state

--- haskell/Control/Monad/State.hs
+++ haskell/Control/Monad/State.hs
@@ -7,18 +7,13 @@ import Data.Function -- for id
 
 -- drat -- my implementation of IO assumes that the state is FIRST, but should be SECOND.
 
--- data State s a = State {runState::(s->(a,s))}
-data State s a = State (s->(a,s))
+data State s a = State { runState :: s->(a,s) }
 
-runState :: State s a -> s -> (a,s)
-runState (IOReturn x) s             = (x,s)
-runState (State f) s                = f s
-runState (IOAction f) s             = let (s', x) = f s in (x,s')
--- maybe we need the msplit operation to generically do interleaved stuff in the State monad?
-runState (LazyIO f) s               = runState f s
-runState (IOAndPass (LazyIO f) g) s = let (x , _) = runState f s in runState (g x) s
-runState (IOAndPass f          g) s = let (x, s') = runState f s in runState (g x) s'
-runState (MFix f) s                 = let xs@(x, s') = runState (f x) s in xs -- is this right?
+instance Monad State where
+    f >>= g = State (\s1 -> let (x,s2) = runState f s1 in runState g s2)
+    return x = State (\s -> (x,s))
+    mfix f = State (\state1 -> let result@(state1, x) = runState (f x) state1 in result)
+    unsafeInterleaveIO f = State (\state -> (state, snd $ runState f state))
 
 get      = State ( \s -> (s , s)   )
 put s    = State ( \_ -> ((), s)   )
