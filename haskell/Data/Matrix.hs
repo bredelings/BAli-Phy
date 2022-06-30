f64ce6fd0ca208e7baf0b8b752e5d6f4f0db7250
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-29 20:20:29 -0400

    Remove Num constraint from Data.Matrix

--- haskell/Data/Matrix.hs
+++ haskell/Data/Matrix.hs
@@ -4,4 +4,4 @@ import Foreign.Vector
 
 fromLists xss = fromVectors $ list_to_vector $ map list_to_vector xss
 
-foreign import bpcall "SModel:scaleMatrix" scaleMatrix :: Num a => a -> Matrix a -> Matrix a
+foreign import bpcall "SModel:scaleMatrix" scaleMatrix :: a -> Matrix a -> Matrix a
