2fa8bd3cb9bfef8a1a8eefcb817cdb065e6bcb15
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-30 16:44:25 -0400

    Allow log_foo to be both in both Distribution and Random

--- haskell/Probability/Distribution/ExpTransform.hs
+++ haskell/Probability/Distribution/ExpTransform.hs
@@ -19,8 +19,24 @@ expTransform dist@(Distribution name d q s r) = Distribution name' pdf' q' s' r'
   r'   = expTransformRange r
   name' = "log_"++name
 
-log_normal mu sigma = expTransform $ normal mu sigma
-log_exponential mu = expTransform $  exponential mu
-log_gamma a b = expTransform $ gamma a b
-log_laplace m s = expTransform $ laplace m s
-log_cauchy m s = expTransform $ cauchy m s
+class HasLogDists d where
+    log_normal :: Double -> Double -> d Double
+    log_exponential :: Double -> d Double
+    log_gamma :: Double -> Double -> d Double
+    log_laplace :: Double -> Double -> d Double
+    log_cauchy :: Double -> Double -> d Double
+
+
+instance HasLogDists Distribution where
+    log_normal mu sigma = expTransform $ normal mu sigma
+    log_exponential mu = expTransform $  exponential mu
+    log_gamma a b = expTransform $ gamma a b
+    log_laplace m s = expTransform $ laplace m s
+    log_cauchy m s = expTransform $ cauchy m s
+
+instance HasLogDists Random where
+    log_normal mu sigma = RanDistribution $ log_normal mu sigma
+    log_exponential mu = RanDistribution $ log_exponential mu
+    log_gamma a b = RanDistribution $ log_gamma a b
+    log_laplace m s = RanDistribution $ log_laplace m s
+    log_cauchy m s = RanDistribution $ log_cauchy m s
