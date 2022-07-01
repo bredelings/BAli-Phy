30df51eb7363306440daaf2a6484c39fb87c6234
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-06-30 21:14:02 -0400

    Fix JSON strings.

--- haskell/Data/JSON.hs
+++ haskell/Data/JSON.hs
@@ -47,9 +47,16 @@ instance ToJSONKey a => ToJSONKey [a] where
 
 class ToJSON a where
     toJSON :: a -> JSON
+    toJSONList :: [a] -> JSON
+
+    toJSONList x = Array $ map toJSON x
 
 instance ToJSON () where
     toJSON () = Null
+
+instance ToJSON Char where
+    toJSON c = String (pack [c])
+    toJSONList s = String (pack s)
 
 instance ToJSON Text where
     toJSON x = String x
@@ -64,7 +71,7 @@ instance ToJSON Int where
     toJSON x = Number (intToDouble x)
 
 instance ToJSON a => ToJSON [a] where
-    toJSON x = Array $ map toJSON x
+    toJSON x = toJSONList x
 
 -- BUG: In instance for 'Data.JSON.ToJSON (Data.Map.Map Compiler.Base.String a)' for type 'Data.Map.Map Compiler.Base.String a': Compiler.Base.String is not a type variable!
 -- BUG: If we just replace String with b, it checks... but should it?
@@ -85,7 +92,7 @@ to_json = toJSON
 foreign import bpcall "Foreign:" ejson_array  :: EVector EJSON -> EJSON
 foreign import bpcall "Foreign:" ejson_object :: EVector (EPair CPPString EJSON) -> EJSON
 foreign import bpcall "Foreign:" ejson_number :: Double -> EJSON
-foreign import bpcall "Foreign:" ejson_string :: Text -> EJSON
+foreign import bpcall "Foreign:" ejson_string :: CPPString -> EJSON
 foreign import bpcall "Foreign:" ejson_bool   :: Bool -> EJSON
 foreign import bpcall "Foreign:" ejson_null   :: () -> EJSON
 
@@ -94,7 +101,7 @@ deep_eval_json (Array xs)  = ejson_array $ list_to_vector $ map deep_eval_json x
 deep_eval_json (Object xs) = ejson_object $ list_to_vector [c_pair key (deep_eval_json value) | (Key (Text key), value) <- xs]
 deep_eval_json (Number n)  = ejson_number n
 deep_eval_json (Bool b)    = ejson_bool b
-deep_eval_json (String s)  = ejson_string s
+deep_eval_json (String (Text s))  = ejson_string s
 deep_eval_json Null        = ejson_null ()
 
 c_json = builtin_c_json . deep_eval_json
