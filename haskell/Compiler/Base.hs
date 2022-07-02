770a49ae692fc517152422645d22f36eadf446f1
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-02 12:04:39 -0400

    Go back to the old implementation of IO.

--- haskell/Compiler/Base.hs
+++ haskell/Compiler/Base.hs
@@ -32,7 +32,7 @@ f $! x = x `seq` f x
 foreign import bpcall "Prelude:seq" seq :: a -> b -> b
 
 foreign import bpcall "Prelude:struct_seq" struct_seq :: a -> b -> b
-
+{-
 data IO a = IOAction { runIO :: RealWorld -> (RealWorld, a) }
 
 -- unsafePerformIO (IOAction f) = snd (f 0)
@@ -40,14 +40,27 @@ unsafePerformIO :: IO a -> a
 unsafePerformIO f = snd $ runIO f 0#
 
 instance Monad IO where
-    -- Here we weirdly start (g x) from `state1` again, not using `state2`.
-    -- We use `seq` to sequence things.
     f >>= g = IOAction (\state1 -> let (state2,x) = runIO f state1 in runIO (g x) state2)
     return x = IOAction (\s -> (s, x))
     mfix f = IOAction (\state1 -> let result@(state2, x) = runIO (f x) state1 in result)
-{- unsafeInterleaveIO x = LazyIO x
-   unsafePerformIO (LazyIO f) = unsafePerformIO f
-   unsafePerformIO (IOAndPass (LazyIO f) g) = let x = unsafePerformIO f in unsafePerformIO (g x)
--}
     unsafeInterleaveIO f = IOAction (\state -> (state, snd $ runIO f state))
 
+-}
+data IO a = IOAction  (RealWorld->(RealWorld,a)) |
+            IOLazy (IO a) |
+            IOMFix (a -> IO a) |
+            IOReturn a |
+            forall b. IOAndPass (IO b) (b -> IO a)
+
+instance Monad IO where
+    f >>= g  = IOAndPass f g
+    return x = IOReturn x
+    mfix f   = IOMFix f
+    unsafeInterleaveIO f = IOLazy f
+
+unsafePerformIO (IOAction f) = snd (f 0#)
+unsafePerformIO (IOLazy f) = unsafePerformIO f
+unsafePerformIO (IOAndPass (IOLazy f) g) = let x = unsafePerformIO f in unsafePerformIO (g x)
+unsafePerformIO (IOAndPass f g) = let x = unsafePerformIO f in x `seq` unsafePerformIO (g x)
+unsafePerformIO (IOMFix f) = let x = unsafePerformIO (f x) in x
+unsafePerformIO (IOReturn x) = x
