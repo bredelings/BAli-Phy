7682c66d1457d37d1c85b8bfb25757198a13e430
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-04 18:39:53 -0400

    Add class Ix.

--- haskell/Data/Ix.hs
+++ haskell/Data/Ix.hs
@@ -6,13 +6,14 @@ import Compiler.Num
 import Data.Ord
 import Data.Bool
 
-range     (start,end) = [start..end]
-index     (start,end) val = val - start
-inRange   (start,end) val = (start <= val) && (val <= end)
-rangeSize (start,end) = end - start + 1
+class Ord a => Ix a where
+    range :: (a, a) -> [a]
+    index :: (a, a) -> a -> Int
+    inRange :: (a, a) -> a -> Bool
+    rangeSize :: (a, a) -> Int
 
-class Ord a => Ix a
--- range :: (a, a) -> [a]
--- index :: (a, a) -> a -> Int
--- inRange :: (a, a) -> a -> Bool
--- rangeSize :: (a, a) -> Int
+instance Ix Int where
+    range     (start,end)     = [start..end]
+    index     (start,end) val = val - start
+    inRange   (start,end) val = (start <= val) && (val <= end)
+    rangeSize (start,end)     = end - start + 1
