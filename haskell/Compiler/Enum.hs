426e5bd24ce3ffff01e06d1bc82701c9ada689ff
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-04 18:33:55 -0400

    Implement class Enum.

--- haskell/Compiler/Enum.hs
+++ haskell/Compiler/Enum.hs
@@ -6,19 +6,50 @@ module Compiler.Enum (Enum,
                       enumFromThenTo)
     where
 
-import Data.Ord      -- for <=
-import Compiler.Num  -- for -,+
-import Data.Bool     -- for otherwise
 
-class Enum a where { }
--- succ :: a -> a
--- pred :: a -> a
--- toEnum :: Int -> a
--- fromEnum :: a -> Int
--- enumFrom :: a -> [a]
--- enumFromThen :: a -> a -> [a]
--- enumFromTo   :: a -> a -> [a]
--- enumFromThenTo :: a -> a -> a -> [a]
+import Compiler.Num   -- for -,+
+import Compiler.Error -- for error
+import Data.Ord       -- for <=
+import Data.Bool      -- for otherwise
+
+class Enum a where
+    succ :: a -> a
+    pred :: a -> a
+    toEnum :: Int -> a
+    fromEnum :: a -> Int
+    enumFrom :: a -> [a]
+    enumFromThen :: a -> a -> [a]
+    enumFromTo   :: a -> a -> [a]
+    enumFromThenTo :: a -> a -> a -> [a]
+
+    enumFrom n = n:enumFrom (succ n)
+
+
+instance Enum Int where
+    succ n = n + 1
+    pred n = n - 1
+    toEnum n = n
+    fromEnum n = n
+
+    enumFromTo n m | fromEnum n <= fromEnum m    = n:enumFromTo (succ n) m
+                   | otherwise                   = []
+
+    enumFromThen from next = enumByFrom (next-from) from
+
+    enumFromThenTo from next to = enumByToFrom (next - from) to from
+
+instance Enum Double where
+    succ x = x + 1
+    pred x = x - 1
+    toEnum n = intToDouble n
+    fromEnum x = error "fromEnum: Double"
+
+    enumFromTo x y | x < y      = x:enumFromTo (x+1) y
+                   | otherwise  = []
+
+    enumFromThen from next = enumByFrom (next-from) from
+
+    enumFromThenTo from next to = enumByToFrom (next - from) to from
 
 -- This isn't a standard function -- I made it up..
 enumByFrom by from = from:enumByFrom by (from+by)
@@ -27,22 +58,5 @@ enumByFrom by from = from:enumByFrom by (from+by)
 enumByToFrom by to from | from <= to    = from:enumByToFrom by to (from+by)
                         | otherwise     = []
 
-succ :: Int -> Int
-succ n = n + 1
 
-pred :: Int -> Int
-pred n = n - 1
-
-enumFrom :: Int -> [Int]
-enumFrom n = n:enumFrom (succ n)
-
-enumFromThen :: Int -> Int -> [Int]
-enumFromThen from next = enumByFrom (next-from) from
-
-enumFromThenTo :: Int -> Int -> Int -> [Int]
-enumFromThenTo from next to = enumByToFrom (next - from) to from
-
-enumFromTo :: Int -> Int -> [Int]
-enumFromTo n m | n <= m    = n:enumFromTo (succ n) m
-               | otherwise = []
 
