3438286692d0571fc94add74cb44ecdce4709c20
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-06 00:18:43 -0400

    Add types for builtins in PopGen.hs

--- haskell/PopGen.hs
+++ haskell/PopGen.hs
@@ -2,16 +2,19 @@ module PopGen where
 
 import Probability
 import Range
+import Bio.Alignment.Matrix
 
-foreign import bpcall "PopGen:read_phase_file" builtin_read_phase_file :: () -> ()
-foreign import bpcall "PopGen:read_phase2_file" builtin_read_phase2_file :: () -> ()
-foreign import bpcall "PopGen:remove_2nd_allele" remove_2nd_allele :: () -> ()
-foreign import bpcall "PopGen:allele_frequency_spectrum" allele_frequency_spectrum :: () -> ()
-foreign import bpcall "PopGen:ewens_sampling_group_probability" ewens_sampling_group_probability :: () -> () -> ()
-foreign import bpcall "PopGen:ewens_sampling_probability" ewens_sampling_probability :: () -> () -> ()
-foreign import bpcall "PopGen:ewens_sampling_mixture_probability" builtin_ewens_sampling_mixture_probability :: () -> () -> () -> ()
-foreign import bpcall "PopGen:selfing_coalescence_probability" builtin_selfing_coalescence_probability :: () -> () -> () -> ()
-foreign import bpcall "SMC:li_stephens_2003_composite_likelihood" li_stephens_2003_composite_likelihood :: () -> () -> () -> ()
+data VVI -- Vector<Vector<int>>
+
+foreign import bpcall "PopGen:read_phase_file" builtin_read_phase_file :: CPPString -> EVector (EVector Int)
+foreign import bpcall "PopGen:read_phase2_file" builtin_read_phase2_file :: CPPString -> EVector (EVector Int)
+foreign import bpcall "PopGen:remove_2nd_allele" remove_2nd_allele :: EVector a -> EVector a
+foreign import bpcall "PopGen:allele_frequency_spectrum" allele_frequency_spectrum :: EVector Int -> EVector Int
+foreign import bpcall "PopGen:ewens_sampling_group_probability" ewens_sampling_group_probability :: Double -> VVI -> LogDouble
+foreign import bpcall "PopGen:ewens_sampling_probability" ewens_sampling_probability :: Double -> EVector Int -> LogDouble
+foreign import bpcall "PopGen:ewens_sampling_mixture_probability" builtin_ewens_sampling_mixture_probability :: EVector Double -> EVector Double -> VVI -> LogDouble
+foreign import bpcall "PopGen:selfing_coalescence_probability" builtin_selfing_coalescence_probability :: Int -> Double -> EVector Int -> LogDouble
+foreign import bpcall "SMC:li_stephens_2003_composite_likelihood" li_stephens_2003_composite_likelihood :: EVector Int -> Double -> AlignmentMatrix -> LogDouble
 
 read_phase_file filename = map list_from_vector $ list_from_vector $ builtin_read_phase_file $ list_to_string filename
 
@@ -21,14 +24,14 @@ ewens_sampling_mixture_probability thetas ps x = builtin_ewens_sampling_mixture_
 
 selfing_coalescence_probability n_loci s i = builtin_selfing_coalescence_probability n_loci s (list_to_vector i)
 
-afs args = Distribution "afs" (make_densities $ ewens_sampling_probability args) (error "afs has no quantile") () ()
+afs args = Distribution "afs" (make_densities $ ewens_sampling_probability args) (error "afs has no quantile") undefined undefined
 
-afsGroup args = Distribution "afsGroup" (make_densities $ ewens_sampling_group_probability args) (error "afsGroup has no quantile") () ()
+afsGroup args = Distribution "afsGroup" (make_densities $ ewens_sampling_group_probability args) (error "afsGroup has no quantile") undefined undefined
 
-afsMixture thetas ps = Distribution "afsMixture" (make_densities $ ewens_sampling_mixture_probability thetas ps) (error "afsMixture has no quantile") () ()
+afsMixture thetas ps = Distribution "afsMixture" (make_densities $ ewens_sampling_mixture_probability thetas ps) (error "afsMixture has no quantile") undefined undefined
 
-selfing_coalescence n_loci s = Distribution "selfing_coalescence" (make_densities $ selfing_coalescence_probability n_loci s) (error "selfing_coalescence has no quantile") (replicate n_loci False) (ListRange (replicate n_loci TrueFalseRange))
+selfing_coalescence n_loci s = Distribution "selfing_coalescence" (make_densities $ selfing_coalescence_probability n_loci s) (error "selfing_coalescence has no quantile") undefined NoRange
 
-li_stephens_2003 locs rho = Distribution "li_stephens_2003" (make_densities $ li_stephens_2003_composite_likelihood locs' rho) (error "li_stephens_2003 has no quantile") () ()
+li_stephens_2003 locs rho = Distribution "li_stephens_2003" (make_densities $ li_stephens_2003_composite_likelihood locs' rho) (error "li_stephens_2003 has no quantile") undefined undefined
     where locs' = list_to_vector locs
 
