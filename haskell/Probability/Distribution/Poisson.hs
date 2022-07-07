cda71abdee62ff8d528d6909616a8ade7596d422
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-06 21:25:35 -0400

    Put poisson and related dists into Random.

--- haskell/Probability/Distribution/Poisson.hs
+++ haskell/Probability/Distribution/Poisson.hs
@@ -16,17 +16,13 @@ poisson_effect x = do
 
 sample_poisson mu = RandomStructure poisson_effect modifiable_structure $ liftIO (IOAction (\s -> (s, builtin_sample_poisson mu s)))
 
-poisson mu = Distribution "poisson" (make_densities $ poisson_density mu) (no_quantile "Poisson") (sample_poisson mu) (integer_above 0)
-
 --- Poisson process, constant rate --
 poisson_process_density' rate t1 t2 n = expToLogDouble $ (-rate*(t2-t1)) + ((intToDouble n)*log rate)
 poisson_process_density  rate t1 t2 points = poisson_process_density' rate t1 t2 n where n = length points
 sample_poisson_process rate t1 t2 = do
-  n <- RanDistribution $ poisson (rate * (t2-t1))
+  n <- poisson (rate * (t2-t1))
   xs <- iid n (uniform t1 t2)
   return $ sort xs
-
-poisson_process rate t1 t2 = Distribution "poisson_process" (make_densities $ poisson_process_density rate t1 t2) (no_quantile "poisson_process") (sample_poisson_process rate t1 t2) NoRange
 
 --- Poisson process, piecewise constant
 
@@ -47,5 +43,18 @@ sample_poisson_processes ((rate,t1,t2):intervals) = do
   -- FIXME - this doesn't seem very efficient!
   return $ points1 ++ points2
 
-poisson_processes intervals = Distribution "poisson_processes" (make_densities' $ poisson_processes_densities intervals) (no_quantile "poisson_processes") (sample_poisson_processes intervals) NoRange
+class HasPoisson d where
+    poisson :: Double -> d Int
+    poisson_process :: Double -> Double -> Double -> d [Double]
+    poisson_processes :: [(Double,Double,Double)] -> d [Double]
 
+instance HasPoisson Distribution where
+    poisson mu = Distribution "poisson" (make_densities $ poisson_density mu) (no_quantile "Poisson") (sample_poisson mu) (integer_above 0)
+    poisson_process rate t1 t2 = Distribution "poisson_process" (make_densities $ poisson_process_density rate t1 t2) (no_quantile "poisson_process") (sample_poisson_process rate t1 t2) NoRange
+
+    poisson_processes intervals = Distribution "poisson_processes" (make_densities' $ poisson_processes_densities intervals) (no_quantile "poisson_processes") (sample_poisson_processes intervals) NoRange
+
+instance HasPoisson Random where
+    poisson mu = RanDistribution $ poisson mu
+    poisson_process rate t1 t2 = RanDistribution $ poisson_process rate t1 t2
+    poisson_processes intervals = RanDistribution $ poisson_processes intervals
