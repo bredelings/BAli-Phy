a30c4a17f366c195c1e54566ce9de3507f5603fc
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-07 17:58:16 -0400

    Update optimizer tests.

--- tests/optimizer/7/test.hs
+++ tests/optimizer/7/test.hs
@@ -1,3 +1,3 @@
 module Test where {
-f x = let {h x y = g y; g y = k y; k y = h 1 y} in g x;
+f x = let {h x y = g y; g y = k y; k y = h (1::Int) y} in g x;
 }
--- tests/optimizer/pat-bind/Test.hs
+++ tests/optimizer/pat-bind/Test.hs
@@ -1,5 +1,5 @@
 module Test where
 
-xys = [(1,2),(3,4)]
+xys = [(1,2),(3,4)] :: [(Int,Int)]
 xxs = [x*x | (x,y) <- xys]
 (x,y) = head xys
