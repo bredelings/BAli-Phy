524feac141d1a8df4eca706e0b09f0fadc651f80
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-07 19:20:08 -0400

    Add a Show instance for (Matrix a)

--- haskell/Data/Matrix.hs
+++ haskell/Data/Matrix.hs
@@ -2,6 +2,13 @@ module Data.Matrix where
 
 import Foreign.Vector
 
+import Text.Show
+
 fromLists xss = fromVectors $ list_to_vector $ map list_to_vector xss
 
 foreign import bpcall "SModel:scaleMatrix" scaleMatrix :: a -> Matrix a -> Matrix a
+
+foreign import bpcall "Prelude:show" showMatrix :: Matrix a -> CPPString
+
+instance Show (Matrix a) where
+    show x = unpack_cpp_string $ showMatrix x
