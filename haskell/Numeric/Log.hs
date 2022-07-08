38dcc0801fbc32799d41ff70d1ec238a9f2051f9
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-07 21:24:15 -0400

    Fix definitions of expm1 and log1p

--- haskell/Numeric/Log.hs
+++ haskell/Numeric/Log.hs
@@ -4,5 +4,5 @@ data Log a = Log {ln :: a}
 
 class Floating a => Precise a where { }
 
-foreign import bpcall "Real:expm1" expm1 :: () -> ()
-foreign import bpcall "Real:log1p" log1p :: () -> ()
+foreign import bpcall "Real:expm1" expm1 :: Double -> Double
+foreign import bpcall "Real:log1p" log1p :: Double -> Double
