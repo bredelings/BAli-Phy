e9ea19e9cca7d17203f3a80b409cdc624d39aa97
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-10-13 13:06:59 -0400

    Also test specifying pattern types.

--- tests/haskell/Types/1/Main.hs
+++ tests/haskell/Types/1/Main.hs
@@ -3,9 +3,14 @@ module Main where
 polyapply :: (forall a.a->a) -> (Int, Char)
 polyapply f = (f 1, f 'a')
 
+polyapply2 (f :: forall a.a->a) = (f 1, f 'a')
+
 main = do putStrLn "HelloWorld!"
           putStrLn $ show [[1],[2]]
           putStrLn $ show (1,'a')
           putStrLn $ show 'a'
           putStrLn $ show $ polyapply (id :: forall b.b->b)
+          putStrLn $ show $ polyapply id
+          putStrLn $ show $ polyapply2 (id :: forall b.b->b)
+          putStrLn $ show $ polyapply2 id
 
