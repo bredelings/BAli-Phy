366faecc5a9316f7e444f41c7430e11c8e420d1a
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-11-10 14:47:52 -0500

    Add context::exchange_regs(r1,r2).

--- src/computation/context.cc
+++ src/computation/context.cc
@@ -412,6 +412,11 @@ void context_ref::set_reg_value(int P, closure&& C)
     memory()->set_reg_value_in_context(P, std::move(C), context_index);
 }
 
+void context_ref::exchange_regs(int r1, int r2)
+{
+    memory()->exchange_regs_in_context(r1, r2, context_index);
+}
+
 
 std::set<int> context_ref::tweak_and_find_affected_sampling_events(const std::function<void(context_ref&)>& tweak)
 {
