288f0bcd5936e4acfd22e27a911f82abddced141
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-11-12 13:20:29 -0500

    Add class LikelihoodMixtureComponent

--- haskell/SModel/LikelihoodMixtureModel.hs
+++ haskell/SModel/LikelihoodMixtureModel.hs
@@ -8,6 +8,24 @@ import Foreign.Vector
 data MixtureIndex = MixtureIndex Int
 
 -- Could we parameterize this class by the method of determining leaf likelihoods?
+-- No, that would probably be an observation model.  We might need one per mixture component.
+
+class LikelihoodMixtureComponent m where
+    -- The most general thing to do would be to specify Pr(observation=o|markov state=s) for each o and s.
+    -- What we are doing here is to specify a list of states for which the probability is 1 or 0.
+    -- Which could be shortened into just the list of which states are 1 - EVector (EVector Int)
+    stateToObservedState :: m -> MixtureIndex -> EVector Int
+    numObservedStates :: m -> Int
+    alphabet :: m -> Alphabet
+
+    -- In order for this to be a general interface, the tree type needs to be hidden!
+    transitionProbabilities :: m -> Int -> Matrix Double
+
+    rootFrequencies :: m -> MixtureIndex -> EVector Double
+    -- This means that all the transition_probability matrices in a component
+    -- satisfy detailed balance with the root frequencies
+    isReversible :: m -> Bool
+
 
 class LikelihoodMixtureModel m where
     alphabet :: m -> Alphabet
