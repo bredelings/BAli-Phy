d34693a4c3ad4018a4507f686b74067944d3c47b
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-11-12 13:20:48 -0500

    Add comments on observation models.

--- haskell/SModel/Simple.hs
+++ haskell/SModel/Simple.hs
@@ -7,6 +7,20 @@ import Tree
 data SingleBranchLengthModel t a = SingleBranchLengthModel t a
 get_tree' (SingleBranchLengthModel t _) = t        -- Avoid aliasing with get_tree from DataPartition
 
+-- See LikelihoodMixtureModel
+
+{-
+ * So arguably getAlphabet + stateLetters is really part of the observation model.
+ *  - arguably, because the markov chains on internal states could perhaps be describes by an alphabet as well...
+ *    but just for naming purposes?
+
+ * We could in theory allow different mixture components to have different observation models.
+   - getAlphabet / stateLetters
+
+ * It should still make sense to have weighted componentFrequencies, but the rows of the weighted frequency MATRIX
+   would have different lengths.  So maybe, weighted_frequenced_vectors: m -> EVector EVector Double.
+-}
+
 class SimpleSModel m where
     stateLetters :: m -> EVector Int
     getAlphabet :: m -> Alphabet
