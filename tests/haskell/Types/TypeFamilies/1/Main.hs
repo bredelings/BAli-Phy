9dffdc34f8076c84f709f7cae723bc0ff550297c
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-11-21 19:55:52 -0500

    Add simple test for TypeFamilies

---
+++ tests/haskell/Types/TypeFamilies/1/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+class Container c where
+    type Element c
+    type instance Element c = c
+
+type family Elem c
+
+type instance Elem [c] = c
+
+type family F (c :: * -> *) :: *
+
+type instance F [] = [Int]
