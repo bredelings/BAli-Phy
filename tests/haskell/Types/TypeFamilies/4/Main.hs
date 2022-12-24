9ca75e233dcd9a33cbe16ddfcef0490c91bd29f1
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-12-24 14:49:56 -0800

    Add comments.

--- tests/haskell/Types/TypeFamilies/4/Main.hs
+++ tests/haskell/Types/TypeFamilies/4/Main.hs
@@ -1,10 +1,33 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
+{- This test creates:
+
+   [W] a ~ (Arg a -> Res a)
+   [W] C a
+
+Processing the first wanted creates two regular metatypevar's to break the cycle:
+
+   [W] a ~ (cbv1 -> cbv2)
+   [W] C a
+   [W] cbv1 ~ Arg a
+   [W] cbv2 ~ Res a
+ 
+We then immediately substitute for a, yielding:
+
+   [W] C (cbv1 -> cbv2)
+   [W] cbv1 ~ Arg (cbv1 -> cbv2)
+   [W] cbv2 ~ Res (cbv1 -> cbv2)
+
+The first wanted can then be satisfied via the class instance for C.
+The remaining wanteds are reduce to reflexivites via the type instances for Arg and Res.
+
+-}
+
 type family Arg a
-type family Result a
+type family Res a
 
 type instance Arg (c -> d) = c
-type instance Result (c -> d) = d
+type instance Res (c -> d) = d
 
 class C a where
     method :: a -> a
@@ -12,7 +35,7 @@ class C a where
 instance C (a -> b) where
     method x = x
 
-foo :: (a ~ (Arg a -> Result a)) => a -> a
+foo :: (a ~ (Arg a -> Res a)) => a -> a
 foo x = x
 
 func x = method (foo x)
