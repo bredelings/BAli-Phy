a05ab6067f5286349750d3f9bb01c458134dcaaa
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-12-28 17:46:01 -0800

    Move TypeCheckerContext back out of err_context.H

--- src/computation/typecheck/err_context.cc
+++ src/computation/typecheck/err_context.cc
@@ -46,24 +46,6 @@ string FileContents::print_range(int line1, int col1, int line2, int col2) const
 }
 
 
-void TypeCheckerContext::pop_note()
-{
-    notes.pop_back();
-}
-
-void TypeCheckerContext::push_note(const Note& e)
-{
-    notes.push_back(e);
-}
-
-string TypeCheckerContext::print_note() const
-{
-    vector<string> estrings;
-    for(auto& note: notes)
-        estrings.push_back("    • "+note.print()+"\n");
-    return "Error:\n"+join(estrings, "\n");
-}
-
 string Message::print(const FileContents& file) const
 {
     std::ostringstream out;
