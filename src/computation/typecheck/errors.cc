3851c6bb168ccaf6f69e7988dbddc15b172fd3da
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-12-30 13:59:28 -0800

    Move check_wanteds to errors.cc

---
+++ src/computation/typecheck/errors.cc
@@ -0,0 +1,57 @@
+#include "typecheck.H"
+#include "kindcheck.H"
+
+#include "util/text.H"      // for ANSI colors
+#include "util/set.H"       // for add( , )
+#include "util/variant.H"   // for to< >()
+#include "haskell/ids.H"
+
+using std::string;
+using std::vector;
+using std::map;
+using std::set;
+using std::pair;
+using std::optional;
+
+
+void TypeChecker::check_wanteds(const WantedConstraints& wanteds, const TypeCheckerContext& context)
+{
+    for(auto& wanted: wanteds.simple)
+    {
+        std::optional<yy::location> loc;
+        Note e;
+        if (auto occ = to<OccurrenceOrigin>(wanted.origin))
+        {
+            e<<"Could not derive `"<<bold_green(print_unqualified(wanted.pred))<<ANSI::bold<<"`";
+            e<<" arising from a use of `"<<cyan(print_unqualified_id(occ->name))<<ANSI::bold<<"`";
+            if (occ->name.loc)
+            {
+                loc = occ->name.loc;
+                e<<" at "<<(*loc);
+            }
+        }
+        else if (auto uorig = to<UnifyOrigin>(wanted.origin))
+        {
+            e<<"Expected `"<<bold_green(print_unqualified(uorig->t2))<<ANSI::bold<<"` but got `"<<bold_green(print_unqualified(uorig->t1))<<ANSI::bold<<"`";
+        }
+        else if (auto sorig = to<StringOrigin>(wanted.origin))
+        {
+            e<<sorig->msg;
+        }
+        else if (auto app = to<AppOrigin>(wanted.origin))
+        {
+            e<<"Applying "<<(app->arg_index+1)<<" arguments to function "<<app->head.print()<<", but it only takes "<<app->arg_index<<"!";
+            loc = app->loc;
+        }
+        else if (auto lsec = to<LeftSectionOrigin>(wanted.origin))
+            e<<"In left section, "<<lsec->op<<" is not a function!";
+        else
+            e<<"Could not derive `"<<bold_green(print_unqualified(wanted.pred))<<ANSI::bold<<"`";
+
+        record_error(loc, context, e);
+    }
+
+    for(auto& implic: wanteds.implications)
+        check_wanteds(implic->wanteds,implic->context);
+}
+
