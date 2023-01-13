ddcba5e7c2ef4db3c0ed85132cd6f948c683a1f8
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-01-12 20:56:40 -0800

    Add an Ix instance for pairs.

--- haskell/Data/Ix.hs
+++ haskell/Data/Ix.hs
@@ -17,3 +17,9 @@ instance Ix Int where
     index     (start,end) val = val - start
     inRange   (start,end) val = (start <= val) && (val <= end)
     rangeSize (start,end)     = end - start + 1
+
+instance (Ix a, Ix b) => Ix (a,b) where
+    range     ((s1,s2),(e1,e2))         = [(x,y) | x <- range (s1,e1), y <- range (s2,e2)]
+    index     ((s1,s2),(e1,e2)) (v1,v2) = index (s1,e1) v1 * rangeSize (s2,e2) + index (s2,e2) v2
+    inRange   ((s1,s2),(e1,e2)) (v1,v2) = inRange (s1,e1) v1 && inRange (s2,e2) v2
+    rangeSize ((s1,s2),(e1,e2))         = rangeSize (s1,e1) * rangeSize (s2,e2)
