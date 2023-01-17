07ec82788c0e9540f8fd45f014498ff3a9da94f3
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-01-17 18:51:31 -0500

    Add IntMap C++ class.

--- src/computation/machine/gcobject.cc
+++ src/computation/machine/gcobject.cc
@@ -1,18 +1,45 @@
 #include "gcobject.H"
 
 
-void GCArray::get_regs(std::vector<int>& regs) const
+void IntArray::get_regs(std::vector<int>& regs) const
 {
     regs = array_regs;
 }
 
-void GCArray::update_regs(const std::vector<int>& remap) const
+void IntArray::update_regs(const std::vector<int>& remap) const
 {
     for(auto& r: array_regs)
         r = remap[r];
 }
 
-GCArray::GCArray(const std::vector<int>& a)
+IntArray::IntArray(const std::vector<int>& a)
     :array_regs(a)
+{
+}
+
+void IntMap::get_regs(std::vector<int>& owned_regs) const
+{
+    owned_regs.resize(regs.size());
+    int i=0;
+    for(auto& [key,reg]: regs)
+        owned_regs[i++] = reg;
+}
+
+void IntMap::update_regs(const std::vector<int>& remap) const
+{
+    // The problem is that we should like these modifications
+    // to affect ALL the shared maps;
+    for(auto& [key,reg]: regs)
+    {
+        if (reg != remap[reg])
+        {
+            regs = regs.erase(key);
+            regs = regs.insert({key,remap[reg]});
+        }
+    }
+}
+
+IntMap::IntMap(const immer::map<int,int>& a)
+    :regs(a)
 {
 }
