7cddaa8f796a495d7a762b922b28bcf01dbad6fc
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-01-29 23:16:34 -0500

    Add `on` to Data.Function

--- haskell/Data/Function.hs
+++ haskell/Data/Function.hs
@@ -3,12 +3,12 @@ module Data.Function where
 
 id x = x
 
-flip f x y = f y x
-
 const x y = x
 
-infixl 9 .
+infixr 9 .
 (f . g) x = f (g x)
+
+flip f x y = f y x
 
 infixr 0 $
 f $ x = f x
@@ -17,3 +17,7 @@ infixl 1 &
 x & f = f x
 
 fix f = let x = f x in x
+
+-- i.e. (compare `on` measure) = \x y -> compare (measure x) (measure y)
+infixl 0 `on`
+on f g = \x y -> g (f x) (f y)
