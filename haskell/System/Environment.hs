7f0002f99d47a201a36d42105d20dd4f1c8ba635
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-02-25 11:24:46 -0800

    Update getArgs for makeIO.

--- haskell/System/Environment.hs
+++ haskell/System/Environment.hs
@@ -1,6 +1,6 @@
 module System.Environment where
 
-foreign import bpcall "Environment:getArgs" builtin_getArgs :: () -> EVector CPPString
+foreign import bpcall "Environment:getArgs" builtin_getArgs :: RealWorld -> EVector CPPString
 
-getArgs = IOAction (\s -> (s, map listFromString $ list_from_vector $ builtin_getArgs ()))
+getArgs = makeIO (\s -> map listFromString $ list_from_vector $ builtin_getArgs s)
 
