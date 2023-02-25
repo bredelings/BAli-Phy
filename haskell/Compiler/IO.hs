0b1d73e83e7a6ad7ed778a193c71783f6e3e154f
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-02-24 17:31:00 -0800

    Add new IO constructor IO.

--- haskell/Compiler/IO.hs
+++ haskell/Compiler/IO.hs
@@ -25,7 +25,8 @@ instance Monad IO where
     unsafeInterleaveIO f = IOAction (\state -> (state, snd $ runIO f state))
 
 -}
-data IO a = IOAction  (RealWorld->(RealWorld,a)) |
+data IO a = IO (RealWorld -> (RealWorld,a)) |
+            IOAction  (RealWorld->(RealWorld,a)) |
             IOChangeable (IO a) |
             IOLazy (IO a) |
             IOMFix (a -> IO a) |
@@ -41,16 +42,18 @@ instance Applicative IO where
 
 instance Monad IO where
     f >>= g  = IOAndPass f g
-    return x = IOReturn x
+    return x = IO (\s -> (s,x))
     mfix f   = IOMFix f
     unsafeInterleaveIO f = IOLazy f
 
 
 unsafePerformIO :: IO c -> c
+unsafePerformIO (IO f) = snd (f 0#)
 unsafePerformIO (IOAction f) = snd (f 0#)
 unsafePerformIO (IOChangeable f) = _changeable_apply unsafePerformIO f
 unsafePerformIO (IOLazy f) = unsafePerformIO f
 unsafePerformIO (IOAndPass (IOLazy f) g) = let x = unsafePerformIO f in unsafePerformIO (g x)
+unsafePerformIO (IOAndPass (IO f) g) = case f 0# of (s,x) -> s `seq` unsafePerformIO (g x)
 unsafePerformIO (IOAndPass f g) = let x = unsafePerformIO f in x `seq` unsafePerformIO (g x)
 unsafePerformIO (IOMFix f) = let x = unsafePerformIO (f x) in x
 unsafePerformIO (IOReturn x) = x
