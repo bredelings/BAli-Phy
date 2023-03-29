710acd9a7deb0960148cd611fd5d1ad5664c4bf7
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-03-29 17:39:38 -0400

    Don't reserve a dictionary field for equality superclasses.

--- src/computation/typecheck/class.cc
+++ src/computation/typecheck/class.cc
@@ -121,6 +121,9 @@ TypeChecker::infer_type_for_class(const Hs::ClassDecl& class_decl)
     // 4. Make global types for superclass extractors
     for(auto& superclass_constraint: class_info.context.constraints)
     {
+        // We don't reserve a field in the dictionary for equality superclasses.
+        if (is_equality_pred(superclass_constraint)) continue;
+
         string cname1 = get_class_name_from_constraint(superclass_constraint);
         string cname2 = get_class_name_from_constraint(class_constraint);
         string extractor_name = cname1+"From"+cname2;
