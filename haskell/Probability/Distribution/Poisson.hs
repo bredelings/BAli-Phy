ee6b603821ea754afdf96daa6c5f8af32ccd0085
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-01 15:20:27 -0400

    Refactor Poisson (but not poisson_process{,es})

--- haskell/Probability/Distribution/Poisson.hs
+++ haskell/Probability/Distribution/Poisson.hs
@@ -8,18 +8,68 @@ import Probability.Distribution.Uniform
 
 foreign import bpcall "Distribution:poisson_density" poisson_density :: Double -> Int -> LogDouble
 foreign import bpcall "Distribution:sample_poisson" builtin_sample_poisson :: Double -> RealWorld -> Int
+sample_poisson mu = makeIO $ builtin_sample_poisson mu
 
+data Poisson = Poisson Double
+
+instance Dist Poisson where
+    type Result Poisson = Int
+    dist_name _ = "poisson"
+
+instance IOSampleable Poisson where
+    sampleIO (Poisson mu) = sample_poisson mu
+
+instance HasPdf Poisson where
+    pdf (Poisson mu) n = poisson_density mu n
+
+instance Dist1D Poisson where
+    cdf (Poisson mu) = undefined
+
+instance MaybeMean Poisson where
+    maybeMean (Poisson mu) = Just mu
+
+instance Mean Poisson
+
+instance MaybeVariance Poisson where
+    maybeVariance (Poisson mu)= Just mu
+
+instance Variance Poisson
+
+instance HasAnnotatedPdf Poisson where
+    annotated_densities dist@(Poisson mu) n = do
+        in_edge "mu" mu
+        return [pdf dist n]
+
+instance Sampleable Poisson where
+    sample dist = RanDistribution2 dist poisson_effect
+    
 poisson_bounds = integer_above 0
 
 poisson_effect x = do
    add_move $ slice_sample_integer_random_variable x poisson_bounds
    add_move $ inc_dec_mh x poisson_bounds
 
-sample_poisson mu = makeIO $ builtin_sample_poisson mu
-ran_sample_poisson mu = RanAtomic poisson_effect (sample_poisson mu)
+poissonDist mu = Poisson mu
+
+poisson mu = sample $ poissonDist mu
+
+------------------------------------------------------------
+data PoissonProcess = PoissonProcess Double Double Double
+
+instance Dist PoissonProcess where
+    type Result PoissonProcess = [Double]
+    dist_name _ = "poisson_process"
+
+instance IOSampleable PoissonProcess where
+    sampleIO (PoissonProcess rate t1 t2) = do
+       n <- sampleIO $ poissonDist (rate * (t2-t1))
+       xs <- sampleIO $ iidDist n (uniform t1 t2)
+       return $ sort xs
+
+
 
 --- Poisson process, constant rate --
-poisson_process_density' rate t1 t2 n = expToLogDouble $ (-rate*(t2-t1)) + (fromIntegral n * log rate)
+poisson_process_density' rate t1 t2 n = expTo $ (-rate*(t2-t1)) + (fromIntegral n * log rate)
 poisson_process_density  rate t1 t2 points = poisson_process_density' rate t1 t2 n where n = length points
 sample_poisson_process rate t1 t2 = do
   n <- poisson (rate * (t2-t1))
@@ -46,17 +96,14 @@ sample_poisson_processes ((rate,t1,t2):intervals) = do
   return $ points1 ++ points2
 
 class HasPoisson d where
-    poisson :: Double -> d Int
     poisson_process :: Double -> Double -> Double -> d [Double]
     poisson_processes :: [(Double,Double,Double)] -> d [Double]
 
 instance HasPoisson Distribution where
-    poisson mu = Distribution "poisson" (make_densities $ poisson_density mu) (no_quantile "Poisson") (ran_sample_poisson mu) (integer_above 0)
     poisson_process rate t1 t2 = Distribution "poisson_process" (make_densities $ poisson_process_density rate t1 t2) (no_quantile "poisson_process") (sample_poisson_process rate t1 t2) NoRange
 
     poisson_processes intervals = Distribution "poisson_processes" (make_densities' $ poisson_processes_densities intervals) (no_quantile "poisson_processes") (sample_poisson_processes intervals) NoRange
 
 instance HasPoisson Random where
-    poisson mu = RanDistribution $ poisson mu
     poisson_process rate t1 t2 = RanDistribution $ poisson_process rate t1 t2
     poisson_processes intervals = RanDistribution $ poisson_processes intervals
