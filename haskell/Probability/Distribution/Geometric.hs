271edc3d9652b1e21be1cb843ae90761fb6edff2
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-01 12:28:22 -0400

    Refactor Geometric

--- haskell/Probability/Distribution/Geometric.hs
+++ haskell/Probability/Distribution/Geometric.hs
@@ -9,10 +9,43 @@ import Range
 foreign import bpcall "Distribution:sample_geometric" builtin_sample_geometric :: Double -> RealWorld -> Int
 sample_geometric p_success = makeIO $ builtin_sample_geometric p_success
 
+data Geometric = Geometric Prob
 
-geometric_density :: Double -> Double -> Int -> LogDouble
-geometric_density p_fail p_success n | n < 0  = 0
-                                     | otherwise = (fromRational p_success) * pow (fromRational p_fail) (fromIntegral n)
+instance Dist Geometric where
+    type Result Geometric = Int
+    dist_name _ = "geometric"
+
+instance IOSampleable Geometric where
+    sampleIO (Geometric p_success) = sample_geometric (toFloating p_success)
+
+
+-- Hmm.. Maybe we want the result of discrete distributionts to be Probability instead of LogDouble?
+
+instance HasPdf Geometric where
+    pdf (Geometric p_success) n | n < 0 = 0
+                                | otherwise = toFloating $ p_success * pow (1-p_success) (fromIntegral n)
+
+
+instance Dist1D Geometric where
+    cdf (Geometric p_success) n = undefined
+
+instance MaybeMean Geometric where
+    maybeMean (Geometric p) = Just $ toFloating $ (1-p)/p
+
+instance Mean Geometric
+
+instance MaybeVariance Geometric where
+    maybeVariance (Geometric p) = Just $ toFloating $ (1-p)/(p*p)
+
+instance Variance Geometric
+
+instance HasAnnotatedPdf Geometric where
+    annotated_densities dist@(Geometric p_success) n = do
+                                     in_edge "p_success" p_success
+                                     return [pdf dist n]
+
+instance Sampleable Geometric where
+    sample dist@(Geometric p_success) = RanDistribution2 dist geometric_effect
 
 geometric_bounds = integer_above 0
 
@@ -20,17 +53,7 @@ geometric_effect x = do
   add_move $ slice_sample_integer_random_variable x geometric_bounds
   add_move $ inc_dec_mh x geometric_bounds
 
-ran_sample_geometric p_success = RanAtomic geometric_effect (sample_geometric p_success)
+geometricDist :: Double -> Geometric
+geometricDist p_success = Geometric (toFloating p_success)
 
-geometric p = geometric2 (1-p) p
-rgeometric q = geometric2 q (1-q)
-
-class HasGeometric d where
-    geometric2 :: Double -> Double -> d Int
-
-instance HasGeometric Distribution where
-    geometric2 p_fail p_success = Distribution "geometric" (make_densities $ geometric_density p_fail p_success) (no_quantile "geometric") (ran_sample_geometric p_success) (integer_above 0)
-
-
-instance HasGeometric Random where
-    geometric2 p_fail p_success = RanDistribution $ geometric2 p_fail p_success
+geometric p_success = sample $ geometricDist p_success
