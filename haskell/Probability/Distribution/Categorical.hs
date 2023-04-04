7909fcdb3647a74a0e1329fe8a91e3ebe8413e69
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-03 22:45:28 -0400

    Refactor Categorical

--- haskell/Probability/Distribution/Categorical.hs
+++ haskell/Probability/Distribution/Categorical.hs
@@ -1,7 +1,8 @@
-module Probability.Distribution.Categorical (categorical,categorical_on, HasCategorical) where
+module Probability.Distribution.Categorical where
 
 import Probability.Random
 import Data.Array
+import Data.Foldable
 import Control.Monad.IO.Class
 import MCMC
 
@@ -9,39 +10,75 @@ import Foreign.Vector
 
 categorical_effect n x = add_move (\c -> gibbs_sample_categorical x n c)
 foreign import bpcall "Distribution:sample_categorical" builtin_sample_categorical :: EVector Double -> RealWorld -> Int
-sample_categorical ps = makeIO $ builtin_sample_categorical (list_to_vector ps)
+sample_categorical ps = makeIO $ builtin_sample_categorical (array_to_vector ps)
 ran_sample_categorical ps = RanAtomic (categorical_effect (length ps)) (sample_categorical ps)
 
--- FIXME: is `make_densities $ qs !` supposed to parse as `make_densities (qs!)`?
-annotated_categorical_density qs x = do
-  in_edge "qs" qs
-  return [qs!x]
+data Categorical = Categorical (Array Int Double)
 
-class HasCategorical d where
-    categorical :: [Double] -> d Int
-    categorical_on :: Eq b => [(b,Double)] -> d b
+instance Dist Categorical where
+    type Result Categorical = Int
+    dist_name _ = "categorical"
 
-instance HasCategorical Distribution where
-    categorical ps = Distribution "categorical" (annotated_categorical_density qs) (no_quantile "categorical") (ran_sample_categorical ps) (integer_between 0 (length ps - 1))
-                where qs = listArray' $ map doubleToLogDouble ps
+instance IOSampleable Categorical where
+    sampleIO (Categorical ps) = sample_categorical ps
 
-    categorical_on pairs = Distribution "categorical_on" (annotated_categorical_on_density pairs) (no_quantile "categorical_on") (sample_categorical_on pairs) NoRange
+instance HasPdf Categorical where
+    pdf (Categorical ps) n | n < 0          = 0
+                           | n >= length ps = 0
+                           | otherwise      = doubleToLogDouble $ ps!n
 
-instance HasCategorical Random where
-    categorical ps = RanDistribution $ categorical ps
-    categorical_on pairs = RanDistribution $ categorical_on pairs
+instance Dist1D Categorical where
+    cdf (Categorical ps) x | n < 0          = 0
+                           | n >= length ps = 1
+                           | otherwise      = sum $ take (n+1) $ toList ps
+                           where n = floor x
 
-sample_categorical_on :: [(a,Double)] -> Random a
-sample_categorical_on pairs = do
-  i <- categorical $ map snd pairs :: Random Int
-  return (fst (pairs!!i))
+instance HasAnnotatedPdf Categorical where
+    annotated_densities dist n = return [pdf dist n]
 
-categorical_on_density pairs x = case lookup x pairs of
-                                   Just p -> doubleToLogDouble p
-                                   Nothing -> 0
+instance Sampleable Categorical where
+    sample dist@(Categorical ps) = RanDistribution2 dist $ categorical_effect (length ps)
 
-annotated_categorical_on_density pairs x = do
-  in_edge "pairs" pairs
-  return [categorical_on_density pairs x]
+categoricalDist ps = Categorical $ listArray' ps
 
+categorical ps = sample $ categoricalDist ps
+
+---
+
+data CategoricalOn a = CategoricalOn (Array Int a) (Array Int Double)
+
+instance Dist (CategoricalOn a) where
+    type Result (CategoricalOn a) = a
+    dist_name _ = "CategoricalOn"
+
+instance IOSampleable (CategoricalOn a) where
+    sampleIO (CategoricalOn xs ps) = do
+      i <- sample_categorical ps
+      return (xs!i)
+
+{-
+Error - why doesn't this work?
+Also, why  can't we rewrite (Result (CategoricalOn a)) to a?
+And, why would we think that xs :: Array Int Int?  Is there some defaulting going on there?     -}
+
+instance Eq a => HasPdf (CategoricalOn a) where
+    pdf (CategoricalOn xs ps) x = case elemIndexArray x xs of
+                                    Nothing -> 0
+                                    Just n -> doubleToLogDouble $ ps!n
+
+instance Eq a => HasAnnotatedPdf (CategoricalOn a) where
+    annotated_densities dist x = return [pdf dist x]
+
+instance Sampleable (CategoricalOn a) where
+    sample dist@(CategoricalOn xs ps) = do
+        i <- sample $ Categorical ps
+        return $ xs!i
+
+
+categoricalOnDist pairs = CategoricalOn (listArray' xs) (listArray' ps)
+    where xs = map fst pairs
+          ps = map snd pairs
+
+categorical_on :: [(a,Double)] -> Random a
+categorical_on pairs = sample $ categoricalOnDist pairs
 
