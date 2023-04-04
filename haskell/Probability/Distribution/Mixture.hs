ed9fc88b4b1a2c4193794858976ad710335f3719
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-04 08:48:32 -0400

    Refactor mixture distribution.

--- haskell/Probability/Distribution/Mixture.hs
+++ haskell/Probability/Distribution/Mixture.hs
@@ -3,6 +3,9 @@ module Probability.Distribution.Mixture where
 import Probability.Random
 import Probability.Distribution.Categorical
 
+mixture_pdf (p:ps) (dist:dists) x = (doubleToLogDouble p)*(pdf dist x) + (mixture_pdf ps dists x)
+mixture_pdf []     []           _ = 0
+
 mixture_density (p:ps) (dist:dists) x = (doubleToLogDouble p)*(density dist x) + (mixture_density ps dists x)
 mixture_density []     []           _ = 0
 
@@ -10,23 +13,28 @@ sample_mixture :: [Double] -> [Distribution a] -> Random a
 sample_mixture weights dists = do cat <- categorical weights
                                   RanDistribution $ dists !! cat
 
-class HasMixture d where
-    mixture :: [Double] -> [d a] -> d a
+data Mixture d = Mixture [Double] [d]
 
-instance HasMixture Distribution where
-    mixture weights dists = Distribution "mixture"
-                                         (make_densities $ mixture_density weights dists)
-                                         (no_quantile "mixture")
-                                         (sample_mixture weights dists)
-                                         NoRange
-instance HasMixture Random where
-    mixture weights dists = do cat <- categorical weights
-                               dists !! cat
+instance Dist (Mixture d) where
+    type Result (Mixture d) = Result d
+    dist_name _ = "mixture"
+
+instance IOSampleable d => IOSampleable (Mixture d) where
+    sampleIO (Mixture ps dists) = do cat <- sampleIO $ categoricalDist ps
+                                     sampleIO $ dists !! cat
+
+instance HasPdf d => HasPdf (Mixture d) where
+    pdf (Mixture ps dists) x = mixture_pdf ps dists x
+
+instance HasAnnotatedPdf d => HasAnnotatedPdf (Mixture d) where
+    annotated_densities (Mixture ps dists) = make_densities $ mixture_density ps dists
+
+instance Sampleable d => Sampleable (Mixture d) where
+    sample (Mixture ps dists) = do cat <- categorical ps
+                                   sample $ dists !! cat
 
 
+mixtureDist ps dists | length ps /= length dists  = error "mixture distribution has different number of weights and distributions"
+                     | otherwise                  = Mixture ps dists
 
-
-
-
-
-
+mixture ps dists = sample $ mixtureDist ps dists
