231e6eec06b7d141c5ab45febb27fa2e4d442dea
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-10 22:18:53 -0400

    Add dirichlet_misture_dist

--- haskell/Probability/Distribution/List.hs
+++ haskell/Probability/Distribution/List.hs
@@ -1,6 +1,8 @@
 module Probability.Distribution.List where
 
 import Probability.Random
+import Probability.Distribution.Gamma
+import Data.Array
 import Effect
 import MCMC
 
@@ -48,4 +50,48 @@ instance HasIndependent Random where
     xs <- lazy $ sequence $ repeat dist  -- lazy means no effects (add_move) or rate changes (sample_rate)
     add_move $ interchange_list_entries xs  -- ideally this is only allowed within strict blocks!  And MCMC-specific blocks?
     return $ take n xs
+-}
+
+
+iid2 n dist1 dist2 = iid n $ (,) <$> dist1 <*> dist2
+
+iid_mixture_dist n weight_dist item_dist = do
+    (ps, items) <- unzip <$> iid2 n weight_dist item_dist
+    return (map (/sum ps) ps, items)
+
+dirichlet_mixture_dist n a item_dist = iid_mixture_dist n (gamma a 1) item_dist
+
+
+even_sorted_on_iid f n dist = do let n_all = 2*n+1
+                                 xs' <- iid n_all dist
+                                 let xs = listArray n_all $ sortOn f xs'
+                                 return [xs!(2*i-1) | i <- [1..n]]
+
+even_sorted_iid = even_sorted_on_iid id
+
+
+odd_sorted_on_iid f n dist = do let n_all = max (2*n-1) 0
+                                xs' <- iid n_all dist
+                                let xs = listArray n_all $ sortOn f xs'
+                                return [xs!(2*i) | i <- [0..n-1]]
+
+odd_sorted_iid = odd_sorted_on_iid id
+{-
+odd_iid_mixture_dist n weight_dist item_dist = do
+    (ps, items) <- unzip <$> odd_sorted_on_iid snd n $ (,) <$> weight_dist <*> item_dist
+    return (map (/sum ps) ps, items)
+
+even_iid_mixture_dist n weight_dist item_dist = do
+    (ps, items) <- unzip <$> even_sorted_on_iid snd n $ (,) <$> weight_dist <*> item_dist
+    return (map (/sum ps) ps, items)
+
+dirichlet_odd_mixture_dist  n a item_dist = odd_iid_mixture_dist  n (gamma a 1) item_dist
+dirichlet_even_mixture_dist n a item_dist = even_iid_mixture_dist n (gamma a 1) item_dist
+-}
+
+{-
+  So, the need to couple the two distributions is very inconvenient!
+  It would be much easier if we could generate the dirichlet separately,
+  ... but that is inherently using the index of the weight and the item to
+  decide which items and weights correspond!
 -}
