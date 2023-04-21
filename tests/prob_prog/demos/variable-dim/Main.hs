447717ca2887c8290fd2019aa19463f4f03ea897
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-20 22:55:06 -0400

    Use prior.

--- tests/prob_prog/demos/variable-dim/Main.hs
+++ tests/prob_prog/demos/variable-dim/Main.hs
@@ -1,8 +1,8 @@
 import           Probability
 
 model = do
-    n <- sample $ geometric 0.5
-    ys <- sample $ iid n (exponential 1)
+    n <- prior $ geometric 0.5
+    ys <- prior $ iid n (exponential 1)
     observe 3 $ normal (sum ys) 1
     return ["n" %=% n, "ys" %=% ys]
 
