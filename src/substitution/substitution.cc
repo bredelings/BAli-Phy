9c81e40a7716903d6b266fdaafc44a64ff14430f
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-07-11 15:32:48 -0400

    Simplify calc_transition_prob_from_parent( ).

--- src/substitution/substitution.cc
+++ src/substitution/substitution.cc
@@ -1669,25 +1669,6 @@ namespace substitution {
         return Pr;
     }
 
-    void calc_transition_prob_from_parent(Matrix& S, const pair<int,int>& state_model_parent, const EVector& Ps, const Matrix& WF)
-    {
-        auto [mp,lp] = state_model_parent;
-
-        int n_states = S.size2();
-
-        // If there IS no parent character, then we can sample from F
-        if (mp == -1)
-            S = WF;
-        else
-        {
-            auto& Pr = Ps[mp].as_<Box<Matrix>>();
-            assert(mp != -1);
-            element_assign(S,0);
-            for(int l=0;l<n_states;l++)
-                S(mp,l) = Pr(lp,l);
-        }
-    }
-
     void calc_transition_prob_from_parent(Matrix& S, const pair<int,int>& state_model_parent, const EVector& Ps)
     {
         auto [mp,lp] = state_model_parent;
@@ -1698,10 +1679,21 @@ namespace substitution {
         assert(mp != -1);
 
         auto& Pr = Ps[mp].as_<Box<Matrix>>();
-        assert(mp != -1);
+        assert(mp >= 0);
         element_assign(S,0);
         for(int l=0;l<n_states;l++)
             S(mp,l) = Pr(lp,l);
+    }
+
+    void calc_transition_prob_from_parent(Matrix& S, const pair<int,int>& state_model_parent, const EVector& Ps, const Matrix& WF)
+    {
+        auto [mp,lp] = state_model_parent;
+
+        // If there IS no parent character, then we can sample from F
+        if (mp == -1)
+            S = WF;
+        else
+            calc_transition_prob_from_parent(S, state_model_parent, Ps);
     }
 
     void calc_leaf_likelihood(Matrix& S, int l, const alphabet& a, const EVector& smap)
