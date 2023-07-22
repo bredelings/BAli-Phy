998f9c6f9bbb6b2418b8cff6e3b653c0e8248d1b
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-07-21 20:49:35 -0400

    Add empty_cpp_string.

--- haskell/Foreign/String.hs
+++ haskell/Foreign/String.hs
@@ -20,3 +20,6 @@ list_to_string :: [Char] -> CPPString
 list_to_string x  = clist_to_string (list_to_CList x)
 
 pack_cpp_string = list_to_string
+
+-- Possibly this should just be "empty", but then we'd have to import this qualified and use something like FS.empty
+empty_cpp_string = list_to_string ""
