75c157b71098a7e3a66ac299f470c295e93318a5
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-08-12 20:34:30 -0700

    Fix typos in equals and less_than builtins for Text.

--- src/builtins/Text.cc
+++ src/builtins/Text.cc
@@ -146,7 +146,8 @@ extern "C" closure builtin_function_equals(OperationArgs& Args)
 
     std::string_view S1 = s1.as_<String>();
     std::string_view S2 = s2.as_<String>();
-    return {S1.substr(offset1,length2) == S2.substr(offset2,length2)};
+
+    return {S1.substr(offset1,length1) == S2.substr(offset2,length2)};
 }
 
 extern "C" closure builtin_function_less_than(OperationArgs& Args)
@@ -161,7 +162,8 @@ extern "C" closure builtin_function_less_than(OperationArgs& Args)
 
     std::string_view S1 = s1.as_<String>();
     std::string_view S2 = s2.as_<String>();
-    return {S1.substr(offset1,length2) < S2.substr(offset2,length2)};
+
+    return {S1.substr(offset1,length1) < S2.substr(offset2,length2)};
 }
 
 extern "C" closure builtin_function_concatRaw(OperationArgs& Args)
