13f00d5975ab6e801fd6647cf0c23b576ba679ef
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-11-14 15:14:34 -0500

    Add function that constructs EPair from (x,y)

--- haskell/Foreign/Pair.hs
+++ haskell/Foreign/Pair.hs
@@ -9,6 +9,8 @@ foreign import bpcall "Pair:c_snd" c_snd :: EPair a b -> b
 
 foreign import bpcall "Pair:c_pair" c_pair :: a -> b -> EPair a b
 
+c_pair' (x,y) = c_pair x y
+
 pair_from_c :: EPair a b -> (a,b)
 pair_from_c p = (c_fst p, c_snd p)
 
