72a2eab750946b363f9d212e113e3ebc804369d0
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-11-14 16:06:10 -0500

    Use peelBranchSEV for knuckles.

--- haskell/SModel/Likelihood.hs
+++ haskell/SModel/Likelihood.hs
@@ -134,7 +134,7 @@ cached_conditional_likelihoods_SEV t seqs alpha ps smap =
                      [] -> let node = sourceNode t b
                                (node_seq, bitmask) = seqs IntMap.! node
                            in peel_leaf_branch_SEV node_seq alpha p bitmask smap
-                     [b1] -> peel_deg2_branch_SEV (lc IntMap.! b1) p
+                     [b1] -> peelBranchSEV [] alpha smap [lc IntMap.! b1] p
                      [b1,b2] -> peelBranchSEV [] alpha smap [lc IntMap.! b1, lc IntMap.! b2] p
     in lc
 
