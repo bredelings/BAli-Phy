a867aa6c7e2979c4ae567bc56e733070dce183d7
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-11-27 13:42:40 -0500

    Initial implementation of non-reversible likelihood.

--- haskell/Probability/Distribution/PhyloCTMC/FixedA.hs
+++ haskell/Probability/Distribution/PhyloCTMC/FixedA.hs
@@ -129,3 +129,103 @@ instance (IsTree t, HasRoot (Rooted t), HasLabels t, HasBranchLengths t, HasBran
     sample dist = RanDistribution2 dist do_nothing
 
 
+--------------
+{-
+ok, so how do we pass IntMaps to C++ functions?
+well, we could turn each IntMap into an EIntMap
+for alignments, we could also use an ordering of the sequences to ensure that the leaves are written first.
+   -}
+annotated_subst_likelihood_fixed_A2 tree length smodel scale sequenceData = do
+  let subst_root = modifiable (head $ internal_nodes tree ++ leaf_nodes tree)
+
+  let (isequences, column_counts, mapping) = compress_alignment $ getSequences sequenceData
+
+      maybeNodeISequences = labelToNodeMap tree isequences
+      maybeNodeSeqsBits = ((\seq -> (strip_gaps seq, bitmask_from_sequence seq)) <$>) <$> maybeNodeISequences
+      nModels = nrows f
+      nodeCLVs = simpleNodeCLVsSEV alphabet smap nModels maybeNodeSeqsBits
+
+      uncompressedNodeSequences :: IntMap (Maybe (EVector Int))
+      uncompressedNodeSequences = labelToNodeMap tree $ getSequences sequenceData
+
+      n_nodes = numNodes tree
+      alphabet = getAlphabet smodel
+      smap   = stateLetters smodel
+      smodel_on_tree = SingleBranchLengthModel tree smodel scale
+      transition_ps = transition_ps_map smodel_on_tree
+      f = weighted_frequency_matrix smodel
+      cls = cachedConditionalLikelihoodsSEV2 tree nodeCLVs transition_ps f
+      likelihood = peelLikelihoodSEV2 nodeCLVs tree cls f alphabet smap subst_root column_counts
+
+--    This also needs the map from columns to compressed columns:
+      ancestralSequences = let ancestralComponentStateSequences :: IntMap VectorPairIntInt
+                               ancestralComponentStateSequences = sample_ancestral_sequences_SEV
+                                                                     tree
+                                                                     subst_root
+                                                                     nodeCLVs
+                                                                     alphabet
+                                                                     transition_ps
+                                                                     f
+                                                                     cls
+                                                                     smap
+                                                                     mapping
+                               ancestralStateSequences :: IntMap (EVector Int)
+                               ancestralStateSequences = extractStates <$> ancestralComponentStateSequences
+                               ancestralStateSequences' = minimally_connect_characters
+                                                                     uncompressedNodeSequences
+                                                                     tree
+                                                                     ancestralStateSequences
+                               ancestralLetterSequences = statesToLetters smap <$> ancestralStateSequences'
+                           in Aligned (CharacterData alphabet $ sequencesFromTree tree ancestralLetterSequences)
+
+      n_muts = parsimony_fixed_A tree maybeNodeSeqsBits alphabet (unitCostMatrix alphabet) column_counts
+
+  in_edge "tree" tree
+  in_edge "smodel" smodel
+
+  -- How about stuff related to alignment compression?
+  let prop = (PhyloCTMCProperties subst_root transition_ps cls ancestralSequences likelihood f smap undefined alphabet (SModel.nStates smodel) (SModel.nBaseModels smodel) n_muts)
+
+  return ([likelihood], prop)
+
+instance Dist (PhyloCTMC2 t Int s) where
+    type Result (PhyloCTMC2 t Int s) = AlignedCharacterData
+    dist_name _ = "PhyloCTMC2FixedA"
+
+-- TODO: make this work on forests!                  -
+instance (HasLabels t, HasRoots t, HasBranchLengths t, IsTree t, SimpleSModel s) => HasAnnotatedPdf (PhyloCTMC2 t Int s) where
+    type DistProperties (PhyloCTMC2 t Int s) = PhyloCTMCProperties
+    annotated_densities (PhyloCTMC2 tree length smodel scale) = annotated_subst_likelihood_fixed_A2 tree length smodel scale
+
+
+sampleComponentStatesFixed2 rtree rootLength smodel scale =  do
+  let ps = transition_ps_map (SingleBranchLengthModel rtree smodel scale)
+      f = (weighted_frequency_matrix smodel)
+
+  rec let simulateSequenceForNode node = case branchToParent rtree node of
+                                   Nothing -> simulateRootSequence rootLength f
+                                   Just b' -> let b = reverseEdge b'
+                                                  parent = sourceNode rtree b
+                                             in simulateFixedSequenceFrom (stateSequences IntMap.! parent) (ps IntMap.! b) f
+      stateSequences <- lazySequence $ IntMap.fromSet simulateSequenceForNode (getNodesSet rtree)
+  return stateSequences
+
+
+-- Should hasRoots t imply HasRoots (Rooted t)?
+-- Where is (Rooted t) coming up?  Can we remove it?
+
+instance (IsTree t, HasRoot (Rooted t), HasLabels t, HasBranchLengths (Rooted t), SimpleSModel s) => IOSampleable (PhyloCTMC2 t Int s) where
+    sampleIO (PhyloCTMC2 tree rootLength smodel scale) = do
+      let alphabet = getAlphabet smodel
+          smap = stateLetters smodel
+
+      stateSequences <- sampleComponentStatesFixed2 (makeRooted tree) rootLength smodel scale
+
+      let sequenceForNode label stateSequence = (label, statesToLetters smap $ extractStates stateSequence)
+
+      return $ Aligned $ CharacterData alphabet $ getLabelled tree sequenceForNode stateSequences
+
+instance (IsTree t, HasRoots t, HasRoot (Rooted t), HasLabels t, HasBranchLengths t, HasBranchLengths (Rooted t), SimpleSModel s) => Sampleable (PhyloCTMC2 t Int s) where
+    sample dist = RanDistribution2 dist do_nothing
+
+
--- haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
+++ haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
@@ -4,3 +4,7 @@ data PhyloCTMC t a s = PhyloCTMC t a s Double
 
 phyloCTMC tree alignment smodel scale = PhyloCTMC tree alignment smodel scale
 
+data PhyloCTMC2 t a s = PhyloCTMC2 t a s Double
+
+phyloCTMC2 tree alignment smodel scale = PhyloCTMC2 tree alignment smodel scale
+
