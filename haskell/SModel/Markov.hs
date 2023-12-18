23d8bb468c4bd155ffecdf6d187cf4b99e3e9f91
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-12-18 13:51:36 +0100

    Split non-reversible stuff from SModel.ReversibleMarkov into SModel.Markov

---
+++ haskell/SModel/Markov.hs
@@ -0,0 +1,90 @@
+module SModel.Markov (module SModel.Markov, module SModel.Frequency, module Data.Matrix) where
+
+import qualified Markov
+import           Markov (CTMC, getQ, getPi, qExp)
+import           SModel.Simple
+import           SModel.Rate
+import           SModel.Frequency
+import           Bio.Alphabet
+import           Data.Matrix
+import           Tree
+import           SModel.EigenExp
+
+foreign import bpcall "SModel:get_equilibrium_rate" get_equilibrium_rate :: Alphabet -> EVector Int -> Matrix Double -> EVector Double -> Double
+
+-- This takes the rate matrix q and adds:
+-- * pi -> a cached version of the equilibrium frequencies
+-- * t -> a scaling factor
+-- * r -> a cached version of the rate at equilibrium
+--
+-- * the observation model
+--   + a -> an alphabet
+--   + smap -> mapping from markov states -> alphabet states
+
+-- Now, all of these except (a,smap) are just caches for things that we can compute from q.
+-- It is the (a,smap) parts that make this into a SUBSTITUTION model.
+
+-- What the LIKELIHOOD wants is just:
+-- * component probabilities
+--   - number of components = component_probabilities.size()
+-- * mapping from states -> observed letters
+--   - number of states = smap(m).size()
+-- * for each branch, the probability of transitioning from i->j in component m.
+--   That is, transition_p(tree, branch, mixture) = matrix(i,j)
+-- * for each component, the root frequencies -> root_frequencies(m)
+-- I guess this is a "LikelihoodMixtureModel"
+-- Ideally, it would also specify if it is reversible
+--   that is, all transition_p matricies in component m satisfy detailed balance with respect to the root frequences in component m
+--
+
+data Markov = Markov Alphabet (EVector Int) Markov.Markov Double
+
+-- This is used both for observations, and also to determine which states are the same for computing rates.
+instance HasSMap Markov where
+    get_smap (Markov _ s _ _) = s
+
+instance CTMC Markov where
+    qExp (Markov _ _ m _) = qExp m
+    getPi (Markov _ _ m _) = getPi m
+    getQ (Markov _ _ m  _) = getQ m
+
+frequencies = getPi
+
+simple_smap a = list_to_vector [0..(alphabetSize a)-1]
+
+-- In theory we could take just (a,q) since we could compute smap from a (if states are simple) and pi from q.
+markov a smap q pi = Markov a smap rm rate where
+    rm = Markov.markov q pi
+    rate = get_equilibrium_rate a smap (getQ rm) pi
+
+-- In theory we could take just (a,q) since we could compute smap from a (if states are simple) and pi from q.
+markov' a smap q = Markov a smap rm rate where
+    rm = Markov.markov' q
+    rate = get_equilibrium_rate a smap (getQ rm) (getPi rm)
+
+instance HasAlphabet Markov where
+    getAlphabet (Markov a _ _ _) = a
+
+instance SimpleSModel Markov where
+    type instance IsReversible Markov = NonReversible
+    branch_transition_p (SingleBranchLengthModel tree smodel factor) b = [qExp $ scale (branch_length tree b * factor / r) smodel]
+        where r = rate smodel
+    distribution _ = [1.0]
+    nBaseModels _ = 1
+    stateLetters rm = get_smap rm
+    componentFrequencies smodel i = [frequencies smodel]!!i
+
+instance Scalable Markov where
+    scale x (Markov a s rm r) = Markov a s (scale x rm) (x*r)
+
+instance RateModel Markov where
+    rate (Markov _ _ _ r) = r
+
+-- A markov model needs a map from state -> letter in order to have a rate!
+-- For codon models, we basically use smap = id for nucleotides (then divide by three)
+--   and amino acids.
+-- If we had a covarion model on codons, then we'd need to first collaps the state to
+-- a codon, and then collapse the codons to either (i) amino acids or (ii) codons, and then divide by three.
+
+instance Show Markov where
+    show q = show $ getQ q
