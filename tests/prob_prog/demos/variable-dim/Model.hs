092d3498b00b6c84a8806d7d67cc21be23e0eab3
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-02-22 01:54:41 -0500

    Avoid running out of stack with variable-dim.

--- tests/prob_prog/demos/variable-dim/Model.hs
+++ tests/prob_prog/demos/variable-dim/Model.hs
@@ -3,7 +3,7 @@ module Model where
 import           Probability
 
 model = do
-    n <- prior $ geometric 0.5
+    n <- min 1000 <$> (prior $ geometric 0.5)
     ys <- prior $ iid n (exponential 1)
     observe 3 $ normal (sum ys) 1
     return ["n" %=% n, "ys" %=% ys]
