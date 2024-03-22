877194ed8a849a1bc60f5ff434c9a15d14eef5b0
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-03-22 09:32:52 -0400

    Collapse index_var_with_force_to_* => index_var_with_force

--- src/computation/machine/evaluate.cc
+++ src/computation/machine/evaluate.cc
@@ -279,18 +279,7 @@ pair<int,int> reg_heap::incremental_evaluate1_(int r)
 	    int r4 = follow_index_var(r3);
 	    if (r3 != r4) regs[r].C.Env[0] = r4;
 
-	    if (reg_is_to_changeable(r3))
-	    {
-		assert(reg_is_changeable(r4));
-
-                mark_reg_index_var_with_force_to_changeable(r);
-	    }
-            else
-	    {
-		assert(reg_is_constant(r4));
-
-                mark_reg_index_var_with_force_to_nonchangeable(r);
-	    }
+	    mark_reg_index_var_with_force(r);
 
             if (not reg_is_constant(r3))
 		set_forced_reg(r,r3);
@@ -707,18 +696,7 @@ pair<int,int> reg_heap::incremental_evaluate2_unevaluated_(int r)
 	    int r4 = follow_index_var(r3);
 	    if (r3 != r4) regs[r].C.Env[0] = r4;
 
-	    if (reg_is_to_changeable(r3))
-	    {
-		assert(reg_is_changeable(r4));
-
-                mark_reg_index_var_with_force_to_changeable(r);
-	    }
-            else
-	    {
-		assert(reg_is_constant(r4));
-
-                mark_reg_index_var_with_force_to_nonchangeable(r);
-	    }
+	    mark_reg_index_var_with_force(r);
 
             if (not reg_is_constant(r3))
 	    {
