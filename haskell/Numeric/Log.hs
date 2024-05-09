4ba6a775e0244afdebefaec45fc1f672ec0d2f70
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-05-09 17:06:02 -0400

    Fill out methods for (Log a)

--- haskell/Numeric/Log.hs
+++ haskell/Numeric/Log.hs
@@ -5,24 +5,21 @@ import Compiler.Floating
 data Log a = Exp a
 
 instance RealFloat a => Num (Log a) where
-    (+) = undefined
-    (-) = undefined
-    (*) = undefined
+    Exp x + Exp y = Exp (x + log1p(exp(y-x)))
+    Exp x - Exp y = Exp (x - log1p(exp(y-x)))
+    Exp x * Exp y = Exp (x + y)
     negate = error "Negation not allowed"
     abs = id
     signum _ = Exp 0
-    fromInteger = undefined
+    fromInteger x = Exp (log (fromInteger x))
 
 instance RealFloat a => Fractional (Log a) where
-    (Exp x) / (Exp y) = Exp (x - y)
+    Exp x / Exp y = Exp (x - y)
     recip (Exp x) = Exp $ -x
 
-{-
-instance RealFloat a => Pow (Log a) where
+instance Pow (Log Double) where
     pow (Exp x) t = Exp $ x*t
     ln (Exp x) = x
     expTo x = Exp x
--}
-
 
 
