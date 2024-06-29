8293e54599e683f88c8c289a63b1364572d441ea
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-06-28 20:49:36 -0400

    Also print the token type.

--- src/computation/machine/show_graph.cc
+++ src/computation/machine/show_graph.cc
@@ -1115,7 +1115,7 @@ void write_token_graph(const reg_heap& C, std::ostream& o)
 	    o<<"t"<<t<<" -> t"<<c<<"\n";
 	int n_steps = C.get_token(t).vm_step.delta().size();
 	int n_results = C.get_token(t).vm_result.delta().size();
-	o<<"t"<<t<<" [label=\"T"<<t<<" steps="<<n_steps<<" results = "<<n_results<<"\"]\n";
+	o<<"t"<<t<<" [label=\"t="<<t<<"  type="<<C.get_token(t).type<<"  steps="<<n_steps<<"  results = "<<n_results<<"\"]\n";
     }
     for(int c=0;c<C.get_n_contexts();c++)
     {
