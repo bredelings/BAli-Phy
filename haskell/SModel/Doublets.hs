efec69197fa786cb0941511a977dedb2fd96938d
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-09-09 15:01:56 -0700

    Stop supplying an alphabet to x2

--- haskell/SModel/Doublets.hs
+++ haskell/SModel/Doublets.hs
@@ -11,13 +11,16 @@ type DoubletAlphabet = Alphabet
 foreign import bpcall "SModel:f2x4_frequencies" f2x4_frequencies_builtin :: DoubletAlphabet -> EVector Double -> EVector Double -> EVector Double
 foreign import bpcall "SModel:singlet_to_doublet_rates" singlet_to_doublet_rates :: DoubletAlphabet -> Matrix Double -> Matrix Double -> Matrix Double
 
-x2x2 a m1 m2 = reversible $ markov a smap q pi where
+x2x2 m1 m2 = reversible $ markov a smap q pi where
+    a = mkDoublets (getAlphabet m1)
     smap = simple_smap a
     q = singlet_to_doublet_rates a (getQ m1) (getQ m2)
     pi = f2x4_frequencies_builtin a (getEqFreqs m1) (getEqFreqs m2)
 
-x2_sym a s = singlet_to_doublet_rates a s s
-x2 a q = x2x2 a q q
+x2_sym (ExchangeModel nucA s) = ExchangeModel a (singlet_to_doublet_rates a s s)
+    where a = mkDoublets nucA
+
+x2 q = x2x2 q q
 
 foreign import bpcall "SModel:rna_16a_exchange" rna_stem_16a_exchange :: DoubletAlphabet -> Double -> Double -> Double -> Double -> Double -> Matrix Double
 
