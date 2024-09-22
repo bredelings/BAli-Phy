b214c56c28b828c98486545ad0fe57e605350272
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-09-21 22:19:33 -0700

    Add slice_sample_node_time( ).

--- src/mcmc/sample-branch-lengths.cc
+++ src/mcmc/sample-branch-lengths.cc
@@ -130,11 +130,9 @@ void slice_sample_branch_length(owned_ptr<Model>& P,MoveStats& Stats,int b)
     const double L = PP.t().branch_length(b);
     const double mu = PP.branch_mean();
 
-
     MCMC::Result result(3);
   
     //------------- Find new length --------------//
-  
     double sigma = P->load_value("slice_branch_sigma",1.5);
     // NOTE - it is OK to depend on L below -- IF AND ONLY IF the likelihood is unimodal.
     double w = sigma*(PP.branch_mean()+L);
@@ -155,6 +153,27 @@ void slice_sample_branch_length(owned_ptr<Model>& P,MoveStats& Stats,int b)
 	Stats.inc("branch-length (slice) 3",result);
     else 
 	Stats.inc("branch-length (slice) 4",result);
+}
+
+void slice_sample_node_time(owned_ptr<Model>& P,MoveStats& Stats,int n)
+{
+    Parameters& PP = *P.as<Parameters>();
+    if (not PP.t().can_set_node_time(n))
+        return;
+
+    PP.set_root(n);
+
+    const double T = PP.t().node_time(n);
+    const double mu = PP.branch_mean();
+
+    //------------- Find new length --------------//
+    double sigma = P->load_value("slice_branch_sigma",1.5);
+
+    double w = sigma;
+
+    node_time_slice_function logp(PP, n);
+
+    double T2 = slice_sample(T, logp, w, 50);
 }
 
 void alignment_slice_sample_branch_length(owned_ptr<Model>& P,MoveStats& Stats,int b)
