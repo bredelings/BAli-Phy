04f307b1e0e160e8fe64e127699cc13a6bb75d29
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-09-26 13:37:32 -0400

    Add test for sampling from (yule n lambda)

---
+++ tests/prob_prog/sample_tree/yule/1/Main.hs
@@ -0,0 +1,26 @@
+import Probability
+import Probability.Random
+import Probability.Distribution.Tree.Yule
+import qualified Data.Text as T
+import Tree.Newick
+import Data.JSON as J
+import Data.Char
+
+model taxa = do
+    lambda <- sample $ logLaplace 0 2
+
+    tree <- sample $ yule taxa lambda
+
+    let loggers   = ["tree" %=% writeNewick tree, "lambda" %=% lambda]
+    return (tree, loggers)
+
+name i | i < 26 = [chr(65+i)]
+       | otherwise = [chr(65+m)] ++ name d
+    where (d,m) = divMod i 26
+
+main = do
+     let n = 20
+         taxa = fmap T.pack $ fmap name [0..n-1]
+
+     (tree,loggers) <- runRandomStrict (model taxa)
+     putStrLn $ show $ J.Object loggers
