e776f8077faf65eda128f0fd8ab1531d0d539f7f
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-10-02 15:05:03 -0400

    Fill out Data.Monoid a bit

--- haskell/Data/Monoid.hs
+++ haskell/Data/Monoid.hs
@@ -5,6 +5,17 @@ import Data.Semigroup
 
 class Semigroup a => Monoid a where
     mempty :: a
-    mconcat :: [a] -> a
 
-mappend = (<>)
+    mappend :: a -> a -> a
+    mappend = (<>)
+
+    mconcat :: [a] -> a
+    mconcat [] = mempty
+    mconcat (x:xs) = x <> (mconcat xs)
+
+instance Monoid () where
+    mempty = ()
+    mconcat _ = ()
+
+instance (Monoid a, Monoid b) => Monoid (a,b) where
+    mempty = (mempty, mempty)
