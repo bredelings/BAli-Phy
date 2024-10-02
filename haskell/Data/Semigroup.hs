4e9e70be817fd8339bab5c38c7b3c9bafa49bdd9
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-10-02 15:04:48 -0400

    Fill out Data.Semigroup a bit.

--- haskell/Data/Semigroup.hs
+++ haskell/Data/Semigroup.hs
@@ -1,7 +1,25 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Data.Semigroup where
 
+import Data.List.NonEmpty
+
 infixr 6 <>
 
 class Semigroup a where
     (<>) :: a -> a -> a
+    x <> y = sconcat ( x :| [y] )
+
+    sconcat :: NonEmpty a -> a
+    sconcat (x :| xs) = go x xs where
+                  go x [] = x
+                  go x (y:ys) = x <> go y ys
+
+-- stimes :: Integral b => b -> a -> a
+
+instance Semigroup () where
+    _ <> _ = ()
+    sconcat _ = ()
+
+instance (Semigroup a, Semigroup b) => Semigroup (a,b) where
+    (x,y) <> (x',y') = (x <> x', y <> y')
+
