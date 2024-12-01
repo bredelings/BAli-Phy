f4d1bc1c6141fa881c8488697c7a2ffb981be13f
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-11-30 22:55:25 -0500

    Add busted model -- first implementation.

--- haskell/SModel.hs
+++ haskell/SModel.hs
@@ -134,50 +134,30 @@ logNormalRates sigmaOverMu n base = rateMixtureUnifBins base (logNormalRatesDist
 -- join collapses a Discrete (Discrete a) -> Discrete a
 freeRates rateDist base = join $ (\r -> scale r base) <$> rateDist
 
--- * OK... so a mixture of rate matrices is NOT the same as a mixture of exponentiated matrices, because the rate matrices are scaled relative to each other.
---   ** Hmm... THAT might explain why the mixtures aren't working well!  We need to scale each of THOSE components separately.
+-- Should we normalize the different entries to have the same rate?
+busted omegaDist posP posW posSelection modelFunc = BranchSiteMixture $ m3Test omegaDist posP posW posSelection modelFunc
 
--- * In theory, we should allow each mixture component to have a different number of states.  This would require
---   that we either split the condition likelihoods into per-component objects, or reserve sum(i,smap(i)) spots per cell.
---   Probably the latter one would be fine.
+-- This doesn't quite work, because busted has transition probabilities but is not a (single) CTMC.                                                    
+bustedS omegaDist posP posW posSelection alpha n modelFunc = gammaRates alpha n $ always $ busted omegaDist posP posW posSelection modelFunc
 
 -- * The model from Sergei Kosakovsky-Pond is a SModelOnTreeMixture, since it is a mixture at the matrix level.
 -- * The MBR models are also SModelOnTree Mixtures, since they are also mixtures at the matrix level.
 --   + We should be able to get them by combining SingleBranchLengthModels.
-
+--
+-- * OK... so a mixture of rate matrices is NOT the same as a mixture of exponentiated matrices, because the rate matrices are scaled relative to each other.
+--   + Hmm... THAT might explain why the mixtures aren't working well!  We need to scale each of THOSE components separately.
+--
+-- * In theory, we should allow each mixture component to have a different number of states.  This would require
+--   that we either split the condition likelihoods into per-component objects, or reserve sum(i,smap(i)) spots per cell.
+--   Probably the latter one would be fine.
+--
 -- OK... so a mixture of rate matrices is NOT the same as a mixture of exponentiated matrices, because the rate matrices are scale with respect to each other.
 -- So, we can have
 --   ReversibleMarkov                          -- rate matrix
 --   MixtureModel ReversibleMarkov             -- mixture of rate matrices
 --   MixtureModels branchCats MixtureModel     -- per-branch mixture of rate matrices, where component i always has the same frequencies.
-
+--
 -- We can construct mixtures of these things with e.g. gamma rate models.
 --   Gamma rate models SHOULD be able to construct unit_mixtures WITHOUT the use of mmm or unitMixture now.
 --   We should also be able to constructing mixtures of mixtures of rate matrices -> mixtures of rate matrices.  This sounds like the join operation.
 
--- class SModelOnTree a where
---   branch_transition_p       :: (SingleBranchLengthModel a) Int -> EVector (Matrix Double)
---   distribution              :: a -> [Double]
---   weighted_frequency_matrix :: a -> Matrix Double
---   weighted_matrix           :: a -> Matrix Double
---   nBaseModels               :: a -> Int
---   stateLetters              :: a -> EVector
---   getAlphabet               :: a -> b
---   componentFrequencies      :: a -> Int -> EVector
-
--- How about
---   scale :: a -> a ?
---   qExp  :: a -> Matrix Double?
--- What kind of things can be scaled?  Things built on rate matrices, I guess?
-
--- If a mixture of mixture can be flattened, Mixture (Mixture) -> Mixture, isn't that like the monadic operation "join"?
-
--- Instances:
---   * ReversibleMarkovModel
---   * MixtureModel
---   * MixtureModels
---   * SModelOnTree a => SModelOnTreeMixture [(Double,a)]
-
--- So, the question is, can we avoid distribution on things like mixtures of Rates.
-
--- So, how are we going to handle rate scaling?  That should be part of the model!
