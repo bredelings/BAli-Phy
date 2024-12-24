25b29a86591de27092e8f8f7c0f0e348bd9e254f
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-12-23 19:55:39 -0800

    Add an annotated expression form for storing free-variables.

--- src/computation/optimization/free-vars.cc
+++ src/computation/optimization/free-vars.cc
@@ -72,6 +72,26 @@ FreeVarSet get_union(const FreeVarSet& s1, const FreeVarSet& s2)
     }
 }
 
+FreeVars get_union(const FreeVars& s1, const FreeVars& s2)
+{
+    if (s1.size() >= s2.size())
+    {
+        auto s3 = s1;
+        for(auto& x: s2)
+            if (not s3.count(x))
+                s3 = s3.insert(x);
+        return s3;
+    }
+    else
+    {
+        auto s3 = s2;
+        for(auto& x: s1)
+            if (not s3.count(x))
+                s3 = s3.insert(x);
+        return s3;
+    }
+}
+
 const FreeVarSet& get_free_vars(const annot_expression_ref<FreeVarSet>& e)
 {
     return e.note;
@@ -165,5 +185,110 @@ add_free_variable_annotations(const expression_ref& E)
     }
 
     std::abort();
+}
+
+const FreeVars& get_free_vars(const FV::Exp& E)
+{
+    return E.note();
+}
+
+FreeVars& get_free_vars(FV::Exp& E)
+{
+    return E.note();
+}
+
+FV::Exp
+add_free_variable_annotations(const Core2::Exp<>& E)
+{
+    // 1. Var
+    if (auto V = E.to_var())
+    {
+        return FV::Exp(*V, {*V});
+    }
+    // 2. Lambda
+    else if (auto L = E.to_lambda())
+    {
+        auto body = add_free_variable_annotations(L->body);
+        auto free_vars = get_free_vars(body);
+        free_vars = free_vars.erase(L->x);
+
+        return FV::Exp(FV::Lambda{L->x, body}, free_vars);
+    }
+    // 3. Apply
+    else if (auto A = E.to_apply())
+    {
+        auto head = add_free_variable_annotations(A->head);
+        auto free_vars = get_free_vars(head);
+        for(auto& arg: A->args)
+            free_vars = free_vars.insert(arg);
+        return FV::Exp(FV::Apply{head, A->args}, free_vars);
+    }
+    // 4. Let
+    else if (auto L = E.to_let())
+    {
+        auto body = add_free_variable_annotations(L->body);
+        FreeVars free_vars = get_free_vars(body);
+
+        FV::Decls decls;
+        for(auto& [x,rhs]: L->decls)
+        {
+            auto fv_rhs = add_free_variable_annotations(rhs);
+            free_vars = get_union(free_vars, get_free_vars(fv_rhs));
+            decls.push_back({x,fv_rhs});
+        }
+
+        for(auto& [x,_]: L->decls)
+            free_vars = free_vars.erase(x);
+
+        return FV::Exp(FV::Let{decls, body}, free_vars);
+    }
+    // 5. Case
+    else if (auto C = E.to_case())
+    {
+        auto object = add_free_variable_annotations(C->object);
+
+        auto free_vars = get_free_vars(object);
+
+        FV::Alts alts;
+        for(auto& [pat, body]: C->alts)
+        {
+            auto fv_body = add_free_variable_annotations(body);
+            auto alt_free_vars = get_free_vars(fv_body);
+
+            if (auto VP = pat.to_var_pat())
+                alt_free_vars = alt_free_vars.erase(VP->var);
+            else if (auto CP = pat.to_con_pat())
+            {
+                for(auto arg: CP->args)
+                    if (auto v = arg.to_var_pat_var())
+                        alt_free_vars = alt_free_vars.erase(*v);
+            }
+
+            alts.push_back({pat, fv_body});
+
+            free_vars = get_union(free_vars, alt_free_vars);
+        }
+        return FV::Exp(FV::Case{object, alts}, free_vars);
+    }
+    // 6. ConApp
+    else if (auto CA = E.to_conApp())
+    {
+        FreeVars free_vars;
+        for(auto& arg: CA->args)
+            free_vars = free_vars.insert(arg);
+        return FV::Exp(*CA, free_vars);
+    }
+    // 7. BuiltinOp
+    else if (auto B = E.to_builtinOp())
+    {
+        FreeVars free_vars;
+        for(auto& arg: B->args)
+            free_vars = free_vars.insert(arg);
+        return FV::Exp(*B, free_vars);
+    }
+    else if (auto C = E.to_constant())
+        return *C;
+    else
+        throw myexception()<<"add_free_variable_annotations: I don't recognize expression '"+ E.print() + "'";
 }
 
