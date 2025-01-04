93ba413c43997a04ae6ffdf9a3749cd1d4eca7a9
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-01-03 19:44:32 -0800

    Add notes about defaulting with FloatConvert

--- haskell/Data/Floating/Types.hs
+++ haskell/Data/Floating/Types.hs
@@ -15,3 +15,28 @@ instance FloatConvert Int     Double where
 
 instance {-# INCOHERENT #-} FloatConvert a       a where
     toFloating x = x
+
+{- NOTE: Problems with defaults.
+
+The geometric distribution is currently specified as:
+
+  geometric :: Double -> Geometric
+  geometric pSuccess = Geometric (toFloating pSuccess)
+
+We need to specify that pSuccess is Double because otherwise
+expressions like (geometric p) would have an ambiguous type
+such as (forall a.FloatConvert a Prob => a)
+
+If we could add a (Num a) constraint and avoid disabling defaulting
+when constraints from outside the standard library are used, then
+we could default to Double.
+
+Alternatively, if the hypothetical extension to multi-parameter
+type-classes from the NamedDefaults proposal was ever proposed
+and implemented, then we could do something like:
+
+   default FloatConvert a b => {a ~ b}
+
+That would default to Prob instead.  Hmm...
+
+-}
