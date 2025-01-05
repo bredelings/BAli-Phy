4102d7b0329c8f458c74dcbb3b3b92097ee14102
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-01-04 16:36:00 -0800

    Express multiFrequency' in terms of multiFrequency

--- haskell/SModel/MultiFrequency.hs
+++ haskell/SModel/MultiFrequency.hs
@@ -11,11 +11,9 @@ import qualified Data.IntSet as IntSet
 import SModel.Frequency (frequenciesFromDict)
 
 -- NOTE: The model here needs to know the list of node names on the tree.
-
-
--- QUESTION: Is it really separate from the tree then?
--- QUESTION: Suppose we make SimpleSModel operate directly on (SModelOnTree t m)?
--- QUESTION: Suppose we put the tree into MultiFrequency?
+-- * QUESTION: Is it really separate from the tree then?
+-- * QUESTION: Suppose we make SimpleSModel operate directly on (SModelOnTree t m)?
+-- * QUESTION: Suppose we put the tree into MultiFrequency?
 --           - This allows getting the nodes for the node info.
 --           - This allows getting the branches to apply q(b) and get an alphabet and smap out...
 
@@ -34,32 +32,40 @@ instance RateModel (MultiFrequency i n e) where
 instance Scalable (MultiFrequency i e n) where
     scaleBy x (MultiFrequency a smap rate nodeInfo nodePi branchQ) = MultiFrequency a smap (x*rate) nodeInfo nodePi branchQ
 
-nodeInfo (MultiFrequency _ _ _ f _ _) node      = f node  -- get the node info
-nodeProp (MultiFrequency _ _ _ f g _) node      = g $ f $ node  -- get the node property
-edgeProp (MultiFrequency _ _ _ f _ h) tree edge = h $ f $ node -- get the node property
+nodeInfo (MultiFrequency _ _ _ f _ _) node      = f node        -- get the node info
+nodeFreq (MultiFrequency _ _ _ f g _) node      = g $ f $ node  -- get the node property
+edgeProp (MultiFrequency _ _ _ f _ h) tree edge = h $ f $ node  -- get the node property
     where edge' | towardRoot tree edge = reverseEdge edge
                 | otherwise = edge
           node = targetNode tree edge'
 
--- Does it make sense to have a weighted list of CTMCs, independent of any root frequencies?
--- * If we attached root frequencies to each CTMC, then we would have a model that we could simulate from.
--- * If we attach a SINGLE root frequency vector to each CTMC, then
--- Suppose we have something that works like a
+-- It would be nice if the branchQ and the nodePi contained the _alphabet_ *before* applying them to a value.
+-- It would be nice if they also revealed the _smap_ before they are applied to a value.
+-- Neither the alphabet nor the smap should depend on any parameter values that determine the rate matrix.
+
+{- NOTE:
+   * The parameter 'nodeInfo' has information (such as the GC content, or the frequencies) at a node.
+     It is stored as an IntMap from Int node names to the information.
+     The Q matrix is a function of this information.
+
+   * The parameter nodePi instead gives the starting frequencies for a node if it is the root node.
+     Q: Couldn't we make a branch-heterogeneous model where the heterogeneousness doesn't affect the root frequencies?
+     A: Sure, but letting it affect the root frequencies is the more general case.
+ -}
+
 
 instance (HasRoot t, RateModel m, HasBranchLengths t, j ~ EVector Double, CTMC m) => SimpleSModel t (MultiFrequency i j m) where
     distribution model = [1]
     stateLetters (SModelOnTree _ model) = getSMap model
     branchTransitionP (SModelOnTree tree model) b = [qExp $ scaleBy (branchLength tree b) $ q]
         where q = scaleTo (rate model) $ edgeProp model tree b
-    componentFrequencies (SModelOnTree tree model) = [nodeProp model (root tree)]
+    componentFrequencies (SModelOnTree tree model) = [nodeFreq model (root tree)]
 
--- It would be nice if the branchQ and the nodePi contained the alphabet before applying them to a value.                                                     
-multiFrequency' tree nodeMap nodePi branchQ = MultiFrequency alphabet smap 1 (nodeMap IntMap.!) (toVector . frequenciesFromDict alphabet . nodePi) branchQ
-    where alphabet = getAlphabet (branchQ (nodeMap IntMap.! node))
-          smap = getSMap (branchQ (nodeMap IntMap.! node))
+multiFrequency tree nodeInfo nodePi branchQ = MultiFrequency alphabet smap 1 (nodeInfo IntMap.!) (toVector . nodePi) branchQ
+    where alphabet = getAlphabet (branchQ (nodeInfo IntMap.! node))
+          smap = getSMap (branchQ (nodeInfo IntMap.! node))
           node = head $ IntSet.elems (getNodesSet tree)
 
-multiFrequency tree nodeMap nodePi branchQ = MultiFrequency alphabet smap 1 (nodeMap IntMap.!) (toVector . nodePi) branchQ
-    where alphabet = getAlphabet (branchQ (nodeMap IntMap.! node))
-          smap = getSMap (branchQ (nodeMap IntMap.! node))
+multiFrequency' tree nodeInfo nodePi branchQ = multiFrequency tree nodeInfo (frequenciesFromDict alphabet . nodePi) branchQ
+    where alphabet = getAlphabet (branchQ (nodeInfo IntMap.! node))
           node = head $ IntSet.elems (getNodesSet tree)
