cc97387ec523b19550b7d3fb4a157f1169433f76
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-08-26 16:20:30 -0400

    Add a function to grab files and put them in the specified directory.

--- haskell/BAliPhy/Util.hs
+++ haskell/BAliPhy/Util.hs
@@ -1,8 +1,10 @@
 module BAliPhy.Util where
 
 import System.FilePath
+import System.Directory    
 
 foreign import bpcall "File:" createUniqueDirectoryRaw :: CPPString -> IO CPPString
 
 createUniqueDirectory dirName = unpack_cpp_string <$> createUniqueDirectoryRaw (list_to_string dirName)
 
+storeFiles dirName files = sequence [renameFile file (dirName </> takeFileName file) | file <- files]
