dad607742fdcc93f34bfd4e5a0329d9984798573
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-08-27 16:37:13 -0400

    Stop adding unfoldings for dfuns and default methods

--- src/computation/module.cc
+++ src/computation/module.cc
@@ -1284,9 +1284,7 @@ void Module::export_small_decls(const Core2::Decls<>& decls)
         // Add the unfolding for this variable.
         auto S = lookup_make_local_symbol(x.name);
 
-        if (simple_size(rhs) <= 5 or
-            S->symbol_type == symbol_type_t::instance_dfun or
-            S->symbol_type == symbol_type_t::default_method)
+        if (simple_size(rhs) <= 5)
         {
             // Label vars with whether they are used or not, and collect free vars.
             auto [occ_rhs, free_vars] = occurrence_analyzer(*this, rhs);
