9f20da4d1ac08777bac9e58adb05c8f9b973a4ab
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-09-01 11:09:44 -0400

    Dead variables are also inlinable.

--- src/computation/optimization/occurrence_info.cc
+++ src/computation/optimization/occurrence_info.cc
@@ -2,8 +2,8 @@
 
 bool occurrence_info::pre_inline() const
 {
-    if (work_dup != amount_t::Once) return false;
-    if (code_dup != amount_t::Once) return false;
+    if (work_dup != amount_t::Once and work_dup != amount_t::None) return false;
+    if (code_dup != amount_t::Once and code_dup != amount_t::None) return false;
     if (context == var_context::argument) return false;
     if (is_loop_breaker) return false;
     return true;
