9e40a201321c07c1fb8faa7dca67f6cb9f9f1b38
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-09-27 16:00:15 -0400

    Don't stop inlining into arguments.

--- src/computation/optimization/occurrence_info.cc
+++ src/computation/optimization/occurrence_info.cc
@@ -4,7 +4,6 @@ bool occurrence_info::pre_inline() const
 {
     if (work_dup != amount_t::Once and work_dup != amount_t::None) return false;
     if (code_dup != amount_t::Once and code_dup != amount_t::None) return false;
-    if (context == var_context::argument) return false;
     if (is_loop_breaker) return false;
     return true;
 }
