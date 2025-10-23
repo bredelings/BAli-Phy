3f219c9b3373cad010ac319f8066ae021f8570cd
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-10-23 08:54:18 -0400

    Move PhyloCTMCPropertiesFixedA to its own module

--- haskell/Probability/Distribution/PhyloCTMC/FixedA.hs
+++ haskell/Probability/Distribution/PhyloCTMC/FixedA.hs
@@ -1,11 +1,13 @@
 module Probability.Distribution.PhyloCTMC.FixedA
     (module Probability.Distribution.PhyloCTMC.Properties,
+     module Probability.Distribution.PhyloCTMC.FixedA.Properties,
      module Probability.Distribution.PhyloCTMC.PhyloCTMC,
      VariablePhyloCTMC(..), variable
     )
 
 where
 
+import Probability.Distribution.PhyloCTMC.FixedA.Properties
 import Probability.Distribution.PhyloCTMC.Properties
 import Probability.Distribution.PhyloCTMC.PhyloCTMC
 import Probability.Random
@@ -30,38 +32,6 @@ import Data.Maybe (fromJust)
 import SModel.Likelihood.CLV (CondLikes)
 
 import Control.Monad.Fix -- for rec
-
--- 0 - subst_root
--- 1 - transition_ps
--- 2 - CLVs
--- 3 - likelihood
--- 4 - alphabet
--- 5 - n_states
--- 6 - n_base_models
--- # - ?ancestral states         -- used through haskell, not parameters.cc
-
--- Some of these things could be accessed through the distribution arguments:
--- 4. alphabet
--- 5. n_states
--- 6. n_base_models
-
-
-data PhyloCTMCPropertiesFixedA = PhyloCTMCPropertiesFixedA {
-      prop_fixed_a_subst_root :: Int,
-      prop_fixed_a_transition_ps :: IntMap (EVector (Matrix Double)),
-      prop_fixed_a_cond_likes :: IntMap CondLikes,
-      prop_fixed_a_likelihood :: LogDouble,
-      prop_fixed_a_alphabet :: Alphabet,
-      prop_fixed_a_n_states :: Int,
-      prop_fixed_a_n_base_models :: Int,
-
-      prop_fixed_a_anc_cat_states :: IntMap VectorPairIntInt
-    }
-
-
-instance PhyloCTMCProperties PhyloCTMCPropertiesFixedA where
-    prop_likelihood = prop_fixed_a_likelihood
-    prop_anc_cat_states = prop_fixed_a_anc_cat_states
 
 {-
 ok, so how do we pass IntMaps to C++ functions?
