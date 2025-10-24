46b216955202ed92396a0920c8c2050b6e6a667f
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-10-24 12:41:59 -0400

    Make phyloCTMC construct a rooted tree.

--- haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
+++ haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
@@ -1,9 +1,11 @@
 module Probability.Distribution.PhyloCTMC.PhyloCTMC where
 
 import SModel.Simple
+import Tree
 
 data PhyloCTMC t a s r where
     PhyloCTMC :: t -> a -> s -> Double -> PhyloCTMC t a s (IsReversible s)
 
-phyloCTMC tree alignment smodel scale = PhyloCTMC tree alignment smodel scale
+phyloCTMC :: IsTree t => t -> a -> s -> Double -> PhyloCTMC (Rooted t) a s (IsReversible s)
+phyloCTMC tree alignment smodel scale = PhyloCTMC (makeRooted tree) alignment smodel scale
 
