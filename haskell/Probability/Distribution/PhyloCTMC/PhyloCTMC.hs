d03c8d50572afdc1aaa0a7ae0e180f9db3154fe7
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-10-24 11:30:00 -0400

    Only construct PhyloCTMC from rooted trees

--- haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
+++ haskell/Probability/Distribution/PhyloCTMC/PhyloCTMC.hs
@@ -2,10 +2,14 @@ module Probability.Distribution.PhyloCTMC.PhyloCTMC where
 
 import SModel.Simple
 import Tree
+import Reversible    
 
 data PhyloCTMC t a s r where
     PhyloCTMC :: t -> a -> s -> Double -> PhyloCTMC t a s (IsReversible s)
 
-phyloCTMC :: IsTree t => t -> a -> s -> Double -> PhyloCTMC (Rooted t) a s (IsReversible s)
-phyloCTMC tree alignment smodel scale = PhyloCTMC (makeRooted tree) alignment smodel scale
-
+phyloCTMC :: (IsTree t, CheckReversible s) => t -> a -> s -> Double -> PhyloCTMC (Rooted t) a s (IsReversible s)
+phyloCTMC tree alignment smodel scale = if isReversible smodel
+                                        then PhyloCTMC (makeRooted tree) alignment smodel scale
+                                        else case isRooted tree of
+                                               Unrooted -> error "phyloCTMC: the tree is unrooted, but the model is not reversible!"
+                                               Rooted -> PhyloCTMC tree alignment smodel scale
