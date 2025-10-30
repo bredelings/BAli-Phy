60860f2a86ce61a1d04c6339354c39d658a6d2cb
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-10-30 16:29:06 -0400

    Eliminate some uses of haskell_name_path

--- src/computation/haskell/ids.cc
+++ src/computation/haskell/ids.cc
@@ -52,7 +52,7 @@ inline bool ok(char c)
     return false;
 }
 
-std::optional<int> find_next(const std::string& s, int i)
+std::optional<int> skip_conid_dot(const std::string& s, int i)
 {
     if (i < s.size())
     {
@@ -73,11 +73,24 @@ std::optional<int> find_next(const std::string& s, int i)
         return {};
 }
 
+std::optional<int> find_module_separator(const std::string& s)
+{
+    int i=0;
+    while(auto i2 = skip_conid_dot(s,i))
+    {
+        i=*i2+1;
+    }
+    if (i > 0)
+        return i-1;
+    else
+        return {};
+}
+
 vector<string> haskell_name_path(const std::string& s)
 {
     vector<string> path;
     int i=0;
-    while(auto i2 = find_next(s,i))
+    while(auto i2 = skip_conid_dot(s,i))
     {
         path.push_back(s.substr(i,*i2-i));
         i=*i2+1;
@@ -245,10 +258,9 @@ bool is_haskell_qid(const std::string& s)
 
 bool is_haskell_normal_con_name(const std::string& s)
 {
-    vector<string> path = haskell_name_path(s);
-    if (not valid_path_prefix(path)) return false;
-    if (not is_haskell_conid(path.back()) and not is_haskell_consym(path.back())) return false;
-    return true;
+    auto uqname = get_unqualified_name(s);
+
+    return is_haskell_conid(uqname) or is_haskell_consym(uqname);
 }
 
 bool is_haskell_con_name(const std::string& s)
@@ -263,7 +275,8 @@ bool is_haskell_module_name(const std::string& s)
 
 bool is_qualified_symbol(const string& s)
 {
-    return (not s.empty() and get_haskell_identifier_path(s).size() >= 2);
+    return find_module_separator(s).has_value();
+//    return (not s.empty() and get_haskell_identifier_path(s).size() >= 2);
 }
 
 bool is_qualified_by_module(const std::string& s, const std::string& modid)
@@ -286,18 +299,18 @@ bool is_local_symbol(const std::string& s, const std::string& modid)
 
 string get_module_name(const std::string& s)
 {
-    vector<string> path = get_haskell_identifier_path(s);
-    path.pop_back();
-
-    if (not path.size())
-        return "";
+    if (auto m = find_module_separator(s))
+        return s.substr(0, *m);
     else
-        return join(path,'.');
+        return {};
 }
 
 string get_unqualified_name(const std::string& s)
 {
-    return get_haskell_identifier_path(s).back();
+    if (auto m = find_module_separator(s))
+        return s.substr(*m + 1);
+    else
+        return s;
 }
 
 
