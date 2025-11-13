b326cc3b6d98b74b1b7a7e4adb9ba797b6a1a533
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-11-13 17:05:55 -0500

    Add getEnv

--- haskell/System/Environment.hs
+++ haskell/System/Environment.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 module System.Environment where
 
+import Foreign.Vector
+import Foreign.String
+import Compiler.Base (String)
+import Compiler.Base (String)
+import Compiler.IO
+import Compiler.FFI.ToFromC
+import Data.List
+import Data.Function
+import Data.Functor
+    
 foreign import bpcall "Environment:getArgs" builtin_getArgs :: IO (EVector CPPString)
 
 getArgs :: IO [String]
 getArgs = fmap (map listFromString . vectorToList) $ builtin_getArgs
 
+foreign import bpcall "Environment:getArgs" getEnvRaw :: CPPString -> RealWorld -> CPPString
+
+getEnv :: String -> IO String
+getEnv = fromC getEnvRaw
--- src/builtins/Environment.cc
+++ src/builtins/Environment.cc
@@ -4,17 +4,24 @@
 #include "computation/machine/graph_register.H"
 #include "util/rng.H"
 #include "probability/choose.H"
+#include <cstdlib> // for getenv
 
-using boost::dynamic_pointer_cast;
 using namespace std;
 
 extern "C" closure builtin_function_getArgs(OperationArgs& Args)
 {
-  reg_heap& M = Args.memory();
+    reg_heap& M = Args.memory();
 
-  EVector V;
-  for(const auto& arg: M.args)
-    V.push_back(String(arg));
+    EVector V;
+    for(const auto& arg: M.args)
+        V.push_back(String(arg));
 
-  return V;
+    return V;
+}
+
+extern "C" closure builtin_function_getEnvRaw(OperationArgs& Args)
+{
+    auto x = Args.evaluate(0).as_<String>();
+
+    return String(getenv(x.c_str()));
 }
