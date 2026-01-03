430041cfbcb15e1074ca49ab255a07b245bf5b87
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-01-02 16:55:20 -0800

    Convert Char: builtins to ecall.

--- haskell/Data/Char.hs
+++ haskell/Data/Char.hs
@@ -9,42 +9,42 @@ import Compiler.Num
 import Data.Eq
 import Data.Ord
 
-foreign import bpcall "Char:isControl" builtin_isControl :: Char -> Int
+foreign import ecall "Char:isControl" builtin_isControl :: Char -> Int
 
 isControl :: Char -> Bool
 isControl c = if builtin_isControl c == 1 then True else False
 
-foreign import bpcall "Char:isSpace" builtin_isSpace :: Char -> Int
+foreign import ecall "Char:isSpace" builtin_isSpace :: Char -> Int
 
 isSpace :: Char -> Bool
 isSpace c = if builtin_isSpace c == 1 then True else False
 
-foreign import bpcall "Char:isLower" builtin_isLower :: Char -> Int
+foreign import ecall "Char:isLower" builtin_isLower :: Char -> Int
 
 isLower :: Char -> Bool
 isLower c = if builtin_isLower c == 1 then True else False
 
-foreign import bpcall "Char:isUpper" builtin_isUpper :: Char -> Int
+foreign import ecall "Char:isUpper" builtin_isUpper :: Char -> Int
 
 isUpper :: Char -> Bool
 isUpper c = if builtin_isUpper c == 1 then True else False
 
-foreign import bpcall "Char:isAlpha" builtin_isAlpha :: Char -> Int
+foreign import ecall "Char:isAlpha" builtin_isAlpha :: Char -> Int
 
 isAlpha :: Char -> Bool
 isAlpha c = if builtin_isAlpha c == 1 then True else False
 
-foreign import bpcall "Char:isAlphaNum" builtin_isAlphaNum :: Char -> Int
+foreign import ecall "Char:isAlphaNum" builtin_isAlphaNum :: Char -> Int
 
 isAlphaNum :: Char -> Bool
 isAlphaNum c = if builtin_isAlphaNum c == 1 then True else False
 
-foreign import bpcall "Char:isPrint" builtin_isPrint :: Char -> Int
+foreign import ecall "Char:isPrint" builtin_isPrint :: Char -> Int
 
 isPrint :: Char -> Bool
 isPrint c = if builtin_isPrint c == 1 then True else False
 
-foreign import bpcall "Char:isDigit" builtin_isDigit :: Char -> Int
+foreign import ecall "Char:isDigit" builtin_isDigit :: Char -> Int
 
 isDigit :: Char -> Bool
 isDigit c = if builtin_isDigit c == 1 then True else False
@@ -52,7 +52,7 @@ isDigit c = if builtin_isDigit c == 1 then True else False
 -- isOctDigit :: Char -> Bool
 -- not implemented
 
-foreign import bpcall "Char:isHexDigit" builtin_isHexDigit :: Char -> Int
+foreign import ecall "Char:isHexDigit" builtin_isHexDigit :: Char -> Int
 
 isHexDigit :: Char -> Bool
 isHexDigit c = if builtin_isHexDigit c == 1 then True else False
@@ -66,7 +66,7 @@ isLetter = isAlpha
 -- isNumber :: Char -> Bool
 -- not implemented
 
-foreign import bpcall "Char:isPunctuation" builtin_isPunctuation :: Char -> Int
+foreign import ecall "Char:isPunctuation" builtin_isPunctuation :: Char -> Int
 
 isPunctuation :: Char -> Bool
 isPunctuation c = if builtin_isPunctuation c == 1 then True else False
@@ -94,9 +94,9 @@ isAsciiLower = isLower
 -- GeneralCategory(..), generalCategory
 
 -- case conversion
-foreign import bpcall "Char:toUpper" toUpper :: Char -> Char
+foreign import ecall "Char:toUpper" toUpper :: Char -> Char
 
-foreign import bpcall "Char:toLower" toLower :: Char -> Char
+foreign import ecall "Char:toLower" toLower :: Char -> Char
 
 -- toTitle :: Char -> Char
 -- not implemented
@@ -112,12 +112,12 @@ digitToInt c | dec <= 9  = dec
     hexl = ord c - ord 'a'
     hexu = ord c - ord 'A'
 
-foreign import bpcall "Char:intToDigit" intToDigit :: Int -> Char
+foreign import ecall "Char:intToDigit" intToDigit :: Int -> Char
 
 -- numeric representations
-foreign import bpcall "Char:ord" ord :: Char -> Int
+foreign import ecall "Char:ord" ord :: Char -> Int
 
-foreign import bpcall "Char:chr" chr :: Int -> Char
+foreign import ecall "Char:chr" chr :: Int -> Char
 
 -- string representations
 -- showLitChar :: Char -> ShowS
--- src/builtins/Char.cc
+++ src/builtins/Char.cc
@@ -13,10 +13,11 @@
 // isSymbol?
 // isSeparator?
 
+using std::vector;
 
-extern "C" closure builtin_function_isDigit(OperationArgs& Args)
+extern "C" expression_ref simple_function_isDigit(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isdigit(c))
         return {expression_ref(1)};
@@ -24,9 +25,9 @@ extern "C" closure builtin_function_isDigit(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isControl(OperationArgs& Args)
+extern "C" expression_ref simple_function_isControl(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::iscntrl(c))
         return {expression_ref(1)};
@@ -34,9 +35,9 @@ extern "C" closure builtin_function_isControl(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isSpace(OperationArgs& Args)
+extern "C" expression_ref simple_function_isSpace(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isspace(c))
         return {expression_ref(1)};
@@ -44,9 +45,9 @@ extern "C" closure builtin_function_isSpace(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isLower(OperationArgs& Args)
+extern "C" expression_ref simple_function_isLower(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::islower(c))
         return {expression_ref(1)};
@@ -54,9 +55,9 @@ extern "C" closure builtin_function_isLower(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isUpper(OperationArgs& Args)
+extern "C" expression_ref simple_function_isUpper(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isupper(c))
         return {expression_ref(1)};
@@ -64,9 +65,9 @@ extern "C" closure builtin_function_isUpper(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isAlphaNum(OperationArgs& Args)
+extern "C" expression_ref simple_function_isAlphaNum(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isalnum(c))
         return {expression_ref(1)};
@@ -74,9 +75,9 @@ extern "C" closure builtin_function_isAlphaNum(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isAlpha(OperationArgs& Args)
+extern "C" expression_ref simple_function_isAlpha(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isalpha(c))
         return {expression_ref(1)};
@@ -84,9 +85,9 @@ extern "C" closure builtin_function_isAlpha(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isPrint(OperationArgs& Args)
+extern "C" expression_ref simple_function_isPrint(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isprint(c))
         return {expression_ref(1)};
@@ -94,9 +95,9 @@ extern "C" closure builtin_function_isPrint(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isPunctuation(OperationArgs& Args)
+extern "C" expression_ref simple_function_isPunctuation(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::ispunct(c))
         return {expression_ref(1)};
@@ -104,9 +105,9 @@ extern "C" closure builtin_function_isPunctuation(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_isHexDigit(OperationArgs& Args)
+extern "C" expression_ref simple_function_isHexDigit(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     if (std::isxdigit(c))
         return {expression_ref(1)};
@@ -114,39 +115,39 @@ extern "C" closure builtin_function_isHexDigit(OperationArgs& Args)
         return {expression_ref(0)};
 }
 
-extern "C" closure builtin_function_toLower(OperationArgs& Args)
+extern "C" expression_ref simple_function_toLower(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     return {expression_ref(std::tolower(c))};
 }
 
-extern "C" closure builtin_function_toUpper(OperationArgs& Args)
+extern "C" expression_ref simple_function_toUpper(vector<expression_ref>& args)
 {
-    char c = Args.evaluate(0).as_char();
+    char c = get_arg(args).as_char();
 
     return {expression_ref(std::toupper(c))};
 }
 
-extern "C" closure builtin_function_ord(OperationArgs& Args)
+extern "C" expression_ref simple_function_ord(vector<expression_ref>& args)
 {
-    auto c = Args.evaluate(0).as_char();
+    auto c = get_arg(args).as_char();
 
     return {expression_ref(int(c))};
 }
 
-extern "C" closure builtin_function_chr(OperationArgs& Args)
+extern "C" expression_ref simple_function_chr(vector<expression_ref>& args)
 {
-    auto i = Args.evaluate(0).as_int();
+    auto i = get_arg(args).as_int();
 
     // complain if we can't make a valid char out of this?
 
     return {expression_ref(char(i))};
 }
 
-extern "C" closure builtin_function_intToDigit(OperationArgs& Args)
+extern "C" expression_ref simple_function_intToDigit(vector<expression_ref>& args)
 {
-    auto i = Args.evaluate(0).as_int();
+    auto i = get_arg(args).as_int();
     char d = i;
 
     // complain if we can't make a valid char out of this?
