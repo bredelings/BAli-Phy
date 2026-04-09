799bc1b5de38f7601a44e0e92d51253bc9efcd1a
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-04-09 16:55:11 -0400

    Add c30

--- haskell/SModel/CXX.hs
+++ haskell/SModel/CXX.hs
@@ -1,6 +1,8 @@
 module SModel.CXX ( module SModel.CXX.C10
                   , module SModel.CXX.C20
+                  , module SModel.CXX.C30
                   ) where
 
 import SModel.CXX.C10
 import SModel.CXX.C20
+import SModel.CXX.C30
