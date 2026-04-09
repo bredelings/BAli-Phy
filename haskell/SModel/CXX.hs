a5c6e8fd03a16cbf69abc42f0c370c239ca669c7
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-04-09 17:09:55 -0400

    Add c40

--- haskell/SModel/CXX.hs
+++ haskell/SModel/CXX.hs
@@ -1,8 +1,12 @@
 module SModel.CXX ( module SModel.CXX.C10
                   , module SModel.CXX.C20
                   , module SModel.CXX.C30
+                  , module SModel.CXX.C40
                   ) where
 
 import SModel.CXX.C10
 import SModel.CXX.C20
 import SModel.CXX.C30
+import SModel.CXX.C40
+import SModel.CXX.C50
+import SModel.CXX.C60
