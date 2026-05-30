ac51a0f91794498d35cac99da79158da445a6beb
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-05-30 15:11:21 -0400

    Remove old indexify(expression_ref)

--- src/computation/expression/indexify.cc
+++ src/computation/expression/indexify.cc
@@ -26,11 +26,6 @@ int find_index_backward(const vector<T>& v,const T& t)
     return -1;
 }
 
-expression_ref make_indexed_lambda(const expression_ref& R)
-{
-    return new expression(lambda2(),{R});
-}
-
 var get_named_var(int n)
 {
     if (n<26) 
@@ -48,110 +43,6 @@ template <typename NoteV>
 var to_runtime_var(const Core::Var<NoteV>& x)
 {
     return var(x.name, x.index, x.is_exported);
-}
-
-/// Convert to using de Bruijn indices.
-expression_ref indexify(const expression_ref& E, vector<var>& variables)
-{
-    // Lambda expression - /\x.e
-    if (E.head().is_a<lambda>())
-    {
-	variables.push_back(E.sub()[0].as_<var>());
-	auto E2 = make_indexed_lambda( indexify(E.sub()[1], variables) );
-	variables.pop_back();
-	return E2;
-    }
-
-    // Let expression
-    else if (is_let_expression(E))
-    {
-        auto& L = E.as_<let_exp>();
-	for(auto& [x,_]: L.binds)
-	    variables.push_back(x);
-
-        vector<expression_ref> es;
-	for(auto& [_,e]: L.binds)
-	    es.push_back( indexify(e, variables) );
-
-        auto body = indexify(L.body, variables);
-
-	for(int i=0;i<L.binds.size();i++)
-	    variables.pop_back();
-
-	return indexed_let_expression(es,body);
-    }
-
-    // case expression
-    else if (auto case_exp = parse_case_expression(E))
-    {
-        auto& [object, alts] = *case_exp;
-
-	object = indexify(object, variables);
-
-	for(auto& [pattern, body]: alts)
-	{
-	    // Handle C x[1..n] -> body[i]
-
-#ifndef NDEBUG
-	    // FIXME - I guess this doesn't handle case a of b -> f(b)?
-	    if (is_var(pattern))
-		assert(is_wildcard(pattern));
-#endif
-
-	    for(int j=0;j<pattern.size();j++)
-		variables.push_back(pattern.sub()[j].as_<var>());
-
-	    body = indexify(body, variables);
-
-	    for(int j=0;j<pattern.size();j++)
-		variables.pop_back();
-
-	    pattern = pattern.head();
-	}
-
-	return make_case_expression(object, alts);
-    }
-
-    // Indexed Variable - This is assumed to be a free variable, so just shift it.
-    else if (E.is_index_var())
-        return index_var(E.as_index_var() + variables.size());
-
-    // Variable
-    else if (E.is_a<var>())
-    {
-        auto& D = E.as_<var>();
-        assert(not is_wildcard(E));
-
-        int index = find_index_backward(variables, D);
-        if (index == -1)
-            throw myexception()<<"Dummy '"<<D<<"' is apparently not a bound variable in '"<<E<<"'?";
-        else
-            return index_var(index);
-    }
-    // Constant or 0-arg constructor
-    else if (is_literal_type(E.type()) or is_constructor(E))
-        return E;
-    else if (is_constructor_exp(E) or is_apply_exp(E) or E.head().is_a<Operation>())
-    {
-        // This handles (modifiable) with no arguments.
-        if (E.is_atomic())
-            return E;
-        else
-        {
-            object_ptr<expression> V = E.as_expression().clone();
-            for(int i=0;i<V->size();i++)
-                V->sub[i] = indexify(V->sub[i], variables);
-            return V;
-        }
-    }
-
-    std::abort();
-}
-
-expression_ref indexify(const expression_ref& E)
-{
-    vector<var> variables;
-    return indexify(E,variables);
 }
 
 Runtime::Exp runtime_atom_from_constant(const Core::Constant& C)
