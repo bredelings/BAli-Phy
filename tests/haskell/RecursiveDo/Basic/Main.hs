acd39e7efa535a577f5b38f773cae983ad0814fb
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-06-11 14:52:21 -0400

    Test RecursiveDo rec rewriting

---
+++ tests/haskell/RecursiveDo/Basic/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude, RecursiveDo #-}
+
+import Compiler.Num
+import Control.Monad (return)
+import Control.Monad.Fix (mfix)
+import System.IO (print)
+
+main = do
+  rec
+    x <- return (1 :: Int)
+    y <- return (x + 2)
+  print (x, y)
