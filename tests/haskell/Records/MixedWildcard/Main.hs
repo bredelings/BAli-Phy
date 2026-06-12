9f4b2e685ed6a1cca516c8b39f13de2c355f0a6b
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-06-12 13:17:51 -0400

    Test mixed record wildcard fields

---
+++ tests/haskell/Records/MixedWildcard/Main.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Main where
+
+import Compiler.Num
+import System.IO (print)
+
+data Point = Point { x :: Int, y :: Int, z :: Int }
+
+makePoint x y = Point { z = 3, .. }
+
+movePoint :: Point -> Int -> Int -> Point
+movePoint p x y = p { z = 9, .. }
+
+main = print (((x p1, y p1), z p1), ((x p2, y p2), z p2))
+  where
+    p1 = makePoint 1 2
+    p2 = movePoint (Point 4 5 6) 7 8
