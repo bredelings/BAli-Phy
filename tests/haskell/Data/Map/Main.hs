892eb0cc27589945382196fd9da75cc7044ba597
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-06-26 20:47:18 -0700

    Test Data.Map submap value semantics

--- tests/haskell/Data/Map/Main.hs
+++ tests/haskell/Data/Map/Main.hs
@@ -11,6 +11,8 @@ main = do
   let m = Map.fromList [("B",7),("A",2),("B",9)] :: Map.Map [Char] Int
       m2 = Map.insert "C" 4 m
       m3 = Map.delete "A" m2
+      subSame = Map.fromList [("B",9)] :: Map.Map [Char] Int
+      subDifferent = Map.fromList [("B",7)] :: Map.Map [Char] Int
   putStrLn (show $ Map.keys m)
   putStrLn (show $ Map.elems m)
   putStrLn (show $ Map.assocs m)
@@ -19,3 +21,7 @@ main = do
   putStrLn (show $ Map.member "A" m3)
   putStrLn (show $ Map.assocs m3)
   putStrLn (show $ Map.assocs $ Map.map (+1) m)
+  putStrLn (show $ Map.isSubmapOf subSame m)
+  putStrLn (show $ Map.isSubmapOf subDifferent m)
+  putStrLn (show $ Map.isProperSubmapOf subSame m)
+  putStrLn (show $ Map.isProperSubmapOf m m)
