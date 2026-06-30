fb9c0fd2211a45d6f236f1d37dce0201b25a547b
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-06-29 18:02:55 -0700

    Extract evaluate1 changeable path

--- src/computation/machine/evaluate.cc
+++ src/computation/machine/evaluate.cc
@@ -301,6 +301,151 @@ pair<int,int> reg_heap::incremental_evaluate1_ref_with_force_(int r)
     return {r, result3};
 }
 
+/// Handle evaluate1 for a changeable reg, including the no-result/no-step path.
+/// That path only chases operation reductions until changeable bookkeeping exists.
+pair<int,int> reg_heap::incremental_evaluate1_changeable_(int r)
+{
+    assert(reg_is_changeable(r));
+
+    total_changeable_eval++;
+    int result = result_for_reg(r);
+
+    if (result > 0)
+    {
+        total_changeable_eval_with_result++;
+
+        return {r, result};
+    }
+
+    // If we know what to call, then call it and use it to set the value
+    if (int s = step_index_for_reg(r); s > 0)
+    {
+        // Evaluate S, looking through unchangeable redirections
+        auto [call, result] = incremental_evaluate1(steps[s].call);
+
+        // If the back-edge has not been set, then we need to update the call to look through
+        // ref_no_force and set the back-edge on the first non-reference if it is changeable.
+        if (not steps[s].call_edge)
+        {
+            // This should only ever happen for modifiable values set with set_reg_value( ).
+            // In such cases, we need this, because the value we set could evaluate to a register reference.
+            // Otherwise, we set the call AFTER we have evaluated to a changeable or a constant.
+            clear_call_for_reg(r);
+            set_call(s, call);
+        }
+        else
+            assert(call == call_for_reg(r));
+
+        // r gets its value from S.
+        set_result_for_reg( r );
+        if (not tokens[root_token].children.empty())
+        {
+            int t = tokens[root_token].children[0];
+            tokens[t].vm_result.add_value(r, non_computed_index);
+        }
+
+        total_changeable_eval_with_call++;
+        assert(not reg_is_unevaluated(r));
+        return {r, result};
+    }
+
+    while (1)
+    {
+        assert(not has_result1(r));
+        assert(not has_step1(r));
+        assert(closure_at(r).has_code());
+
+        if (closure_at(r).is_reg_ref())
+            std::abort();
+
+        else if (closure_at(r).get_code().is_whnf())
+            std::abort();
+
+#ifndef NDEBUG
+        else if (is_trim_code(closure_at(r)))
+            std::abort();
+#endif
+
+        // 3. Reduction: Operation (includes @, case, +, etc.)
+        else
+        {
+            // The only we reason we are getting this here is to store created_regs on it,
+            // if we perform allocations AFTER using/forcing something changeable.
+            int s = get_shared_step(r);
+
+            try
+            {
+                RegOperationArgs1 Args(r, s, *this);
+                auto O = operation_for_reduction(closure_at(r));
+                closure value = (*O)(Args);
+                total_reductions++;
+
+                // If the reduction doesn't depend on modifiable, then replace E with the value.
+                if (not Args.used_changeable)
+                {
+                    assert( not reg_has_call(r) );
+                    assert( not has_result1(r) );
+                    assert( regs[r].used_regs.empty() );
+                    assert( steps[s].created_regs.empty() ); // Any allocations should have gone to creator_step
+                    set_C( r, std::move(value) );
+                    steps.reclaim_used(s);
+                }
+                else
+                {
+                    total_changeable_reductions++;
+                    mark_reg_changeable(r);
+
+                    int r2;
+                    if (value.is_reg_ref())
+                    {
+                        r2 = value.reg_for_ref();
+                    }
+                    else
+                    {
+                        r2 = Args.allocate( std::move(value) ) ;
+                        assert(regs[r2].created_by_step.value().first == s);
+                        assert(not has_step1(r2));
+                    }
+
+                    auto [call,result] = incremental_evaluate1(r2);
+
+                    set_call(s, call);
+
+                    prog_steps[r] = s;
+                    set_result_for_reg(r);
+
+                    if (not tokens[root_token].children.empty())
+                    {
+                        int t = tokens[root_token].children[0];
+                        tokens[t].vm_result.add_value(r, non_computed_index);
+                        tokens[t].vm_step.add_value(r, non_computed_index);
+                    }
+
+                    assert(not reg_is_unevaluated(r));
+                    return {r, result};
+                }
+            }
+            catch (error_exception& e)
+            {
+                if (log_verbose)
+                    throw_reg_exception(*this, root_token, r, e, true);
+                else
+                    throw;
+            }
+            catch (myexception& e)
+            {
+                throw_reg_exception(*this, root_token, r, e, true);
+            }
+            catch (const std::exception& ee)
+            {
+                myexception e;
+                e<<ee.what();
+                throw_reg_exception(*this, root_token, r, e, true);
+            }
+        }
+    }
+}
+
 pair<int,int> reg_heap::incremental_evaluate1_(int r)
 {
     assert(regs.is_valid_address(r));
@@ -320,49 +465,7 @@ pair<int,int> reg_heap::incremental_evaluate1_(int r)
     if (reg_is_constant(r)) return {r,r};
 
     else if (reg_is_changeable(r))
-    {
-        total_changeable_eval++;
-        int result = result_for_reg(r);
-
-        if (result > 0)
-        {
-            total_changeable_eval_with_result++;
-
-            return {r, result};
-        }
-
-        // If we know what to call, then call it and use it to set the value
-        if (int s = step_index_for_reg(r); s > 0)
-        {
-            // Evaluate S, looking through unchangeable redirections
-            auto [call, result] = incremental_evaluate1(steps[s].call);
-
-	    // If the back-edge has not been set, then we need to update the call to look through
-	    // ref_no_force and set the back-edge on the first non-reference if it is changeable.
-            if (not steps[s].call_edge)
-            {
-                // This should only ever happen for modifiable values set with set_reg_value( ).
-                // In such cases, we need this, because the value we set could evaluate to a register reference.
-                // Otherwise, we set the call AFTER we have evaluated to a changeable or a constant.
-                clear_call_for_reg(r);
-                set_call(s, call);
-            }
-	    else
-		assert(call == call_for_reg(r));
-
-            // r gets its value from S.
-            set_result_for_reg( r );
-            if (not tokens[root_token].children.empty())
-            {
-                int t = tokens[root_token].children[0];
-                tokens[t].vm_result.add_value(r, non_computed_index);
-            }
-
-            total_changeable_eval_with_call++;
-            assert(not reg_is_unevaluated(r));
-            return {r, result};
-        }
-    }
+        return incremental_evaluate1_changeable_(r);
     else if (unevaluated_reg_is_ref_no_force(r))
     {
         int r2 = closure_at(r).reg_for_ref();
