4f32af9ce975e904475ff0177cb5660a5805f2d6
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-07-15 15:41:01 -0400

    Translate codon vector inputs

--- haskell/SModel/Codons.hs
+++ haskell/SModel/Codons.hs
@@ -14,24 +14,25 @@ type TripletAlphabet = Alphabet
 type CodonAlphabet = TripletAlphabet
 
 foreign import bpcall "SModel:singletToTripletSym" singletToTripletNative :: TripletAlphabet -> NativeMatrix Double -> NativeMatrix Double
-foreign import bpcall "SModel:f3x4_frequencies" f3x4Native :: TripletAlphabet -> NativeVector Double -> NativeVector Double -> NativeVector Double -> NativeVector Double
+foreign import trcall "SModel:f3x4_frequencies" f3x4Native :: TripletAlphabet -> Vector Double -> Vector Double -> Vector Double -> Vector Double
 foreign import bpcall "SModel:singlet_to_triplet_rates" tripletRatesNative :: TripletAlphabet -> NativeMatrix Double -> NativeMatrix Double -> NativeMatrix Double -> NativeMatrix Double
-foreign import bpcall "SModel:multiNucleotideMutationRates" multiNucleotideNative :: TripletAlphabet -> Double -> Double -> NativeMatrix Double -> NativeVector Double -> NativeMatrix Double
+foreign import trcall "SModel:multiNucleotideMutationRates" multiNucleotideNative :: TripletAlphabet -> Double -> Double -> Matrix Double -> Vector Double -> Matrix Double
 foreign import bpcall "SModel:dNdS_matrix" dNdSNative :: CodonAlphabet -> Double -> NativeMatrix Double
 
 singletToTripletSym alphabet rates = squareFor alphabet
     (singletToTripletNative alphabet (nativeMatrix rates))
 
-f3x4_frequencies_builtin alphabet pi1 pi2 pi3 = vectorFromNative (alphabetSize alphabet)
-    (f3x4Native alphabet (nativeVector pi1) (nativeVector pi2) (nativeVector pi3))
+f3x4_frequencies_builtin alphabet pi1 pi2 pi3 = overrideVectorSize (alphabetSize alphabet)
+    (f3x4Native alphabet pi1 pi2 pi3)
 
 singlet_to_triplet_rates alphabet rates1 rates2 rates3 = squareFor alphabet
     (tripletRatesNative alphabet (nativeMatrix rates1) (nativeMatrix rates2)
         (nativeMatrix rates3))
 
-multiNucleotideMutationRates alphabet v2 v3 rates frequencies = squareFor alphabet
-    (multiNucleotideNative alphabet v2 v3 (nativeMatrix rates)
-        (nativeVector frequencies))
+multiNucleotideMutationRates alphabet v2 v3 rates frequencies =
+    overrideMatrixDims dimension dimension
+        (multiNucleotideNative alphabet v2 v3 rates frequencies)
+  where dimension = alphabetSize alphabet
 
 dNdS_matrix alphabet omega = squareFor alphabet (dNdSNative alphabet omega)
 
