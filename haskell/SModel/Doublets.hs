5d3e2e2eae536aeae3ffefcbe96f25dc676985ff
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-07-15 15:40:42 -0400

    Translate doublet frequency inputs

--- haskell/SModel/Doublets.hs
+++ haskell/SModel/Doublets.hs
@@ -10,11 +10,11 @@ import Numeric.LinearAlgebra.Data
 
 type DoubletAlphabet = Alphabet
 
-foreign import bpcall "SModel:f2x4_frequencies" f2x4Native :: DoubletAlphabet -> NativeVector Double -> NativeVector Double -> NativeVector Double
+foreign import trcall "SModel:f2x4_frequencies" f2x4Native :: DoubletAlphabet -> Vector Double -> Vector Double -> Vector Double
 foreign import bpcall "SModel:singlet_to_doublet_rates" doubletRatesNative :: DoubletAlphabet -> NativeMatrix Double -> NativeMatrix Double -> NativeMatrix Double
 
-f2x4_frequencies_builtin alphabet pi1 pi2 = vectorFromNative (alphabetSize alphabet)
-    (f2x4Native alphabet (nativeVector pi1) (nativeVector pi2))
+f2x4_frequencies_builtin alphabet pi1 pi2 = overrideVectorSize (alphabetSize alphabet)
+    (f2x4Native alphabet pi1 pi2)
 
 singlet_to_doublet_rates alphabet rates1 rates2 = matrixFromNative dimension dimension
     (doubletRatesNative alphabet (nativeMatrix rates1) (nativeMatrix rates2))
