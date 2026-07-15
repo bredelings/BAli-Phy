656546a34a0b50a091e497c252ecbf6a74f0db38
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-07-15 15:30:59 -0400

    Translate eigen-exponential vector arguments

--- haskell/EigenExp.hs
+++ haskell/EigenExp.hs
@@ -1,24 +1,27 @@
 module EigenExp where
 
+import Compiler.FFI.Import (CInput)
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
 import Foreign.Maybe    
 
 data EigenSystem
 
+instance CInput EigenSystem
+
 data NoDecompReason = NoDiagReason deriving Show
 
 data MatDecomp = NoDecomp (Maybe NoDecompReason) | RealEigenDecomp EigenSystem
 
-foreign import bpcall "Matrix:getEigensystemRaw" getEigensystemNative :: NativeMatrix Double -> NativeVector Double -> CMaybe EigenSystem
-foreign import bpcall "Matrix:lExpRaw" lExpNative :: EigenSystem -> NativeVector Double -> Double -> CMaybe (NativeMatrix Double)
+foreign import trcall "Matrix:getEigensystemRaw" getEigensystemNative :: Matrix Double -> Vector Double -> CMaybe EigenSystem
+foreign import trcall "Matrix:lExpRaw" lExpNative :: EigenSystem -> Vector Double -> Double -> CMaybe (NativeMatrix Double)
 
 getEigensystem q pi = fromCMaybe $
-    getEigensystemNative (nativeMatrix q) (nativeVector pi)
+    getEigensystemNative q pi
 
 -- Wrap a successful eigensystem exponential with the frequency dimension.
 lExp esystem pi factor =
-    case fromCMaybe $ lExpNative esystem (nativeVector pi) factor of
+    case fromCMaybe $ lExpNative esystem pi factor of
         Nothing -> Nothing
         Just payload -> Just (matrixFromNative dimension dimension payload)
   where dimension = vectorSize pi
--- haskell/Foreign/Maybe.hs
+++ haskell/Foreign/Maybe.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Foreign.Maybe where
 
+import Compiler.FFI.Import (COutput)
 import Data.Bool -- for not
 import Data.Function -- for (.)
 import Data.Maybe
 
 data CMaybe a
+
+-- CMaybe is an opaque runtime result; translating it does not translate a.
+instance COutput (CMaybe a)
 
 foreign import ecall "Prelude:" cNothing :: CMaybe a
 foreign import ecall "Prelude:" cJust :: a -> CMaybe a
