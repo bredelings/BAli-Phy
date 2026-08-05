65bacc9b2b385e942316967725b6e23ef96be854
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-08-05 11:15:48 -0400

    Test recoverable mathematical MH proposal failures

--- tests/haskell/MCMC/ProbNaN/Main.hs
+++ tests/haskell/MCMC/ProbNaN/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Compiler.Fractional ((/))
+import Compiler.Floating (Pow(expTo))
 import Compiler.Num
 import Control.Monad (return)
 import Data.Bool
@@ -9,26 +10,33 @@ import Data.Eq
 import Data.Function (($))
 import MCMC (runMCMC)
 import MCMC.Moves.Integer (discreteUniformAvoidMH)
+import MCMC.Moves.MH (metropolisHastings, propose)
 import Probability.Distribution.Bernoulli (bernoulli)
-import Probability.Random (addMove, makeMCMCModel, modifiable, prior, toProb)
+import Probability.Distribution.Geometric (geometric)
+import Probability.Distribution.NegativeBinomial (negativeBinomial)
+import Probability.Random (addMove, makeMCMCModel, modifiable, sample, toProb)
 import System.IO (IO, putStrLn)
 
--- Exercise NaN as both an existing variable's density and a newly sampled
--- variable's parameter; both proposed contexts should be rejected.
+-- Protect candidate-local density and sampling failures across the separate native and generic MH
+-- transaction paths. This is obsolete when both paths share one typed proposal-rejection boundary.
 model = do
   let densitySelector = modifiable (0 :: Int)
       samplingSelector = modifiable (0 :: Int)
+      geometricValue = modifiable (0 :: Int)
+      negativeBinomialValue = modifiable (0 :: Int)
   let probability = if densitySelector == 0 then 0.5 else 0/0
-  _ <- prior $ bernoulli $ toProb probability
+  _ <- sample $ bernoulli $ toProb probability
   addMove 1 $ discreteUniformAvoidMH densitySelector 0 1
   addMove 1 $ discreteUniformAvoidMH samplingSelector 0 1
+  addMove 2 $ metropolisHastings $ propose geometricValue (\_ -> geometric $ expTo (-1000))
+  addMove 2 $ metropolisHastings $
+    propose negativeBinomialValue (\_ -> negativeBinomial 2 $ expTo (-1000))
   if samplingSelector == 0
     then return []
     else do
-      _ <- prior $ bernoulli (0/0)
+      _ <- sample $ bernoulli (0/0)
       return []
 
--- Completion proves that both invalid-proposal paths are recoverable.
 main :: IO ()
 main = do
   context <- makeMCMCModel model
